f21e9a6e8261fca42e5fb77e53b393938034175f
Oleg Tsarev <oleg.tsarev@percona.com>
2010-08-17 14:08:30 +0400

    improve normalize_patches.sh - now not change atime fix offsets in patches

--- normalize_patches.sh
+++ normalize_patches.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+ls -1 *.patch | grep -v repair | xargs bzr revert
 rm -rf a b Percona-Server;
 tar zxf mysql-5.1.49.tar.gz;
 mv mysql-5.1.49 Percona-Server;
@@ -20,7 +21,29 @@ for patch_name in `cat series`; do
     if [ $hunk -ne 0 ]; then
 	echo "==== Patch $patch_name need adaptation...";
 	find b -name "*.orig" | xargs rm;
-	diff -Nur a b > $patch_name;
+	patch_name_new=$patch_name.new;
+	patch_name_split=$patch_name.split;
+	rm $patch_name_new;
+	rm -rf $patch_name_split;
+	mkdir $patch_name_split;
+	cd $patch_name_split;
+	for filename in `splitdiff -a -d ../$patch_name | awk '{ print $2 }' | sed -e "s/>//g"`; do
+	    echo $filename;
+	    a_head=`cat $filename | head -n2 | head -n1`;
+	    b_head=`cat $filename | head -n2 | tail -n1`;
+	    echo "PATCH='$patch_name' FILE='$filename' A_HEAD='$a_head' B_HEAD='$b_head'";
+	    a_path=`echo $a_head | awk '{ print $2 }'`;
+	    b_path=`echo $b_head | awk '{ print $2 }'`;
+	    echo "A_PATH='$a_path' B_PATH='$b_path'";
+	    (cd ..; echo "diff -Nur $a_path $b_path" >> $patch_name_new);
+	    (cat $filename | head -n2 | head -n1 >> ../$patch_name_new);
+	    (cat $filename | head -n2 | tail -n1 >> ../$patch_name_new);
+	    (cd ..; diff -Nur $a_path $b_path | tail -n+3 >> $patch_name_new);
+	done;
+	cd ..;
+	cat $patch_name_new > $patch_name;
+	rm $patch_name_new;
+	rm -rf $patch_name_split;
     fi;
     patch -p1 -d Percona-Server < $patch_name > /dev/null;
     rm -rf a b;

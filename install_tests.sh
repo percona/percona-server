b63fa58f49d1e3a6af0cd6be7a97d4025072ce8f
Oleg Tsarev <oleg.tsarev@percona.com>
2010-11-02 15:52:19 +0000

    fix bug 663757

--- install_tests.sh
+++ install_tests.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 install_path()
 {
     echo "Installing mysql-test files: $2"
-    install -m 644 $1/*.opt $1/*.test Percona-Server/mysql-test/t/
+    install -m 644 $1/*.test Percona-Server/mysql-test/t/
     install -m 644 $1/*.result Percona-Server/mysql-test/r/
+    install -m 644 $1/.*opt Percona-Server/mysql-test/t/
     install -m 644 $1/*.inc Percona-Server/mysql-test/include/
 }
 

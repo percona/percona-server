5246d0868db9b6e641ecd1d9224d6210527e1ce2
Oleg Tsarev <oleg.tsarev@percona.com>
2010-12-03 21:17:33 +0300

    merge from ~catap/percona-server/fix-install_tests

--- install_tests.sh
+++ install_tests.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 install_path()
 {
     echo "Installing mysql-test files: $2"
-    install -m 644 $1/*.opt $1/*.test Percona-Server/mysql-test/t/
-    install -m 644 $1/*.result Percona-Server/mysql-test/r/
-    install -m 644 $1/*.inc Percona-Server/mysql-test/include/
+    find $1 -iname '*.opt' -o -iname '*.test' -exec install -m 644 {} Percona-Server/mysql-test/t/ ';'
+    find $1 -iname '*.result' -exec install -m 644 {} Percona-Server/mysql-test/r/ ';'
+    find $1 -iname '*.inc' -exec install -m 644 {} Percona-Server/mysql-test/include/ ';'
 }
 
 install_path mysql-test "global"
 for test_name in `cat series`; do
-    install_path mysql-test/$test_name $test_name
+    test -d mysql-test/$test_name && install_path mysql-test/$test_name $test_name
 done
 echo "Done"

fbcea7749dcf85a9592f4f54d59aa5a8b0f9bcec
Akira Higuchi <ahiguti100@gmail.com>
2011-01-07 13:27:16 +0900

    temp commit

--- regtest/test_01_lib/test14.pl
+++ regtest/test_01_lib/test14.pl
@@ -66,47 +66,6 @@ for (my $i = 0; $i <= 30; ++$i) {
   print "I2 $rk $rv1 $rv2\n";
 }
 
-=pod
-my %valmap = ();
-
-print "HSINSERT\n";
-my $hs = hstest::get_hs_connection(undef, 9999);
-my $dbname = $hstest::conf{dbname};
-$hs->open_index(1, $dbname, $table, '', 'k,v1,v2');
-# inserts with auto_increment
-for (my $i = 0; $i < $tablesize; ++$i) {
-  my $k = 0;
-  my $v1 = "v1hs_" . $i;
-  my $v2 = "v2hs_" . $i;
-  my $r = $hs->execute_insert(1, [ $k, $v1, $v2 ]);
-  my $err = $r->[0];
-  if ($err != 0) {
-    my $err_str = $r->[1];
-    print "$err $err_str\n";
-  }
-}
-# make sure that it works even when inserts are pipelined. these requests
-# are possibly executed in a single transaction.
-for (my $i = 0; $i < $tablesize; ++$i) {
-  my $k = 0;
-  my $v1 = "v1hs3_" . $i;
-  my $v2 = "v2hs3_" . $i;
-  my $r = $hs->execute_multi([
-  	[ 1, '+', [$k, $v1, $v2] ],
-  	[ 1, '+', [$k, $v1, $v2] ],
-  	[ 1, '+', [$k, $v1, $v2] ],
-	]);
-  for (my $i = 0; $i < 3; ++$i) {
-    my $err = $r->[$i]->[0];
-    if ($err != 0) {
-      my $err_str = $r->[1];
-      print "$err $err_str\n";
-    }
-  }
-}
-undef $hs;
-=cut
-
 sub dump_table {
   print "DUMP_TABLE\n";
   my $aref = $dbh->selectall_arrayref("select k,v1,v2 from $table order by k");

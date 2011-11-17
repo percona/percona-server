e96d16194d2989278db8bf3cef231a2212cb32c3
Stewart Smith <stewart@flamingspork.com>
2011-11-18 06:36:12 +1100

    add Percona PAM Authentication Plugin for MySQL logo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -139,7 +139,7 @@ html_short_title = 'PAM Plugin Docs'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'percona-pam-plugin-logo.jpg'
+html_logo = 'percona-pam-plugin-logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32

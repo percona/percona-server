e1337f33d82f2deadb58a7007807ee66627608af
Sergei Glushchenko <sergei.glushchenko@percona.com>
2012-02-13 13:19:11 +0700

    two-plugins blueprint. Full-featured PAM auth server plugin was split into two parts. First one is generic PAM conversation function, second one performs client-specific message sending and receiving. (auth_pam_common.c and auth_pam.c) modules. Oracle-compatible server-side plugin was implemented (auth_pam_compat.c). Both plugins use auth_pam_common.c. All client-specific behavior localized in auth_pam.c and auth_pam_compat.c.

--- src/auth_pam_compat.c
+++ src/auth_pam_compat.c
@@ -21,32 +21,30 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  PAM authentication for MySQL, server-side plugin for the
  production use.
 
- A general-purpose PAM authentication plugin for MySQL.  Acts as a mediator
- between the MySQL server, the MySQL client, and the PAM backend. Dialog plugin
- used as client plugin.
+ Oracle MySQL-compatible plugin.  Acts as a mediator
+ between the MySQL server, the MySQL client, and the PAM backend.
 
- The server plugin requests authentication from the PAM backend, forwards any
- requests and messages from the PAM backend over the wire to the client (in
- cleartext) and reads back any replies for the backend.
+ The server plugin requests authentication from the PAM backend, and reads one
+ phrase from client plugin. mysql_clear_password plugin used as client plugin.
 
  This plugin does not encrypt the communication channel in any way.  If this is
  required, a SSL connection should be used.
 
  To install this plugin, copy the .so file to the plugin directory and do
 
- INSTALL PLUGIN auth_pam SONAME 'auth_pam.so';
+ INSTALL PLUGIN auth_pam SONAME 'auth_pam_compat.so';
 
  To use this plugin for one particular user, specify it at user's creation time
  (TODO: tested with localhost only):
 
- CREATE USER 'username'@'hostname' IDENTIFIED WITH auth_pam;
+ CREATE USER 'username'@'hostname' IDENTIFIED WITH auth_pam_compat;
 
  Alternatively UPDATE the mysql.user table to set the plugin value for an
  existing user.
 
  Also it is possible to use this plugin to authenticate anonymous users:
 
- CREATE USER ''@'hostname' IDENTIFIED WITH auth_pam;
+ CREATE USER ''@'hostname' IDENTIFIED WITH auth_pam_compat;
 
 */
 
@@ -56,33 +54,11 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include "auth_pam_common.h"
 
-/** The maximum length of buffered PAM messages, i.e. any messages up to the
-    next PAM reply-requiring message. 10K should be more than enough by order
-    of magnitude. */
-enum { max_pam_buffered_msg_len = 10240 };
-
-struct pam_msg_buf {
-    unsigned char buf[max_pam_buffered_msg_len];
-    unsigned char* ptr;
-};
-
-static char pam_msg_style_to_char (int pam_msg_style)
-{
-  /* Magic byte for the dialog plugin, '\2' is defined as ORDINARY_QUESTION
-     and '\4' as PASSWORD_QUESTION there. */
-  return (pam_msg_style == PAM_PROMPT_ECHO_ON) ? '\2' : '\4';
-}
-
 int auth_pam_client_talk_init(void **talk_data)
 {
-  struct pam_msg_buf *msg_buf= calloc(1, sizeof(struct pam_msg_buf));
-  *talk_data= (void*)msg_buf;
-  if (msg_buf != NULL)
-  {
-    msg_buf->ptr= msg_buf->buf + 1;
-    return PAM_SUCCESS;
-  }
-  return PAM_BUF_ERR;
+  int *num_talks= calloc(1, sizeof(int));
+  *talk_data= (void*)num_talks;
+  return (num_talks != NULL) ? PAM_SUCCESS : PAM_BUF_ERR;
 }
 
 void auth_pam_client_talk_finalize(void *talk_data)
@@ -95,34 +71,15 @@ int auth_pam_talk_perform(const struct pam_message *msg,
                           struct pam_conv_data *data,
                           void *talk_data)
 {
-  struct pam_msg_buf *msg_buf= (struct pam_msg_buf*)talk_data;
-
-  /* Append the PAM message or prompt to the unsent message buffer */
-  if (msg->msg)
-  {
-    unsigned char *last_buf_pos = msg_buf->buf + max_pam_buffered_msg_len - 1;
-    if (msg_buf->ptr + strlen(msg->msg) >= last_buf_pos)
-    {
-      /* Cannot happen: the PAM message buffer too small. */
-      MY_ASSERT_UNREACHABLE();
-      return PAM_CONV_ERR;
-    }
-    memcpy(msg_buf->ptr, msg->msg, strlen(msg->msg));
-    msg_buf->ptr+= strlen(msg->msg);
-    *(msg_buf->ptr)++= '\n';
-  }
+  int pkt_len;
+  unsigned char *pkt;
+  int *num_talks= (int*)talk_data;
 
   if (msg->msg_style == PAM_PROMPT_ECHO_OFF
       || msg->msg_style == PAM_PROMPT_ECHO_ON)
   {
-    int pkt_len;
-    unsigned char *pkt;
-
-    msg_buf->buf[0]= pam_msg_style_to_char(msg->msg_style);
-
-    /* Write the message.  */
-    if (data->vio->write_packet(data->vio, msg_buf->buf,
-                                msg_buf->ptr - msg_buf->buf - 1))
+    /* mysql_clear_password plugin has support for only single phrase */
+    if (*num_talks > 1)
       return PAM_CONV_ERR;
 
     /* Read the answer */
@@ -137,10 +94,9 @@ int auth_pam_talk_perform(const struct pam_message *msg,
     strncpy(resp->resp, (char *)pkt, pkt_len);
     resp->resp[pkt_len]= '\0';
 
-    if (msg->msg_style == PAM_PROMPT_ECHO_OFF)
-      data->info->password_used= PASSWORD_USED_YES;
-
-    msg_buf->ptr= msg_buf->buf + 1;
+    /* we could only guess whether password was used or not */
+    data->info->password_used= PASSWORD_USED_NO_MENTION;
+    ++(*num_talks);
   }
 
   return PAM_SUCCESS;
@@ -149,7 +105,7 @@ int auth_pam_talk_perform(const struct pam_message *msg,
 static struct st_mysql_auth pam_auth_handler=
 {
   MYSQL_AUTHENTICATION_INTERFACE_VERSION,
-  "dialog",
+  "mysql_clear_password",
   &authenticate_user_with_pam_server
 };
 
@@ -157,7 +113,7 @@ mysql_declare_plugin(auth_pam)
 {
   MYSQL_AUTHENTICATION_PLUGIN,
   &pam_auth_handler,
-  "auth_pam",
+  "auth_pam_compat",
   "Percona, Inc.",
   "PAM authentication plugin",
   PLUGIN_LICENSE_GPL,

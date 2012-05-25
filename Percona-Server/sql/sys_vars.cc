5de0eea06c568731c54dd370ecc00fd04264e900
Sergei Glushchenko <sergei.glushchenko@percona.com>
2012-05-25 16:04:32 +0700

    BP 5.5-prevent-in-outfile-option Extend --secure-file-priv. When it is specified without argument disable SELECT INTO OUTFILE, LOAD DATAINFILE and LOAD_DATA()

--- Percona-Server/sql/sys_vars.cc
+++ Percona-Server/sql/sys_vars.cc
@@ -2006,9 +2006,10 @@ static Sys_var_mybool Sys_secure_auth(
 static Sys_var_charptr Sys_secure_file_priv(
        "secure_file_priv",
        "Limit LOAD DATA, SELECT ... OUTFILE, and LOAD_FILE() to files "
-       "within specified directory",
+       "within specified directory. "
+       "If no argument is specified disable loading files.",
        PREALLOCATED READ_ONLY GLOBAL_VAR(opt_secure_file_priv),
-       CMD_LINE(REQUIRED_ARG), IN_FS_CHARSET, DEFAULT(0));
+       CMD_LINE(OPT_ARG, OPT_SECURE_FILE_PRIV), IN_FS_CHARSET, DEFAULT(0));
 
 static bool fix_server_id(sys_var *self, THD *thd, enum_var_type type)
 {

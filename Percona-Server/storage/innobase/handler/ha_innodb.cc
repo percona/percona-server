2ec1f66388bebbfcf026ae6a4720b4153af7fd05
George O. Lorch III <george.lorch@percona.com>
2012-09-21 10:13:40 -0700

    Fixed issue where LRU dump would hold LRU_list_mutex during the entire dump process for each buffer pool. Now the mutex is only held when navigating the buffer pool and filling LRU dump pages but not during the LRU dump disk write. Modified dump page logic to allow number of dump pages that are to be filled on each buffer pool mutex cycle to be specified at compile time.
    Fixed issue where innodb_blocking_buffer_pool_restore does not take an
    optional bool argument similar to other bool options.

--- Percona-Server/storage/innobase/handler/ha_innodb.cc
+++ Percona-Server/storage/innobase/handler/ha_innodb.cc
@@ -12837,7 +12837,7 @@ static MYSQL_SYSVAR_UINT(buffer_pool_restore_at_startup, srv_auto_lru_dump,
 
 static MYSQL_SYSVAR_BOOL(blocking_buffer_pool_restore,
   innobase_blocking_lru_restore,
-  PLUGIN_VAR_NOCMDARG | PLUGIN_VAR_READONLY,
+  PLUGIN_VAR_OPCMDARG | PLUGIN_VAR_READONLY,
   "Block XtraDB startup process until buffer pool is full restored from a "
   "dump file (if present). Disabled by default.",
   NULL, NULL, FALSE);

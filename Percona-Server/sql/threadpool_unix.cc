5f4e2e00f5e914df62cabb9194d8cbd7e6f2f2cb
Sergei Glushchenko <sergei.glushchenko@percona.com>
2013-08-08 23:43:40 +0700

    Fix for bug #1196383: thread pool breaks -DWITH_PERFSCHEMA_ENGINE=OFF. This patch introduces some directives for conditional compilation in threadpool.cc and threadpool_common.cc.

--- Percona-Server/sql/threadpool_unix.cc
+++ Percona-Server/sql/threadpool_unix.cc
@@ -53,6 +53,7 @@ static bool threadpool_started= false;
 */
  
  
+#ifdef HAVE_PSI_INTERFACE
 static PSI_mutex_key key_group_mutex;
 static PSI_mutex_key key_timer_mutex;
 static PSI_mutex_info mutex_list[]=
@@ -80,6 +81,7 @@ static PSI_thread_info	thread_list[] =
 /* Macro to simplify performance schema registration */ 
 #define PSI_register(X) \
  if(PSI_server) PSI_server->register_ ## X("threadpool", X ## _list, array_elements(X ## _list))
+#endif
 
 
 struct thread_group_t;
@@ -1546,9 +1548,11 @@ bool tp_init()
     sql_print_error("Can't set threadpool size to %d",threadpool_size);
     DBUG_RETURN(1);
   }
+#ifdef HAVE_PSI_INTERFACE
   PSI_register(mutex);
   PSI_register(cond);
   PSI_register(thread);
+#endif
   
   pool_timer.tick_interval= threadpool_stall_limit;
   start_timer(&pool_timer);

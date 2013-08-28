dfc4afceff7b6f1f3a97545b0f4e08f125b66f20
Sergei Glushchenko <sergei.glushchenko@percona.com>
2013-08-29 04:14:46 +0700

    Blueprint: https://blueprints.launchpad.net/percona-server/+spec/rate-limit-to-slow-log New section is written to log file for the cases when log_slow_rate_limit is effective. Global variable slow_query_log_always_write_time is added. It allows to specify the age of queries. Older queries will be written to slow log regardless of log_slow_rate_limit.

--- Percona-Server/sql/sql_parse.cc
+++ Percona-Server/sql/sql_parse.cc
@@ -1588,6 +1588,7 @@ void log_slow_statement(THD *thd)
   if (opt_slow_query_log_rate_type == SLOG_RT_QUERY
       && thd->variables.log_slow_rate_limit
       && thd->query_id % thd->variables.log_slow_rate_limit
+      && query_exec_time < slow_query_log_always_write_time
       && (thd->variables.long_query_time >= 1000000
           || (ulong) query_exec_time < 1000000)) {
     DBUG_VOID_RETURN;
@@ -1595,6 +1596,7 @@ void log_slow_statement(THD *thd)
   if (opt_slow_query_log_rate_type == SLOG_RT_SESSION
       && thd->variables.log_slow_rate_limit
       && thd->thread_id % thd->variables.log_slow_rate_limit
+      && query_exec_time < slow_query_log_always_write_time
       && (thd->variables.long_query_time >= 1000000
           || (ulong) query_exec_time < 1000000)) {
     DBUG_VOID_RETURN;

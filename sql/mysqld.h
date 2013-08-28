e4fbd59052a66a5536264dbbea78c1e1600b6bdd
Sergei Glushchenko <sergei.glushchenko@percona.com>
2013-08-29 04:14:46 +0700

    [slow_query_log_rate_limit] Blueprint: https://blueprints.launchpad.net/percona-server/+spec/rate-limit-to-slow-log New
    Original title:
    Blueprint: https://blueprints.launchpad.net/percona-server/+spec/rate-limit-to-slow-log New section is written to log file for the cases when log_slow_rate_limit is effective. Global variable slow_query_log_always_write_time is added. It allows to specify the age of queries. Older queries will be written to slow log regardless of log_slow_rate_limit.
    
    New section is written to log file for the cases when log_slow_rate_limit
    is effective.
    Global variable slow_query_log_always_write_time is added. It allows to
    specify the age of queries. Older queries will be written to slow log
    regardless of log_slow_rate_limit.

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -99,6 +99,7 @@ extern MY_BITMAP temp_pool;
 extern bool opt_large_files, server_id_supplied;
 extern bool opt_update_log, opt_bin_log, opt_error_log;
 extern my_bool opt_log, opt_slow_log, opt_log_raw;
+extern ulonglong slow_query_log_always_write_time;
 extern my_bool opt_backup_history_log;
 extern my_bool opt_backup_progress_log;
 extern my_bool opt_query_cache_strip_comments;
--- sql/sql_parse.cc
+++ sql/sql_parse.cc
@@ -1948,6 +1948,7 @@ bool log_slow_applicable(THD *thd)
   if (opt_slow_query_log_rate_type == SLOG_RT_QUERY
       && thd->variables.log_slow_rate_limit
       && thd->query_id % thd->variables.log_slow_rate_limit
+      && query_exec_time < slow_query_log_always_write_time
       && (thd->variables.long_query_time >= 1000000
           || (ulong) query_exec_time < 1000000)) {
     DBUG_RETURN(false);
@@ -1955,6 +1956,7 @@ bool log_slow_applicable(THD *thd)
   if (opt_slow_query_log_rate_type == SLOG_RT_SESSION
       && thd->variables.log_slow_rate_limit
       && thd->thread_id % thd->variables.log_slow_rate_limit
+      && query_exec_time < slow_query_log_always_write_time
       && (thd->variables.long_query_time >= 1000000
           || (ulong) query_exec_time < 1000000)) {
     DBUG_RETURN(false);

a32d83bc6b2e87d553b3373001c72d3cdce95c6a
Laurynas Biveinis <laurynas.biveinis@percona.com>
2013-09-25 08:20:37 +0300

    [buf_flush_remove_hash] Fix bug 1227581 (InnoDB: Failing assertion: buf_page_in_file(bpage) in file buf0flu.cc line
    Original title:
    Fix bug 1227581 (InnoDB: Failing assertion: buf_page_in_file(bpage) in file buf0flu.cc line 2674 | abort (sig=6) in buf_pool_get_dirty_pages_count on ALTER TABLE).
    
    file buf0flu.cc line 2674 | abort (sig=6) in
    buf_pool_get_dirty_pages_count on ALTER TABLE).
    
    The issue is that buf_LRU_free_page() may perform a flush list
    relocation for a dirty zip page, making it possible to observe a page
    in BUF_BLOCK_REMOVE_HASH state on the flush list between
    buf_LRU_block_remove_hashed() and buf_flush_relocate_on_flush_list()
    calls, if another thread reads flush list pages without the LRU list
    mutex.
    
    Places where a thread iterates through the flush list without the LRU
    list mutex and does not expect to see a BUF_BLOCK_REMOVE_HASH page are
    buf_pool_get_dirty_pages_count(), buf_do_flush_list_batch(), and
    buf_get_latched_pages_number_instance().  The fix is to accept such
    pages there:
    - in buf_pool_get_dirty_pages_count() check such page as if it were
      BUF_BLOCK_FILE_PAGE;
    - in buf_flush_page_and_try_neighbors(), called from
      buf_do_flush_list_batch(), do not attempt to flush such page.
    - in buf_flush_set_hp(), also called from buf_do_flush_list_batch(),
      allow setting hazard pointer on such page.
    - in buf_get_latched_pages_number_instance(), silently accept such
      page as BUF_BLOCK_FILE_PAGE.
    
    Fix bug 1269352 (InnoDB: failing assertion: buf_page_in_file(bpage)
    below buf_flush_page_and_try_neighbors()), a follow-up to bug
    1227581.
    
    The latter bug attempted to fix race conditions involving buffer
    blocks in the middle of flush list relocations having
    BUF_BLOCK_REMOVE_HASH state, but the fix was not fully
    correct. buf_LRU_block_remove_hashed_page()/buf_LRU_free_block() will
    ake a page BUF_BLOCK_REMOVE_HASH while holding an LRU list mutex, a
    page hash X latch, and the block mutex.
    buf_flush_page_and_try_neighbors() may hold a flush list mutex
    instead. So, even though it checks for BUF_BLOCK_REMOVE_HASH pages,
    there is nothing to protect the flushing candidate page from becoming
    BUF_BLOCK_REMOVE_HASH after that check.
    
    Fix by reverting BUF_BLOCK_REMOVE_HASH check in
    buf_flush_page_and_try_neighbors() and adjusting it to accept
    BUF_BLOCK_REMOVE_HASH pages until the mutex release before the
    buf_flush_try_neighbors() call.

--- storage/innobase/buf/buf0flu.cc
+++ storage/innobase/buf/buf0flu.cc
@@ -141,7 +141,8 @@ buf_flush_set_hp(
 {
 	ut_ad(buf_flush_list_mutex_own(buf_pool));
 	ut_ad(buf_pool->flush_list_hp == NULL || bpage == NULL);
-	ut_ad(!bpage || buf_page_in_file(bpage));
+	ut_ad(!bpage || buf_page_in_file(bpage)
+	      || buf_page_get_state(bpage) == BUF_BLOCK_REMOVE_HASH);
 	ut_ad(!bpage || bpage->in_flush_list);
 	ut_ad(!bpage || buf_pool_from_bpage(bpage) == buf_pool);
 
@@ -1346,7 +1347,8 @@ buf_flush_page_and_try_neighbors(
 		mutex_enter(block_mutex);
 	}
 
-	ut_a(buf_page_in_file(bpage));
+	ut_a(buf_page_in_file(bpage)
+	     || buf_page_get_state(bpage) == BUF_BLOCK_REMOVE_HASH);
 
 	if (buf_flush_ready_for_flush(bpage, flush_type)) {
 		buf_pool_t*	buf_pool;
@@ -1359,8 +1361,10 @@ buf_flush_page_and_try_neighbors(
 
 		/* These fields are protected by the buf_page_get_mutex()
 		mutex. */
-		space = buf_page_get_space(bpage);
-		offset = buf_page_get_page_no(bpage);
+		/* Read the fields directly in order to avoid asserting on
+		BUF_BLOCK_REMOVE_HASH pages. */
+		ulint	space = bpage->space;
+		ulint	offset = bpage->offset;
 
 		if (flush_type == BUF_FLUSH_LRU) {
 			mutex_exit(block_mutex);
@@ -2681,11 +2685,12 @@ buf_pool_get_dirty_pages_count(
 	     bpage != 0;
 	     bpage = UT_LIST_GET_NEXT(list, bpage)) {
 
-		ut_ad(buf_page_in_file(bpage));
+		ut_ad(buf_page_in_file(bpage)
+		      || buf_page_get_state(bpage) == BUF_BLOCK_REMOVE_HASH);
 		ut_ad(bpage->in_flush_list);
 		ut_ad(bpage->oldest_modification > 0);
 
-		if (buf_page_get_space(bpage) == id) {
+		if (bpage->space == id) {
 			++count;
 		}
 	}

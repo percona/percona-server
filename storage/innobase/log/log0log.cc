20183ca50cacdcc015aa7e23551deab99c768e2b
George O. Lorch III <george.lorch@percona.com>
2013-09-25 21:57:55 -0700

    [all_o_direct] Port innodb_flush_method=ALL_O_DIRECT to 5.6
    Brings the ALL_O_DIRECT flush method from 5.5: adds the SRV_UNIX_ALL_O_DIRECT
    enum value in srv0srv.h, wires srv0start.cc to recognize the new string,
    applies os_file_set_nocache() to both data and log files in fil0fil.cc /
    log0log.cc / os0file.cc, and adds the percona_changed_page_bmp_flush MTR
    test.
    
    ===============================================
    
    [all_o_direct] Port of innodb_flush_method=ALL_O_DIRECT to Percona Server 5.6.

--- storage/innobase/log/log0log.cc
+++ storage/innobase/log/log0log.cc
@@ -1260,6 +1260,7 @@ log_io_complete(
 		group = (log_group_t*)((ulint) group - 1);
 
 		if (srv_unix_file_flush_method != SRV_UNIX_O_DSYNC
+		    && srv_unix_file_flush_method != SRV_UNIX_ALL_O_DIRECT
 		    && srv_unix_file_flush_method != SRV_UNIX_NOSYNC) {
 
 			fil_flush(group->space_id);
@@ -1281,6 +1282,7 @@ log_io_complete(
 			logs and cannot end up here! */
 
 	if (srv_unix_file_flush_method != SRV_UNIX_O_DSYNC
+	    && srv_unix_file_flush_method != SRV_UNIX_ALL_O_DIRECT
 	    && srv_unix_file_flush_method != SRV_UNIX_NOSYNC
 	    && thd_flush_log_at_trx_commit(NULL) != 2) {
 
@@ -1680,9 +1682,11 @@ loop:
 
 	mutex_exit(&(log_sys->mutex));
 
-	if (srv_unix_file_flush_method == SRV_UNIX_O_DSYNC) {
-		/* O_DSYNC means the OS did not buffer the log file at all:
-		so we have also flushed to disk what we have written */
+	if (srv_unix_file_flush_method == SRV_UNIX_O_DSYNC
+	    || srv_unix_file_flush_method == SRV_UNIX_ALL_O_DIRECT) {
+		/* O_DSYNC or ALL_O_DIRECT means the OS did not buffer the log
+		file at all: so we have also flushed to disk what we have
+		written */
 
 		log_sys->flushed_to_disk_lsn = log_sys->write_lsn;
 
@@ -2165,7 +2169,8 @@ log_checkpoint(
 		recv_apply_hashed_log_recs(TRUE);
 	}
 
-	if (srv_unix_file_flush_method != SRV_UNIX_NOSYNC) {
+	if (srv_unix_file_flush_method != SRV_UNIX_NOSYNC &&
+	    srv_unix_file_flush_method != SRV_UNIX_ALL_O_DIRECT) {
 		fil_flush_file_spaces(FIL_TABLESPACE);
 	}
 

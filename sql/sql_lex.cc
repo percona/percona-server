f4a2344df65365d7c3e83be2f8215cae5a07eb50
Vlad Lesin <vladislav.lesin@percona.com>
2013-09-27 21:47:04 +0400

    [statement_set] This is implementation of this https://blueprints.launchpad.net/percona-server/+spec/per-qu
    Original title:
    This is implementation of this https://blueprints.launchpad.net/percona-server/+spec/per-query-variables-statement blueprint.
    
    https://blueprints.launchpad.net/percona-server/+spec/per-query-variables-statement
    blueprint.
    
    Per query variable settings allow to set variable values only for the certain
    query after execution of which the previous values are restored.
    
    The original implementation can be found here: lp:~jlukas79/+junk/mysql-server

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -503,6 +503,7 @@ void lex_start(THD *thd)
   lex->is_change_password= false;
   lex->is_set_password_sql= false;
   lex->mark_broken(false);
+  lex->set_statement= false;
   DBUG_VOID_RETURN;
 }
 

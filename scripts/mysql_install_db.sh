6f99356ca3b69861e65733cbe8b524b69e7c40cd
Ignacio Nin <ignacio.nin@percona.com>
2013-09-30 16:48:29 -0400

    Remove the logic that creates my.cnf in basedir from mysql_install_db
    This is a fix for upstream bug http://bugs.mysql.com/bug.php?id=68416.
    
    Currently mysql_install_db installs a my.cnf file in basedir, which for
    rpm and dpkg is /usr, with some common options. Since we don't want a
    my.cnf created anywhere, this patch removes this code from the script
    altogether.
    
    In order to facilitate future merges, in particular if in the future the
    bug gets fixed upstream (possibly adding an option not to install the
    file but that keeps the code), the code is commented out and not deleted.
    
    ======================================================
    
    Fixed search path for libraries
    
    ======================================================
    
    Reverted changes to install_layout.cmake and fixes around that change
    
    ======================================================
    
    Updated debian changelog
    
    ======================================================
    
    Defines the variable $cnfext in mysql_install_db Fixes warning Useless use of a constant in void context in mysql_install_db
    
    Fixes warning Useless use of a constant in void context in mysql_install_db
    
    ======================================================
    
    Bug #1382782 fix.
    
    The return value of 'close' perl function is checked to detect error
    on mysqld execution during creating and filling system tables from
    mysql_install_db script.

--- scripts/mysql_install_db.sh
+++ scripts/mysql_install_db.sh
@@ -269,16 +269,16 @@ then
     cannot_find_file mysqld $basedir/libexec $basedir/sbin $basedir/bin
     exit 1
   fi
-  langdir=`find_in_basedir --dir errmsg.sys share/english share/mysql/english`
+  langdir=`find_in_basedir --dir errmsg.sys share/english share/percona-server/english share/mysql/english`
   if test -z "$langdir"
   then
-    cannot_find_file errmsg.sys $basedir/share/english $basedir/share/mysql/english
+    cannot_find_file errmsg.sys $basedir/share/english $basedir/share/percona-server/english $basedir/share/mysql/english
     exit 1
   fi
-  pkgdatadir=`find_in_basedir --dir fill_help_tables.sql share share/mysql`
+  pkgdatadir=`find_in_basedir --dir fill_help_tables.sql share share/percona-server share/mysql`
   if test -z "$pkgdatadir"
   then
-    cannot_find_file fill_help_tables.sql $basedir/share $basedir/share/mysql
+    cannot_find_file fill_help_tables.sql $basedir/share $basedir/share/percona-server $basedir/share/mysql
     exit 1
   fi
   scriptdir="$basedir/scripts"
@@ -426,8 +426,8 @@ else
   echo "describes problems on your OS.  Another information source are the"
   echo "MySQL email archives available at http://lists.mysql.com/."
   echo
-  echo "Please check all of the above before submitting a bug report"
-  echo "at http://bugs.mysql.com/"
+  echo "Please check all of the above before submitting a bug report at"
+  echo "  https://bugs.launchpad.net/percona-server/+filebug"
   echo
   exit 1
 fi
@@ -478,7 +478,8 @@ then
   fi
 
   echo
-  echo "Please report any problems at http://bugs.mysql.com/"
+  echo "Please report any problems at"
+  echo "  https://bugs.launchpad.net/percona-server/+filebug"
   echo
 fi
 

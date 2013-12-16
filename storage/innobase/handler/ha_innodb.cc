138610a9856d695d565d9560b4184a14a889fd6e
Laurynas Biveinis <laurynas.biveinis@percona.com>
2013-12-16 14:54:12 +0200

    [tokudb_row_type] Add TokuDB compression format ROW_TYPE support, implementing https://blueprints.launchpad.n
    Original title:
    Add TokuDB compression format ROW_TYPE support, implementing https://blueprints.launchpad.net/percona-server/+spec/support-tokudb-compression-algorithms.
    
    https://blueprints.launchpad.net/percona-server/+spec/support-tokudb-compression-algorithms.
    
    Add new lexer symbols TOKUDB_UNCOMPRESSED, TOKUDB_ZLIB,
    TOKUDB_QUICKLZ, TOKUDB_LZMA, TOKUDB_FAST, TOKUDB_SMALL, accept them as
    valid ROW_TYPE option values in the parser, add corresponding values
    to enum row_type.
    
    Add support for them to get_schema_tables_record() so that
    INFORMATION_SCHEMA.TABLES can return them, as well as bump
    ROW_FORMAT field length for I_S.TABLES and I_S.FILES tables to
    accomodate longer strings.
    
    In InnoDB, handle the new option values as unsupported in
    get_row_format_name(), create_options_are_invalid(),
    innobase_table_flags().  Other SEs do not require this.
    
    Add a testcase tokudb_zip_grammar to test the grammar changes and
    InnoDB/MyISAM/Memory handling of compression formats as unsupported
    create options.  Re-record information_schema, mysqldump, show_check,
    funcs_1.is_tables, and funcs_1.is_columns_is for the changed
    I_S.*.ROW_FORMAT width.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -9515,6 +9515,7 @@ get_row_format_name(
 		return("FIXED");
 	case ROW_TYPE_PAGE:
 	case ROW_TYPE_NOT_USED:
+	default:
 		break;
 	}
 	return("NOT USED");
@@ -9660,6 +9661,7 @@ create_options_are_invalid(
 	case ROW_TYPE_FIXED:
 	case ROW_TYPE_PAGE:
 	case ROW_TYPE_NOT_USED:
+	default:
 		push_warning(
 			thd, Sql_condition::WARN_LEVEL_WARN,
 			ER_ILLEGAL_HA_CREATE_OPTION,		\
@@ -10033,6 +10035,7 @@ index_bad:
 	case ROW_TYPE_NOT_USED:
 	case ROW_TYPE_FIXED:
 	case ROW_TYPE_PAGE:
+	default:
 		push_warning(
 			thd, Sql_condition::WARN_LEVEL_WARN,
 			ER_ILLEGAL_HA_CREATE_OPTION,

437e07b717ba59f18e4a06e025d394d8bf132f90
Davi Arnaut <davi@verdesmares.com>
2014-03-24 20:19:10 +0000

    PS-6849 Add JUnit reporting support to the MySQL test suite runner.
    Summary:
    WebScaleSQL Feature: JUnit Support for MTR
    
    Produces JUnit test reports that can be consumed by tools such as Jenkins.
    
    Two new options are added to MTR:
    
      --junit-output=FILE   Output JUnit test summary XML to FILE.
      --junit-package=NAME  Set the JUnit package name to NAME for this test run.
    
    A test run using junit reporting would look like:
    
      ./mtr --comment=rpl_row --junit-package=rpl_row --junit-output=rpl_row.xml \
            --suite=rpl --mysqld=--binlog-format=row
    
    Typically, the package name should be the name for the test run, in
    this case the suite name and its variation. Tests results are written
    to the rpl_row.xml file (including test timing and output). If using
    with Jenkins, the XML file can be used for publishing test results and
    Jenkins would be able to show how many test failed, test duration and
    whether to mark the build as failed or unstable (if there were failing
    tests that succeeded on retry).
    
    Test Plan: mtr
    
    Reviewers: steaphan
    
    Reviewed By: steaphan
    
    Differential Revision: https://reviews.facebook.net/D17139

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -103,6 +103,7 @@ use My::CoreDump;
 use mtr_cases;
 use mtr_cases_from_list;
 use mtr_report;
+use mtr_report_junit;
 use mtr_match;
 use mtr_unique;
 use mtr_results;
@@ -382,6 +383,9 @@ our $opt_warnings= 1;
 our $ndbcluster_enabled= 0;
 my $opt_include_ndbcluster= 0;
 my $opt_skip_ndbcluster= 0;
+
+our $opt_junit_output= undef;
+our $opt_junit_package= undef;
 
 my $opt_skip_sys_schema= 0;
 
@@ -695,11 +699,22 @@ sub main {
 
   print_total_times($opt_parallel) if $opt_report_times;
 
-  mtr_report_stats("Completed", $completed);
+  report_stats("Completed", $completed);
 
   remove_vardir_subs() if $opt_clean_vardir;
 
   exit(0);
+}
+
+
+sub report_stats($$;$) {
+  my ($prefix, $tests, $skip_error) = @_;
+
+  if ($opt_junit_output) {
+    mtr_report_stats_junit($tests, $opt_junit_output, $opt_junit_package);
+  }
+
+  mtr_report_stats($prefix, $tests, $skip_error);
 }
 
 
@@ -829,7 +844,7 @@ sub run_test_server ($$$) {
 	    elsif ($opt_max_test_fail > 0 and
 		   $num_failed_test >= $opt_max_test_fail) {
 	      push(@$completed, $result);
-	      mtr_report_stats("Too many failed", $completed, 1);
+	      report_stats("Too many failed", $completed, 1);
 	      mtr_report("Too many tests($num_failed_test) failed!",
 			 "Terminating...");
 	      return undef;
@@ -995,7 +1010,7 @@ sub run_test_server ($$$) {
     # ----------------------------------------------------
     if ( has_expired($suite_timeout) )
     {
-      mtr_report_stats("Timeout", $completed, 1);
+      report_stats("Timeout", $completed, 1);
       mtr_report("Test suite timeout! Terminating...");
       return undef;
     }
@@ -1347,6 +1362,8 @@ sub command_line_setup {
 	     'unit-tests-report!'	=> \$opt_ctest_report,
 	     'stress=s'                 => \$opt_stress,
              'suite-opt=s'              => \$opt_suite_opt,
+	     'junit-output=s'           => \$opt_junit_output,
+	     'junit-package=s'          => \$opt_junit_package,
 
              'help|h'                   => \$opt_usage,
 	     # list-options is internal, not listed in help
@@ -1361,6 +1378,12 @@ sub command_line_setup {
 
   usage("") if $opt_usage;
   list_options(\%options) if $opt_list_options;
+
+  # Make sure that XML::Simple support exists for JUnit output
+  if ($opt_junit_output and !mtr_junit_supported()) {
+    mtr_error("JUnit XML reporting is not supported.  The XML::Simple package",
+              "could not be loaded.");
+  }
 
   # --------------------------------------------------------------------------
   # Setup verbosity
@@ -7670,6 +7693,8 @@ Misc options
   sanitize              Scan server log files for warnings from various
                         sanitizers. Assumes that you have built with
                         -DWITH_ASAN.
+  junit-output=FILE     Output JUnit test summary XML to FILE.
+  junit-package=NAME    Set the JUnit package name to NAME for this test run.
 
 Some options that control enabling a feature for normal test runs,
 can be turned off by prepending 'no' to the option, e.g. --notimer.

d8acdf090395ff7e5a2bfc3ceadbe36012557419
Laurynas Biveinis <laurynas.biveinis@percona.com>
2014-05-14 13:20:06 +0300

    Fix bug 1198775 (Help for 5.5.32-31.0 includes a link to 5.1 manual) by fixing a typo in the printed URL.

--- client/mysql.cc
+++ client/mysql.cc
@@ -2956,7 +2956,7 @@ com_help(String *buffer __attribute__((unused)),
 
   put_info("\nFor information about Percona products and services, visit:\n"
            "   http://www.percona.com/\n"
-	   "Percona Server manual: http://www.percona.com/doc/percona-server/5.1\n"
+	   "Percona Server manual: http://www.percona.com/doc/percona-server/5.5\n"
            "For the MySQL Reference Manual: http://dev.mysql.com/\n"
            "To buy Percona support, training, or other products, visit:\n"
            "   https://www.percona.com/\n", INFO_INFO);

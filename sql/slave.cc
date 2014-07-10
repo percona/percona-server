c5f8e6fe6346b3837d5c23d9989073b47c326b7a
Laurynas Biveinis <laurynas.biveinis@percona.com>
2014-07-10 14:37:32 +0300

    Fix bug 1334743 (Numerous Valgrind errors in OpenSSL).
    Add the necessary OpenSSL library calls to release its allocated
    memory. The patch is based on lp:mysql-server/5.6 rev 4591.1.60
    with some extra cleanups.
    
    At the same time, mark Valgrind error of still reachable memory as an
    MTR Valgrind reporter error.

--- sql/slave.cc
+++ sql/slave.cc
@@ -3179,6 +3179,7 @@ err:
 
   DBUG_LEAVE;                                   // Must match DBUG_ENTER()
   my_thread_end();
+  ERR_remove_state(0);
   pthread_exit(0);
   return 0;                                     // Avoid compiler warnings
 }

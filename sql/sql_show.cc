e9421386d6f001c6304eb855270cb838d02f4de8
Laurynas Biveinis <laurynas.biveinis@percona.com>
2014-07-22 16:33:21 +0300

    Fix bug 1182050 (Duplicate LIST_PROCESS_HOST_LEN definition) by removing a copy from sql_show.cc, so that there is only one definition and that upstream changes to it result in a merge conflict.

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -1062,8 +1062,6 @@ static void append_directory(THD *thd, String *packet, const char *dir_type,
 }
 
 
-#define LIST_PROCESS_HOST_LEN 64
-
 static bool get_field_default_value(THD *thd, Field *timestamp_field,
                                     Field *field, String *def_value,
                                     bool quoted)

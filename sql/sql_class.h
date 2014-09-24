a1416dd8a94c31d90029c38835cbe3212862a3e2
Alexey Kopytov <akopytov@gmail.com>
2014-09-24 20:58:42 +0400

    [backup_locks_slave_status] Bug #1358836: Backup locks do not guarantee consistent SHOW SLAVE STATUS               with
    Original title:
    Bug #1358836: Backup locks do not guarantee consistent SHOW SLAVE STATUS               with binary log disabled
    
                  with binary log disabled
    
    Bug #1372806: Deadlock with LOCK BINLOG FOR BACKUP + SHOW SLAVE STATUS
                  with binary log enabled
    
    The fixes are inter-dependent, hence a single revision for both bugs.
    
    The problem in bug #1358836 was that with the binary log disabled slave
    SQL threads did not acquire binlog protection locks when committing in
    some cases. The original backup locks implementation was done in
    assumption that the only place where master binary logs are updated is
    Relay_log_info::inc_event_relay_log_pos(). Which is incorrect and
    replication code does not make figuring things like these
    easy. Relay_log_info::inc_event_relay_log_pos() is only called for
    multi-statement transactions. There are different code paths for
    AUTOCOMMIT transactions and other kinds of updates, depending on the
    storage engines involved, event types, and whether the slave is
    multi-threaded.
    
    Inspecting all possible code paths was difficult, because Relay_log_info
    members that have to be protected by an active BINLOG backup lock were
    sometimes modified directly, rather than with the ‘setter’ functions,
    i.e. set_group_master_log_name() and set_group_master_log_pos().
    
    Fixed by:
    
    - asserting that the current connection has acquired a binlog protection
      lock in Relay_log_info::set_group_master_log_name() and
      Relay_log_info::set_group_master_log_pos();
    
    - changing all code that accessed those variables directly to use
      getter/setter functions instead;
    
    - making sure that whenever a setter function is called (i.e. master
      binary log coordinates are updated), binlog protection is acquired;
    
    - renaming the group_master_log_{name,pos} member variables of
      Relay_log_info to m_group_master_log_{name,pos} to prevent other code
      that modifies those variables directly from creeping in on future
      changes or upstream merges;
    
    The problem in bug #1372806 was that with binary log enabled, an SQL
    thread would acquire a binlog protection lock after locking the
    Relay_log_info::data_lock mutex, which resulted in a deadlock with a
    user connection holding a binlog lock while executing SHOW SLAVE STATUS.
    
    Fixed by inspecting the code and making sure that no binlog protection
    locks are taken while the Relay_log_info::data_locks mutex is locked by
    the same connection, and adding assertions where possible.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -2191,6 +2191,8 @@ public:
 
   bool is_acquired() const { return m_lock != NULL; }
 
+  bool is_protection_acquired() const { return m_prot_lock != NULL; }
+
 private:
   MDL_key::enum_mdl_namespace m_namespace;
   MDL_ticket *m_lock;

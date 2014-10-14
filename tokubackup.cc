61a345263444c02a707d67d9a6cf54eb941a35f2
Rich Prohaska <prohaska@tokutek.com>
2014-10-14 14:40:05 -0400

    add tokubackup version variable

--- tokubackup.cc
+++ tokubackup.cc
@@ -7,7 +7,13 @@
 #include <dlfcn.h>
 #include "backup/backup.h"
 
-static volatile int tokubackup_debug = 0;
+static char *tokubackup_version = (char *) tokubackup_version_string;
+
+static MYSQL_SYSVAR_STR(version, tokubackup_version, PLUGIN_VAR_NOCMDARG | PLUGIN_VAR_READONLY, "version",
+                        NULL, NULL, NULL);
+
+static MYSQL_THDVAR_ULONG(debug, PLUGIN_VAR_THDLOCAL, "debug", 
+                          NULL, NULL, 0 /*default*/, 0 /*min*/, 1 /*max*/, 1 /*blocksize*/);
 
 static MYSQL_THDVAR_ULONG(last_error, PLUGIN_VAR_THDLOCAL, "last error",
                           NULL, NULL, 0 /*default*/, 0 /*min*/, ~0ULL /*max*/, 1 /*blocksize*/);
@@ -18,22 +24,24 @@ struct tokubackup_progress_extra {
 };
 
 static int tokubackup_progress_fun(float progress, const char *progress_string, void *extra) {
-    tokubackup_progress_extra *bp = static_cast<tokubackup_progress_extra *>(extra);
-    if (thd_killed(bp->_thd)) {
-        return ER_ABORTING_CONNECTION;
-    }
+    tokubackup_progress_extra *be = static_cast<tokubackup_progress_extra *>(extra);
 
     // print to error log
-    if (tokubackup_debug)
+    if (THDVAR(be->_thd, debug))
         sql_print_information("tokubackup progress %f %s", progress, progress_string);
 
     // set thd proc info
+    thd_proc_info(be->_thd, "");
     size_t len = 100 + strlen(progress_string);
-    bp->_the_string = (char *) my_realloc(bp->_the_string, len, MYF(MY_FAE+MY_ALLOW_ZERO_PTR));
+    be->_the_string = (char *) my_realloc(be->_the_string, len, MYF(MY_FAE+MY_ALLOW_ZERO_PTR));
     float percentage = progress * 100;
-    int r = snprintf(bp->_the_string, len, "Tokubackup about %.0f%% done: %s", percentage, progress_string);
+    int r = snprintf(be->_the_string, len, "Tokubackup about %.0f%% done: %s", percentage, progress_string);
     assert(0 < r && (size_t)r <= len);
-    thd_proc_info(bp->_thd, bp->_the_string);
+    thd_proc_info(be->_thd, be->_the_string);
+
+    if (thd_killed(be->_thd)) {
+        return ER_ABORTING_CONNECTION;
+    }
 
     return 0;
 }
@@ -43,8 +51,10 @@ struct tokubackup_error_extra {
 };
 
 static void tokubackup_error_fun(int error_number, const char *error_string, void *extra) {
-    // print to error log (debug)
-    if (tokubackup_debug)
+    tokubackup_error_extra *be = static_cast<tokubackup_error_extra *>(extra);
+
+    // print to error log
+    if (THDVAR(be->_thd, debug))
         sql_print_information("tokubackup error %d %s", error_number, error_string);
 
     // TODO set thd last_error and last_error_string
@@ -55,10 +65,10 @@ static void tokubackup_update_dir(THD *thd, struct st_mysql_sys_var *var, void *
     tokubackup_error_extra error_extra = { thd };
     const char *source_dirs[1] = { mysql_real_data_home };
     const char *dest_dirs[1] = { *(const char **) save };
-    if (tokubackup_debug)
+    if (THDVAR(thd, debug))
         sql_print_information("%s backup %s %s", __FUNCTION__, source_dirs[0], dest_dirs[0]);
     int error = tokubackup_create_backup(source_dirs, dest_dirs, 1, tokubackup_progress_fun, &progress_extra, tokubackup_error_fun, &error_extra);
-    if (tokubackup_debug)
+    if (THDVAR(thd, debug))
         sql_print_information("%s backup error %d", __FUNCTION__, error);
     THDVAR(thd, last_error) = error;
     thd_proc_info(thd, "tokubackup done"); // must be a static string
@@ -71,7 +81,7 @@ static void tokubackup_update_throttle(THD *thd, struct st_mysql_sys_var *var, v
     my_ulonglong *val = (my_ulonglong *) var_ptr;
     *val = *(my_ulonglong*) save;
     unsigned long nb = *val;
-    if (tokubackup_debug)
+    if (THDVAR(thd, debug))
         sql_print_information("%s %lu", __FUNCTION__, nb);
     tokubackup_throttle_backup(nb);
 }
@@ -80,6 +90,8 @@ static MYSQL_THDVAR_ULONGLONG(throttle, PLUGIN_VAR_THDLOCAL, "backup throttle",
                               NULL, tokubackup_update_throttle, 0 /*default*/, 0 /*min*/, ~0ULL /*max*/, 1 /*blocksize*/);
 
 static struct st_mysql_sys_var *tokubackup_system_variables[] = {
+    MYSQL_SYSVAR(version),
+    MYSQL_SYSVAR(debug),
     MYSQL_SYSVAR(dir),
     MYSQL_SYSVAR(throttle),
     MYSQL_SYSVAR(last_error),
@@ -88,8 +100,6 @@ static struct st_mysql_sys_var *tokubackup_system_variables[] = {
 
 static int tokubackup_plugin_init(void *p) {
     DBUG_ENTER(__FUNCTION__);
-    if (tokubackup_debug)
-        sql_print_information("tokubackup %s", tokubackup_version_string);
     DBUG_RETURN(0);
 }
 

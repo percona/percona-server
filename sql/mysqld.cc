f235561785bb7aef26cb85e6a7435d727556a9a0
Laurynas Biveinis <laurynas.biveinis@percona.com>
2014-11-28 10:56:03 +0200

    Fix bug 1319904 (mysqld daemon with installed handlersocket plugin ignores any signals).
    the shutdown hang is caused by mysqld signal handler never having its
    initialisation completed if HS is installed. That happens because both
    HS and signal_hand wait for COND_server_started, which is signalled
    using mysql_cond_signal, meaning that only one waiter is woken up,
    which happens to be the HS one. The signal handler waiter does not
    have a time out.
    
    This is a regression introduced in the upstream fix for
    http://bugs.mysql.com/bug.php?id=62311, backported to our 5.5 as bug
    1249193. The upstream 5.6 later fixed the regression in rev 4754.1.1.
    
    Fix by doing the same in 5.5, that is, broadcasting the signal.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -4882,7 +4882,7 @@ int mysqld_main(int argc, char **argv)
   /* Signal threads waiting for server to be started */
   mysql_mutex_lock(&LOCK_server_started);
   mysqld_server_started= 1;
-  mysql_cond_signal(&COND_server_started);
+  mysql_cond_broadcast(&COND_server_started);
   mysql_mutex_unlock(&LOCK_server_started);
 
 #if defined(_WIN32) || defined(HAVE_SMEM)

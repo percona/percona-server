f2857b993bf947ac8606e70410a81f0bce4d03b1
Rich Prohaska <prohaska@tokutek.com>
2015-01-04 12:16:40 -0500

    DB-787 test718 needs the tokudb_fractal_tree_info plugin

--- run.mysql.tokudb.tests.with.valgrind.bash
+++ run.mysql.tokudb.tests.with.valgrind.bash
@@ -3,7 +3,7 @@ for suite in funcs iuds ; do
 done
 
 for suite in tokudb tokudb.bugs tokudb.add_index tokudb.alter_table ; do
-    ./mtr --suite=$suite --skip-test=fast_up.*\|rows-32m.*\|5585\|xa-.* --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1 
+    ./mtr --suite=$suite --skip-test='fast_up.*\|rows-32m.*\|5585\|xa-.*' --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1 
 done
 
 ./mtr  --suite=parts --do-test=.*tokudb.* --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >parts.valgrind.out 2>&1

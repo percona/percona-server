a1c4382a6b46fab7856fdf580bbc8d11a54fe103
Rich Prohaska <prohaska@tokutek.com>
2015-01-05 06:50:27 -0500

    DB-787 fix mtr test run script

--- run.mysql.tokudb.tests.with.valgrind.bash
+++ run.mysql.tokudb.tests.with.valgrind.bash
@@ -1,9 +1,9 @@
-for suite in funcs iuds ; do
-    ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=tokudb --mysqld=--default-tmp-storage-engine=tokudb --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1
+for suite in tokudb.add_index tokudb.alter_table tokudb tokudb.bugs ; do
+    ./mtr --suite=$suite --skip-test=fast_up.*\|rows-32m.*\|5585\|xa-.* --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1 
 done
 
-for suite in tokudb tokudb.bugs tokudb.add_index tokudb.alter_table ; do
-    ./mtr --suite=$suite --skip-test='fast_up.*\|rows-32m.*\|5585\|xa-.*' --mysqld='--plugin-load=tokudb=ha_tokudb.so;tokudb_trx=ha_tokudb.so;tokudb_locks=ha_tokudb.so;tokudb_lock_waits=ha_tokudb.so;tokudb_fractal_tree_info=ha_tokudb.so' --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1 
+for suite in funcs iuds ; do
+    ./mtr --suite=engines/$suite --mysqld=--default-storage-engine=tokudb --mysqld=--default-tmp-storage-engine=tokudb --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --parallel=auto --force --retry=0 --max-test-fail=0 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >$suite.valgrind.out 2>&1
 done
 
 ./mtr  --suite=parts --do-test=.*tokudb.* --mysqld=--plugin-load=tokudb=ha_tokudb.so --mysqld=--loose-tokudb-check-jemalloc=0 --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --valgrind-mysqld --valgrind-option=--suppressions=tokudb.supp --valgrind-option=--show-reachable=yes >parts.valgrind.out 2>&1

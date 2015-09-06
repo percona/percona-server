44407408835fd342fca85600bfd03548bdc0c6f4
Nickolay Ihalainen <ihanick@gmail.com>
2015-09-06 14:04:12 +0300

    Fix for path separator problem #1364315

--- storage/innobase/log/log0online.cc
+++ storage/innobase/log/log0online.cc
@@ -1489,11 +1489,24 @@ log_online_open_bitmap_file_read_only(
 	log_online_bitmap_file_t*	bitmap_file)	/*!<out: opened bitmap
 							file */
 {
+	size_t	srv_data_home_len;
 	ibool	success	= FALSE;
 
 	ut_ad(name[0] != '\0');
 
-	ut_snprintf(bitmap_file->name, FN_REFLEN, "%s%s", srv_data_home, name);
+	srv_data_home_len = strlen(srv_data_home);
+	ut_a (srv_data_home_len < FN_REFLEN);
+
+	if (srv_data_home_len
+	    && srv_data_home[srv_data_home_len-1] != SRV_PATH_SEPARATOR) {
+		ut_snprintf(bitmap_file->name, FN_REFLEN, "%s%c%s",
+				srv_data_home, SRV_PATH_SEPARATOR, name);
+	} else {
+		ut_snprintf(bitmap_file->name, FN_REFLEN, "%s%s",
+				srv_data_home, name);
+	}
+
+
 	bitmap_file->file
 		= os_file_create_simple_no_error_handling(innodb_file_bmp_key,
 							  bitmap_file->name,

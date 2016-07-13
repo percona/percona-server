66cd376b423297c66e97a833c75cb62eb39c197c
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2016-07-13 17:09:32 +0300

    Fix bug 79610 / 1525407 (Failed DROP DATABASE due FK constraint on master breaks slave)
    If DROP DATABASE fails to delete any of the tables, it is binlogged as
    DROP TABLE t1, t2, ... for the tables for which drop succeeded. If slave
    has different schema so that it references any of the dropped tables,
    replication will break.
    
    Fix by running foreign key checks on all the DROP DATABASE tables before
    any table is actually dropped. If any FK constraint would fail, do not
    convert DROP DATABASE to DROP TABLE in the binary log and abort
    immediately.
    
    To implement the fix,
    - add a new handlerton method get_parent_fk_list, that returns the list
    of foreign keys referencing the given table;
    - implement this method for InnoDB, make it share code with
    ha_innobase::get_parent_foreign_key_list handler method;
    - call it from a new function check_drop_database_foreign_keys, which is
    called from mysql_rm_table_no_locks.
    
    Add a testcase to rpl.rpl_foreign_key_innodb, and adjust
    binlog.binlog_database as needed.

--- sql/handler.h
+++ sql/handler.h
@@ -842,6 +842,17 @@ struct handlerton
    my_bool (*flush_changed_page_bitmaps)(void);
    my_bool (*purge_changed_page_bitmaps)(ulonglong lsn);
    my_bool (*is_fake_change)(handlerton *hton, THD *thd);
+   /**
+      Get the list of foreign keys referencing a specified table
+
+      @param thd        The thread handle
+      @param path       Path to the table
+      @param f_key_list[out]    The list of foreign keys
+
+      @return error code or zero for success
+    */
+   int (*get_parent_fk_list)(THD *thd, const char *path,
+                             List<FOREIGN_KEY_INFO>* f_key_list);
    uint32 flags;                                /* global handler flags */
    /*
       Those handlerton functions below are properly initialized at handler

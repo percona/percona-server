c89b1341b4f21da97a609357a2a847b767c035a6
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2016-08-03 15:23:56 +0300

    Fix bug 1609364 (--innodb_purge_threads=1 may result in incomplete purge with innodb_fast_shutdown=0)
    The separate purge thread loop exits immediately on any
    srv_shutdown_state > 0 value even though it's supposed to keep on
    purging if srv_fast_shutdown == 0.
    
    Fix by
    - signalling the purge thread to wake up at the start of
    logs_empty_and_mark_files_at_shutdown;
    - adjusting the purge thread suspend condition so that it's never
    suspended during shutdown;
    - adjusting the purge thread loop break condition to exit the loop
    immediately if we are at fast shutdown, or if we are at the slow
    shutdown and the last TRX_SYS_N_RSEGS attempts to purge did nothing;
    - reverting what looks the previous attempt to fix this, which caused
    master thread to do purging even with separate purge thread enabled,
    but was racy;
    
    This also fixes bug 756387.

--- storage/innobase/srv/srv0srv.c
+++ storage/innobase/srv/srv0srv.c
@@ -3327,7 +3327,7 @@ srv_master_do_purge(void)
 
 	ut_ad(!mutex_own(&kernel_mutex));
 
-	ut_a(srv_n_purge_threads == 0 || (srv_shutdown_state > 0 && srv_n_threads_active[SRV_WORKER] == 0));
+	ut_a(srv_n_purge_threads == 0);
 
 	do {
 		/* Check for shutdown and change in purge config. */
@@ -3848,7 +3848,7 @@ retry_flush_batch:
 	/* Flush logs if needed */
 	srv_sync_log_buffer_in_background();
 
-	if (srv_n_purge_threads == 0 || (srv_shutdown_state > 0 && srv_n_threads_active[SRV_WORKER] == 0)) {
+	if (srv_n_purge_threads == 0) {
 		srv_main_thread_op_info = "master purging";
 
 		srv_master_do_purge();
@@ -3926,7 +3926,7 @@ background_loop:
 		}
 	}
 
-	if (srv_n_purge_threads == 0 || (srv_shutdown_state > 0 && srv_n_threads_active[SRV_WORKER] == 0)) {
+	if (srv_n_purge_threads == 0) {
 		srv_main_thread_op_info = "master purging";
 
 		srv_master_do_purge();
@@ -4142,9 +4142,10 @@ srv_purge_thread(
 	        We peek at the history len without holding any mutex
 		because in the worst case we will end up waiting for
 		the next purge event. */
-		if (trx_sys->rseg_history_len < srv_purge_batch_size
-		    || (n_total_purged == 0
-			&& retries >= TRX_SYS_N_RSEGS)) {
+		if (srv_shutdown_state == SRV_SHUTDOWN_NONE
+		    && (trx_sys->rseg_history_len < srv_purge_batch_size
+			|| (n_total_purged == 0
+			    && retries >= TRX_SYS_N_RSEGS))) {
 
 			mutex_enter(&kernel_mutex);
 
@@ -4159,8 +4160,12 @@ srv_purge_thread(
 
 		/* Check for shutdown and whether we should do purge at all. */
 		if (srv_force_recovery >= SRV_FORCE_NO_BACKGROUND
-		    || srv_shutdown_state != 0
-		    || srv_fast_shutdown) {
+		    || (srv_shutdown_state != SRV_SHUTDOWN_NONE
+			&& srv_fast_shutdown)
+		    || (srv_shutdown_state != SRV_SHUTDOWN_NONE
+			&& srv_fast_shutdown == 0
+			&& n_total_purged == 0
+			&& retries >= TRX_SYS_N_RSEGS)) {
 
 			break;
 		}

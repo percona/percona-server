961b376a65dc6dbe501ce58b5f43d8c537de2b8e
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2016-09-15 13:30:14 +0300

    Fix bug 1622034 / 82935 (Cipher ECDHE-RSA-AES128-GCM-SHA256 listed in man/Ssl_cipher_list, not supported)
    Invoke OpenSSL eliptic curve setup functions so that EC-DHE ciphers
    are supported. Do this conditionally if the SSL library supports EC.  To
    properly skip testcases when it does not, add a
    have_elliptic_curve_crypto server read-only variable (the name is chosen
    specifically not to contain "ssl" substring in order to avoid updating
    all the "SHOW STATUS LIKE "%ssl%" testcases). Re-record testcases which
    now negotiate EC ciphers by default.

--- sql/sys_vars.cc
+++ sql/sys_vars.cc
@@ -4173,6 +4173,10 @@ static Sys_var_have Sys_have_tlsv1_2(
        "have_tlsv1_2", "have_tlsv1_2",
        READ_ONLY GLOBAL_VAR(have_tlsv1_2), NO_CMD_LINE);
 
+static Sys_var_have Sys_have_elliptic_curve_crypto(
+       "have_elliptic_curve_crypto", "have_elliptic_curve_crypto",
+       READ_ONLY GLOBAL_VAR(have_elliptic_curve_crypto), NO_CMD_LINE);
+
 static bool fix_log_state(sys_var *self, THD *thd, enum_var_type type);
 static Sys_var_mybool Sys_general_log(
        "general_log", "Log connections and queries to a table or log file. "

0aee0c7328538af20cadcbe80bdb1c03c69819dd
Fungo Wang <bo@fungo.me>
2016-09-18 23:14:09 +0800

    DB-1012: Add read-free-replication support for TokuDB partition table
    Copyright (c) <2016>, <Fungo Wang> All rights reserved.
    
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions
    are met:
    
    1. Redistributions of source code must retain the above copyright
    notice, this list of conditions and the following disclaimer.
    
    2. Redistributions in binary form must reproduce the above copyright
    notice, this list of conditions and the following disclaimer in the
    documentation and/or other materials provided with the distribution.
    
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
    COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
    STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
    OF THE POSSIBILITY OF SUCH DAMAGE.

--- sql/ha_partition.cc
+++ sql/ha_partition.cc
@@ -4089,10 +4089,14 @@ int ha_partition::update_row(const uchar *old_data, uchar *new_data)
     error instead of correcting m_last_part, to make the user aware of the
     problem!
 
+    For TokuDB Read-Free-Replication optimization, there is no need to do
+    a read before update(row lookup is omitted), so m_last_part is not
+    necessarily same with old_part_id.
+
     Notice that HA_READ_BEFORE_WRITE_REMOVAL does not require this protocol,
     so this is not supported for this engine.
   */
-  if (old_part_id != m_last_part)
+  if (old_part_id != m_last_part && rpl_lookup_rows())
   {
     m_err_rec= old_data;
     DBUG_RETURN(HA_ERR_ROW_IN_WRONG_PARTITION);
@@ -4229,13 +4233,17 @@ int ha_partition::delete_row(const uchar *buf)
     error instead of forwarding the delete to the correct (m_last_part)
     partition!
 
+    For TokuDB Read-Free-Replication optimization, there is no need to do
+    a read before delete(row lookup is omitted), so m_last_part is not
+    necessarily same with part_id.
+
     Notice that HA_READ_BEFORE_WRITE_REMOVAL does not require this protocol,
     so this is not supported for this engine.
 
     TODO: change the assert in InnoDB into an error instead and make this one
     an assert instead and remove the get_part_for_delete()!
   */
-  if (part_id != m_last_part)
+  if (part_id != m_last_part && rpl_lookup_rows())
   {
     m_err_rec= buf;
     DBUG_RETURN(HA_ERR_ROW_IN_WRONG_PARTITION);
@@ -9035,6 +9043,92 @@ int ha_partition::check_for_upgrade(HA_CHECK_OPT *check_opt)
   DBUG_RETURN(error);
 }
 
+
+/*
+  We don't know which partition table will be updated before executing
+  Write_rows_log_event, so update all partitions.
+*/
+void ha_partition::rpl_before_write_rows()
+{
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_before_write_rows();
+  }
+}
+
+/*
+  Clear flag of all paritions after executing Write_rows_log_event.
+*/
+void ha_partition::rpl_after_write_rows()
+{
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_after_write_rows();
+  }
+}
+
+/*
+  We don't know which partition table will be updated before executing
+  Delete_rows_log_event, so update all partitions.
+*/
+void ha_partition::rpl_before_delete_rows()
+{
+
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_before_delete_rows();
+  }
+}
+
+/*
+  Clear flag of all paritions after executing Delete_rows_log_event.
+*/
+void ha_partition::rpl_after_delete_rows()
+{
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_after_delete_rows();
+  }
+}
+
+/*
+  We don't know which partition table will be updated before executing
+  Update_rows_log_event, so update all partitions.
+*/
+void ha_partition::rpl_before_update_rows()
+{
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_before_update_rows();
+  }
+}
+
+/*
+  Clear flag of all paritions after executing Update_rows_log_event.
+*/
+void ha_partition::rpl_after_update_rows()
+{
+  uint i;
+  for (i= 0; i < m_tot_parts; i++)
+  {
+    m_file[i]->rpl_after_update_rows();
+  }
+}
+
+/*
+  Check whether we need to perform row lookup when executing
+  Update_rows_log_event or Delete_rows_log_event. Use the 1st
+  partition is enough, see @c ha_tokudb::rpl_lookup_rows().
+*/
+bool ha_partition::rpl_lookup_rows()
+{
+  return m_file[0]->rpl_lookup_rows();
+}
 
 struct st_mysql_storage_engine partition_storage_engine=
 { MYSQL_HANDLERTON_INTERFACE_VERSION };

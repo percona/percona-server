d29005b6301ebf5904364f13cf415177df8a1dbe
Nickolay Ihalainen <ihanick@gmail.com>
2016-10-23 18:47:15 +0300

    Fix bug 728082 (USER_STATISTICS shows incorrect value for number of concurrent connections)

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -2351,6 +2351,7 @@ public:
   ulonglong diff_access_denied_errors;
   // Number of queries that return 0 rows
   ulonglong diff_empty_queries;
+  ulonglong diff_disconnects;
 
   // Per account query delay in miliseconds. When not 0, sleep this number of
   // milliseconds before every SQL command.

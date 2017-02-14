17b87417f8e8745e235cb62c6e15f5caaeb6562f
Xiaofei Du <xiaofeidu@fb.com>
2017-02-13 16:05:25 -0800

    [#1817] Adding delayed_write_rate RocksDB option for MyRocks
    Upstream commit ID : fb-mysql-5.6.35/9c54e825abdb3bdca4c1a02f4be0f3184b9db86e
    
    Summary:
    Currently delayed_write_rate is not configurable in MySQL. This
    diff adds the configuration to MySQL so it can be configured in MySQL
    Closes https://github.com/facebook/mysql-5.6/pull/526
    
    Reviewed By: alxyang
    
    Differential Revision: D4554870
    
    Pulled By: xiaofeidu008
    
    fbshipit-source-id: 0724e8f
    
    ==========================================
    
    [#1817] Update rocksdb submodule
    
    Upstream commit ID : fb-mysql-5.6.35/c14f2b226121f926ad17becfe2ec2e052e40303c
    
    Summary:
    Get the latest change from rocksdb master. As a result, a bunch of tests are
    updated.
    
    update-submodule: rocksdb
    
    Differential Revision: D4566041
    
    fbshipit-source-id: fb7f437
    
    ==========================================
    
    [#1817] Updated rocksdb submodule and fixed cont build by removing two RocksDB options
    
    Upstream commit ID : fb-mysql-5.6.35/ddf379b1e139abf7dafc94b110e6418a6a7f8163
    
    Summary:
    update-submodule: rocksdb
    
    The submodule is updated to ba4c77bd6b16ea493c555561ed2e59bdc4c15fc0
    
    RocksDB master has:
    * Remove option min_partial_merge_operands. Partial merge operands will always be merged in flush or compaction if there are more than one.
    * Remove option verify_checksums_in_compaction. Compaction will always verify checksum.
    
    This breaks the cont build.
    
    Fixing this by removing these two options in INFORMATION_SCHEMA.ROCKSDB_CFOPTIONS dynamic table
    
    Reviewed By: gunnarku, jkedgar
    
    Differential Revision: D4630638
    
    fbshipit-source-id: 1ecd9bd
    
    ==========================================
    
    [#1817] Update the rocksdb submodule
    
    Upstream commit ID : fb-mysql-5.6.35/05b350b520fdace7d7d61a1d761f28bf2500eb06
    
    Summary:
    Update to the latest 5.1.fb branch to contain fixes for performance
    regressions.
    
    update-submodule: rocksdb
    
    Differential Revision: D4524307
    
    fbshipit-source-id: de26f8f
    
    ==========================================
    
    [#1839] use SetDBOptions to dynamically set delayed_write_rate
    
    Upstream commit ID : fb-mysql-5.6.35/6fccba0872a1e19dd712fa13c0267c281e9b5ee4
    
    Summary:
    If the DB is already running we need to use SetDBOptions to dynamically change
    the delayed_write)rate, rather than directly change `rocksdb_db_options` which
    only works when opening the DB.
    
    Squash with D4554870
    
    Reviewed By: jkedgar
    
    Differential Revision: D4782567
    
    fbshipit-source-id: c353ceb

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -327,6 +327,11 @@ static void rocksdb_set_rate_limiter_bytes_per_sec(THD *thd,
                                                    void *var_ptr,
                                                    const void *save);
 
+static void rocksdb_set_delayed_write_rate(THD *thd,
+                                           struct st_mysql_sys_var *var,
+                                           void *var_ptr,
+                                           const void *save);
+
 static void rdb_set_collation_exception_list(const char *exception_list);
 static void rocksdb_set_collation_exception_list(THD *thd,
                                                  struct st_mysql_sys_var *var,
@@ -347,6 +352,7 @@ static long long rocksdb_block_cache_size;
 /* Use unsigned long long instead of uint64_t because of MySQL compatibility */
 static unsigned long long // NOLINT(runtime/int)
     rocksdb_rate_limiter_bytes_per_sec;
+static unsigned long long rocksdb_delayed_write_rate;
 static unsigned long // NOLINT(runtime/int)
     rocksdb_persistent_cache_size;
 static uint64_t rocksdb_info_log_level;
@@ -558,6 +564,12 @@ static MYSQL_SYSVAR_ULONGLONG(
     nullptr, rocksdb_set_rate_limiter_bytes_per_sec, /* default */ 0L,
     /* min */ 0L, /* max */ MAX_RATE_LIMITER_BYTES_PER_SEC, 0);
 
+static MYSQL_SYSVAR_ULONGLONG(
+    delayed_write_rate, rocksdb_delayed_write_rate,
+    PLUGIN_VAR_RQCMDARG, "DBOptions::delayed_write_rate", nullptr,
+    rocksdb_set_delayed_write_rate, rocksdb_db_options.delayed_write_rate,
+    0, UINT64_MAX, 0);
+
 static MYSQL_SYSVAR_ENUM(
     info_log_level, rocksdb_info_log_level, PLUGIN_VAR_RQCMDARG,
     "Filter level for info logs to be written mysqld error log. "
@@ -634,13 +646,6 @@ static MYSQL_SYSVAR_ULONG(max_total_wal_size,
                           "DBOptions::max_total_wal_size for RocksDB", nullptr,
                           nullptr, rocksdb_db_options.max_total_wal_size,
                           /* min */ 0L, /* max */ LONG_MAX, 0);
-
-static MYSQL_SYSVAR_BOOL(
-    disabledatasync,
-    *reinterpret_cast<my_bool *>(&rocksdb_db_options.disableDataSync),
-    PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,
-    "DBOptions::disableDataSync for RocksDB", nullptr, nullptr,
-    rocksdb_db_options.disableDataSync);
 
 static MYSQL_SYSVAR_BOOL(
     use_fsync, *reinterpret_cast<my_bool *>(&rocksdb_db_options.use_fsync),
@@ -1166,10 +1171,10 @@ static struct st_mysql_sys_var *rocksdb_system_variables[] = {
     MYSQL_SYSVAR(error_if_exists),
     MYSQL_SYSVAR(paranoid_checks),
     MYSQL_SYSVAR(rate_limiter_bytes_per_sec),
+    MYSQL_SYSVAR(delayed_write_rate),
     MYSQL_SYSVAR(info_log_level),
     MYSQL_SYSVAR(max_open_files),
     MYSQL_SYSVAR(max_total_wal_size),
-    MYSQL_SYSVAR(disabledatasync),
     MYSQL_SYSVAR(use_fsync),
     MYSQL_SYSVAR(wal_dir),
     MYSQL_SYSVAR(persistent_cache_path),
@@ -3049,6 +3054,8 @@ static int rocksdb_init_func(void *const p) {
         rocksdb::NewGenericRateLimiter(rocksdb_rate_limiter_bytes_per_sec));
     rocksdb_db_options.rate_limiter = rocksdb_rate_limiter;
   }
+
+  rocksdb_db_options.delayed_write_rate = rocksdb_delayed_write_rate;
 
   std::shared_ptr<Rdb_logger> myrocks_logger = std::make_shared<Rdb_logger>();
   rocksdb::Status s = rocksdb::CreateLoggerFromOptions(
@@ -9912,6 +9919,25 @@ void rocksdb_set_rate_limiter_bytes_per_sec(my_core::THD *const thd,
     rocksdb_rate_limiter_bytes_per_sec = new_val;
     rocksdb_rate_limiter->SetBytesPerSecond(new_val);
   }
+}
+
+void rocksdb_set_delayed_write_rate(THD *thd, struct st_mysql_sys_var *var,
+                                    void *var_ptr, const void *save) {
+  RDB_MUTEX_LOCK_CHECK(rdb_sysvars_mutex);
+  const uint64_t new_val = *static_cast<const uint64_t *>(save);
+  if (rocksdb_delayed_write_rate != new_val) {
+    rocksdb_delayed_write_rate = new_val;
+    rocksdb::Status s =
+        rdb->SetDBOptions({{"delayed_write_rate", std::to_string(new_val)}});
+
+    if (!s.ok()) {
+      /* NO_LINT_DEBUG */
+      sql_print_warning("MyRocks: failed to update delayed_write_rate. "
+                        "status code = %d, status = %s",
+                        s.code(), s.ToString().c_str());
+    }
+  }
+  RDB_MUTEX_UNLOCK_CHECK(rdb_sysvars_mutex);
 }
 
 void rdb_set_collation_exception_list(const char *const exception_list) {

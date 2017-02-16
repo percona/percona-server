eafac5a1bbd631c03adb298701284faf15370b86
George O. Lorch III <george.lorch@percona.com>
2017-02-15 23:50:39 -0700

    MYR-55 : Port MyRocks from PS 5.6 to PS 5.7 - Fixed m_thread* interface touches and stripped thread renaming as it isn't   exposed in my_thread. Maybe consider posting this as a patch to PS.

--- storage/rocksdb/rdb_threads.h
+++ storage/rocksdb/rdb_threads.h
@@ -40,7 +40,7 @@ private:
   // Make sure we run only once
   std::atomic_bool m_run_once;
 
-  pthread_t m_handle;
+  my_thread_handle m_handle;
 
 protected:
   mysql_mutex_t m_signal_mutex;
@@ -64,7 +64,7 @@ public:
 
   void signal(const bool &stop_thread = false);
 
-  int join() { return pthread_join(m_handle, nullptr); }
+  int join() { return my_thread_join(&m_handle, nullptr); }
 
   void uninit();
 

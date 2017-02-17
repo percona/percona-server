cd8abdda6e31c8e923eccfe35ce85882f2598d8c
Nickolay Ihalainen <ihanick@gmail.com>
2017-02-17 12:15:14 +0300

    [userstat] Fix lp:1661488, Allow to enable userstat=1 for existing connections, keep concurren
    Original title:
    [#1765] Fix lp:1661488, Allow to enable userstat=1 for existing connections, keep concurrent connections tracking after FLUSH USER/CLIENT_STATISTICS

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -3269,6 +3269,7 @@ int fill_schema_user_stats(THD* thd, TABLE_LIST* tables, Item* cond)
   if (check_global_access(thd, SUPER_ACL | PROCESS_ACL))
           DBUG_RETURN(1);
 
+  refresh_concurrent_conn_stats();
   // Iterates through all the global stats and sends them to the client.
   // Pattern matching on the client IP is supported.
 
@@ -3308,6 +3309,7 @@ int fill_schema_client_stats(THD* thd, TABLE_LIST* tables, Item* cond)
   if (check_global_access(thd, SUPER_ACL | PROCESS_ACL))
           DBUG_RETURN(1);
 
+  refresh_concurrent_conn_stats();
   // Iterates through all the global stats and sends them to the client.
   // Pattern matching on the client IP is supported.
 

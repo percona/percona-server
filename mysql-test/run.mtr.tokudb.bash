e8f214a2f7832b628fdc929c181dc73f8244ddab
BohuTANG <overred.shuttler@gmail.com>
2017-03-31 16:27:33 +0800

    add travis test

--- mysql-test/run.mtr.tokudb.bash
+++ mysql-test/run.mtr.tokudb.bash
@@ -1,9 +1,3 @@
 engine=tokudb
 suites=tokudb.add_index,tokudb.alter_table,tokudb,tokudb.bugs,tokudb.parts,tokudb.rpl,tokudb.sys_vars,engines/funcs,engines/iuds
-run="./mtr --suite=$suites --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test"
-$run &>tokudb.mtr.outs
-
-engine=innodb
-suites=innodb,engines/funcs,engines/iuds
-run="./mtr --suite=$suites --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test"
-$run &>innodb.mtr.outs
+./mtr --suite=$suites --mysqld=--default-storage-engine=$engine --mysqld=--default-tmp-storage-engine=$engine --skip-test=fast_up.* --force --retry=0 --max-test-fail=0 --parallel=auto --no-warnings --testcase-timeout=60 --big-test

facebook/mysql-5.6

Fix tests from error code clean up
Summary:
Some error codes were added or removed in 6f99962f8d2d2411d7a68c27037dc96801c4b487. This just rerecords the tests so that they pass again.

Squash with: 6f99962f8d2d2411d7a68c27037dc96801c4b487

Test Plan: mtr

Reviewers: herman, gunnarku, jkedgar, horuff, alexyang

Reviewed By: alexyang

Subscribers: webscalesql-eng@fb.com

Differential Revision: https://phabricator.intern.facebook.com/D4790579

Signature: t1:4790579:1490738783:84d6e12a07b38abc6e7bb8fbb7b186f5dce0f666

storage/rocksdb/ha_rocksdb.cc
@@ -246,6 +246,10 @@ static int rocksdb_create_checkpoint(
                             checkpoint_dir.c_str());
       rocksdb::Checkpoint *checkpoint;
       auto status = rocksdb::Checkpoint::Create(rdb, &checkpoint);
+      // We can only return HA_EXIT_FAILURE/HA_EXIT_SUCCESS here which is why
+      // the return code is ignored, but by calling into rdb_error_to_mysql,
+      // it will call my_error for us, which will propogate up to the client.
+      int rc __attribute__((__unused__));
       if (status.ok()) {
         status = checkpoint->CreateCheckpoint(checkpoint_dir.c_str());
         delete checkpoint;
@@ -255,11 +259,10 @@ static int rocksdb_create_checkpoint(
               checkpoint_dir.c_str());
           return HA_EXIT_SUCCESS;
         } else {
-          rdb_log_status_error(status,
-                               "Failed to create checkpoint directory.");
+          rc = ha_rocksdb::rdb_error_to_mysql(status);
         }
       } else {
-        rdb_log_status_error(status, "Failed to initialize checkpoint.");
+        rc = ha_rocksdb::rdb_error_to_mysql(status);
       }
     }
   }

percona/percona-server

Upstream commit ID : fb-mysql-5.6.35/d344ec1b59508b5e4956b398eef632563529ca98
Reviewed By: alxyang
Differential Revision: D4790579
fbshipit-source-id: 72f06b8

storage/rocksdb/ha_rocksdb.cc
@@ -243,6 +243,10 @@ static int rocksdb_create_checkpoint(THD *const thd MY_ATTRIBUTE((__unused__)),
                             checkpoint_dir.c_str());
       rocksdb::Checkpoint *checkpoint;
       auto status = rocksdb::Checkpoint::Create(rdb, &checkpoint);
+      // We can only return HA_EXIT_FAILURE/HA_EXIT_SUCCESS here which is why
+      // the return code is ignored, but by calling into rdb_error_to_mysql,
+      // it will call my_error for us, which will propogate up to the client.
+      int rc MY_ATTRIBUTE((__unused__));
       if (status.ok()) {
         status = checkpoint->CreateCheckpoint(checkpoint_dir.c_str());
         delete checkpoint;
@@ -252,11 +256,10 @@ static int rocksdb_create_checkpoint(THD *const thd MY_ATTRIBUTE((__unused__)),
               checkpoint_dir.c_str());
           return HA_EXIT_SUCCESS;
         } else {
-          rdb_log_status_error(status,
-                               "Failed to create checkpoint directory.");
+          rc = ha_rocksdb::rdb_error_to_mysql(status);
         }
       } else {
-        rdb_log_status_error(status, "Failed to initialize checkpoint.");
+        rc = ha_rocksdb::rdb_error_to_mysql(status);
       }
     }
   }
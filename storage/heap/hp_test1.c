70b3cc820af38509be80810ea80808338e521403
Olernov <olernov@gmail.com>
2017-10-04 13:47:39 +0300

    Fix bug 1706356 5.7 (Adding index corrupts data in memory table) part2
    Field fixed_key_fieldnr removed from HP_CREATE_INFO structure.
    Submodules updated.

--- storage/heap/hp_test1.c
+++ storage/heap/hp_test1.c
@@ -56,7 +56,6 @@ int main(int argc, char **argv)
   hp_create_info.min_records= 10UL;
   hp_create_info.columns= 2;
   hp_create_info.columndef= columndef;
-  hp_create_info.fixed_key_fieldnr= 30;
   hp_create_info.fixed_data_size= sizeof(char*) * 2;
 
   keyinfo[0].keysegs=1;

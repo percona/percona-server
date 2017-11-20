a7238179ddc3a068d974ad9100077e2cc598ac3e
Vlad Lesin <vlad_lesin@mail.ru>
2017-11-20 17:29:25 +0300

    lp-1657737: Print locking transaction which cause "Lock wait timeout" errors into log file
    Post push fixes.

--- storage/innobase/lock/lock0wait.cc
+++ storage/innobase/lock/lock0wait.cc
@@ -198,26 +198,27 @@ lock_wait_table_reserve_slot(
 	return(NULL);
 }
 
-/***************************************************************//**
-Print lock wait timeout info to stderr. It's supposed this function
+/** Print lock wait timeout info to stderr. It's supposed this function
 is executed in trx's THD thread as it calls some non-thread-safe
-functions to get some info from THD. */
+functions to get some info from THD.
+@param[in]	trx	requested trx
+@param[in]	blocking	blocking info array
+@param[in]	blocking_count	blocking info array size */
 void
 print_lock_wait_timeout(
-/*====================*/
-	trx_t *trx, /*!< in: requested trx */
-	blocking_trx_info *blocking, /*!< in: blocking info array */
-	size_t blocking_count) /*!< in: blocking info array size */
+	const trx_t &trx,
+	blocking_trx_info *blocking,
+	size_t blocking_count)
 {
 	std::ostringstream outs;
 
 	outs << "Lock wait timeout info:\n";
 	outs << "Requested thread id: " <<
-		thd_get_thread_id(trx->mysql_thd) <<
+		thd_get_thread_id(trx.mysql_thd) <<
 		"\n";
-	outs << "Requested trx id: " << trx->id << "\n";
+	outs << "Requested trx id: " << trx.id << "\n";
 	outs << "Requested query: " <<
-		thd_query_string(trx->mysql_thd)->str << "\n";
+		thd_query_string(trx.mysql_thd)->str << "\n";
 
 	outs << "Total blocking transactions count: " <<
 		blocking_count <<
@@ -454,7 +455,7 @@ lock_wait_suspend_thread(
 
 		trx->error_state = DB_LOCK_WAIT_TIMEOUT;
 		if (srv_print_lock_wait_timeout_info)
-			print_lock_wait_timeout(trx, blocking, blocking_count);
+			print_lock_wait_timeout(*trx, blocking, blocking_count);
 
 		MONITOR_INC(MONITOR_TIMEOUT);
 	}

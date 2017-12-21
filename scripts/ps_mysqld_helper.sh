5dc8725cdda11de7187801885e2712f99872b2af
Evgeniy Patlan <evgeniy.patlan@percona.com>
2017-12-21 11:50:32 +0200

    [#2030] [BLD-911] No automatcal support of jemalloc
    ==================================================
    
    [#2120] [PS-3784] ps_mysqld_helper.sh script errors

--- scripts/ps_mysqld_helper.sh
+++ scripts/ps_mysqld_helper.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+if [ -f /etc/redhat-release ]
+then
+    SYSTEMD_ENV_FILE="/etc/sysconfig/mysql"
+else
+    SYSTEMD_ENV_FILE="/etc/default/mysql"
+fi
 enable_thp() {
-    if [ -f /etc/redhat-release ]
-    then
-        SYSTEMD_ENV_FILE="/etc/sysconfig/mysql"
-    else
-        SYSTEMD_ENV_FILE="/etc/default/mysql"
-    fi
+    STATUS_THP_SYSTEM=0
     if [ -f /sys/kernel/mm/transparent_hugepage/enabled ]; then
         CONTENT_TRANSHP=$(</sys/kernel/mm/transparent_hugepage/enabled)
         STATUS_THP_SYSTEM=$(echo $CONTENT_TRANSHP | grep -cv '\[never\]')
@@ -28,5 +29,22 @@ enable_thp() {
     fi
 }
 
+enable_jemalloc(){
+    JEMALLOC_LOCATION=0
+    for libjemall in "/usr/lib64" "/usr/lib/x86_64-linux-gnu" "/usr/lib"; do
+        if [ -r "${libjemall}/libjemalloc.so.1" ]; then
+            JEMALLOC_LOCATION="${libjemall}/libjemalloc.so.1"
+            break
+        fi
+    done
+    if [ "x${JEMALLOC_LOCATION}" != "x0" ]; then
+        STATUS_JEMALLOC_CONFIG=$(grep -c -e "LD_PRELOAD=.*libjemalloc.so.1" ${SYSTEMD_ENV_FILE})
+        if [ $STATUS_JEMALLOC_CONFIG = 0 ]; then
+            echo "LD_PRELOAD=${JEMALLOC_LOCATION}" >> ${SYSTEMD_ENV_FILE}
+        fi
+    fi
+}
+
 #main
 enable_thp
+enable_jemalloc

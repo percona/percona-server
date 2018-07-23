54cb08276ac1bc909f49c8a2fdad5dc37562d99a
Sergei Petrunia <psergey@askmonty.org>
2018-07-23 11:07:06 -0700

    Issue#857: MyRocks: Incorrect behavior when miltiple statements fail inside a transaction (#858)
    Upstream commit ID : fb-mysql-5.6.35/1775ed264c06793dff108ceec2f7945f8ca9b30a
    PS-5217 : Merge fb-prod201803
    
    Summary:
    rollback_to_stmt_savepoint() calls do_rollback_to_savepoint(). This
    removes the changes made by this statement, and also removes the last
    set savepoint.
    
    Before we start processing the next statement, we need to set the new
    savepoint, so we will have something to rollback to if the next statement
    fails.
    
    Since rollback_to_stmt_savepoint always sets a new savepoint now,
    m_n_savepoints is now redundant and is removed.
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/858
    
    Differential Revision: D8848229
    
    Pulled By: yoshinorim
    
    fbshipit-source-id: 9917838ca2a

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -1778,8 +1778,6 @@ class Rdb_transaction {
   bool m_is_delayed_snapshot = false;
 
  private:
-  /* Number of RockDB savepoints taken */
-  int m_n_savepoints;
   /*
     Number of write operations this transaction had when we took the last
     savepoint (the idea is not to take another savepoint if we haven't made
@@ -2301,7 +2299,6 @@ public:
       entire transaction.
     */
     do_set_savepoint();
-    m_n_savepoints = 1;
     m_writes_at_last_savepoint = m_write_count;
   }
 
@@ -2317,7 +2314,6 @@ public:
     if (m_writes_at_last_savepoint != m_write_count) {
       do_set_savepoint();
       m_writes_at_last_savepoint = m_write_count;
-      m_n_savepoints++;
     }
   }
 
@@ -2327,10 +2323,14 @@ public:
   void rollback_to_stmt_savepoint() {
     if (m_writes_at_last_savepoint != m_write_count) {
       do_rollback_to_savepoint();
-      if (!--m_n_savepoints) {
-        do_set_savepoint();
-        m_n_savepoints = 1;
-      }
+      /*
+        RollbackToSavePoint "removes the most recent SetSavePoint()", so
+        we need to set it again so that next statement can roll back to this
+        stage.
+        It's ok to do it here at statement end (instead of doing it at next
+        statement start) because setting a savepoint is cheap.
+      */
+      do_set_savepoint();
       m_writes_at_last_savepoint = m_write_count;
     }
   }

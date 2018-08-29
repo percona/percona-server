572d9ad234863739b08139e7dff7bc2bab029581
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-08-29 10:57:16 +0300

    PS-269 (Initial Percona Server 8.0.12 tree)
    Fix THD::mark_innodb_used debug build assert on an attempt to pass
    InnoDB trx id zero where THD already has a non-zero id. The zero can
    be passed from a DD transaction, thus handle this case both in the
    assert and by keeping the previously-set non-zero trx id.

--- sql/sql_class.h
+++ sql/sql_class.h
@@ -1484,8 +1484,9 @@ class THD : public MDL_context_owner,
   ulong innodb_page_access;
 
   void mark_innodb_used(ulonglong trx_id) noexcept {
-    DBUG_ASSERT(innodb_trx_id == 0 || innodb_trx_id == trx_id);
-    innodb_trx_id = trx_id;
+    DBUG_ASSERT(innodb_trx_id == 0 || innodb_trx_id == trx_id ||
+                is_attachable_ro_transaction_active());
+    if (trx_id) innodb_trx_id = trx_id;
     innodb_was_used = true;
   }
 

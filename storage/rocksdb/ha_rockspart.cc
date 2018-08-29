9d2a7195ae4b21b4c0569699c43ee266febc9d4e
Vlad Lesin <vlad_lesin@mail.ru>
2018-08-29 20:10:39 +0300

    PS-3890: Native Partitioning for TokuDB and RocksDB for 8.0
    partition_base.* are copied from ha_partition.* in 5.7 with corresponding fixes.
    
    5.7 required some additional code to read partition info from .frm files, in
    8.0 this functionality is not required, the corresponding code was removed.
    
    Currently "inline alter partition" and "exchange partition" are not implemented.
    
    As "inline alter partition" is not implemented, all "alter partition" operations
    involve full table copying. There can be such errors as "no partition found"
    duriring copying, which must be ignored, what is also implemented in the server
    code.
    
    The warning is added on alter partitioning to notify that inplace partition
    altering is not supported.
    
    The following test results were rewritten due to new warning:
    
    rocksdb      partition
    tokudb.bugs  alter_part_tokudb_bug_155
    tokudb.parts partition_mgm_lc10_tokudb partition_special_tokudb
                 partition_alter3_tokudb part_supported_sql_func_tokudb
    
    Testcase for http://bugs.mysql.com/bug.php?id=71270 was ported from 5.7. See
    bug71270.* files.
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (use std::sort() instead of removed varlen_sort)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (use Scope_guard instead of BOOST_SCOPE_EXIT_ALL)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    ----------------------------------------------------------------------
    
    PS-9218 merge: Merge MySQL 8.4.0 (partition_names fixes) (#5357)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    Reworked  "table_list->partition_names = &alter_info->partition_names;"
    part from the WL#15517
    "[5] HeatWave Support for InnoDB Partitions - Support for Loading Partitions"
    patch introduced in mysql/mysql-server@951ffba.
    
    It turned out that changing this 'partition_names' affects
    "ALTER TABLE ... ALGORITHM=INPLACE, DROP PARTITION ..." logic
    under '--rocksdb-allow-unsafe-alter=ON'.
    In particular, in the following call stack
    
    1. 'mysql_inplace_alter_table()'
    2. 'open_table()' from the
    /*
      Tell the SE that the changed table in the data-dictionary.
      For engines which don't support atomic DDL this needs to be
      done before trying to rename the table.
    */
    3. 'table->part_info->set_partition_bitmaps()' from the
    /*
      Position for each partition in the bitmap is read from the Handler_share
      instance of the table. In MYSQL_OPEN_NO_NEW_TABLE_IN_SE mode, table is not
      opened in the SE and Handler_share instance for it is not created. Hence
      skipping partitions bitmap setting in the MYSQL_OPEN_NO_NEW_TABLE_IN_SE
      mode.
    */
    4. 'set_read_partitions()'
    5. 'add_named_partition()'
    
    if the "table_list->partition_names = &alter_info->partition_names;" assignment
    is made, we will get directly into
    'my_error(ER_UNKNOWN_PARTITION, MYF(0), part_name, table->alias);'
    as 'partition_names' will include the list of partitions mentioned in the
    "DROP PARTITION ..." clause and 'find_or_nullptr()' will not be able to find
    already dropped partition in the 'partition_name_hash'.
    
    Fixed by saving the old value of 'partition_names' before calling
    'secondary_engine_unload_table()' and restoring it afterwards.
    
    Stabilized 'rocksdb.partition' MTR test case by adding '--sorted_result' to
    'SHOW TABLES".

---
+++ storage/rocksdb/ha_rockspart.cc
@@ -0,0 +1,118 @@
+/* Copyright (C) 2018 Percona
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+   USA */
+#include "ha_rockspart.h"
+#include "ha_rocksdb.h"
+
+using myrocks::ha_rocksdb;
+namespace myrocks {
+extern handlerton *rocksdb_hton;
+}
+
+handler *ha_rockspart::get_file_handler(TABLE_SHARE *share,
+                                        MEM_ROOT *alloc) const {
+  ha_rocksdb *file = new (alloc) ha_rocksdb(myrocks::rocksdb_hton, share);
+  file->init();
+  return file;
+}
+
+void ha_rockspart::set_pk_can_be_decoded_for_each_partition() {
+  for (auto file = reinterpret_cast<ha_rocksdb **>(m_file); *file; file++)
+    (*file)->set_pk_can_be_decoded(m_pk_can_be_decoded);
+}
+
+int ha_rockspart::open(const char *name, int mode, uint test_if_locked,
+                       const dd::Table *table_def) {
+  int result =
+      native_part::Partition_base::open(name, mode, test_if_locked, table_def);
+  set_pk_can_be_decoded_for_each_partition();
+  return result;
+}
+
+int ha_rockspart::create(const char *name, TABLE *form,
+                         HA_CREATE_INFO *create_info, dd::Table *table_def) {
+  int result =
+      native_part::Partition_base::create(name, form, create_info, table_def);
+  set_pk_can_be_decoded_for_each_partition();
+  return result;
+}
+
+/**
+  Clone the open and locked partitioning handler.
+
+  @param  mem_root  MEM_ROOT to use.
+
+  @return Pointer to the successfully created clone or nullptr
+
+  @details
+  This function creates a new Partition_base handler as a clone/copy. The
+  original (this) must already be opened and locked. The clone will use
+  the originals m_part_info.
+  It also allocates memory for ref + ref_dup.
+  In Partition_base::open() it will clone its original handlers partitions
+  which will allocate then on the correct MEM_ROOT and also open them.
+*/
+
+handler *ha_rockspart::clone(const char *name, MEM_ROOT *mem_root) {
+  ha_rockspart *new_handler;
+
+  DBUG_ENTER("Partition_base::clone");
+
+  /* If this->table == nullptr, then the current handler has been created but
+  not opened. Prohibit cloning such handler. */
+  if (!table)
+    DBUG_RETURN(nullptr);
+
+  new_handler =
+      new (mem_root) ha_rockspart(ht, table_share, m_part_info, this, mem_root);
+  if (!new_handler)
+    DBUG_RETURN(nullptr);
+
+  /*
+    We will not clone each partition's handler here, it will be done in
+    Partition_base::open() for clones. Also set_ha_share_ref is not needed
+    here, since 1) ha_share is copied in the constructor used above
+    2) each partition's cloned handler will set it from its original.
+  */
+
+  /*
+    Allocate new_handler->ref here because otherwise ha_open will allocate it
+    on this->table->mem_root and we will not be able to reclaim that memory
+    when the clone handler object is destroyed.
+  */
+  if (!(new_handler->ref =
+            (uchar *)alloc_root(mem_root, ALIGN_SIZE(ref_length) * 2)))
+    goto err;
+
+  if (new_handler->ha_open(table, name, table->db_stat,
+                           HA_OPEN_IGNORE_IF_LOCKED, nullptr))
+    goto err;
+
+  new_handler->m_pk_can_be_decoded = m_pk_can_be_decoded;
+  new_handler->set_pk_can_be_decoded_for_each_partition();
+
+  DBUG_RETURN((handler *)new_handler);
+
+err:
+  delete new_handler;
+  DBUG_RETURN(nullptr);
+}
+
+ulong ha_rockspart::index_flags(uint idx, uint part, bool all_parts) const {
+  return myrocks::ha_rocksdb::index_flags(m_pk_can_be_decoded, table_share, idx,
+                                          part, all_parts);
+}
+
+bool ha_rockspart::rpl_lookup_rows() { return true; }

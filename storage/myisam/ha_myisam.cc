f6b6efa8daa2e59ffe03b7514e0c7906cc77e4e4
Satya Bodapati <satya.bodapati@percona.com>
2018-08-31 14:35:28 +0530

    PS-3966: Re-implement SET STATEMENT ... FOR ... using 8.0 hint syntax
    SET STATEMENT .. FOR .. syntax is not retained but the same
    functionality is achieved by the SET_VAR hint syntax
    
    SET_VAR syntax is extended to have the SET STATEMENT features
    
    ----------------------------------------------------------------------
    
    PS-8185 - Support hint updateable variables of type PLUGIN
    
    https://jira.percona.com/browse/PS-8185
    
    *Problem:*
    
    mysql@8ba084272bbc8000fc945fcbd92490f9d2162270 introduces a new
    requirement for hint updateable variables: Their scope has to be `STATIC`.
    
    PS supports hint updateable variables whose scope is `PLUGIN`. But due to MySQL change, they are not supported anymore.
    
    *Solution:*
    
    Modify `System_variable_tracker::is_hint_updateable` so variables whose
    scope is `PLUGIN` are also checked for hint updateability.

--- storage/myisam/ha_myisam.cc
+++ storage/myisam/ha_myisam.cc
@@ -112,7 +112,7 @@ static MYSQL_SYSVAR_SET(
     nullptr, nullptr, 0, &myisam_recover_typelib);
 
 static MYSQL_THDVAR_ULONGLONG(
-    sort_buffer_size, PLUGIN_VAR_RQCMDARG,
+    sort_buffer_size, PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_HINTUPDATEABLE,
     "The buffer that is allocated when sorting the index when doing "
     "a REPAIR or when creating indexes with CREATE INDEX or ALTER TABLE",
     nullptr, nullptr, 8192 * 1024, (long)(MIN_SORT_BUFFER + MALLOC_OVERHEAD),
@@ -131,7 +131,7 @@ static MYSQL_SYSVAR_ULONGLONG(mmap_size, myisam_mmap_size,
                               SIZE_T_MAX, 1);
 
 static MYSQL_THDVAR_ENUM(
-    stats_method, PLUGIN_VAR_RQCMDARG,
+    stats_method, PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_HINTUPDATEABLE,
     "Specifies how MyISAM index statistics collection code should "
     "treat NULLs. Possible values of name are NULLS_UNEQUAL (default "
     "behavior for 4.1 and later), NULLS_EQUAL (emulate 4.0 behavior), "

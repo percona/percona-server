cb03f1289f340ba2af391d9d66d1c3a34c779dcd
Satya Bodapati <satya.bodapati@percona.com>
2018-09-11 10:09:21 +0530

    PS-4439: Port clustering secondary index patch to 8.0
    Summary:
    --------
    Support CLUSTERING KEY type with new Data Dictionary
    
    A new options property "clustering_key" is used to determine the
    CLUSTERING KEY type. This is stored as part of dd::Index::options
    
    KEY* flag HA_CLUSTERING is set based on clustering key property of
    dd::Index::options (fill_index_from_dd())
    
    Tests which are failing due to collation changes, "row" count
    mismatch of explain output (wrong column masked) are rerecorded.

--- sql/dd/dd_table.cc
+++ sql/dd/dd_table.cc
@@ -1115,6 +1115,10 @@ static void fill_dd_indexes_from_keyinfo(
     if (key->parser_name.str)
       idx_options->set("parser_name", key->parser_name.str);
 
+    if (key->flags & HA_CLUSTERING) {
+      idx_options->set("clustering_key", true);
+    }
+
     /*
       If we have no primary key, then we pick the first candidate primary
       key and promote it. When we promote, the field's of key_part needs to

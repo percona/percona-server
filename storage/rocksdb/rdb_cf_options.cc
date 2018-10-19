f40619ab7de190f79ae20062962c9ff519b4b609
George O. Lorch III <george.lorch@percona.com>
2018-10-19 09:52:20 -0700

    PS-4946 : Add option to prevent implicit creation of column family in MyRocks
    - Added new option rocksdb_no_create_column_family to prevent a user from
      creating a new column family that is not already named in the
      rocksdb_override_cf_options.  When enabled, users will get an error if they
      specify a non-existing column family in an index comment on CREATE or ALTER
      table for a MyRocks table.
    
    (cherry picked from commit 853dd48d3548fbbdaac71cab421a09f060172cad)

--- storage/rocksdb/rdb_cf_options.cc
+++ storage/rocksdb/rdb_cf_options.cc
@@ -65,7 +65,7 @@ bool Rdb_cf_options::init(
   return true;
 }
 
-void Rdb_cf_options::get(const std::string &cf_name,
+bool Rdb_cf_options::get(const std::string &cf_name,
                          rocksdb::ColumnFamilyOptions *const opts) {
   assert(opts != nullptr);
 
@@ -77,7 +77,9 @@ void Rdb_cf_options::get(const std::string &cf_name,
 
   if (it != m_name_map.end()) {
     rocksdb::GetColumnFamilyOptionsFromString(*opts, it->second, opts);
+    return true;
   }
+  return false;
 }
 
 void Rdb_cf_options::update(const std::string &cf_name,
@@ -326,16 +328,17 @@ Rdb_cf_options::get_cf_merge_operator(const std::string &cf_name) {
              : nullptr;
 }
 
-void Rdb_cf_options::get_cf_options(const std::string &cf_name,
+bool Rdb_cf_options::get_cf_options(const std::string &cf_name,
                                     rocksdb::ColumnFamilyOptions *const opts) {
   assert(opts != nullptr);
 
   *opts = m_default_cf_opts;
-  get(cf_name, opts);
+  bool ret = get(cf_name, opts);
 
   // Set the comparator according to 'rev:'
   opts->comparator = get_cf_comparator(cf_name);
   opts->merge_operator = get_cf_merge_operator(cf_name);
+  return ret;
 }
 
 }  // namespace myrocks

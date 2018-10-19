1ef1e1d862644c4283c8c54e6bb6194c2631dee1
George O. Lorch III <george.lorch@percona.com>
2018-10-19 09:52:20 -0700

    PS-4946 : Add option to prevent implicit creation of column family in MyRocks
    - Added new option rocksdb_no_create_column_family to prevent a user from
      creating a new column family that is not already named in the
      rocksdb_override_cf_options.  When enabled, users will get an error if they
      specify a non-existing column family in an index comment on CREATE or ALTER
      table for a MyRocks table.
    
    (cherry picked from commit 853dd48d3548fbbdaac71cab421a09f060172cad)

--- storage/rocksdb/rdb_cf_options.h
+++ storage/rocksdb/rdb_cf_options.h
@@ -46,8 +46,9 @@ class Rdb_cf_options {
   Rdb_cf_options &operator=(const Rdb_cf_options &) = delete;
   Rdb_cf_options() = default;
 
-  void get(const std::string &cf_name,
-           rocksdb::ColumnFamilyOptions *const opts);
+  /* bool true return indicates cf_name was found */
+  MY_NODISCARD bool get(const std::string &cf_name,
+                        rocksdb::ColumnFamilyOptions *const opts);
 
   void update(const std::string &cf_name, const std::string &cf_options);
 
@@ -67,7 +68,8 @@ class Rdb_cf_options {
   std::shared_ptr<rocksdb::MergeOperator>
   get_cf_merge_operator(const std::string &cf_name);
 
-  void get_cf_options(const std::string &cf_name,
+  /* bool true return indicates cf_name was found, otherwise default */
+  bool get_cf_options(const std::string &cf_name,
                       rocksdb::ColumnFamilyOptions *const opts);
 
   static bool parse_cf_options(const std::string &cf_options,

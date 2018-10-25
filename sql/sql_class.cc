34ce860bf97efdb594161d11641e3e4d0f5e700e
przemyslaw.skibinski@percona.com <przemyslaw.skibinski@percona.com>
2018-10-25 16:34:24 +0200

    [plugin/fulltext] PS-269: Revert Oracle's fix for fulltext search
    Reverted Oracle's fix for the Bug #25873310
    "FULLTEXT SEARCH CAN NOT FIND WORD WHICH CONTAINS ',' OR '.'"
    (commit https://github.com/mysql/mysql-server/commit/c1a5784)
    as it does not allow special characters to be indexed by NGRAM
    FTS parser any more which makes Percona-specific variable
    'ft_query_extra_word_chars' introduced
    in the fix for PS-2501
    "LP #1689268: Fulltext search can not find word which contains punctuation marks"
    (https://jira.percona.com/browse/PS-2501)
    (commit https://github.com/percona/percona-server/commit/b7cb587ac422f1003f3fda43ba91d32910fdc8a4)
    completely useless.
    
    Changes in `innodb_fts/include/ngram.inc` were also reverted.
    `innodb_fts/t/ngram_1.test` was not reverted.
    `innodb_fts/r/ngram_1.result` was re-recorded as `ngram.inc` was changed.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -1634,6 +1634,17 @@ void THD::awake(THD::killed_state state_to_set) {
 }
 
 /**
+   Check whether ft_query_extra_word_chars server variable is enabled for the
+   current session
+
+   @return ft_query_extra_word_chars value
+*/
+extern "C" int thd_get_ft_query_extra_word_chars(void) {
+  const THD *thd = current_thd;
+  return thd ? thd->variables.ft_query_extra_word_chars : 0;
+}
+
+/**
   Close the Vio associated this session.
 
   @remark LOCK_thd_data is taken due to the fact that

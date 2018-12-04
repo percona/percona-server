9daf2a63eebb92d74a7517642ae6e2862ed0f5f4
George O. Lorch III <george.lorch@percona.com>
2018-12-03 20:16:26 -0700

    PS-5027 : [PS8QA] handle_fatal_signal (sig=11) in THD::mark_innodb_used
    - Added assignment to 'clear' transaction statistics 'take_stats' on transaction
      allocation.
    
    - Added additional assertion in thd_report_innodb_stat to validate that thd is
      valid and not a background thread.
    
    - Introduced new mtr helper wait_for_search_pattern.inc that combines the
      functionality of search_pattern.inc with a retry/wait loop similar to
      wait_condition to be able to wait for a pattern to appear in a file.  this
      is needed/useful to wait for a background thread to report an error to the
      mysqld log file.
    
    - Added new test case that validates issue is fixed.

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -1105,6 +1105,7 @@ THD::~THD() {
 extern "C" void thd_report_innodb_stat(THD *thd, unsigned long long trx_id,
                                        enum mysql_trx_stat_type type,
                                        unsigned long long value) {
+  DBUG_ASSERT(thd && !thd_is_background_thread(thd));
   thd->mark_innodb_used(trx_id);
   switch (type) {
     case MYSQL_TRX_STAT_IO_READ_BYTES:

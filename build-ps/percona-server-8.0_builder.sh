b56dd45091dceb9cbe5ea997cf4b18fb927cd402
Evgeniy Patlan <evgeniy.patlan@percona.com>
2018-12-11 12:11:48 +0200

    Fix rpm patch

--- build-ps/percona-server-8.0_builder.sh
+++ build-ps/percona-server-8.0_builder.sh
@@ -118,7 +118,6 @@ deb-src http://jenkins.percona.com/apt-repo/ @@DIST@@ main
 EOL
     sed -i "s:@@DIST@@:$OS_NAME:g" /etc/apt/sources.list.d/percona-dev.list
   fi
-  
   add_key="apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 9334A25F8507EFA5"
   until ${add_key}; do
     sleep 1

ffcb94d8478eeb0ff354cf70532040d609b86595
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2018-12-18 08:46:45 +0200

    PS-3672 / PS-3673 / PS-3568 / PS-3535 in preparation for PS-5041 / PS-4999
    Improve, cleanup and attempt to stabilize extended slow query log
    testcases:
    - create new include file log_prepare.inc to be included from each
      testcase that uses log_start.inc/log_stop.inc/log_cleanup.inc, and
      move saving the old slow_query_log, slow_query_log_file, and
      log_output values, and the setting of log_output to FILE from
      log_start.inc. These actions are redundant (and unstable in 8.0) for
      each log_start.inc invocation. Consequently move the restoring of
      these variables from log_stop.inc to log_cleanup.inc;
    - adjust the testcases to restore global variables to initial values,
      not to defaults;
    - drop any DROP thing IF EXISTS foo; idioms;
    - drop any restoring of session variables to default values;
    - use SET SESSION instead of plain SET consistently;
    - delete main.percona_bug1016991-master.opt and set
      slow_query_log_use_global_control to all in the testcase
      dynamically;
    - deleted stray
      innodb.percona_log_slow_slave_statements-innodb.result.
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix terminology in replication tests)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    https://github.com/mysql/mysql-server/commit/44a77b5ba29994f8d3634b62c2825481574fc057

--- sql/sql_class.cc
+++ sql/sql_class.cc
@@ -2642,6 +2642,7 @@ void THD::inc_examined_row_count(ha_rows count) {
 void THD::inc_status_created_tmp_disk_tables() {
   assert(!status_var_aggregated);
   status_var.created_tmp_disk_tables++;
+  tmp_tables_disk_used++;
   query_plan_flags |= QPLAN_TMP_DISK;
 #ifdef HAVE_PSI_STATEMENT_INTERFACE
   PSI_STATEMENT_CALL(inc_statement_created_tmp_disk_tables)(m_statement_psi, 1);
@@ -2651,6 +2652,7 @@ void THD::inc_status_created_tmp_disk_tables() {
 void THD::inc_status_created_tmp_tables() {
   assert(!status_var_aggregated);
   status_var.created_tmp_tables++;
+  tmp_tables_used++;
   query_plan_flags |= QPLAN_TMP_TABLE;
 #ifdef HAVE_PSI_STATEMENT_INTERFACE
   PSI_STATEMENT_CALL(inc_statement_created_tmp_tables)(m_statement_psi, 1);

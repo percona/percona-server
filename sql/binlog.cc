6beae115bf1503d69111145bd005c22b0dfd4923
przemyslaw.skibinski@percona.com <przemyslaw.skibinski@percona.com>
2019-01-01 21:51:09 +0100

    PS-5263: no tmpdir space can lead to assert in THD::send_statement_status
    1. Update `handle_binlog_flush_or_sync_error()` to set `my_ok(thd)` after `thd->clear_error()`.
    2. Update `rpl_bug72457.inc` to support errorless statements that disable binary logging.
    3. Re-record `rpl_bug72457_innodb` and `rpl_bug72457_myisam`.
    
    PS-5263: Follow up fix for binlog_cache_write_failure testcase failure
    
    Problem:
    --------
    PS-5263 added extra my_ok() in MYSQL_BIN_LOG::handle_binlog_flush_or_sync_error
    to handle binlog_error_action = ABORT_SERVER for autocommit SQLs (like INSERT, DELETE, UPDATE etc)
    
    Because of this my_ok(), SQLs like COMMIT, ROLLBACK, etc failed with assert failure in my_ok().
    These SQLs do extra my_ok() after executing the commands.
    
    Double my_ok() is not allowed. the DBUG_ASSERT(!is_set()) fails for second my_ok().
    
    Fix:
    ----
    Avoid the extra my_ok() in MYSQL_BIN_LOG::handle_binlog_flush_or_sync_error() for SQLs
    COMMIT, ROLLBACK, ROLLBACK TO SAVEPOINT
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix terminology in replication tests)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    https://github.com/mysql/mysql-server/commit/44a77b5ba29994f8d3634b62c2825481574fc057

--- sql/binlog.cc
+++ sql/binlog.cc
@@ -9184,7 +9184,18 @@ void MYSQL_BIN_LOG::handle_binlog_flush_or_sync_error(THD *thd,
       binlog_error_action=IGNORE_ERROR, clear the error
       and allow the commit to happen in storage engine.
     */
-    if (check_write_error(thd)) thd->clear_error();
+    if (check_write_error(thd)) {
+      /* we have DA_ERROR */
+      thd->clear_error(); /* sets thd->get_stmt_da()->status() to DA_EMPTY */
+      /* For SQLCOM_COMMIT, ROLLBACK, ROLLBACK TO SAVEPOINT, there is already
+      my_ok() in mysql_execute_command. Doing double my_ok() is not allowed. So
+      we avoid that here */
+      if (thd_sql_command(thd) != SQLCOM_COMMIT &&
+          thd_sql_command(thd) != SQLCOM_ROLLBACK &&
+          thd_sql_command(thd) != SQLCOM_ROLLBACK_TO_SAVEPOINT) {
+        my_ok(thd); /* sets thd->get_stmt_da()->status() to DA_OK */
+      }
+    }
 
     if (need_lock_log) mysql_mutex_unlock(&LOCK_log);
     DEBUG_SYNC(thd, "after_binlog_closed_due_to_error");

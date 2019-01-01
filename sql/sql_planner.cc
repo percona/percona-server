0eaf4824e7011101d6745ab6d26bd406ecefe1f4
Sergei Glushchenko <gl.sergei@gmail.com>
2019-01-01 23:53:25 +0700

    PS-4949: Bad select+order by+limit performance in 5.7
    Optimizer choosing ref-access when range access is more optimal. This is
    related to upstream bug #26727773 (commit ddd9d614a32). This bug was
    fixed differently in 5.7 and 8.0. 5.7 fix doesn't work for this query
    and 8.0 looks more robust. It is choosing range access over ref access
    if ref access using more keyparts.
    
    Fixed by taking fix and test cases from 8.0.
    
    Take 8.0 version of fix for #26727773
    
    fix/re-record some test cases

--- sql/sql_planner.cc
+++ sql/sql_planner.cc
@@ -498,8 +498,6 @@ Key_use* Optimize_table_order::find_best_ref(JOIN_TAB *tab,
               on the same index,
               (2) and that quick select uses more keyparts (i.e. it will
               scan equal/smaller interval then this ref(const))
-              (3) and E(#rows) for quick select is higher then our
-              estimate,
               Then use E(#rows) from quick select.
 
               One observation is that when there are multiple
@@ -515,11 +513,12 @@ Key_use* Optimize_table_order::find_best_ref(JOIN_TAB *tab,
               TODO: figure this out and adjust the plan choice if needed.
             */
             if (!table_deps && table->quick_keys.is_set(key) &&     // (1)
-                table->quick_key_parts[key] > cur_used_keyparts &&  // (2)
-                cur_fanout <= (double)table->quick_rows[key])        // (3)
+                table->quick_key_parts[key] > cur_used_keyparts)    // (2)
                 {
-                  cur_fanout= (double)table->quick_rows[key];
+                  trace_access_idx.add("chosen", false)
+                      .add_alnum("cause", "range_uses_more_keyparts");
                   is_dodgy= true;
+                  continue;
                 }
 
             tmp_fanout= cur_fanout;

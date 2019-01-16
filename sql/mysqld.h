d50e2cd58aaf5f7dd27248887a86b6d4d9f82782
Yura Sorokin <yura@Yuras-MacBook-Pro.local>
2019-01-16 16:11:33 +0100

    PS-1827 (Inconsistent and unsafe FLUSH behavior in terms of replication)
    https://jira.percona.com/browse/PS-1827
    
    Introduced new global boolean system variable 'binlog_skip_flush_commands'.
    When set to 'ON', all 'FLUSH <XXX>' statements are not written to the binary
    log.
    
    Also, changed the behavior of 'read_only' / 'super_read_only' modes.
    - If global 'read_only' system variable is set to 'ON', 'FLUSH <XXX>' statements
    are never written to the binary log (regardless of the value of 'binlog_skip_flush_commands')
    when executed by a user without 'SUPER' privilege.
    - If global 'super_read_only' system variable is set to 'ON', 'FLUSH <XXX>'
    statements are never written to the binary log (regardless of the value of 'binlog_skip_flush_commands')
    even if executed by a user with 'SUPER' privilege.
    
    Added 'binlog.percona_binlog_skip_flush_commands' MTR test case which checks
    if various 'FLUSH <XXX>' statements change 'gtid_executed' when 'binlog_skip_flush_commands'
    is set to 'ON'/ 'OFF' for different types of users.
    
    Added 'sys_vars.binlog_skip_flush_commands_basic' MTR test case which checks
    basic global boolean system variable functionality.
    
    Re-recorded 'main.mysqld--help-notwin' MTR test case because of the new
    variable added.
    
    ----------------------------------------------------------------------
    
    PS-9071: Merge MySQL 8.3.0 (fix percona_binlog_skip_flush_commands)
    
    https://perconadev.atlassian.net/browse/PS-9071
    
    Deprecated FLUSH HOSTS has been removed.
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix binlog_gtid.percona_binlog_skip_flush_commands test)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    Separate FLUSH PRIVILEGES privilege was introduced in
    https://github.com/mysql/mysql-server/commit/fd3a42fe6c943023c142b7131d43d65fe7087826

--- sql/mysqld.h
+++ sql/mysqld.h
@@ -357,6 +357,7 @@ enum enum_binlog_error_action {
   ABORT_SERVER = 1
 };
 extern const char *binlog_error_action_list[];
+extern bool opt_binlog_skip_flush_commands;
 extern char *opt_authentication_policy;
 extern bool opt_replica_allow_higher_version_source;
 

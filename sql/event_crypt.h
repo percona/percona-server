55c6d8c943673dca9cd60225a4e7d913b06d14ea
Robert Golebiowski <robert.golebiowski@percona.com>
2019-02-12 13:53:40 +0100

    [encryption] PS-5326 8.0.14 should be able to decrypt binary logs encrypted with 8.0.13
    5.7 binlog encryption was removed. The part resposible for decrypting
    5.7 encrypted binary log was kept. binlog_encryption test suite was
    renamed to bin_57_decryption. The tests in this test suite were
    rewritten to test 5.7 decryption, some of the tests were removed.
    Also almost all tests from rpl_encryption test suite were removed,
    apart from the tests that are "greping" the files that could contain
    sensitive information to check whether they are encrypted.
    The START_ENCRYPTION_EVENT was changed to START_5_7_ENCRYPTION_EVENT
    and error messages were amended accordingly.

--- sql/event_crypt.h
+++ sql/event_crypt.h
@@ -4,6 +4,7 @@
 #include "binlog_crypt_data.h"
 #include "my_crypt.h"
 
+// Decrypt Percona binlog encryption encrypted-event
 bool decrypt_event(uint32 offs, const Binlog_crypt_data &crypto, uchar *buf,
                    uchar *ebuf, size_t buf_len);
 

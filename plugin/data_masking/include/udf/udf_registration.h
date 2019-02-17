81b8eeabbe0176ca33ae0d5668b1f371e112c4c6
Francisco Miguel Biete Banon <fbiete@gmail.com>
2019-02-17 16:08:47 +0000

    [plugin/data_masking] GPL version of Data Masking for MySQL v8.0
    PS-5674: Fix mismatched new/delete errors in data_masking plugin
    
    ----------------------------------------------------------------------------
    
    PS-7309 : Data Masking Plugin: gen_range() doesn't support negative numbers
    
    Problem:
    --------
    gen_range() when given a range with negative numbers, it produces a
    positive number
    
    Example: SELECT gen_range(-1000, -800); -> 4294966357
    
    Fix:
    ----
    1. Support negative numbers.
    2. Support a range from negative and postive. ie gen_range(-100, 100) is valid
    3. We now support a range of long. So the valid range is -9223372036854775808 to
       9223372036854775807
    
    ----------------------------------------------------------------------------
    
    PS-7322 : Datamasking: mask_outer() doesn't work if the right mask length is string length - 1
    
    Problem:
    --------
    mask_outer() the input string as is if the right mask length is string length - 1
    
    ie. SELECT mask_outer('abcdef',0, 5); returns original string as is
    
    Fix:
    ----
    Fix the right mask length calculation to handle up to string length
    
    ----------------------------------------------------------------------------
    
    PS-7321 : Data masking: gen_rnd_pan() sometimes generates 17 digit numbers.
    Should be only either 15 or 16
    
    Problem:
    --------
    SELECT gen_rnd_pan() generates random credit card numbers. They should be of
    either 15 or 16 digits.
    
    Sometimes, we see 17 digit numbers.
    
    Fix:
    ----
    1. Fix American express card number genearation. 2nd digit should be always 4
       or 7. Not  4 or 5 or 6 or 7
    2. Fix the calculation that use last number as 10 instead of 0.
    3. Added assert that number generated should be of length 15 or 16

---
+++ plugin/data_masking/include/udf/udf_registration.h
@@ -0,0 +1,23 @@
+#ifndef _PLUGIN_DATA_MASKING_UDFREGISTRATION_H
+/* Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2018, 2019 Francisco Miguel Biete Banon. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
+
+#define _PLUGIN_DATA_MASKING_UDFREGISTRATION_H
+
+bool register_udfs();
+bool unregister_udfs();
+
+#endif  // _PLUGIN_DATA_MASKING_UDFREGISTRATION_H

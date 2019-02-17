85ca2d984edfcd0b8ce1e080c591764906592f0d
Francisco Miguel Biete Banon <fbiete@gmail.com>
2019-02-17 16:08:47 +0000

    [plugin/data_masking] GPL version of Data Masking for MySQL v8.0
    PS-5674: Fix mismatched new/delete errors in data_masking plugin
    
    ----------------------------------------------------------------------------
    
    PS-7309 : Data Masking Plugin: gen_range() doesn't support negative numbers
    
    Problem:
    --------
    gen_range() when given a range with negative numbers, it produces a
    positive number
    
    Example: SELECT gen_range(-1000, -800); -> 4294966357
    
    Fix:
    ----
    1. Support negative numbers.
    2. Support a range from negative and postive. ie gen_range(-100, 100) is valid
    3. We now support a range of long. So the valid range is -9223372036854775808 to
       9223372036854775807
    
    ----------------------------------------------------------------------------
    
    PS-7322 : Datamasking: mask_outer() doesn't work if the right mask length is string length - 1
    
    Problem:
    --------
    mask_outer() the input string as is if the right mask length is string length - 1
    
    ie. SELECT mask_outer('abcdef',0, 5); returns original string as is
    
    Fix:
    ----
    Fix the right mask length calculation to handle up to string length
    
    ----------------------------------------------------------------------------
    
    PS-7321 : Data masking: gen_rnd_pan() sometimes generates 17 digit numbers.
    Should be only either 15 or 16
    
    Problem:
    --------
    SELECT gen_rnd_pan() generates random credit card numbers. They should be of
    either 15 or 16 digits.
    
    Sometimes, we see 17 digit numbers.
    
    Fix:
    ----
    1. Fix American express card number genearation. 2nd digit should be always 4
       or 7. Not  4 or 5 or 6 or 7
    2. Fix the calculation that use last number as 10 instead of 0.
    3. Added assert that number generated should be of length 15 or 16

---
+++ plugin/data_masking/src/plugin_memory.cc
@@ -0,0 +1,59 @@
+/* Copyright (c) 2018, 2019 Francisco Miguel Biete Banon. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
+
+#include "plugin/data_masking/include/plugin_memory.h"
+
+void init_data_masking_psi_keys() {
+  PSI_rwlock_info all_data_masking_rwlock[] = {
+      {&key_data_masking_rwlock, "data_masking::rwlock", PSI_FLAG_SINGLETON, 0,
+       PSI_DOCUMENT_ME}};
+
+  PSI_memory_info all_data_masking_memory[] = {
+      {&key_memory_data_masking, "data_masking", 0, 0, PSI_DOCUMENT_ME}};
+
+  const char *category = "data_masking";
+  int count;
+
+  count = static_cast<int>(array_elements(all_data_masking_memory));
+  mysql_memory_register(category, all_data_masking_memory, count);
+
+  count = static_cast<int>(array_elements(all_data_masking_rwlock));
+  mysql_rwlock_register(category, all_data_masking_rwlock, count);
+}
+
+void init_data_masking_memory() {
+#ifdef HAVE_PSI_INTERFACE
+  init_data_masking_psi_keys();
+
+  // Allocate dynamic variables at plugin level
+  void *pBuffer = (t_mask_dict *)my_malloc(key_memory_data_masking,
+                                           sizeof(t_mask_dict), MYF(0));
+#else
+  void *pBuffer = (t_mask_dict *)my_malloc(sizeof(t_mask_dict), MYF(0));
+#endif  // HAVE_PSI_INTERFACE
+  if (pBuffer) g_data_masking_dict = new (pBuffer) t_mask_dict();
+
+  mysql_rwlock_init(key_data_masking_rwlock, &g_data_masking_dict_rwlock);
+}
+
+void deinit_data_masking_memory() {
+  // Destroy global variables and rwlock protecting them
+  mysql_rwlock_wrlock(&g_data_masking_dict_rwlock);
+  g_data_masking_dict->~t_mask_dict();
+  my_free(g_data_masking_dict);
+  mysql_rwlock_unlock(&g_data_masking_dict_rwlock);
+
+  mysql_rwlock_destroy(&g_data_masking_dict_rwlock);
+}

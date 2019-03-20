01dcdbd6644ac9e5b5e834b742e5ded3ab4cd424
Satya Bodapati <satya.bodapati@percona.com>
2019-03-20 12:06:13 +0530

    PS-5206 : [PS8QA] handle_fatal_signal (sig=11) in handler::get_ha_share_ptr
    Problem:
    -------
    Server crashes when ha_tokupart::clone() interface is used.
    
    Fix:
    ----
    To clone native partition tokupart handler, we will not use clone()
    on each partition handler. This is because tokudb_create_handler() gives
    ha_tokupart handler instead of ha_tokudb handlers. This happens becuase of
    presence of parition info in TABLE_SHARE
    
    We create a new partition handler in clone() with part_info from the current
    handler.
    
    Note that problem is exactly same for rocksdb as well and the same fix
    applies.

--- storage/rocksdb/ha_rockspart.cc
+++ storage/rocksdb/ha_rockspart.cc
@@ -57,35 +57,26 @@ int ha_rockspart::create(const char *name, TABLE *form,
   @return Pointer to the successfully created clone or nullptr
 
   @details
-  This function creates a new Partition_base handler as a clone/copy. The
-  original (this) must already be opened and locked. The clone will use
-  the originals m_part_info.
-  It also allocates memory for ref + ref_dup.
-  In Partition_base::open() it will clone its original handlers partitions
-  which will allocate then on the correct MEM_ROOT and also open them.
+  This function creates a new native_part::Partition_base handler as a
+  clone/copy. The clone will use the original m_part_info. It also allocates
+  memory for ref + ref_dup. In native_part::Partition_base::open()
+  a new partition handlers are created (on the passed mem_root)
+  for each partition and are also opened.
 */
 
 handler *ha_rockspart::clone(const char *name, MEM_ROOT *mem_root) {
   ha_rockspart *new_handler;
 
-  DBUG_ENTER("Partition_base::clone");
+  DBUG_ENTER("ha_rockspart::clone");
 
   /* If this->table == nullptr, then the current handler has been created but
   not opened. Prohibit cloning such handler. */
   if (!table)
     DBUG_RETURN(nullptr);
 
-  new_handler =
-      new (mem_root) ha_rockspart(ht, table_share, m_part_info, this, mem_root);
+  new_handler = new (mem_root) ha_rockspart(ht, table_share, m_part_info);
   if (!new_handler)
     DBUG_RETURN(nullptr);
-
-  /*
-    We will not clone each partition's handler here, it will be done in
-    Partition_base::open() for clones. Also set_ha_share_ref is not needed
-    here, since 1) ha_share is copied in the constructor used above
-    2) each partition's cloned handler will set it from its original.
-  */
 
   /*
     Allocate new_handler->ref here because otherwise ha_open will allocate it
@@ -96,6 +87,11 @@ handler *ha_rockspart::clone(const char *name, MEM_ROOT *mem_root) {
             (uchar *)alloc_root(mem_root, ALIGN_SIZE(ref_length) * 2)))
     goto err;
 
+  /* We will not use clone() interface to clone individual partition
+  handlers. This is because tokudb_create_handler() gives ha_tokupart handler
+  instead of ha_tokudb handlers. This happens because of presence of parition
+  info in TABLE_SHARE. New partition handlers are created for each partiton
+  in native_part::Partition_base::open() */
   if (new_handler->ha_open(table, name, table->db_stat,
                            HA_OPEN_IGNORE_IF_LOCKED, nullptr))
     goto err;

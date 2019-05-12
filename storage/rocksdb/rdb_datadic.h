39733635b4c3ef356ed55d975d984314af6636f3
Yi Zhang <yzha@fb.com>
2019-05-11 22:06:58 -0700

    Use unordered_map instead of my_hash
    Upstream commit ID : fb-mysql-5.6.35/1023429b32fd596722497d4fad576a4aecc1539b
    PS-6867 : Merge fb-prod201905
    
    Summary:
    This is part of MyRocks 8.0 prep work - front loading as much work as possible in 5.6 before we actually port MyRocks to 8.0.
    
    In MySQL 8.0 the my_hash completely stuff is gone and replaced by unordered_map and various sub-class (such as collation_unordered_map, etc). And in their source code blog they claim that by switching to unordered_map gives them performance wins. So it makes sense for us to switch to it preemptively before we port to MySQL 8.0.
    
    This is rewrite/enhancements of below changes from georgelorchpercona (not a literal port):
    
    https://github.com/facebook/mysql-5.6/pull/930/commits/20d4b2b7376194144ce05ef02949ea1f479928c5
    https://github.com/facebook/mysql-5.6/pull/930/commits/8a5e758dbdd5c4b9e4f5839bb0791edfac4e7638
    
    Reviewed By: lth
    
    Differential Revision: D15313077
    
    fbshipit-source-id: fb65bf27de0

--- storage/rocksdb/rdb_datadic.h
+++ storage/rocksdb/rdb_datadic.h
@@ -24,6 +24,7 @@
 #include <map>
 #include <mutex>
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
@@ -1242,8 +1243,10 @@ interface Rdb_tables_scanner {
 
 class Rdb_ddl_manager {
   Rdb_dict_manager *m_dict = nullptr;
+
   // Contains Rdb_tbl_def elements
-  std::unordered_map<std::string, Rdb_tbl_def *> m_ddl_hash;
+  std::unordered_map<std::string, Rdb_tbl_def *> m_ddl_map;
+
   // Maps index id to <table_name, index number>
   std::map<GL_INDEX_ID, std::pair<std::string, uint>> m_index_num_to_keydef;
 

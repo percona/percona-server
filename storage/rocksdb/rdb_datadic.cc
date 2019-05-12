608f6d5a20eb1e02f72d72e393fa1337009bc097
Yi Zhang <yzha@fb.com>
2019-05-11 22:06:58 -0700

    Use unordered_map instead of my_hash
    Upstream commit ID : fb-mysql-5.6.35/1023429b32fd596722497d4fad576a4aecc1539b
    PS-6867 : Merge fb-prod201905
    
    Summary:
    This is part of MyRocks 8.0 prep work - front loading as much work as possible in 5.6 before we actually port MyRocks to 8.0.
    
    In MySQL 8.0 the my_hash completely stuff is gone and replaced by unordered_map and various sub-class (such as collation_unordered_map, etc). And in their source code blog they claim that by switching to unordered_map gives them performance wins. So it makes sense for us to switch to it preemptively before we port to MySQL 8.0.
    
    This is rewrite/enhancements of below changes from georgelorchpercona (not a literal port):
    
    https://github.com/facebook/mysql-5.6/pull/930/commits/20d4b2b7376194144ce05ef02949ea1f479928c5
    https://github.com/facebook/mysql-5.6/pull/930/commits/8a5e758dbdd5c4b9e4f5839bb0791edfac4e7638
    
    Reviewed By: lth
    
    Differential Revision: D15313077
    
    fbshipit-source-id: fb65bf27de0

--- storage/rocksdb/rdb_datadic.cc
+++ storage/rocksdb/rdb_datadic.cc
@@ -4790,7 +4790,7 @@ bool Rdb_ddl_manager::init(Rdb_dict_manager *const dict_arg,
 
 Rdb_tbl_def *Rdb_ddl_manager::find(const std::string &table_name,
                                    const bool lock) {
-  Rdb_tbl_def *ret = nullptr;
+  Rdb_tbl_def *rec = nullptr;
 
   if (lock) {
     mysql_rwlock_rdlock(&m_rwlock);
@@ -4804,7 +4804,7 @@ Rdb_tbl_def *Rdb_ddl_manager::find(const std::string &table_name,
     mysql_rwlock_unlock(&m_rwlock);
   }
 
-  return ret;
+  return rec;
 }
 
 // this is a safe version of the find() function below.  It acquires a read
@@ -4960,12 +4960,13 @@ int Rdb_ddl_manager::put_and_write(Rdb_tbl_def *const tbl,
 
 /* Return 0 - ok, other value - error */
 /* TODO:
-  This function modifies m_ddl_hash and m_index_num_to_keydef.
+  This function modifies m_ddl_map and m_index_num_to_keydef.
   However, these changes need to be reversed if dict_manager.commit fails
   See the discussion here: https://reviews.facebook.net/D35925#inline-259167
   Tracked by https://github.com/facebook/mysql-5.6/issues/33
 */
 int Rdb_ddl_manager::put(Rdb_tbl_def *const tbl, const bool lock) {
+  Rdb_tbl_def *rec;
   const std::string &dbname_tablename = tbl->full_tablename();
 
   if (lock)
@@ -4973,12 +4974,12 @@ int Rdb_ddl_manager::put(Rdb_tbl_def *const tbl, const bool lock) {
 
   // We have to do this find because 'tbl' is not yet in the list.  We need
   // to find the one we are replacing ('rec')
-  const auto &it = m_ddl_hash.find(dbname_tablename);
-  if (it != m_ddl_hash.end()) {
+  const auto &it = m_ddl_map.find(dbname_tablename);
+  if (it != m_ddl_map.end()) {
     delete it->second;
-    m_ddl_hash.erase(it);
+    m_ddl_map.erase(it);
   }
-  m_ddl_hash.insert({dbname_tablename, tbl});
+  m_ddl_map.emplace(dbname_tablename, tbl);
 
   for (uint keyno = 0; keyno < tbl->m_key_count; keyno++) {
     m_index_num_to_keydef[tbl->m_key_descr_arr[keyno]->get_gl_index_id()] =
@@ -5004,10 +5005,12 @@ void Rdb_ddl_manager::remove(Rdb_tbl_def *const tbl,
 
   m_dict->delete_key(batch, key_writer.to_slice());
 
-  const auto &it = m_ddl_hash.find(dbname_tablename);
-  if (it != m_ddl_hash.end()) {
+  const auto &it = m_ddl_map.find(dbname_tablename);
+  if (it != m_ddl_map.end()) {
+    // Free Rdb_tbl_def
     delete it->second;
-    m_ddl_hash.erase(it);
+
+    m_ddl_map.erase(it);
   }
 
   if (lock)
@@ -5068,7 +5071,7 @@ void Rdb_ddl_manager::cleanup() {
 }
 
 int Rdb_ddl_manager::scan_for_tables(Rdb_tables_scanner *const tables_scanner) {
-  int i, ret;
+  int ret;
 
   DBUG_ASSERT(tables_scanner != nullptr);
 
@@ -5077,8 +5080,8 @@ int Rdb_ddl_manager::scan_for_tables(Rdb_tables_scanner *const tables_scanner) {
   ret = 0;
   i = 0;
 
-  for (const auto &it : m_ddl_hash) {
-    ret = tables_scanner->add_table(it.second);
+  for (const auto &kv : m_ddl_map) {
+    ret = tables_scanner->add_table(kv.second);
     if (ret)
       break;
     i++;

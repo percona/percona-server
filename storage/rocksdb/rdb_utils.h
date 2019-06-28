629df394672de27682360a3313de7ac67b1dd3cd
Zsolt Parragi <zsolt.parragi@percona.com>
2019-06-28 14:32:37 +0200

    PS-5102: MyRocks errors after 8.0.16 merge
    * Regular expressions are now based on std::regex
    * Added include my_systime.h to get declaration of my_sleep
    * Fix initialization of PSI_stage_info_v1 structs that are now C++14-ized
    * Fix use of Item::print as it now needs a const THD* passed in.
    * Added include of mysql/psi/mysql_rwlock.h to re-gain rwlock types/definitions.
    * Added include of sql/sql_table.h to re-gain declaration of ST_FIELD_INFO
    * Fix initialization of PSI thread, mutex, rwlock, cond, and memory structs that all had new members added.
    * Rerecorded many testcases where warnings now refer to different rows, but are still correct.
    * Rerecorded 2 tests (collation_exceptions_lctn_0, collation_exceptions_lctn_1) where additional warnings are reported now
    * Rerecorded rocksdb.rocksdb, where an explain changed (impossible where) (?)
    * Rerecorded rocksdb_cf_per_partition: information_schema.rocksdb_cfstats now has an upper case value
    * Rerecorded rocksdb.type_decimal: warning formatting changed
    * Fixed rocksdb.create_table: ENCRYPTION="y" returns a different error code
    * Fixed a syntax error in rocksdb.select and rerecorded it
    
    Remaining MTR failures:
    
    * rocksdb.unique_sec_rev_cf - Using Gap Lock without full unique key in multi-table or multi-statement transactions is not allowed.
    * rocksdb.unique_sec - Using Gap Lock without full unique key in multi-table or multi-statement transactions is not allowed.
    * rocksdb.bulk_load_sk - select count(*) with force index returns a different result
    * upgrade_parts_from_prev_ver - mysql_upgrade deprecated
    
    PS-5102 : Merge MySQL 8.0.16
    PS-5129 : rocksdb.bulk_load_sk failing
    
    - Reworked test rocksdb.bulk_load_sk to avoid COUNT() optimization and maintain
      accurate results.
    
    PS-5102 : Merge MySQL 8.0.16
    PS-5130 : rocksdb.unique_sec and rocksdb.unique_sec_rev_cf failing
    
    - There is a change in the way the mtr interpreter now works with '--error' and
      'if'.  Prior to 8.0, this is valid:
        if ($foo == "a")
        {
          --error ER_SOME_ERROR
        }
        if ($foo == "b")
        {
          --error ER_SOME_OTHER_ERROR
        }
        SELECT ...some error happens...;
      The error anticipation would happen properly on the SELECT statement. In 8.0,
      this does not happen, it is as if --error has no effect at all.
    - This commit reorders some of the test logic to get around this new
      logic/flow control behavior.
    
    PS-5102 : Merge MySQL 8.0.16
    PS-5753 : Tests that use upgrade include framework failing.
    
    - Removed explicit call to mysqlupgrade from test and allow natural, automatic
      upgrade to happen when 8.0.16 serter is started.
    - Re-recorded impacted test(s).
    
    PS-5102 : Merge MySQL 8.0.16
    
    - Re-recorded various tests due to changes in common mtr includes and EXPLAIN
      outputs.
    
    - Re-work and re-record test due to change in case of results in I_S table and
      apparant collision with other test on un-restarted server in rocksdb data.

--- storage/rocksdb/rdb_utils.h
+++ storage/rocksdb/rdb_utils.h
@@ -26,7 +26,6 @@
 #include "mysql/components/services/log_builtins.h"
 
 #include "my_stacktrace.h"
-#include "sql/sql_regex.h"
 #include "sql_string.h"
 
 /* RocksDB header files */
@@ -309,7 +308,7 @@ std::string rdb_hexdump(const char *data, const std::size_t data_len,
  */
 bool rdb_database_exists(const std::string &db_name);
 
-void warn_about_bad_patterns(const Regex &regex, const char *name);
+void warn_about_bad_patterns(const char *regex, const char *name);
 
 std::vector<std::string> split_into_vector(const std::string &input,
                                            char delimiter);

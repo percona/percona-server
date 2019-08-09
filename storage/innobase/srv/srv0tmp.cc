bbe5fa92961530f1fa73715677b6274374e9538a
Satya Bodapati <satya.bodapati@percona.com>
2019-08-09 15:23:33 +0530

    [encryption] PS-5835 : Sig 6 Assertion failure: fsp0fsp.cc:914:space_id == page_get_space_id(buf_block_get_frame(block))
    Problem:
    --------
    When ALTER INSTANCE ROTATE INNODB MASTER KEY is rotating keys of
    encrypted session temporary tablespaces, session disconnect are
    concurrently truncating the IBT files. This causes the assert
    
    Fix:
    ----
    Add synchronization mechanism (tablespace mutex) to handle ALTER INSTANCE ROTATE MASTER KEY
    and session temp tablespace truncate.
    
    Also encrypted session temporary tablespaces are now rotated by
    iterating all active tablespaces from IBT pool.

--- storage/innobase/srv/srv0tmp.cc
+++ storage/innobase/srv/srv0tmp.cc
@@ -70,9 +70,12 @@ Tablespace::Tablespace()
     : m_space_id(++m_last_used_space_id), m_inited(), m_thread_id() {
   ut_ad(m_space_id <= dict_sys_t::s_max_temp_space_id);
   m_purpose = TBSP_NONE;
+  mutex_create(LATCH_ID_TEMP_POOL_TBLSP, &m_mutex);
 }
 
 Tablespace::~Tablespace() {
+  mutex_destroy(&m_mutex);
+
   if (!m_inited) {
     return;
   }
@@ -140,7 +143,10 @@ bool Tablespace::truncate() {
     return false;
   }
 
+  acquire();
+
   if (!fil_truncate_tablespace(m_space_id, FIL_IBT_FILE_INITIAL_SIZE)) {
+    release();
     return false;
   }
 
@@ -152,6 +158,8 @@ bool Tablespace::truncate() {
   mtr_set_log_mode(&mtr, MTR_LOG_NO_REDO);
   fsp_header_init(m_space_id, FIL_IBT_FILE_INITIAL_SIZE, &mtr);
   mtr_commit(&mtr);
+
+  release();
 
   return true;
 }
@@ -192,6 +200,9 @@ void Tablespace::decrypt() {
   ut_a(err == DB_SUCCESS);
 
   rw_lock_x_unlock(&space->latch);
+}
+
+void Tablespace::rotate_encryption_key() {
 }
 
 uint32_t Tablespace::file_id() const {

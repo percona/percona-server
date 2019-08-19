d15e9ae626b8a1cf90802695c9ae261dd618d635
Zsolt Parragi <zsolt.parragi@percona.com>
2019-08-19 13:58:12 +0200

    [utility_user] PS-5216: Port utility user to 8.0
    PS-5952, PS-5956: Fix utility user bugs
    
    PS-5952: Utility user visible in performance_schema.threads
    PS-5956: Root session must not be allowed to kill Utility user session
    
    (cherry picked from commit c6ac4db9d10be35526ff755aa7963c863bafa643)
    (cherry picked from commit 14718d380573a30c4e328dc06a3c7eb1bd16ad05)
    
    ----------------------------------------------------------------------
    
    PS-9328: Merge 8.4.2 - post merge MTR fixes.
    
    Updated percona.utility_user test case to take into account that after
    Upstream's fix for bug#35317691 "authentication_ldap_sasl plugin without
    proxying work incorrectly" server now produces more specific error message
    in cases when PROXY privilege is required but is missing.
    
    See: https://github.com/mysql/mysql-server/commit/c62af60f1bb98a5b77038c08b43ec2cbefd160f7

--- sql/sql_show.cc
+++ sql/sql_show.cc
@@ -2890,6 +2890,9 @@ class List_process_list : public Do_THD_Impl {
       const LEX_CSTRING inspect_sctx_host = inspect_sctx->host();
       const LEX_CSTRING inspect_sctx_host_or_ip = inspect_sctx->host_or_ip();
 
+      const bool is_utility_user = acl_is_utility_user(
+          inspect_sctx_user.str, inspect_sctx_host.str, inspect_sctx->ip().str);
+
       /*
         Since we only access a cached value of connection_alive, which is
         also an atomic, we do not need to lock LOCK_thd_protocol here. We
@@ -2898,7 +2901,8 @@ class List_process_list : public Do_THD_Impl {
       */
       if (!inspect_thd->is_connected(true) ||
           (m_user && (inspect_thd->system_thread || !inspect_sctx_user.str ||
-                      strcmp(inspect_sctx_user.str, m_user)))) {
+                      strcmp(inspect_sctx_user.str, m_user))) ||
+          is_utility_user) {
         return;
       }
 
@@ -3169,6 +3173,9 @@ class Fill_process_list : public Do_THD_Impl {
       const LEX_CSTRING inspect_sctx_host = inspect_sctx->host();
       const LEX_CSTRING inspect_sctx_host_or_ip = inspect_sctx->host_or_ip();
 
+      const bool is_utility_user = acl_is_utility_user(
+          inspect_sctx_user.str, inspect_sctx_host.str, inspect_sctx->ip().str);
+
       const char *client_priv_user =
           m_client_thd->security_context()->priv_user().str;
       const char *user =
@@ -3185,7 +3192,8 @@ class Fill_process_list : public Do_THD_Impl {
       */
       if (!inspect_thd->is_connected(true) ||
           (user && (inspect_thd->system_thread || !inspect_sctx_user.str ||
-                    strcmp(inspect_sctx_user.str, user)))) {
+                    strcmp(inspect_sctx_user.str, user))) ||
+          is_utility_user) {
         return;
       }
 

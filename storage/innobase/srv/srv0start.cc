e0e623eeedf3f1a0310d5eebf92d8d94f9a3168a
Yura Sorokin <yura.sorokin@percona.com>
2019-10-09 15:32:05 +0300

    PS-5363 (Merge MySQL 8.0.17): fixed/re-recorded main.backup_safe_binlog_info MTR test case
    The implementation of WL #11636 "InnoDB: Clone Remote provisioning"
    (https://dev.mysql.com/worklog/task/?id=11636)
    (commit mysql/mysql-server@1efd16d)
    removed 'trx_sys_print_mysql_binlog_offset()' function and its call inside
    'srv_start()'.
    Therefore, 'Last MySQL binlog file position <n1> <n2>, file name <file_name>'
    is no longer printed to the server log as part of the recovery process, which
    is required by the 'main.backup_safe_binlog_info' MTR test case.
    
    Fixed by reintroducing 'trx_sys_print_mysql_binlog_offset()' function under
    'UNIV_DEBUG' and calling it inside 'srv_start()' only under
    'DEBUG_EXECUTE_IF("ib_recovery_print_mysql_binlog_offset", ...)' directive.
    Release builds should not have this function at all.
    
    'main.backup_safe_binlog_info' MTR test case mofified to include additional
    "--debug=+d,ib_recovery_print_mysql_binlog_offset" parameter during restart.

--- storage/innobase/srv/srv0start.cc
+++ storage/innobase/srv/srv0start.cc
@@ -2699,6 +2699,11 @@ files_checked:
     /* We have successfully recovered from the redo log. The
     data dictionary should now be readable. */
 
+    DBUG_EXECUTE_IF(
+        "ib_recovery_print_mysql_binlog_offset",
+        if (srv_force_recovery < SRV_FORCE_NO_LOG_REDO &&
+            recv_needed_recovery) { trx_sys_print_mysql_binlog_offset(); });
+
     if (recv_sys->found_corrupt_log) {
       ib::warn(ER_IB_MSG_1140);
     }

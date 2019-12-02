2eaaa65cda0cd38fc28dfab3b5b7c13710d3ac9b
Zsolt Parragi <zsolt.parragi@percona.com>
2019-12-02 14:59:27 +0100

    PS-5674: Changing default empty free list algorithm to legacy

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -23226,10 +23226,10 @@ static MYSQL_SYSVAR_ENUM(
     empty_free_list_algorithm, srv_empty_free_list_algorithm,
     PLUGIN_VAR_OPCMDARG,
     "The algorithm to use for empty free list handling.  Allowed values: "
-    "LEGACY: Original Oracle MySQL handling with single page flushes; "
-    "BACKOFF: (the default) Wait until cleaner produces a free page.",
+    "LEGACY: (the default) Original Oracle MySQL handling with single page flushes; "
+    "BACKOFF: Wait until cleaner produces a free page.",
     innodb_srv_empty_free_list_algorithm_validate, nullptr,
-    SRV_EMPTY_FREE_LIST_BACKOFF, &innodb_empty_free_list_algorithm_typelib);
+    SRV_EMPTY_FREE_LIST_LEGACY, &innodb_empty_free_list_algorithm_typelib);
 
 static MYSQL_SYSVAR_ULONG(buffer_pool_instances, srv_buf_pool_instances,
                           PLUGIN_VAR_RQCMDARG | PLUGIN_VAR_READONLY,

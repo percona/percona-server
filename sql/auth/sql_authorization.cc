2fee9f82f92231bf68d9dc1859205ab3935acd74
Zsolt Parragi <zsolt.parragi@percona.com>
2020-01-31 10:25:38 +0100

    [utility_user] PS-5955: Must not be possible to grant or revoke a role to/from utility user
    This commit also improves on the invisibility of the utility user by
    hiding it from the event_errorS_summary_by_thread_by_error table,
    originally part of PS-5952.

--- sql/auth/sql_authorization.cc
+++ sql/auth/sql_authorization.cc
@@ -3463,6 +3463,14 @@ bool mysql_grant_role(THD *thd, const List<LEX_USER> *users,
         errors = true;
         break;
       }
+
+      if (acl_is_utility_user(lex_user->user.str, lex_user->host.str,
+                              nullptr)) {
+        my_error(ER_UNKNOWN_AUTHID, MYF(0), lex_user->user.str,
+                 lex_user->host.str);
+        return true;
+      }
+
       while ((role = roles_it++) && !errors) {
         ACL_USER *acl_role;
         // keep the check in sync with check_authorization_id_string

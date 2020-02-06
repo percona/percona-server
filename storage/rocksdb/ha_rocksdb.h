529a60e7e301a723ef7431e4f90a6b1d646603d7
Pushap Goyal <pgl@fb.com>
2020-02-06 12:03:47 -0800

    Extend support to add more active compaction jobs details from RocksDB in MyRocks.
    Summary: Extend support to add more active compaction jobs details from RocksDB in MyRocks.
    
    Reviewed By: yizhang82
    
    Differential Revision: D19784066
    
    fbshipit-source-id: 3e2cd7acbf6

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -23,6 +23,7 @@
 
 /* C++ standard header files */
 #include <cinttypes>
+#include <map>
 #include <regex>
 #include <set>
 #include <string>
@@ -1080,5 +1081,32 @@ struct Rdb_hton_init_state {
 
 // file name indicating RocksDB data corruption
 std::string rdb_corruption_marker_file_name();
+
+// Holds the compaction stats for ongoing compaction jobs. This class contains a
+// thread safe map which being accessed under a mutex lock
+class Active_compaction_stats {
+ public:
+  std::map<uint64_t, rocksdb::CompactionJobInfo> get_current_stats() {
+    std::lock_guard<std::mutex> guard(map_mutex);
+    return std::map<uint64_t, rocksdb::CompactionJobInfo>(
+        active_compaction_stats_map);
+  }
+
+  void put(const uint64_t &thread_id, const rocksdb::CompactionJobInfo info) {
+    std::lock_guard<std::mutex> guard(map_mutex);
+    active_compaction_stats_map.insert({thread_id, info});
+  }
+
+  void remove(const uint64_t &thread_id) {
+    std::lock_guard<std::mutex> guard(map_mutex);
+    active_compaction_stats_map.erase(thread_id);
+  }
+
+ private:
+  std::map<uint64_t, rocksdb::CompactionJobInfo> active_compaction_stats_map;
+  std::mutex map_mutex;
+};
+
+extern Active_compaction_stats active_compaction_stats;
 
 }  // namespace myrocks

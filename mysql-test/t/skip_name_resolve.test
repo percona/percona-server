mysql/mysql-server

commit 93b813a3ca44bf3bc3ef194c55570bb99f866fc0

Bug #23329861 - WARNING ABOUT LOCALHOST WHEN USING SKIP-NAME-RESOLVE BACKPORT to MYSQL-5.7
    On Unix, MySQL programs treat the host name localhost specially,
    in a way that is likely different from what you expect compared
    to other network-based programs. If the host is not specified or
    is localhost, a connection to the local host is assumed:

    On Windows, the client connects using a shared-memory connection,
    if the server has shared-memory connections enabled.

    On Unix, the client connects using a Unix socket file.
    The --socket option or the MYSQL_UNIX_PORT environment variable
    may be used to specify the socket name.

    Problem:
    While having --skip-name-resolve enabled one does still get
    warnings and such it pollutes the logs. These warnings state
    that the "localhost" entries in the user, db, and proxies_priv
    will be ignored but they are used and not ignored.

    Solution:
    This is a hopefully temporary solution to not print warnings if
    the host is localhost. Added checks to not print the warnings.
    For the proper solution an ACL flag needs to be implemented.
    Filed new bug for this Bug #28817102.

    Approval: Umesh Shastry <umesh.shastry@oracle.com>

Change-Id: I3c9ae7faf41367a43ff1ea7787be09728752cbfa

sql/auth/sql_auth_cache.cc
@@ -1,4 +1,4 @@
-/* Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
+/* Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -263,8 +263,8 @@ ACL_PROXY_USER::check_validity(bool check_no_resolve)
 {
   if (check_no_resolve && 
       (hostname_requires_resolving(host.get_host()) ||
-       hostname_requires_resolving(proxied_host.get_host())))
-  {
+       hostname_requires_resolving(proxied_host.get_host())) &&
+      strcmp(host.get_host(), "localhost") != 0) {
     sql_print_warning("'proxies_priv' entry '%s@%s %s@%s' "
                       "ignored in --skip-name-resolve mode.",
                       proxied_user ? proxied_user : "",
@@ -1535,8 +1535,8 @@ static my_bool acl_load(THD *thd, TABLE_LIST *tables)
                                       table->field[table_schema->host_idx()]));
     user.user= get_field(&global_acl_memory,
                          table->field[table_schema->user_idx()]);
-    if (check_no_resolve && hostname_requires_resolving(user.host.get_host()))
-    {
+  if (check_no_resolve && hostname_requires_resolving(user.host.get_host()) &&
+      strcmp(user.host.get_host(), "localhost") != 0) {
       sql_print_warning("'user' entry '%s@%s' "
                         "ignored in --skip-name-resolve mode.",
                         user.user ? user.user : "",
@@ -1910,8 +1910,8 @@ static my_bool acl_load(THD *thd, TABLE_LIST *tables)
       continue;
     }
     db.user=get_field(&global_acl_memory, table->field[MYSQL_DB_FIELD_USER]);
-    if (check_no_resolve && hostname_requires_resolving(db.host.get_host()))
-    {
+    if (check_no_resolve && hostname_requires_resolving(db.host.get_host()) &&
+        strcmp(db.host.get_host(), "localhost") != 0) {
       sql_print_warning("'db' entry '%s %s@%s' "
                         "ignored in --skip-name-resolve mode.",
                         db.db,
@@ -2455,8 +2455,8 @@ static my_bool grant_load(THD *thd, TABLE_LIST *tables)
 
       if (check_no_resolve)
       {
-        if (hostname_requires_resolving(mem_check->host.get_host()))
-        {
+        if (hostname_requires_resolving(mem_check->host.get_host()) &&
+            strcmp(mem_check->host.get_host(), "localhost") != 0) {
           sql_print_warning("'tables_priv' entry '%s %s@%s' "
                             "ignored in --skip-name-resolve mode.",
                             mem_check->tname,

mysql-test/r/skip_name_resolve.result
@@ -50,10 +50,20 @@ End of 5.1 tests
 #
 CREATE USER b20438524@'%' IDENTIFIED BY 'pwd';
 # set some valid addresses
-UPDATE mysql.user SET host='localhost' WHERE user='b20438524';
+UPDATE mysql.user SET host='localhost1' WHERE user='b20438524';
 FLUSH PRIVILEGES;
 # cleanup
 DELETE FROM mysql.user WHERE user='b20438524';
 FLUSH PRIVILEGES;
 # must find the pattern
+#
+# Bug #23329861: WARNING ABOUT LOCALHOST WHEN USING SKIP-NAME-RESOLVE
+#
+CREATE USER b20438524@'%' IDENTIFIED BY 'pwd';
+# set some valid addresses
+UPDATE mysql.user SET host='localhost' WHERE user='b20438524';
+FLUSH PRIVILEGES;
+# must not find the pattern
+Pattern "'user' entry 'b20438524@localhost' ignored in --skip-name-resolve mode" not found
+DELETE FROM mysql.user WHERE user='b20438524';
 End of 5.7 tests

mysql-test/t/skip_name_resolve.test
@@ -62,7 +62,7 @@ SET @@GLOBAL.skip_name_resolve=0;
 
 CREATE USER b20438524@'%' IDENTIFIED BY 'pwd';
 --echo # set some valid addresses
-UPDATE mysql.user SET host='localhost' WHERE user='b20438524';
+UPDATE mysql.user SET host='localhost1' WHERE user='b20438524';
 FLUSH PRIVILEGES;
 
 --echo # cleanup
@@ -71,9 +71,25 @@ FLUSH PRIVILEGES;
 
 --echo # must find the pattern
 --let SEARCH_FILE=$MYSQLTEST_VARDIR/log/mysqld.1.err
---let SEARCH_PATTERN='user' entry 'b20438524@localhost' ignored in --skip-name-resolve mode
+--let SEARCH_PATTERN='user' entry 'b20438524@localhost1' ignored in --skip-name-resolve mode
 source include/search_pattern_in_file.inc;
 
+
+--echo #
+--echo # Bug #23329861: WARNING ABOUT LOCALHOST WHEN USING SKIP-NAME-RESOLVE
+--echo #
+
+CREATE USER b20438524@'%' IDENTIFIED BY 'pwd';
+--echo # set some valid addresses
+UPDATE mysql.user SET host='localhost' WHERE user='b20438524';
+FLUSH PRIVILEGES;
+
+--echo # must not find the pattern
+--let SEARCH_FILE=$MYSQLTEST_VARDIR/log/mysqld.1.err
+--let SEARCH_PATTERN='user' entry 'b20438524@localhost' ignored in --skip-name-resolve mode
+source include/search_pattern.inc;
+
+DELETE FROM mysql.user WHERE user='b20438524';
 
 --echo End of 5.7 tests
 

percona/percona-server

commit 9bd435c558835c10351f22c3904ccdd1285a6d2a

PS-6811: Crash in ACL_PROXY_USER::check_validity with --skip-name-resolve=1 and Proxy Users
This patch fixes the issue when `host.get_host()` returns `nullptr`.
It also expands `main.skip_name_resolve` with additional tests.

The bug was introduced with https://github.com/mysql/mysql-server/commit/93b813a3ca44

sql/auth/sql_auth_cache.cc
@@ -100,6 +100,21 @@ static void acl_free_utility_user();
 #endif /* NO_EMBEDDED_ACCESS_CHECKS */
 
 /**
+  Check if the given host name is equal to "localhost".
+
+  @return a flag telling if the given host name is equal to "localhost".
+  @retval TRUE the given host name is equal to "localhost".
+  @retval FALSE the given host name is not equal to "localhost".
+}
+*/
+static bool is_localhost_string(const char *hostname)
+{
+  if (!hostname) return false;
+
+  return !strcmp(hostname, "localhost");
+}
+
+/**
   Add an internal schema to the registry.
   @param name the schema name
   @param access the schema ACL specific rules
@@ -280,7 +295,7 @@ ACL_PROXY_USER::check_validity(bool check_no_resolve)
   if (check_no_resolve && 
       (hostname_requires_resolving(host.get_host()) ||
        hostname_requires_resolving(proxied_host.get_host())) &&
-      strcmp(host.get_host(), "localhost") != 0) {
+       !is_localhost_string(host.get_host())) {
     sql_print_warning("'proxies_priv' entry '%s@%s %s@%s' "
                       "ignored in --skip-name-resolve mode.",
                       proxied_user ? proxied_user : "",
@@ -1776,7 +1791,7 @@ static my_bool acl_load(THD *thd, TABLE_LIST *tables)
     user.user= get_field(&global_acl_memory,
                          table->field[table_schema->user_idx()]);
   if (check_no_resolve && hostname_requires_resolving(user.host.get_host()) &&
-      strcmp(user.host.get_host(), "localhost") != 0) {
+      !is_localhost_string(user.host.get_host())) {
       sql_print_warning("'user' entry '%s@%s' "
                         "ignored in --skip-name-resolve mode.",
                         user.user ? user.user : "",
@@ -2154,7 +2169,7 @@ static my_bool acl_load(THD *thd, TABLE_LIST *tables)
     }
     db.user=get_field(&global_acl_memory, table->field[MYSQL_DB_FIELD_USER]);
     if (check_no_resolve && hostname_requires_resolving(db.host.get_host()) &&
-        strcmp(db.host.get_host(), "localhost") != 0) {
+        !is_localhost_string(db.host.get_host())) {
       sql_print_warning("'db' entry '%s %s@%s' "
                         "ignored in --skip-name-resolve mode.",
                         db.db,
@@ -2700,7 +2715,7 @@ static my_bool grant_load(THD *thd, TABLE_LIST *tables)
       if (check_no_resolve)
       {
         if (hostname_requires_resolving(mem_check->host.get_host()) &&
-            strcmp(mem_check->host.get_host(), "localhost") != 0) {
+            !is_localhost_string(mem_check->host.get_host())) {
           sql_print_warning("'tables_priv' entry '%s %s@%s' "
                             "ignored in --skip-name-resolve mode.",
                             mem_check->tname,

mysql-test/r/skip_name_resolve.result
@@ -66,4 +66,18 @@ FLUSH PRIVILEGES;
 # must not find the pattern
 Pattern "'user' entry 'b20438524@localhost' ignored in --skip-name-resolve mode" not found
 DELETE FROM mysql.user WHERE user='b20438524';
+#
+# Bug #98908: Crash in ACL_PROXY_USER::check_validity with --skip-name-resolve=1
+#             https://bugs.mysql.com/bug.php?id=98908
+#
+CREATE USER IF NOT EXISTS ''@'';
+CREATE USER IF NOT EXISTS dba@'localhost' IDENTIFIED BY 'pass';
+GRANT PROXY ON dba@'localhost' TO ''@'';
+Warnings:
+Warning	1285	MySQL is started in --skip-name-resolve mode; you must restart it without this switch for this grant to work
+# restart:--skip-name-resolve=1
+Grants for dba@localhost
+GRANT USAGE ON *.* TO 'dba'@'localhost'
+DROP USER dba@'localhost';
+DROP USER ''@'';
 End of 5.7 tests

mysql-test/t/skip_name_resolve.test
@@ -91,5 +91,24 @@ source include/search_pattern.inc;
 
 DELETE FROM mysql.user WHERE user='b20438524';
 
+
+--echo #
+--echo # Bug #98908: Crash in ACL_PROXY_USER::check_validity with --skip-name-resolve=1
+--echo #             https://bugs.mysql.com/bug.php?id=98908
+--echo #
+
+CREATE USER IF NOT EXISTS ''@'';
+CREATE USER IF NOT EXISTS dba@'localhost' IDENTIFIED BY 'pass';
+GRANT PROXY ON dba@'localhost' TO ''@'';
+
+--let $restart_parameters=restart:--skip-name-resolve=1
+--source include/restart_mysqld.inc
+
+--exec $MYSQL $AUTH_PAM_OPT -udba -ppass -e "SHOW GRANTS;"
+
+DROP USER dba@'localhost';
+DROP USER ''@'';
+
+
 --echo End of 5.7 tests
 
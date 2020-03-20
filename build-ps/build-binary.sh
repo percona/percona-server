99ed612d2560b4b22cff075428932cd0e9812b18
Evgeniy Patlan <evgeniy.patlan@percona.com>
2020-03-20 17:07:54 +0200

    Improve packaging

--- build-ps/build-binary.sh
+++ build-ps/build-binary.sh
@@ -255,6 +255,7 @@ fi
         -DWITH_INNODB_MEMCACHED=ON \
         -DWITH_ZLIB=system \
         -DWITH_NUMA=ON \
+        -DWITH_LDAP=ON \
         -DDOWNLOAD_BOOST=1 \
         -DFORCE_INSOURCE_BUILD=1 \
         -DWITH_LIBEVENT=bundled \

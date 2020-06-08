a0c4a534057cf7c251e645c4cacd9790bc664022
Robert Golebiowski <robert.golebiowski@percona.com>
2020-06-08 12:05:40 +0200

    PS-5730: Change SELECT rotate_system_key to ALTER INSTANCE for percona system key rotation.
    Problem:
    
    SELECT rotate_system_key() is not properly replicated with the default
    binlog format ROW. Also, ENCRYPTION_KEY_ADMIN privlege is not checked
    for the key rotation.
    
    Fix:
    
    Replace rotate_system_key function with statements
    ALTER INSTANCE ROTATE * SYSTEM KEY. We introduce two new statements to
    rotate percona system key:
    
     1. ALTER INSTANCE ROTATE INNODB SYSTEM KEY key_id
    
    This statement is used in order to rotate percona innodb system key.
    key_id - is the id of the tablespace encryption key that
    the user wishes to rotate. ALTER INSTANCE ROTATE INNODB SYSTEM KEY
    statement
    is replicated in all binlog formats.
    
    Apart from that, the ALTER INSTANCE checks for sufficient privileges
    (SUPER or ENCRYPTION_KEY_ADMIN) before allowing rotation.
    Also ALTER INSTANCE takes backup logs to block concurrent backup
    and concurrent DDL.
    
     2. ALTER INSTANCE ROTATE REDO SYSTEM KEY
    
    Either SUPER or ENCRYPTION_KEY_ADMIN privileges are required to issue
    this statement. The same as binlog key rotation, redo log rotation is
    not replicated. The redo log rotation does not need any extra
    synchronization, as it will be synchronized by the keyring framework

--- sql/sql_admin.h
+++ sql/sql_admin.h
@@ -375,6 +375,8 @@ class Sql_cmd_show_grants : public Sql_cmd {
 
 enum alter_instance_action_enum {
   ROTATE_INNODB_MASTER_KEY,
+  ROTATE_INNODB_SYSTEM_KEY,
+  ROTATE_REDO_SYSTEM_KEY,
   ALTER_INSTANCE_RELOAD_TLS,
   ALTER_INSTANCE_RELOAD_TLS_ROLLBACK_ON_ERROR,
   ROTATE_BINLOG_MASTER_KEY,
@@ -390,12 +392,20 @@ class Alter_instance;
 class Sql_cmd_alter_instance : public Sql_cmd {
   friend class PT_alter_instance;
   const enum alter_instance_action_enum alter_instance_action;
+  uint system_key_id;
   Alter_instance *alter_instance;
 
  public:
   explicit Sql_cmd_alter_instance(
       enum alter_instance_action_enum alter_instance_action_arg)
       : alter_instance_action(alter_instance_action_arg),
+        alter_instance(NULL) {}
+
+  explicit Sql_cmd_alter_instance(
+      enum alter_instance_action_enum alter_instance_action_arg,
+      uint system_key_id_arg)
+      : alter_instance_action(alter_instance_action_arg),
+        system_key_id(system_key_id_arg),
         alter_instance(NULL) {}
 
   virtual bool execute(THD *thd);

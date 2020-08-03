mysql/mysql-server

commit 6d587a60738842b1f7e5e5b0839acd62db6b9d43

Bug #30776132: MAKE FILESORT KEYS CONSISTENT BETWEEN FIELDS AND ITEMS
Since the dawn of time, filesort has had different paths for making sort keys
for fields and items. There's no good reason to keep carry this distinction
forward anymore, so remove the special handling of Field and just sort Items
everywhere, making filesort somewhat simpler. There are some special legacy
cases with sorting enums etc. where we still need to look into Item_field and
look at the actual underlying file type, but they are fairly localized and now
much more visible.

There are some edge cases where keys used to be shorter, e.g. all integers
will now get 64-bit sort keys instead of 8-bit, 16-bit, etc.. Also, some
EXPLAIN output changes, as we also no longer have specialized formatting of
Field.

WEIGHT_STRING() now is able to get the actual weight strings for more kinds
of items, in particular integers.

Inadvertently, we fix a bug that was only half-visible in the tests;
common-tests.inc contain a query whose results were not properly deduplicated
before, but are now, and while this was hidden by LIMIT/OFFSET, it affects
which rows are visible in the result set.

sysbench sorting tests (ranges and distinct ranges) show strong improvements
at high thread counts; as much as +17%. Lower thread counts are neutral or
in the noise.

Unfortunately, replication started using Field::make_sort_key() in 8.0, and
cannot easily change it back, as they need exact byte-by-byte matches of the
data. Fixing this would enable removing 500+ lines of complicated sort key
code.

Change-Id: Ieb5038e8afbea19a5f0e0e7f83b3177cb3176e84

mysql-test/r/order_by_sortkey.result
@@ -152,7 +152,7 @@ f0	f1	f2
 9901	0	0
 SHOW SESSION STATUS LIKE 'Sort%';
 Variable_name	Value
-Sort_merge_passes	99
+Sort_merge_passes	102
 Sort_range	0
 Sort_rows	100
 Sort_scan	1

mysql-test/r/compress.result
@@ -610,7 +610,6 @@ companynr	fld3	count(*)
 SET BIG_TABLES=0;
 select distinct fld3,repeat("a",length(fld3)),count(*) from t2 group by companynr,fld3 limit 100,10;
 fld3	repeat("a",length(fld3))	count(*)
-babysitting	aaaaaaaaaaa	1
 Baird	aaaaa	1
 balled	aaaaaa	1
 ballgown	aaaaaaaa	1
@@ -620,6 +619,7 @@ Barry	aaaaa	1
 batting	aaaaaaa	1
 beaner	aaaaaa	1
 beasts	aaaaaa	1
+beaters	aaaaaaa	1
 select distinct companynr,rtrim(space(512+companynr)) from t3 order by 1,2;
 companynr	rtrim(space(512+companynr))
 37	
@@ -2873,7 +2873,6 @@ companynr	fld3	count(*)
 SET BIG_TABLES=0;
 select distinct fld3,repeat("a",length(fld3)),count(*) from t2 group by companynr,fld3 limit 100,10;
 fld3	repeat("a",length(fld3))	count(*)
-babysitting	aaaaaaaaaaa	1
 Baird	aaaaa	1
 balled	aaaaaa	1
 ballgown	aaaaaaaa	1
@@ -2883,6 +2882,7 @@ Barry	aaaaa	1
 batting	aaaaaaa	1
 beaner	aaaaaa	1
 beasts	aaaaaa	1
+beaters	aaaaaaa	1
 select distinct companynr,rtrim(space(512+companynr)) from t3 order by 1,2;
 companynr	rtrim(space(512+companynr))
 37	

mysql-test/r/ctype_latin2_ch_myisam.result
@@ -53,5 +53,5 @@ t1	CREATE TABLE `t1` (
 INSERT INTO t1 (ID,Post) VALUES (00041,'');
 SELECT ID, Post, HEX(WEIGHT_STRING(Post)) FROM t1;
 ID	Post	HEX(WEIGHT_STRING(Post))
-00041		01010100
+00041		8000000000000001
 DROP TABLE t1;

mysql-test/r/distinct.result
@@ -1004,7 +1004,7 @@ c1	c2	COUNT(*)
 4	4	1
 EXPLAIN FORMAT=tree SELECT DISTINCT c2 FROM t1 GROUP BY c1 HAVING COUNT(*) > 1;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.c2
+-> Sort with duplicate removal: t1.c2
     -> Filter: (count(0) > 1)
         -> Table scan on <temporary>
             -> Aggregate using temporary table
@@ -1254,7 +1254,7 @@ INSERT INTO t1 SELECT RAND() FROM t1;
 INSERT INTO t1 SELECT RAND() FROM t1;
 EXPLAIN FORMAT=tree SELECT DISTINCT COUNT(*) AS num FROM t1 GROUP BY f HAVING num=1;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.num
+-> Sort with duplicate removal: num
     -> Filter: (num = 1)
         -> Table scan on <temporary>
             -> Aggregate using temporary table
@@ -1285,7 +1285,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT DISTINCT SQL_BIG_RESULT a FROM t1 GROUP BY a,b ORDER BY a DESC;
 EXPLAIN
--> Sort with duplicate removal: <temporary>.a DESC
+-> Sort with duplicate removal: t1.a DESC
     -> Stream results
         -> Group (no aggregates)
             -> Sort: t1.a DESC, t1.b  (cost=0.80 rows=3)

mysql-test/r/explain_tree.result
@@ -232,7 +232,7 @@ EXPLAIN
     -> Table scan on t1  (cost=0.35 rows=1)
     -> Select #2 (subquery in condition; dependent)
         -> Group aggregate: group_concat(t.b separator ',')
-            -> Sort row IDs: <temporary>.a
+            -> Sort row IDs: t1.a
                 -> Table scan on <temporary>
                     -> Temporary table
                         -> Table scan on t  (cost=0.35 rows=1)
@@ -382,7 +382,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT ( SELECT f1 FROM t1 AS inner_t1 WHERE inner_t1.f1 > t1.f1 LIMIT 1 ) AS tmp1 FROM t1 ORDER BY tmp1;
 EXPLAIN
--> Sort: <temporary>.tmp1
+-> Sort: `<temporary>`.tmp1
     -> Stream results
         -> Index scan on t1 using PRIMARY  (cost=0.55 rows=3)
 -> Select #2 (subquery in projection; dependent)
@@ -707,7 +707,7 @@ test.t1	analyze	status	OK
 EXPLAIN ANALYZE SELECT * FROM t1 AS a JOIN t1 AS b ON a.a=b.b ORDER BY a.b+b.a LIMIT 3;
 EXPLAIN
 -> Limit: 3 row(s)  (actual time=N.NNN..N.NNN rows=1 loops=1)
-    -> Sort: <temporary>.(a.b + b.a), limit input to 3 row(s) per chunk  (actual time=N.NNN..N.NNN rows=1 loops=1)
+    -> Sort: `<temporary>`.`(a.b + b.a)`, limit input to 3 row(s) per chunk  (actual time=N.NNN..N.NNN rows=1 loops=1)
         -> Stream results  (actual time=N.NNN..N.NNN rows=1 loops=1)
             -> Nested loop inner join  (cost=1.60 rows=3) (actual time=N.NNN..N.NNN rows=1 loops=1)
                 -> Filter: (b.b is not null)  (cost=0.55 rows=3) (actual time=N.NNN..N.NNN rows=3 loops=1)

mysql-test/r/filesort_debug.result
@@ -135,8 +135,8 @@ CREATE TABLE t1(a DATE);
 INSERT INTO t1 VALUES('1000-01-01'), ('2017-10-31');
 SELECT HEX(WEIGHT_STRING(a)) FROM t1;
 HEX(WEIGHT_STRING(a))
-07D021
-0FC35F
+8CB2420000000000
+999DFE0000000000
 SELECT HEX(WEIGHT_STRING(a COLLATE utf8mb4_0900_ai_ci, 3, 3, 0xC0)) FROM t1;
 HEX(WEIGHT_STRING(a COLLATE utf8mb4_0900_ai_ci, 3, 3, 0xC0))
 1C3E1C

mysql-test/r/group_by.result
@@ -3912,7 +3912,7 @@ Table	Op	Msg_type	Msg_text
 test.t1	analyze	status	OK
 EXPLAIN FORMAT=tree SELECT f1, f1 + 1, COUNT(DISTINCT f2) AS x FROM t1 GROUP BY f1 ORDER BY x;
 EXPLAIN
--> Sort: <temporary>.x
+-> Sort: x
     -> Stream results
         -> Group aggregate: count(distinct t1.f2)
             -> Index scan on t1 using k1  (cost=0.85 rows=6)

mysql-test/r/hash_join.result
@@ -13,7 +13,7 @@ test.t2	analyze	status	OK
 EXPLAIN FORMAT=tree
 SELECT t1.col1, t2.col1 FROM t1 JOIN t2 ON t1.col1 = t2.col1 ORDER BY t1.col1;
 EXPLAIN
--> Sort: <temporary>.col1
+-> Sort: t1.col1
     -> Stream results
         -> Inner hash join (t2.col1 = t1.col1)  (cost=2.50 rows=4)
             -> Table scan on t2  (cost=0.09 rows=4)
@@ -37,7 +37,7 @@ test.t2	analyze	status	OK
 EXPLAIN FORMAT=tree
 SELECT t1.col1, t2.col1 FROM t1 JOIN t2 ON t1.col1 = t2.col1 ORDER BY t1.col1;
 EXPLAIN
--> Sort: <temporary>.col1
+-> Sort: t1.col1
     -> Stream results
         -> Inner hash join (t1.col1 = t2.col1)  (cost=2.50 rows=4)
             -> Table scan on t2  (cost=0.09 rows=4)
@@ -1506,7 +1506,7 @@ alias1.col_int_key DESC, field1
 LIMIT 2 OFFSET 2;
 EXPLAIN
 -> Limit/Offset: 2/2 row(s)
-    -> Sort: <temporary>.col_int_key DESC, <temporary>.field1
+    -> Sort: alias1.col_int_key DESC, alias1.pk
         -> Filter: (field1 <> 0)
             -> Table scan on <temporary>
                 -> Temporary table with deduplication
@@ -3062,7 +3062,7 @@ t2 LEFT JOIN t3 ON t2.col1 = t3.col1)
 ON t2.col1 = t1.col1
 ORDER BY t1.col1, t2.col1, t3.col1;
 EXPLAIN
--> Sort: <temporary>.col1, <temporary>.col1, <temporary>.col1
+-> Sort: t1.col1, t2.col1, t3.col1
     -> Stream results
         -> Left hash join (t2.col1 = t1.col1)  (cost=1.36 rows=6)
             -> Table scan on t1  (cost=0.55 rows=3)

percona/percona-server

commit de0ca73ef2ae03284b0c01d5d49fff61a62df498

Fixed PS-7221 (crash on select Sys_var_tz::session_value_ptr)
https://jira.percona.com/browse/PS-7221

This is a regression introduced in 6d587a60738842b1f7e5e5b0839acd62db6b9d43.
A new function called get_int_sort_key_for_item_inline temporarily sets
the time_zone session variable to UTC.
This results on *Time_zone_utc::get_name() been called and up to now, get_name
was not meant to be called directly and thus always asserting.

Adjusted the function to properly return UTC string instead of asserting.

sql/tztime.cc
@@ -992,6 +992,7 @@ static my_time_t TIME_to_gmt_sec(const MYSQL_TIME *t, const TIME_ZONE_INFO *sp,
   String with names of SYSTEM time zone.
 */
 static const String tz_SYSTEM_name("SYSTEM", 6, &my_charset_latin1);
+static const String tz_UTC_name("UTC", 3, &my_charset_latin1);
 
 Time_zone *my_tz_find(const int64 displacement);
 
@@ -1169,19 +1170,10 @@ void Time_zone_utc::gmt_sec_to_TIME(MYSQL_TIME *tmp, my_time_t t) const {
   SYNOPSIS
     get_name()
 
-  DESCRIPTION
-    Since Time_zone_utc is used only internally by SQL's UTC_* functions it
-    is not accessible directly, and hence this function of Time_zone
-    interface is not implemented for this class and should not be called.
-
   RETURN VALUE
-    0
+    Name of time zone as String
 */
-const String *Time_zone_utc::get_name() const {
-  /* Should be never called */
-  DBUG_ASSERT(0);
-  return nullptr;
-}
+const String *Time_zone_utc::get_name() const { return &tz_UTC_name; }
 
 /*
   Instance of this class represents some time zone which is

mysql-test/r/order_by_sortkey.result
@@ -172,3 +172,17 @@ Sort_range	0
 Sort_rows	2
 Sort_scan	2
 DROP TABLE t1, t2, tmp;
+#
+# BUG: PS-7221 - crash on select Sys_var_tz::session_value_ptr
+#
+CREATE TABLE t1 (
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+dt DATETIME NOT NULL
+);
+INSERT INTO t1 SET dt=NOW();
+INSERT INTO t1 (dt) SELECT NOW() FROM t1;
+SELECT id FROM t1 ORDER BY convert_tz(dt,'UTC',@@session.time_zone);
+id
+1
+2
+DROP TABLE t1;

mysql-test/t/order_by_sortkey.test
@@ -78,3 +78,16 @@ SELECT * FROM t2 where f1 =
 SHOW SESSION STATUS LIKE 'Sort%';
 
 DROP TABLE t1, t2, tmp;
+
+--echo #
+--echo # BUG: PS-7221 - crash on select Sys_var_tz::session_value_ptr
+--echo #
+
+CREATE TABLE t1 (
+  id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  dt DATETIME NOT NULL
+);
+INSERT INTO t1 SET dt=NOW();
+INSERT INTO t1 (dt) SELECT NOW() FROM t1;
+SELECT id FROM t1 ORDER BY convert_tz(dt,'UTC',@@session.time_zone);
+DROP TABLE t1;
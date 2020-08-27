c1644aa3961a3f1d8c6ca3edd2b859f36df04ce9
IP <27162227+impimp@users.noreply.github.com>
2020-08-27 09:39:28 +0200

    PS-7270: extra_port should not accept proxied connections

--- sql/sql_connect.cc
+++ sql/sql_connect.cc
@@ -737,6 +737,10 @@ static int check_connection(THD *thd) {
     char ip[NI_MAXHOST];
     LEX_CSTRING main_sctx_ip;
 
+    if (thd->is_admin_connection()) {
+      vio_force_skip_proxy(net->vio);
+    }
+
     /* Set the remote (peer) port for this THD. */
     peer_rc = vio_peer_addr(net->vio, ip, &thd->peer_port, NI_MAXHOST);
     mysql_thread_set_peer_port(thd->peer_port);

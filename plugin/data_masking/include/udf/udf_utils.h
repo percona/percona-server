f6111b5040f2cba5b39c6e91d4c8ece05a8ff9b6
Satya Bodapati <satya.bodapati@percona.com>
2020-09-14 12:38:28 +0000

    [plugin/data_masking] PS-7307 : Datamasking UDFs display output as hex decimals on mysql client interactive mode
    Problem:
    --------
    Since MySQL 8.0.19, due to the WL#13038 - Make mysql command line tool's
    --binary-as-hex be on by default for interactive terminals, all UDFs, BLOB
    datatypes are shown as hexadecimal.
    
    All UDFs until 8.0.19, used CHARSET binary. mysql client terminals in
    interactive mode (input or output not redirected) showed the binary data as
    text.
    
    After WL#13038, the binary data is shown as hex (--binary-as-hex is ON by
    default)
    
    Fix:
    ----
    Declare that all the strings used by data masking UDFs use latin1 character set.
    With this fix, mysql client in interactive mode, interpret the data as
    non-binary(latin1) and doesn't show it as hex.
    
    --------------------------------------------------------------------------------
    
    PS-6029 - Fix format of result of data_masking.gen_rnd_us_phone
    
    https://jira.percona.com/browse/PS-6029
    
    --------------------------------------------------------------
    
    PS-6029 - Use file suite.opt in the suite data_masking
    
    https://jira.percona.com/browse/PS-6029
    
    It allows a more succinct implementation of the test suite.

--- plugin/data_masking/include/udf/udf_utils.h
+++ plugin/data_masking/include/udf/udf_utils.h
@@ -17,6 +17,9 @@
 #define _UTILS_H
 
 #include <my_inttypes.h>
+#include <mysql/components/services/registry.h>
+#include <mysql/components/services/udf_metadata.h>
+#include <mysql/udf_registration_types.h>
 #include <random>
 #include <string>
 
@@ -34,6 +37,69 @@ std::string random_ssn();
 
 std::string random_us_phone();
 
+/**
+ @class Charset_service
+
+ Class that acquire/release the udf_metadata_service from registry service.
+ It provides the APIs to set the character set of return value and arguments
+ of UDFs using the udf_metadata service.
+*/
+class Charset_service {
+ public:
+  /**
+    Acquires the udf_metadata_service from the registry  service.
+    @param[in]  reg_srv Registry service from which udf_metadata service
+                        will be acquired
+
+    @retval true if service could not be acquired
+    @retval false Otherwise
+  */
+  static bool init(SERVICE_TYPE(registry) * reg_srv);
+
+  /**
+    Release the udf_metadata service
+
+    @param[in]  reg_srv Registry service from which the udf_metadata
+                        service will be released.
+
+    @retval true if service could not be released
+    @retval false Otherwise
+  */
+  static bool deinit(SERVICE_TYPE(registry) * reg_srv);
+
+  /**
+    Set the specified character set of UDF return value
+
+    @param[in] initid  UDF_INIT structure
+    @param[in] charset_name Character set that has to be set.
+               The default charset is set to 'latin1'
+
+    @retval true Could not set the character set of return value
+    @retval false Otherwise
+  */
+  static bool set_return_value_charset(
+      UDF_INIT *initid, const std::string &charset_name = "latin1");
+  /**
+    Set the specified character set of all UDF arguments
+
+    @param[in] args UDF_ARGS structure
+    @param[in] charset_name Character set that has to be set.
+               The default charset is set to 'latin1'
+
+    @retval true Could not set the character set of any of the argument
+    @retval false Otherwise
+  */
+  static bool set_args_charset(UDF_ARGS *args,
+                               const std::string &charset_name = "latin1");
+
+ private:
+  /* Argument type to specify in the metadata service methods */
+  static const char *arg_type;
+  /* udf_metadata service name */
+  static const char *service_name;
+  /* Handle of udf_metadata_service */
+  static SERVICE_TYPE(mysql_udf_metadata) * udf_metadata_service;
+};
 }  // namespace plugins
 }  // namespace mysql
 

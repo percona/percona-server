0c9d9185a7405a5fed3cf2b18410d74edc86951c
Illia P <illia.pshonkin@percona.com>
2020-09-17 10:19:53 +0300

    PS-7313: Add dependency curl4 to binary tests

--- build-ps/test-build.sh
+++ build-ps/test-build.sh
@@ -28,9 +28,9 @@ function prepare {
 function install_deps {
     if [ -f /etc/redhat-release ]; then
         yum install -y epel-release
-        yum install -y wget perl-Time-HiRes perl numactl numactl-libs libaio libidn || true
+        yum install -y wget perl-Time-HiRes perl numactl numactl-libs libaio libidn libcurl-devel || true
     else
-        apt install -y wget perl numactl libaio-dev libidn11 || true
+        apt install -y wget perl numactl libaio-dev libidn11 libcurl4-openssl-dev || true
     fi
     wget -c https://github.com/datacharmer/dbdeployer/releases/download/v1.52.0/dbdeployer-1.52.0.linux.tar.gz -O - | tar -xz
     mv dbdeployer*.linux /usr/local/bin/dbdeployer

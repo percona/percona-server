8d69934a63f4705abbd78c24963a143c911a478d
Kamil Holubicki <kamil.holubicki@percona.com>
2020-11-19 11:47:03 +0100

    PS-7333 merge: Merge MySQL 8.0.22 (fixes for unstable tests)
    https://jira.percona.com/browse/PS-7333
    
    Fixes for unstable tests.
    (tests that fail on trunk as well)
    
    1. sys_vars.innodb_sys_tablespace_encrypt_basic - failure caused by
    changes done in commit e760f89b3429cbf0923fa25cc7dd422052f32500
    
    2. main.percona_dd_upgrade_8013 - in heavy loaded environments default
    server start timeout was too low and test failed randomly with timeout.
    Server start timeout increased.
    
    3. Disabled main.percona_changed_pages_big
    https://jira.percona.com/browse/PS-7435

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -22009,12 +22009,24 @@ static int innodb_sys_tablespace_encyption_validate(
 
   bool legit_value = false;
   uint use = 0;
-  for (; use < array_elements(sys_tablespace_encrypt_names); use++) {
+  // The last item of sys_tablespace_encrypt_names is nullptr
+  const size_t encrypt_names_cnt = array_elements(sys_tablespace_encrypt_names) - 1;
+
+  for (; use < encrypt_names_cnt; use++) {
     if (!innobase_strcasecmp(innodb_sys_tablespace_encryption_input,
                              sys_tablespace_encrypt_names[use])) {
       legit_value = true;
       break;
     }
+  }
+  // It is possible that enum value was provided as the integer, not literal
+  if (!legit_value) {
+      int error = 0;
+      use = (uint)my_strtoll10(innodb_sys_tablespace_encryption_input, nullptr,
+                               &error);
+      if (!error && use < encrypt_names_cnt) {
+        legit_value = true;
+      }
   }
 
   if (!legit_value) return 1;
@@ -23998,6 +24010,7 @@ static MYSQL_SYSVAR_BOOL(
     temp_tablespace_encrypt, srv_tmp_tablespace_encrypt, PLUGIN_VAR_OPCMDARG,
     "Enable or disable encryption of temporary tablespace.", nullptr,
     innodb_temp_tablespace_encryption_update, false);
+
 
 static MYSQL_SYSVAR_ENUM(
     sys_tablespace_encrypt, srv_sys_tablespace_encrypt,

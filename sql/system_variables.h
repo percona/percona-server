6751faa27cbb7b1f6569f6316f6a3889696e47fc
Luis Donoso <luis.donoso@percona.com>
2021-01-18 13:53:41 +0100

    PS-7364 - Revert changes wrt LAST_STATUS_VAR
    So it matches upstream

--- sql/system_variables.h
+++ sql/system_variables.h
@@ -470,7 +470,7 @@ struct System_variables {
 
 /**
   Per thread status variables.
-  Must be ulonglong up to LAST_STATUS_VAR so that
+  Must be long/ulong up to last_system_status_var so that
   add_to_status/add_diff_to_status can work.
 */
 
@@ -544,8 +544,8 @@ struct System_status_var {
   ulong com_stat[(uint)SQLCOM_END];
 
   /*
-    IMPORTANT! See LAST_STATUS_VAR definition below. Variables after
-    'LAST_STATUS_VAR' cannot be handled automatically by add_to_status()
+    IMPORTANT! See last_system_status_var definition below. Variables after
+    'last_system_status_var' cannot be handled automatically by add_to_status()
     and add_diff_to_status().
   */
   double last_query_cost;

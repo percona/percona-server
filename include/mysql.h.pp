0b323defedb5b79b491ae7ee1ec6e4b9f0aa8b0a
Luis Donoso <luis.donoso@percona.com>
2021-01-27 10:51:14 +0100

    PS-7364 - Use unsigned long in my_net_shrink_buffer
    To prevent compilation errors in mac os

--- include/mysql.h.pp
+++ include/mysql.h.pp
@@ -202,7 +202,7 @@ bool net_write_command(struct NET *net, unsigned char command,
 bool net_write_packet(struct NET *net, const unsigned char *packet,
                       size_t length);
 unsigned long my_net_read(struct NET *net);
-bool my_net_shrink_buffer(NET *net, ulong min_buf_size);
+bool my_net_shrink_buffer(NET *net, unsigned long min_buf_size);
 void my_net_set_write_timeout(struct NET *net, unsigned int timeout);
 void my_net_set_read_timeout(struct NET *net, unsigned int timeout);
 void my_net_set_retry_count(struct NET *net, unsigned int retry_count);

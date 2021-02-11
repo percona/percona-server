5ee6342e8c08ed9cc6ef8ac3fcac93e8fe5eb677
Yura Sorokin <yura.sorokin@percona.com>
2021-02-11 22:39:50 +0200

    PS-7507 merge: Merge MySQL 8.0.23 (Revert PS-4026)
    https://jira.percona.com/browse/PS-7507
    
    Reverted PS-4026
    "MYR-7 is incomplete patch, MyRocks fails to allow index only scans on partitioned tables"
    https://jira.percona.com/browse/PS-4026
    (commit 53b273d)
    because it is in conflict with the Oracle's fix for the Bug #31617858
    "INCORRECT RESULTS WHEN USING INDEX ON LONG CHARACTER COLS"
    (commit mysql/mysql-server@dd838aa).

--- sql/table.cc
+++ sql/table.cc
@@ -2076,16 +2076,6 @@ static int open_binary_frm(THD *thd, TABLE_SHARE *share,
     const int pk_off =
         find_type(primary_key_name, &share->keynames, FIND_TYPE_NO_PREFIX);
     uint primary_key = (pk_off > 0 ? pk_off - 1 : MAX_KEY);
-    /*
-      The following if-else is here for MyRocks:
-      set share->primary_key as early as possible, because the return value
-      of ha_rocksdb::index_flags(key, ...) (HA_KEYREAD_ONLY bit in particular)
-      depends on whether the key is the primary key.
-    */
-    if (primary_key < MAX_KEY && share->keys_in_use.is_set(primary_key))
-      share->primary_key = primary_key;
-    else
-      share->primary_key = MAX_KEY;
 
     longlong ha_option = handler_file->ha_table_flags();
     keyinfo = share->key_info;
@@ -2136,13 +2126,6 @@ static int open_binary_frm(THD *thd, TABLE_SHARE *share,
             break;
           }
         }
-
-        /*
-          The following is here for MyRocks. See the comment above
-          about "set share->primary_key as early as possible"
-        */
-        if (primary_key < MAX_KEY && share->keys_in_use.is_set(primary_key))
-          share->primary_key = primary_key;
       }
 
       for (i = 0; i < keyinfo->user_defined_key_parts; key_part++, i++) {
@@ -2255,25 +2238,8 @@ static int open_binary_frm(THD *thd, TABLE_SHARE *share,
     */
     if (handler_file->init_with_fields()) goto err;
 
-    if (primary_key < MAX_KEY &&
-        (handler_file->ha_table_flags() & HA_PRIMARY_KEY_IN_READ_INDEX)) {
-      keyinfo = &share->key_info[primary_key];
-      key_part = keyinfo->key_part;
-      for (i = 0; i < keyinfo->user_defined_key_parts; key_part++, i++) {
-        Field *field = key_part->field;
-        /*
-          If this field is part of the primary key and all keys contains
-          the primary key, then we can use any key to find this column
-        */
-        if (field->key_length() == key_part->length &&
-            !field->is_flag_set(BLOB_FLAG))
-          field->part_of_key = share->keys_in_use;
-        if (field->part_of_sortkey.is_set(primary_key))
-          field->part_of_sortkey = share->keys_in_use;
-      }
-    }
-
-    if (share->primary_key != MAX_KEY) {
+    if (primary_key < MAX_KEY && (share->keys_in_use.is_set(primary_key))) {
+      share->primary_key = primary_key;
       /*
         If we are using an integer as the primary key then allow the user to
         refer to it as '_rowid'
@@ -2286,7 +2252,8 @@ static int open_binary_frm(THD *thd, TABLE_SHARE *share,
               (share->key_info[primary_key].key_part[0].fieldnr);
         }
       }
-    }
+    } else
+      share->primary_key = MAX_KEY;  // we do not have a primary key
   } else
     share->primary_key = MAX_KEY;
   my_free(disk_buff);

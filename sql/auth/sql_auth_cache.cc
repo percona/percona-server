23d63b3cea4d0f2baad238e3e5c8ee89e552a393
Yura Sorokin <yura.sorokin@percona.com>
2021-02-11 22:29:36 +0200

    PS-7507 merge: Merge MySQL 8.0.23 (Revert PS-7143)
    https://jira.percona.com/browse/PS-7507
    
    Reverted PS-7143
    "Suppress deadlock check for ACL Cache MDL lock to prevent server freeze"
    (https://jira.percona.com/browse/PS-7143)
    (commit 1e59822)
    in favor of Oracle's WL#13574
    "Include MDL and ACL locks in MTS deadlock detection infra-structure"
    (https://dev.mysql.com/worklog/task/?id=13574)
    (commit mysql/mysql-server@0a9a215e22f)
    (commit mysql/mysql-server@3e96a2196c3)
    (commit mysql/mysql-server@b2102eafc43)
    (commit mysql/mysql-server@a038ae423e6).

--- sql/auth/sql_auth_cache.cc
+++ sql/auth/sql_auth_cache.cc
@@ -2439,8 +2439,6 @@ bool acl_reload(THD *thd, bool mdl_locked) {
 
   if (!acl_cache_lock.lock()) goto end;
 
-  DEBUG_SYNC(thd, "acl_x_lock");
-
   old_acl_users = acl_users;
   old_acl_dbs = acl_dbs;
   old_acl_proxy_users = acl_proxy_users;
@@ -3658,11 +3656,8 @@ void shutdown_acl_cache() {
   delete g_mandatory_roles;
 }
 
-/* Constants used by Acl_cache_lock_guard. Upstream value of 3600UL
-(60mins) is too high. InnoDB max latch wait timeout
-(srv_fatal_semaphore_wait_threshold is 600 sec). 15mins (900 seconds)
-is good enough value for timeout */
-static const ulong ACL_CACHE_LOCK_TIMEOUT = 900UL;
+/* Constants used by Acl_cache_lock_guard */
+static const ulong ACL_CACHE_LOCK_TIMEOUT = 3600UL;
 static const MDL_key ACL_CACHE_KEY(MDL_key::ACL_CACHE, "", "");
 
 /**

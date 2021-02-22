c91849da7f848040f1973be31bc0b4eea5010ce7
Illia Pshonkin <illia.pshonkin@percona.com>
2021-02-22 11:04:56 +0200

    PS-7566: Fix changelog error for RHEL Software certification utility (#4198)
    * PS-7566: Fix changelog error for RHEL Software certification utility
    
    * PS-7566: Fix build error caused by unpackaged man pages for CentOS 6
    
    * PS-7566: Generate changelog on RELEASE_BUILD
    
    * PS-7566: Review fixes

--- build-ps/percona-server-8.0_builder.sh
+++ build-ps/percona-server-8.0_builder.sh
@@ -483,6 +483,9 @@ build_srpm(){
     cd ${WORKDIR}/rpmbuild/SPECS
     tar vxzf ${WORKDIR}/${TARFILE} --wildcards '*/build-ps/*.spec' --strip=2
     #
+    sed -i "/^%changelog/a - Release ${VERSION}-${RELEASE}" percona-server.spec
+    sed -i "/^%changelog/a * $(date "+%a") $(date "+%b") $(date "+%d") $(date "+%Y") Percona Development Team <info@percona.com> - ${VERSION}-${RELEASE}" percona-server.spec
+    #
     cd ${WORKDIR}/rpmbuild/SOURCES
     wget https://dl.bintray.com/boostorg/release/1.73.0/source/${BOOST_PACKAGE_NAME}.tar.gz
     #wget http://downloads.sourceforge.net/boost/${BOOST_PACKAGE_NAME}.tar.gz

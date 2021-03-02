07b83d82366e1fc10ad79605eda7143874f1de87
ldonoso <luisantoniod@gmail.com>
2021-03-02 07:11:08 -0500

    PS-7364 Adaptive network buffers (#4178)
    * Adaptive network buffers
    
    This feature allows periodically shrink network buffers according to the
    workload during the interval.
    Threads network buffer size is set to the maximum packet size seen in the
    interval.
    Thus it allows to "forget" spikes in the packet size which happend before last
    interval.
    
    Currently in the long living connections, network buffers tend to grow towards
    max_allowed_packet size and never shrink. For example, occasional big query will
    eventually increase buffers of all connections(if connections picked at random
    from the pool), while at any moment of time only one such buffer is sufficient.
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * Adaptive network buffers MTR test
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * Net_buffer_length show status variable
    
    Show the size of the network buffer of the current thread(show status),
    and the total size of network buffers of all connections(show global status).
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * Increase client library minor version
    
    This is needed because adaptive network buffers ABI change.
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * Fixing nits. Tests
    
    * PS-7364 - Fix test main.variables
    
    * PS-7364 - Fix type of System_variables::net_buffer_length
    
    All the variables of this structure must be ulonglong
    
    * PS-7364 - Process correctly net_buffer_length
    
    net_buffer_length just shows the accumulative value of the current
    connections so it can't be processed as the rest of variables of
    System_status_var.
    
    System_status_var accumulates data of activate and closed connections.
    
    * PS-7364 - Revert changes wrt LAST_STATUS_VAR
    
    So it matches upstream
    
    * PS-7364 - The initial value of max_interval_packet should not include
    headers
    
    * PS-7364 - Fix typos
    
    * PS-7364 - Use lock when accessing net_buffer_shrink_time
    
    * PS-7364 - Document NET::net_buffer_shrink_interval
    
    * PS-7364 - Assign net_buffer_length to 0 after add_status
    
    That variable does not acumulate all the historial data
    as the other ones.
    
    * PS-7364 - Remove libperconaserverclient18.2.* files
    
    They were contained in the cherry-picked commits and are not
    necessary in 8.0
    
    * PS-7364 - Realloc the net buffer after dispatch_command
    
    The buffer is accessed in dispatch_command, so it can not
    be realloc before dispatch_command is called.
    
    * PS-7364 - Use unsigned long in my_net_shrink_buffer
    
    To prevent compilation errors in mac os
    
    * PS-7364 - Initialize net_buffer_shrink_time
    
    * PS-7364 - Fix test all_persisted_variables
    
    * PS-7364 - Improve test net_buffer_shrink_interval
    
    Restarting the serve is not necessary
    
    * PS-7364 - Use assert.inc to check the value of variables
    
    Instead of rely on `show`
    
    * PS-7364 - Restore previous values in tests instead of defaults
    
    * PS-7364 - Use constant for allowed pct increase in my_net_shrink_buffer
    
    * PS-7364 - Restore changes in buf0buf.ic
    
    These changes are not related with the current scope of the change
    
    * PS-7364 - move max_interval_packet to NET_SERVER
    
    This way it is not needed a new version of libperconaserverclient
    
    * PS-7364 - Refactor test net_buffer_shrink_interval
    
    Co-authored-by: Evgeniy Firsov <firsov@uber.com>
    Co-authored-by: Luis Donoso <luis.donoso@percona.com>
    
    ---------------------------------------------------------------------------
    
    PS-7626 - Remove lock when reading net_buffer_shrink_time (#4258)
    
    LOCK_global_system_variables is not owned when reading other
    global variables.
    
    As the type of net_buffer_shrink_interval is ulong the read/writes
    are atomic but there still the hazards of using stale values.
    
    As this is a mild hazard, the risk is accepted.
    
    Co-authored-by: Luis Donoso <luis.donoso@percona.com>

--- include/mysql_com_server.h
+++ include/mysql_com_server.h
@@ -63,6 +63,10 @@ typedef struct NET_SERVER {
   struct compression_attributes compression;
   mysql_compress_context compress_ctx;
   bool timeout_on_full_packet;
+  /** Max buffer length, without headers, received during the last
+  global.net_buffer_shrink_interval. */
+  unsigned long max_interval_packet;
+  unsigned long long net_buffer_shrink_time;
 } NET_SERVER;
 
 inline void net_server_ext_init(NET_SERVER *ns) {
@@ -71,6 +75,7 @@ inline void net_server_ext_init(NET_SERVER *ns) {
   ns->m_after_header = nullptr;
   ns->compress_ctx.algorithm = MYSQL_UNCOMPRESSED;
   ns->timeout_on_full_packet = false;
+  ns->max_interval_packet = 0;
 }
 
 #endif

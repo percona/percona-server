0c5087879906b6216e56d4e2bc3d746dff6a84be
ldonoso <luisantoniod@gmail.com>
2021-05-13 06:15:58 -0400

    PS 7366 - Add jemalloc memory allocation profiling (#4223)
    * Integrated Jemalloc memory introspection tool
    
    Added the following:
    
      * Two new perfschema tables (MALLOC_STATS_SUMMARY, MALLOC_STATS) can be used
    to observer current state of memory during runtime.
    
      * FLUSH MEMORY PROFILE command can be used to dump current profile to file,
        jeprof tool can be used later to inspect what happened between two profiles.
    
    In order functionality to work mysqld must be started with jemalloc and
    profiling enabled, othewise it'll be a noop. Profiling can be enabled using
    MALLOC_CONF="prof:true" environment variable.
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * MTR: Fix perfschema mysql-test suite for malloc_stats
    
    Signed-off-by: Evgeniy Firsov <firsov@uber.com>
    
    * PS-7333 - Fix compilation
    
    * PS-7366-80. - Fix compilation warning and comments
    
    * PS-7366 - Solve pull request issues
    
    * PS-7366 - Convert epoch into an atomic variable
    
    * PS-7366 - Revert changes setting table entry to 0
    
    It is already implemented
    
    * PS-7366 - Use nullptr instead of NULL
    
    * PS-7366 - Fix formatting
    
    * PS-7366 - Fix formatting
    
    Co-authored-by: Evgeniy Firsov <firsov@uber.com>
    Co-authored-by: Luis Donoso <luis.donoso@percona.com>

--- storage/perfschema/pfs_engine_table.cc
+++ storage/perfschema/pfs_engine_table.cc
@@ -102,6 +102,7 @@
 #include "storage/perfschema/table_hosts.h"
 #include "storage/perfschema/table_keyring_component_status.h"
 #include "storage/perfschema/table_keyring_keys.h"
+#include "storage/perfschema/table_malloc_stats.h"
 #include "storage/perfschema/table_md_locks.h"
 #include "storage/perfschema/table_mems_by_account_by_event_name.h"
 #include "storage/perfschema/table_mems_by_host_by_event_name.h"
@@ -606,7 +607,12 @@ static PFS_engine_table_share *all_shares[] = {
     &table_user_defined_functions::m_share,
     &table_binary_log_transaction_compression_stats::m_share,
     &table_tls_channel_status::m_share,
+
+    &table_malloc_stats_totals::m_share,
+    &table_malloc_stats::m_share,
+
     &table_keyring_component_status::m_share,
+
     nullptr};
 
 static PSI_mutex_key key_LOCK_pfs_share_list;

f4bc9d86dece2ca7769a0b32a6b928f5d3034cd6
Yura Sorokin <yura.sorokin@percona.com>
2021-05-22 15:19:05 +0300

    [components/keyrings/keyring_kmip] PS-7586: Implementing KMIP keyring component
    [components/keyrings] PS-8102: Fixing memory leak in keyring_common
    
    get_default_mr in keyring_common created a new instance of the default
    memory resource every time the funciton was called instead of using a
    single static instance.
    
    Since we isn't using the full functionity of polymorphic allocators,
    this commit changes pmr_string to use a traditional PFS aware allocator
    instead.
    
    -----
    PS-8979: Merge MySQL 8.2.0 (update libkmip submodule)
    
    ---------------------------------------------------------------------------
    
    PS-9286 KMIP key activation implementation
    
    https://perconadev.atlassian.net/browse/PS-9286
    
    extra/libkmip head is at baf6832 with activate operation implementation.
    keyring_kmip/backend/backend.cc updateed to execute op_activate after key
    registration.
    
    ---------------------------------------------------------------------------
    
    PS-9416 better kmip error messages
    
    https://perconadev.atlassian.net/browse/PS-9416
    
    Messages from OpenSSL level added to the connection excetpions messages.

--- components/keyrings/keyring_file/keyring_file.cc
+++ components/keyrings/keyring_file/keyring_file.cc
@@ -45,6 +45,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 #include <components/keyrings/common/component_helpers/include/keyring_writer_service_definition.h>
 #include <mysql/components/services/component_status_var_service.h>
 
+#include <mysql/components/services/psi_memory.h>
+
 /* clang-format off */
 /**
   @page PAGE_COMPONENT_KEYRING_FILE component_keyring_file
@@ -327,6 +329,8 @@ PROVIDES_SERVICE(component_keyring_file, keyring_aes),
     PROVIDES_SERVICE(component_keyring_file, log_builtins_string),
     END_COMPONENT_PROVIDES();
 
+REQUIRES_SERVICE_PLACEHOLDER(psi_memory_v2);
+
 /** List of dependencies */
 BEGIN_COMPONENT_REQUIRES(component_keyring_file)
 REQUIRES_SERVICE(log_builtins), REQUIRES_SERVICE(log_builtins_string),
@@ -337,6 +341,7 @@ REQUIRES_SERVICE(log_builtins), REQUIRES_SERVICE(log_builtins_string),
                                        mysql_service_registration_no_lock),
     REQUIRES_SERVICE_IMPLEMENTATION_AS(registry, mysql_minimal_chassis_no_lock,
                                        mysql_service_registry_no_lock),
+    REQUIRES_PSI_MEMORY_SERVICE,
     END_COMPONENT_REQUIRES();
 
 /** Component description */

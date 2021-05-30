4e61d769c9b5af262259a7d64c85926789a2b991
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2021-05-31 01:32:17 +0300

    PS-7726 add gcc-toolset-10 for centgos8

--- build-ps/percona-server-8.0_builder.sh
+++ build-ps/percona-server-8.0_builder.sh
@@ -351,10 +351,16 @@ install_deps() {
 	    yum -y install rpcgen re2-devel libtirpc-devel
 	    yum -y install zstd libzstd libzstd-devel
         fi
-        if [ "x${RHEL}" = "x7" -o "x${RHEL}" = "x8" ]; then
+        if [ "x${RHEL}" = "x8" ]; then
+            yum -y install centos-release-stream
+            yum -y install git gcc-toolset-10-gcc gcc-toolset-10-gcc-c++ gcc-toolset-10-annobin
+            source /opt/rh/gcc-toolset-10/enable
+        fi
+        if [ "x${RHEL}" = "x7" ]; then
             yum -y install devtoolset-10
             source /opt/rh/devtoolset-10/enable
-        else
+        fi
+	 if [ "x${RHEL}" = "x6" ]; then
             source /opt/rh/devtoolset-8/enable
         fi
         if [ "x$RHEL" = "x6" ]; then
@@ -609,8 +615,12 @@ build_rpm(){
     mv *.src.rpm rpmbuild/SRPMS
     if [ "x${RHEL}" = "x6" ]; then
         source /opt/rh/devtoolset-8/enable
-    else
+    fi
+    if [ "x${RHEL}" = "x7" ]; then
         source /opt/rh/devtoolset-10/enable
+    fi
+    if [ "x${RHEL}" = "x8" ]; then
+        source /opt/rh/gcc-toolset-10/enable
     fi
     build_mecab_lib
     build_mecab_dict
@@ -618,8 +628,12 @@ build_rpm(){
     cd ${WORKDIR}
     if [ "x${RHEL}" = "x6" ]; then
         source /opt/rh/devtoolset-8/enable
-    else
+    fi
+    if [ "x${RHEL}" = "x7" ]; then
         source /opt/rh/devtoolset-10/enable
+    fi
+    if [ "x${RHEL}" = "x8" ]; then
+        source /opt/rh/gcc-toolset-10/enable
     fi
     #
     if [ ${ARCH} = x86_64 ]; then
@@ -776,8 +790,12 @@ build_tarball(){
       RHEL=$(rpm --eval %rhel)
       if [ "x${RHEL}" = "x6" ]; then
           source /opt/rh/devtoolset-8/enable
-      else
+      fi
+      if [ "x${RHEL}" = "x7" ]; then
           source /opt/rh/devtoolset-10/enable
+      fi
+      if [ "x${RHEL}" = "x8" ]; then
+          source /opt/rh/gcc-toolset-10/enable
       fi
     fi
     #

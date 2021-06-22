0788df3eb07d4de0a71c800bd50a244ce6eade0a
Luis Donoso <luis.donoso@percona.com>
2021-06-22 10:52:34 +0200

    PS-7746 - Call dict_table_close in all returns of ha_innobase::open
    In `ha_innobase::open`, PS have added some extra checks which if they
    fail might call `free_share(m_share)` and return an error.
    
    If that happens, `dict_table_close` must be called as MySQL does when
    its checks fail.

--- storage/innobase/handler/ha_innodb.cc
+++ storage/innobase/handler/ha_innodb.cc
@@ -1472,6 +1472,15 @@ static INNOBASE_SHARE *get_share(
 /** Free the shared object that was registered with get_share(). */
 static void free_share(INNOBASE_SHARE *share); /*!< in/own: share to free */
 
+/** Calls free_share and assign nullptr to share.
+@param[in,out]	share	table share to free */
+static void free_share_and_nullify(
+    INNOBASE_SHARE **share) /*!< in/own: table share to free */
+{
+  free_share(*share);
+  *share = nullptr;
+}
+
 /** Frees a possible InnoDB trx object associated with the current THD.
  @return 0 or error number */
 static int innobase_close_connection(
@@ -7830,7 +7839,8 @@ int ha_innobase::open(const char *name, int, uint open_flags,
 
     if (UNIV_UNLIKELY(m_share->ib_table && m_share->ib_table->is_corrupt &&
                       srv_pass_corrupt_table <= 1)) {
-      free_share(m_share);
+      free_share_and_nullify(&m_share);
+      dict_table_close(ib_table, false, false);
       return HA_ERR_CRASHED_ON_USAGE;
     }
   }
@@ -7853,14 +7863,15 @@ int ha_innobase::open(const char *name, int, uint open_flags,
     /* Mark this table as corrupted, so the drop table
     or force recovery can still use it, but not others. */
     ib_table->first_index()->type |= DICT_CORRUPT;
-    free_share(m_share);
+    free_share_and_nullify(&m_share);
     dict_table_close(ib_table, false, false);
     ib_table = nullptr;
   }
 
   if (UNIV_UNLIKELY(ib_table && ib_table->is_corrupt &&
                     srv_pass_corrupt_table <= 1)) {
-    free_share(m_share);
+    free_share_and_nullify(&m_share);
+    dict_table_close(ib_table, false, false);
     return HA_ERR_CRASHED_ON_USAGE;
   }
 
@@ -7871,10 +7882,10 @@ int ha_innobase::open(const char *name, int, uint open_flags,
     /* Mark this table as corrupted, so the drop table
     or force recovery can still use it, but not others. */
 
-    free_share(m_share);
     dict_table_close(ib_table, false, false);
     ib_table = nullptr;
 
+    free_share_and_nullify(&m_share);
     my_error(ER_CANNOT_FIND_KEY_IN_KEYRING, MYF(0));
 
     return HA_ERR_TABLE_CORRUPT;
@@ -7922,7 +7933,7 @@ int ha_innobase::open(const char *name, int, uint open_flags,
   }
 
   if (!thd_tablespace_op(thd) && no_tablespace) {
-    free_share(m_share);
+    free_share_and_nullify(&m_share);
     set_my_errno(ENOENT);
 
     dict_table_close(ib_table, false, false);
@@ -8222,7 +8233,7 @@ int ha_innobase::close() {
     vec->erase(std::remove(vec->begin(), vec->end(), m_prebuilt), vec->end());
   }
 
-  free_share(m_share);
+  free_share_and_nullify(&m_share);
 
   row_prebuilt_free(m_prebuilt, false);
 
@@ -20017,6 +20028,10 @@ static INNOBASE_SHARE *get_share(const char *table_name) {
 static void free_share(
     INNOBASE_SHARE *share) /*!< in/own: table share to free */
 {
+  if (!share) {
+    return;
+  }
+
   mysql_mutex_lock(&innobase_share_mutex);
 
 #ifdef UNIV_DEBUG

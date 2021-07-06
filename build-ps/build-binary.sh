0e929532208fa0eb0d03776e600d0544c0bb165c
Illia Pshonkin <illia.pshonkin@percona.com>
2021-07-06 14:56:51 +0300

    PS-7764: Implement ZenFS for binary tarballs and create a separate package for Debian (#4368)
    * PS-7764: Implement ZenFS support in buildscripts and prepare Debian files

--- build-ps/build-binary.sh
+++ build-ps/build-binary.sh
@@ -20,6 +20,8 @@ WITH_JEMALLOC=''
 WITH_MECAB_OPTION=''
 DEBUG_EXTRA=''
 WITH_SSL='/usr'
+WITH_ZENFS='OFF'
+ZENFS_EXTRA=''
 OPENSSL_INCLUDE=''
 OPENSSL_LIBRARY=''
 CRYPTO_LIBRARY=''
@@ -39,7 +41,7 @@ TAR=${TAR:-tar}
 if ! getopt --test
 then
     go_out="$(getopt --options=iqdvj:m:t: \
-        --longoptions=i686,quiet,debug,valgrind,with-jemalloc:,with-mecab:,with-ssl:,tag: \
+        --longoptions=i686,quiet,debug,valgrind,with-jemalloc:,with-zenfs,with-mecab:,with-ssl:,tag: \
         --name="$(basename "$0")" -- "$@")"
     test $? -eq 0 || exit 1
     eval set -- $go_out
@@ -103,6 +105,12 @@ do
             echo >&2 "Cannot find libssl.a in $WITH_SSL"
             exit 3
         fi
+        ;;
+    --with-zenfs )
+        shift
+        TARBALL_SUFFIX="-zenfs"
+        WITH_ZENFS="ON"
+        ZENFS_EXTRA="-DROCKSDB_PLUGINS=zenfs"
         ;;
     -t | --tag )
         shift
@@ -254,6 +262,7 @@ fi
     cmake $SOURCEDIR ${CMAKE_OPTS:-} -DBUILD_CONFIG=mysql_release \
         -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-RelWithDebInfo} \
         $DEBUG_EXTRA \
+        $ZENFS_EXTRA \
         -DFEATURE_SET=community \
         -DCMAKE_INSTALL_PREFIX="/usr/local/$PRODUCT_FULL" \
         -DMYSQL_DATADIR="/usr/local/$PRODUCT_FULL/data" \
@@ -296,6 +305,29 @@ fi
         # Copy COPYING file
         cp COPYING "$INSTALLDIR/usr/local/$PRODUCT_FULL/COPYING-jemalloc"
     )
+    fi
+    # Build zenfs
+    if [[ ${WITH_ZENFS} == "ON" ]]; then
+        if [[ -f $INSTALLDIR/zenfs ]]; then
+            echo "ZenFS utils is built"
+        else
+            SOURCEDIR=${WORKDIR_ABS}/rocksdb-source
+            INSTALL_ROOT=${WORKDIR_ABS}/rocksdb-root
+            BUILD_ROOT=${WORKDIR_ABS}/rocksdb-build
+
+            mkdir ${WORKDIR_ABS}/rocksdb-root ${WORKDIR_ABS}/rocksdb-build
+
+            pushd $SOURCEDIR
+            CC=clang-12 CXX=clang++-12 make DISABLE_WARNING_AS_ERROR=1 PREFIX=${INSTALL_ROOT}/usr OBJ_DIR=${BUILD_ROOT} ROCKSDB_PLUGINS=zenfs -j$(nproc) install-static
+            popd
+
+            pushd $SOURCEDIR/plugin/zenfs/util
+            PKG_CONFIG_PATH=$INSTALL_ROOT/usr/lib/pkgconfig make CC=clang-12 CXX=clang++-12 -j$(nproc)
+            popd
+
+            cp $SOURCEDIR/plugin/zenfs/util/zenfs $INSTALLDIR/
+            rm -rf $INSTALL_ROOT $BUILD_ROOT $SOURCEDIR
+        fi
     fi
 )
 
@@ -423,15 +455,19 @@ fi
     fi
 
     # NORMAL TARBALL
-    cd "$INSTALLDIR/usr/local/$PRODUCT_FULL"
-    link
+    if [[ ${WITH_ZENFS} != "ON" ]]; then
+        cd "$INSTALLDIR/usr/local/$PRODUCT_FULL"
+        link
+    fi
 
     # MIN TARBALL
     if [[ $CMAKE_BUILD_TYPE != "Debug" ]]; then
         cd "$INSTALLDIR/usr/local/minimal/$PRODUCT_FULL-minimal"
         rm -rf mysql-test 2> /dev/null
         find . -type f -exec file '{}' \; | grep ': ELF ' | cut -d':' -f1 | xargs strip --strip-unneeded
-        link
+        if [[ ${WITH_ZENFS} != "ON" ]]; then
+            link
+        fi
     fi
 )
 
@@ -440,11 +476,17 @@ fi
     cd "$INSTALLDIR/usr/local/"
     #PS-4854 Percona Server for MySQL tarball without AGPLv3 dependency/license
     find $PRODUCT_FULL -type f -name 'COPYING.AGPLv3' -delete
+    if [[ ${WITH_ZENFS} == "ON" ]]; then
+        install -m 0755 $INSTALLDIR/zenfs $PRODUCT_FULL/bin
+    fi
     $TAR --owner=0 --group=0 -czf "$WORKDIR_ABS/$PRODUCT_FULL.tar.gz" $PRODUCT_FULL
 
     if [[ $CMAKE_BUILD_TYPE != "Debug" ]]; then
         cd "$INSTALLDIR/usr/local/minimal/"
         find $PRODUCT_FULL-minimal -type f -name 'COPYING.AGPLv3' -delete
+        if [[ ${WITH_ZENFS} == "ON" ]]; then
+            install -m 0755 $INSTALLDIR/zenfs $PRODUCT_FULL-minimal/bin
+        fi
         $TAR --owner=0 --group=0 -czf "$WORKDIR_ABS/$PRODUCT_FULL-minimal.tar.gz" $PRODUCT_FULL-minimal
     fi
 )

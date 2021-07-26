7af8fa654832105be4eb530971ea400e1267cb5a
Oleksandr Kachan <oleksandr.kachan@percona.com>
2021-07-26 17:43:31 +0300

    PS-7811 merge: Merge MySQL 8.0.26 (fix usage of innobase_get_computed_value())
    https://jira.percona.com/browse/PS-7811
    
    Added missing parameter for innobase_get_computed_value() invocation.

--- storage/innobase/row/row0upd.cc
+++ storage/innobase/row/row0upd.cc
@@ -1899,8 +1899,8 @@ static void row_upd_store_v_row(upd_node_t *node, const upd_t *update, THD *thd,
             }
             if (dfield_is_null(dfield)) {
               innobase_get_computed_value(node->row, col, index, &heap,
-                                          node->heap, NULL, thd, mysql_table,
-                                          NULL, NULL, NULL);
+                                          node->heap, nullptr, thd, mysql_table,
+                                          nullptr, nullptr, nullptr, prebuilt);
             }
           }
         } else {

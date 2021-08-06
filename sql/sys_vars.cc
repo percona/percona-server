mysql/mysql-server

BUG#31774422: SERVER HANGS IF BINLOG ROTATION CAUSED BY               VARIABLE UPDATE + CONCURRENT READ
PROBLEM:
========
Transaction commit during update of the system variable
which resulted in binlog rotation, while concurrently
reading variables from another connection might have led to
deadlock.

ANALYSIS:
=========
When connection commits transaction during update of the
system variable it is done after taking THD::LOCK_thd_sysvar
lock. During this commit, binlog rotation can happen, which
will cause THD::LOCK_thd_data lock being acquired when
opening gtid_executed table to write the executed GTIDs.
Code which reads value of system variables for the first
connection from another connection will first take
THD::LOCK_thd_data of the first thread (to prevent it from
going away) and later take its THD::LOCK_thd_sysvar (to
prevent it from updating system variable concurrently).

This opposite lock order in two connections causes a
deadlock.

FIX:
====
Introduced a pre_update hook as a preparatory step for
updating a system variable to be executed before acquiring
THD::LOCK_sys_var. Transaction commit happens on this step
without any lock held and thus we can safely acquire
THD::LOCK_thd_data without creating problems with lock
order.

RB#25744

Change-Id: I415f5bae06ff105f1af83b53dc672f3addb92235

sql/sys_vars.cc
@@ -4463,6 +4463,35 @@ static Sys_var_charptr Sys_time_format(
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0), ON_UPDATE(0),
        DEPRECATED_VAR(""));
 
+/**
+  Pre-update function to commit connection's active transactions when autocommit
+  is enabled.
+
+  @note This hook relies on the fact that it is called while not holding any
+        locks. Breaking this assumption might result in deadlocks as commit
+        acquires many different locks in its process (e.g. to open GTID-related
+        tables).
+
+  @param[in] self   A pointer to the sys_var, i.e. Sys_autocommit.
+  @param[in] thd    A reference to THD object.
+  @param[in] var    A pointer to the set_var created by the parser.
+
+  @retval true   Error during commit
+  @retval false  Otherwise
+*/
+static bool pre_autocommit(sys_var *self, THD *thd, set_var *var)
+{
+  if (!(var->type == OPT_GLOBAL) &&
+      (thd->variables.option_bits & OPTION_NOT_AUTOCOMMIT) &&
+       var->save_result.ulonglong_value)
+  {
+    // Autocommit mode is about to be activated.
+    if (trans_commit_stmt(thd) || trans_commit(thd))
+      return true;
+  }
+  return false;
+}
+
 static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
 {
   if (type == OPT_GLOBAL)
@@ -4477,12 +4506,6 @@ static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
   if (thd->variables.option_bits & OPTION_AUTOCOMMIT &&
       thd->variables.option_bits & OPTION_NOT_AUTOCOMMIT)
   { // activating autocommit
-
-    if (trans_commit_stmt(thd) || trans_commit(thd))
-    {
-      thd->variables.option_bits&= ~OPTION_AUTOCOMMIT;
-      return true;
-    }
     /*
       Don't close thread tables or release metadata locks: if we do so, we
       risk releasing locks/closing tables of expressions used to assign
@@ -4517,7 +4540,8 @@ static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
 static Sys_var_bit Sys_autocommit(
        "autocommit", "autocommit",
        SESSION_VAR(option_bits), NO_CMD_LINE, OPTION_AUTOCOMMIT, DEFAULT(TRUE),
-       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0), ON_UPDATE(fix_autocommit));
+       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
+       PRE_UPDATE(pre_autocommit), ON_UPDATE(fix_autocommit));
 export sys_var *Sys_autocommit_ptr= &Sys_autocommit; // for sql_yacc.yy
 
 static Sys_var_mybool Sys_big_tables(
@@ -4651,7 +4675,7 @@ static Sys_var_bit Sys_profiling(
        "profiling", "profiling",
        SESSION_VAR(option_bits), NO_CMD_LINE, OPTION_PROFILING,
        DEFAULT(FALSE), NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
-       ON_UPDATE(0), DEPRECATED_VAR(""));
+       PRE_UPDATE(0), ON_UPDATE(0), DEPRECATED_VAR(""));
 
 static Sys_var_ulong Sys_profiling_history_size(
        "profiling_history_size", "Limit of query profiling memory",

percona/percona-server

sql/sys_vars.cc
@@ -1,4 +1,5 @@
 /* Copyright (c) 2002, 2018, Oracle and/or its affiliates. All rights reserved.
+   Copyright (c) 2021, Percona and/or its affiliates. All rights reserved.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -3636,6 +3637,35 @@ static Sys_var_charptr Sys_time_format(
        NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0), ON_UPDATE(0),
        DEPRECATED(""));
 
+/**
+  Pre-update function to commit connection's active transactions when autocommit
+  is enabled.
+
+  @note This hook relies on the fact that it is called while not holding any
+        locks. Breaking this assumption might result in deadlocks as commit
+        acquires many different locks in its process (e.g. to open GTID-related
+        tables).
+
+  @param[in] self   A pointer to the sys_var, i.e. Sys_autocommit.
+  @param[in] thd    A reference to THD object.
+  @param[in] var    A pointer to the set_var created by the parser.
+
+  @retval true   Error during commit
+  @retval false  Otherwise
+*/
+static bool pre_autocommit(sys_var *self, THD *thd, set_var *var)
+{
+  if (!(var->type == OPT_GLOBAL) &&
+      (thd->variables.option_bits & OPTION_NOT_AUTOCOMMIT) &&
+       var->save_result.ulonglong_value)
+  {
+    // Autocommit mode is about to be activated.
+    if (trans_commit_stmt(thd) || trans_commit(thd))
+      return true;
+  }
+  return false;
+}
+
 static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
 {
   if (type == OPT_GLOBAL)
@@ -3650,12 +3680,6 @@ static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
   if (thd->variables.option_bits & OPTION_AUTOCOMMIT &&
       thd->variables.option_bits & OPTION_NOT_AUTOCOMMIT)
   { // activating autocommit
-
-    if (trans_commit_stmt(thd) || trans_commit(thd))
-    {
-      thd->variables.option_bits&= ~OPTION_AUTOCOMMIT;
-      return true;
-    }
     /*
       Don't close thread tables or release metadata locks: if we do so, we
       risk releasing locks/closing tables of expressions used to assign
@@ -3688,7 +3712,8 @@ static bool fix_autocommit(sys_var *self, THD *thd, enum_var_type type)
 static Sys_var_bit Sys_autocommit(
        "autocommit", "autocommit",
        SESSION_VAR(option_bits), NO_CMD_LINE, OPTION_AUTOCOMMIT, DEFAULT(TRUE),
-       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0), ON_UPDATE(fix_autocommit));
+       NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
+       PRE_UPDATE(pre_autocommit), ON_UPDATE(fix_autocommit));
 export sys_var *Sys_autocommit_ptr= &Sys_autocommit; // for sql_yacc.yy
 
 static Sys_var_mybool Sys_big_tables(
@@ -3822,7 +3847,7 @@ static Sys_var_bit Sys_profiling(
        "profiling", "profiling",
        SESSION_VAR(option_bits), NO_CMD_LINE, OPTION_PROFILING,
        DEFAULT(FALSE), NO_MUTEX_GUARD, NOT_IN_BINLOG, ON_CHECK(0),
-       ON_UPDATE(0), DEPRECATED(""));
+       PRE_UPDATE(0), ON_UPDATE(0), DEPRECATED(""));
 
 static Sys_var_ulong Sys_profiling_history_size(
        "profiling_history_size", "Limit of query profiling memory",
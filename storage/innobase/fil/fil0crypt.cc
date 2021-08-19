mysql/mysql-server

commit 9f51d4fb13b590715929988ab15a3f19ffd43ee6

Bug#32476724 : DEFAULTMASTERKEY IS PLAIN TEXT USING WHICH UNDO/REDO                KEYS ENCRYPTED TEMPORARILY
Fix:
  Server code fix - Satish
  InnoDB code fix - Mayank

RB: 26011
Reviewed by: DEBARUN.BANERJEE <debarun.banerjee@oracle.com>

storage/innobase/os/os0enc.cc
@@ -212,6 +212,9 @@ uint32_t Encryption::s_master_key_id = Encryption::DEFAULT_MASTER_KEY_ID;
 /** Current uuid of server instance */
 char Encryption::s_uuid[Encryption::SERVER_UUID_LEN + 1] = {0};
 
+/** Tablespaces whose key needs to be reencrypted */
+std::vector<space_id_t> Encryption::s_tablespaces_to_reencrypt;
+
 const char *Encryption::to_string(Type type) noexcept {
   switch (type) {
     case NONE:
@@ -449,39 +452,37 @@ bool Encryption::fill_encryption_info(byte *key, byte *iv, byte *encrypt_info,
                                       bool is_boot, bool encrypt_key) noexcept {
   byte *master_key = nullptr;
   uint32_t master_key_id = DEFAULT_MASTER_KEY_ID;
-  bool is_default_master_key = false;
 
-  /* Get master key from keyring. For bootstrap, we use a default
-  master key which master_key_id is 0. */
-  if (encrypt_key) {
-    if (is_boot
 #ifndef UNIV_HOTBACKUP
-        || (strlen(server_uuid) == 0)
+  /* Server uuid must have already been generated */
+  ut_ad(strlen(server_uuid) > 0);
 #endif
-    ) {
-      master_key = static_cast<byte *>(ut_zalloc_nokey(KEY_LEN));
 
-      ut_ad(KEY_LEN >= sizeof(DEFAULT_MASTER_KEY));
+  /* Get master key from keyring. */
+  if (encrypt_key) {
+    get_master_key(&master_key_id, &master_key);
 
-      strcpy(reinterpret_cast<char *>(master_key), DEFAULT_MASTER_KEY);
-      is_default_master_key = true;
-    } else {
-      get_master_key(&master_key_id, &master_key);
-
-      if (master_key == nullptr) {
-        return (false);
-      }
+    if (master_key == nullptr) {
+      return (false);
     }
+
+    ut_ad(master_key_id != DEFAULT_MASTER_KEY_ID);
+    ut_ad(memcmp(master_key, DEFAULT_MASTER_KEY, sizeof(DEFAULT_MASTER_KEY)) !=
+          0);
   }
 
+  /* Encryption info to be filled in following format
+    --------------------------------------------------------------------------
+   | Magic bytes | master key id | server uuid | tablespace key|iv | checksum |
+    --------------------------------------------------------------------------
+  */
+  ut_ad(encrypt_info != nullptr);
   memset(encrypt_info, 0, INFO_SIZE);
 
-  /* Use the new master key to encrypt the key. */
-  ut_ad(encrypt_info != nullptr);
   auto ptr = encrypt_info;
 
+  /* Write Magic bytes */
   memcpy(ptr, KEY_MAGIC_V3, MAGIC_SIZE);
-
   ptr += MAGIC_SIZE;
 
   /* Write master key id. */
@@ -492,14 +493,11 @@ bool Encryption::fill_encryption_info(byte *key, byte *iv, byte *encrypt_info,
   memcpy(reinterpret_cast<char *>(ptr), s_uuid, sizeof(s_uuid));
   ptr += sizeof(s_uuid) - 1;
 
+  /* Write (and encrypt if needed) key and iv */
   byte key_info[KEY_LEN * 2];
-
   memset(key_info, 0x0, sizeof(key_info));
-
   memcpy(key_info, key, KEY_LEN);
-
   memcpy(key_info + KEY_LEN, iv, KEY_LEN);
-
   if (encrypt_key) {
     /* Encrypt key and iv. */
     auto elen = my_aes_encrypt(key_info, sizeof(key_info), ptr, master_key,
@@ -513,21 +511,15 @@ bool Encryption::fill_encryption_info(byte *key, byte *iv, byte *encrypt_info,
     /* Keep tablespace key unencrypted. Used by clone. */
     memcpy(ptr, key_info, sizeof(key_info));
   }
-
   ptr += sizeof(key_info);
 
   /* Write checksum bytes. */
   auto crc = ut_crc32(key_info, sizeof(key_info));
-
   mach_write_to_4(ptr, crc);
 
   if (encrypt_key) {
     ut_ad(master_key != nullptr);
-    if (is_default_master_key) {
-      ut_free(master_key);
-    } else {
-      my_free(master_key);
-    }
+    my_free(master_key);
   }
 
   return (true);
@@ -536,25 +528,22 @@ bool Encryption::fill_encryption_info(byte *key, byte *iv, byte *encrypt_info,
 byte *Encryption::get_master_key_from_info(byte *encrypt_info, Version version,
                                            uint32_t *m_key_id, char *srv_uuid,
                                            byte **master_key) noexcept {
-  byte *ptr;
-  uint32 key_id;
-
-  ptr = encrypt_info;
+  byte *ptr = encrypt_info;
   *m_key_id = 0;
 
   /* Get master key id. */
-  key_id = mach_read_from_4(ptr);
-  ptr += sizeof(uint32);
+  uint32_t key_id = mach_read_from_4(ptr);
+  ptr += sizeof(uint32_t);
 
   /* Handle different version encryption information. */
   switch (version) {
     case VERSION_1:
-      /* For version 1, it's possible master key id
-      occupied 8 bytes. */
+      /* For version 1, it's possible master key id occupied 8 bytes. */
       if (mach_read_from_4(ptr) == 0) {
         ptr += sizeof(uint32);
       }
 
+      /* Get master key. */
       get_master_key(key_id, nullptr, master_key);
       if (*master_key == nullptr) {
         return (encrypt_info);
@@ -564,13 +553,12 @@ byte *Encryption::get_master_key_from_info(byte *encrypt_info, Version version,
       return (ptr);
 
     case VERSION_2:
-      /* For version 2, it's also possible master key id
-      occupied 8 bytes. */
+      /* For version 2, it's also possible master key id occupied 8 bytes. */
       if (mach_read_from_4(ptr) == 0) {
         ptr += sizeof(uint32);
       }
 
-      /* Get server uuid. */
+      /* Read server uuid. */
       memset(srv_uuid, 0, SERVER_UUID_LEN + 1);
       memcpy(srv_uuid, ptr, SERVER_UUID_LEN);
 
@@ -587,18 +575,15 @@ byte *Encryption::get_master_key_from_info(byte *encrypt_info, Version version,
       break;
 
     case VERSION_3:
-      /* Get server uuid. */
+      /* Read server uuid. */
       memset(srv_uuid, 0, SERVER_UUID_LEN + 1);
       memcpy(srv_uuid, ptr, SERVER_UUID_LEN);
 
       ptr += SERVER_UUID_LEN;
 
-      if (key_id == 0) {
-        /* When key_id is 0, which means it's the
-        default master key for bootstrap. */
+      if (key_id == DEFAULT_MASTER_KEY_ID) {
         *master_key = static_cast<byte *>(ut_zalloc_nokey(KEY_LEN));
         memcpy(*master_key, DEFAULT_MASTER_KEY, strlen(DEFAULT_MASTER_KEY));
-        *m_key_id = 0;
       } else {
         ut_ad(strlen(srv_uuid) != 0);
 
@@ -607,9 +592,9 @@ byte *Encryption::get_master_key_from_info(byte *encrypt_info, Version version,
         if (*master_key == nullptr) {
           return (encrypt_info);
         }
-
-        *m_key_id = key_id;
       }
+
+      *m_key_id = key_id;
       break;
   }
 
@@ -618,22 +603,18 @@ byte *Encryption::get_master_key_from_info(byte *encrypt_info, Version version,
   return (ptr);
 }
 
-bool Encryption::decode_encryption_info(byte *key, byte *iv,
+bool Encryption::decode_encryption_info(space_id_t space_id,
+                                        Encryption_key &e_key,
                                         byte *encryption_info,
                                         bool decrypt_key) noexcept {
-  byte *ptr;
-  byte *master_key = nullptr;
-  uint32_t master_key_id = DEFAULT_MASTER_KEY_ID;
-  byte key_info[KEY_LEN * 2];
-  ulint crc1;
-  ulint crc2;
-  char srv_uuid[SERVER_UUID_LEN + 1];
-  Version version;
-
-  ptr = encryption_info;
+  byte *ptr = encryption_info;
+  byte *key = e_key.m_key;
+  byte *iv = e_key.m_iv;
+  uint32_t &master_key_id = e_key.m_master_key_id;
 
   /* For compatibility with 5.7.11, we need to handle the
   encryption information which created in this old version. */
+  Version version;
   if (memcmp(ptr, KEY_MAGIC_V1, MAGIC_SIZE) == 0) {
     version = VERSION_1;
   } else if (memcmp(ptr, KEY_MAGIC_V2, MAGIC_SIZE) == 0) {
@@ -652,9 +633,12 @@ bool Encryption::decode_encryption_info(byte *key, byte *iv,
                              << " found unexpected version of it!";
     return (false);
   }
-
   ptr += MAGIC_SIZE;
 
+  /* Read master key id and read (decrypt if needed) tablespace key and iv. */
+  byte *master_key = nullptr;
+  char srv_uuid[SERVER_UUID_LEN + 1];
+  byte key_info[KEY_LEN * 2];
   if (decrypt_key) {
     /* Get master key by key id. */
     ptr = get_master_key_from_info(ptr, version, &master_key_id, srv_uuid,
@@ -665,25 +649,13 @@ bool Encryption::decode_encryption_info(byte *key, byte *iv,
       return (false);
     }
 
-#ifdef UNIV_ENCRYPT_DEBUG
-    {
-      std::ostringstream msg;
-
-      msg << "Master Key ID: " << master_key_id;
-      msg << " hex: {";
-      ut_print_buf_hex(msg, master_key, KEY_LEN);
-      msg << "}";
-
-      ib::info(ER_IB_MSG_838) << msg.str();
-    }
-#endif /* UNIV_ENCRYPT_DEBUG */
-
     /* Decrypt tablespace key and iv. */
     auto len = my_aes_decrypt(ptr, sizeof(key_info), key_info, master_key,
                               KEY_LEN, my_aes_256_ecb, nullptr, false);
 
     if (master_key_id == DEFAULT_MASTER_KEY_ID) {
       ut_free(master_key);
+      /* Re-encrypt tablespace key with current master key */
     } else {
       my_free(master_key);
     }
@@ -695,19 +667,17 @@ bool Encryption::decode_encryption_info(byte *key, byte *iv,
   } else {
     ut_ad(version == VERSION_3);
     /* Skip master Key and server UUID*/
-    ptr += sizeof(uint32);
+    ptr += sizeof(uint32_t);
     ptr += SERVER_UUID_LEN;
 
     /* Get tablespace key information. */
     memcpy(key_info, ptr, sizeof(key_info));
   }
-
-  /* Check checksum bytes. */
   ptr += sizeof(key_info);
 
-  crc1 = mach_read_from_4(ptr);
-  crc2 = ut_crc32(key_info, sizeof(key_info));
-
+  /* Check checksum bytes. */
+  uint32_t crc1 = mach_read_from_4(ptr);
+  uint32_t crc2 = ut_crc32(key_info, sizeof(key_info));
   if (crc1 != crc2) {
     /* This check could fail only while decrypting key. */
     ut_ad(decrypt_key);
@@ -725,25 +695,27 @@ bool Encryption::decode_encryption_info(byte *key, byte *iv,
   /* Get tablespace iv */
   memcpy(iv, key_info + KEY_LEN, KEY_LEN);
 
-#ifdef UNIV_ENCRYPT_DEBUG
-  {
-    std::ostringstream msg;
+  if (decrypt_key) {
+    /* Update server uuid and master key id in encryption metadata */
+    if (master_key_id > s_master_key_id) {
+      s_master_key_id = master_key_id;
+      memcpy(s_uuid, srv_uuid, sizeof(s_uuid) - 1);
+    }
 
-    ut_print_buf_hex(msg, key, KEY_LEN);
+#ifndef UNIV_HOTBACKUP
+    if (master_key_id == DEFAULT_MASTER_KEY_ID &&
+        space_id != dict_sys_t::s_invalid_space_id) {
+      /* Tablespace key needs to be reencrypted with master key */
 
-    ib::info(ER_IB_MSG_840) << "Key: {" << msg.str() << "}";
-  }
-  {
-    std::ostringstream msg;
-
-    ut_print_buf_hex(msg, iv, KEY_LEN);
-    ib::info(ER_IB_MSG_841) << "IV: {" << msg.str() << "}";
-  }
-#endif /* UNIV_ENCRYPT_DEBUG */
-
-  if (decrypt_key && (s_master_key_id < master_key_id)) {
-    s_master_key_id = master_key_id;
-    memcpy(s_uuid, srv_uuid, sizeof(s_uuid) - 1);
+      if (!srv_master_thread_is_active()) {
+        /* Note down this space and rotate key at the end of recovery */
+        s_tablespaces_to_reencrypt.push_back(space_id);
+      } else {
+        /* This tablespace might not be loaded yet. It's tablepace key will be
+        reencrypted with new master key once it is loaded in fil_ibd_open() */
+      }
+    }
+#endif
   }
 
   return (true);
@@ -1445,8 +1417,9 @@ bool Encryption::check_keyring() noexcept {
   bool ret = false;
   byte *master_key = nullptr;
 
-  /* During bootstrap, default master key is used */
   if (s_master_key_id == DEFAULT_MASTER_KEY_ID) {
+    /* This is the first time encryption is being used or till now no encrypted
+    tablespace is loaded. */
     static bool checked = false;
     if (checked) {
       return true;

storage/innobase/srv/srv0start.cc
@@ -695,7 +695,8 @@ static dberr_t srv_undo_tablespace_read_encryption(pfs_os_file_t fh,
 
   byte key[Encryption::KEY_LEN];
   byte iv[Encryption::KEY_LEN];
-  if (fsp_header_get_encryption_key(space->flags, key, iv, first_page)) {
+  Encryption_key e_key{key, iv};
+  if (fsp_header_get_encryption_key(space->flags, e_key, first_page)) {
     fsp_flags_set_encryption(space->flags);
     err = fil_set_encryption(space->id, Encryption::AES, key, iv);
     ut_ad(err == DB_SUCCESS);
@@ -2912,13 +2913,6 @@ void srv_dict_recover_on_restart() {
   if (srv_force_recovery < SRV_FORCE_NO_IBUF_MERGE) {
     srv_sys_tablespaces_open = true;
   }
-}
-
-/* If early redo/undo log encryption processing is done. */
-bool is_early_redo_undo_encryption_done() {
-  /* Early redo/undo encryption is done during post recovery before purge
-  thread is started. */
-  return (srv_start_state_is_set(SRV_START_STATE_PURGE));
 }
 
 /** Start purge threads. During upgrade we start

mysql-test/suite/innodb/r/bootstrap.result
@@ -5,6 +5,7 @@
 # Start mysqld without the possibility to create undo tablespaces
 # because a directory exists named 'undo_002'.
 Pattern "\[ERROR\] .*MY-\d+.* File .*ibdata1: 'open' returned OS error 71. Cannot continue operation" found
+auto.cnf
 undo_002
 # Start mysqld to create tablespaces according to my.cnf
 # Backup the datadir

mysql-test/suite/innodb/t/bootstrap.test
@@ -44,6 +44,7 @@ let SEARCH_PATTERN=\[ERROR\] .*MY-\\d+.* File .*ibdata1: 'open' returned OS erro
 --force-rmdir $bugdir/data/mysql
 --list_files $bugdir/data
 --rmdir $bugdir/data/undo_002
+--remove_file $bugdir/data/auto.cnf
 
 --echo # Start mysqld to create tablespaces according to my.cnf
 --exec $MYSQLD $args --initialize-insecure

mysql-test/suite/perfschema/r/error_log.result
@@ -92,6 +92,7 @@ AND error_code NOT IN("MY-011243", "MY-011240", "MY-011245", "MY-010931",
 ORDER BY logged, error_code;
 prio	error_code	subsystem
 System	MY-010116	Server
+Warning	MY-010075	Server
 System	MY-013576	InnoDB
 Note	MY-012944	InnoDB
 Note	MY-012945	InnoDB
@@ -129,7 +130,6 @@ Note	MY-012357	InnoDB
 Note	MY-012356	InnoDB
 Note	MY-010006	Server
 Note	MY-011332	Server
-Warning	MY-010075	Server
 Note	MY-012487	InnoDB
 Note	MY-012488	InnoDB
 Note	MY-010303	Server

sql/mysqld.cc
@@ -5200,9 +5200,10 @@ static int generate_server_uuid() {
   String uuid;
 
   /*
-    To be able to run this from boot, we allocate a temporary THD
+    To be able to run this from boot, we allocate a temporary THD,
+    since plugins are not yet loaded we pass false to temporary THD.
    */
-  if (!(thd = new THD)) {
+  if (!(thd = new THD(false))) {
     LogErr(ERROR_LEVEL, ER_NO_THD_NO_UUID);
     return 1;
   }
@@ -5982,6 +5983,16 @@ static int init_server_components() {
   /* This limits ability to configure SSL library through config options */
   init_ssl();
 
+  /*
+   Each server should have one UUID. We will create it automatically, if it
+   does not exist. It should be initialized before opening binlog file. Because
+   server's uuid will be stored into the new binlog file.
+  */
+  if (init_server_auto_options()) {
+    LogErr(ERROR_LEVEL, ER_CANT_CREATE_UUID);
+    unireg_abort(MYSQLD_ABORT_EXIT);
+  }
+
   /*Load early plugins */
   if (plugin_register_early_plugins(&remaining_argc, remaining_argv,
                                     (is_help_or_validate_option())
@@ -6309,16 +6320,6 @@ static int init_server_components() {
   query_logger.set_log_file(QUERY_LOG_GENERAL);
   if (opt_general_log && query_logger.reopen_log_file(QUERY_LOG_GENERAL))
     opt_general_log = false;
-
-  /*
-    Each server should have one UUID. We will create it automatically, if it
-    does not exist. It should be initialized before opening binlog file. Because
-    server's uuid will be stored into the new binlog file.
-  */
-  if (init_server_auto_options()) {
-    LogErr(ERROR_LEVEL, ER_CANT_CREATE_UUID);
-    unireg_abort(MYSQLD_ABORT_EXIT);
-  }
 
   /*
     Set the default storage engines

storage/innobase/clone/clone0snapshot.cc
@@ -743,8 +743,9 @@ bool Clone_Snapshot::encrypt_key_in_log_header(byte *log_header,
   auto encryption_info = log_header + offset;
 
   /* Get log Encryption Key and IV. */
+  Encryption_key e_key{encryption_key, encryption_iv};
   auto success = Encryption::decode_encryption_info(
-      &encryption_key[0], &encryption_iv[0], encryption_info, false);
+      dict_sys_t::s_invalid_space_id, e_key, encryption_info, false);
 
   if (success) {
     /* Encrypt with master key and fill encryption information. */
@@ -765,8 +766,9 @@ bool Clone_Snapshot::encrypt_key_in_header(const page_size_t &page_size,
   auto encryption_info = page_data + offset;
 
   /* Get tablespace Encryption Key and IV. */
+  Encryption_key e_key{encryption_key, encryption_iv};
   auto success = Encryption::decode_encryption_info(
-      &encryption_key[0], &encryption_iv[0], encryption_info, false);
+      dict_sys_t::s_invalid_space_id, e_key, encryption_info, false);
   if (!success) {
     return (false);
   }

storage/innobase/fil/fil0fil.cc
@@ -1631,6 +1631,9 @@ class Fil_system {
   @return the number of tablespaces that failed to rotate. */
   size_t encryption_rotate() MY_ATTRIBUTE((warn_unused_result));
 
+  /** Reencrypt tablespace keys by current master key. */
+  void encryption_reencrypt(std::vector<space_id_t> &sid_vector);
+
   /** Detach a space object from the tablespace memory cache.
   Closes the tablespace files but does not delete them.
   There must not be any pending I/O's or flushes on the files.
@@ -5840,7 +5843,7 @@ dberr_t fil_ibd_open(bool validate, fil_type_t purpose, space_id_t space_id,
     ut_ad(df.space_version() == DD_SPACE_CURRENT_SPACE_VERSION);
   }
 
-  /* We are done validating.  If the tablespace is already open,
+  /* We are done validating. If the tablespace is already open,
   return success. */
   if (space != nullptr) {
     return DB_SUCCESS;
@@ -5892,6 +5895,18 @@ dberr_t fil_ibd_open(bool validate, fil_type_t purpose, space_id_t space_id,
 
     if (err != DB_SUCCESS) {
       return DB_ERROR;
+    }
+
+    /* If tablespace is encrypted with default master key and server has already
+    started, rotate it now. */
+    if (df.m_encryption_master_key_id == Encryption::DEFAULT_MASTER_KEY_ID &&
+        /* There is no dependency on master thread but we are trying to check if
+        server is in initial phase or not. */
+        srv_master_thread_is_active()) {
+      /* Reencrypt tablespace key */
+      std::vector<space_id_t> sid;
+      sid.push_back(space->id);
+      fil_encryption_reencrypt(sid);
     }
   }
 
@@ -9353,13 +9368,6 @@ bool Fil_shard::needs_encryption_rotate(fil_space_t *space) {
     return false;
   }
 
-  /* Skip the tablespace when it's in default key status,
-  since it's the first server startup after bootstrap,
-  and the server uuid is not ready yet. */
-  if (Encryption::get_master_key_id() == Encryption::DEFAULT_MASTER_KEY_ID) {
-    return false;
-  }
-
   DBUG_EXECUTE_IF(
       "ib_encryption_rotate_skip",
       ib::info(ER_IB_MSG_INJECT_FAILURE, "ib_encryption_rotate_skip");
@@ -9439,8 +9447,56 @@ size_t Fil_system::encryption_rotate() {
   return fail_count;
 }
 
+void Fil_system::encryption_reencrypt(
+    std::vector<space_id_t> &space_id_vector) {
+  /* If there are no tablespaces to reencrypt, return true. */
+  if (space_id_vector.empty()) {
+    return;
+  }
+
+  size_t fail_count = 0;
+  size_t rotate_count = 0;
+  byte encrypt_info[Encryption::INFO_SIZE];
+
+  /* This operation is done either post recovery or when the first time
+  tablespace is loaded. */
+
+  for (auto &space_id : space_id_vector) {
+    fil_space_t *space = fil_space_get(space_id);
+    ut_ad(space != nullptr);
+    ut_ad(FSP_FLAGS_GET_ENCRYPTION(space->flags));
+
+    /* Rotate this encrypted tablespace. */
+    mtr_t mtr;
+    mtr_start(&mtr);
+    memset(encrypt_info, 0, Encryption::INFO_SIZE);
+    bool rotate_ok = fsp_header_rotate_encryption(space, encrypt_info, &mtr);
+    ut_ad(rotate_ok);
+    mtr_commit(&mtr);
+
+    if (rotate_ok) {
+      ++rotate_count;
+      if (fsp_is_ibd_tablespace(space_id)) {
+        if (fsp_is_file_per_table(space_id, space->flags)) {
+          ib::info(ER_IB_MSG_REENCRYPTED_TABLESPACE_KEY, space->name);
+        } else {
+          ib::info(ER_IB_MSG_REENCRYPTED_GENERAL_TABLESPACE_KEY, space->name);
+        }
+      }
+    } else {
+      ++fail_count;
+    }
+  }
+
+  /* The operation should finish successfully for all tablespaces */
+  ut_a(fail_count == 0);
+}
+
 size_t fil_encryption_rotate() { return (fil_system->encryption_rotate()); }
 
+void fil_encryption_reencrypt(std::vector<space_id_t> &sid_vector) {
+  fil_system->encryption_reencrypt(sid_vector);
+}
 #endif /* !UNIV_HOTBACKUP */
 
 /** Constructor
@@ -10943,7 +10999,9 @@ byte *fil_tablespace_redo_encryption(byte *ptr, const byte *end,
   byte iv[Encryption::KEY_LEN] = {0};
   byte key[Encryption::KEY_LEN] = {0};
 
-  if (!Encryption::decode_encryption_info(key, iv, encryption_ptr, true)) {
+  Encryption_key e_key{key, iv};
+  if (!Encryption::decode_encryption_info(space_id, e_key, encryption_ptr,
+                                          true)) {
     recv_sys->found_corrupt_log = true;
 
     ib::warn(ER_IB_MSG_364)

percona/percona-server

commit 67c684691477cc52fb69175417d56ccfc075dae9

PS-7811 merge: Merge MySQL 8.0.26 (fix redo log default key generation)
https://jira.percona.com/browse/PS-7811

The redo log default encryption key fails after changes
in https://github.com/mysql/mysql-server/commit/9f51d4fb13b590715929988ab15a3f19ffd43ee6.
With this change server_uuid is already initialized at the point
where log file gets created.

storage/innobase/fil/fil0crypt.cc
@@ -4091,7 +4091,8 @@ redo_log_key *redo_log_keys::load_key_version(THD *thd, const char *uuid,
   char *key_type = nullptr;
   byte *rkey = nullptr;
 
-  std::string redo_key_with_ver{get_key_name(uuid, version)};
+  std::string redo_key_with_ver{get_key_name(
+      version != REDO_LOG_ENCRYPT_NO_VERSION ? uuid : "", version)};
   if (innobase::encryption::read_key(redo_key_with_ver.c_str(), &rkey, &klen,
                                      &key_type) != 1 ||
       rkey == nullptr || strncmp(key_type, "AES", 4) != 0) {
@@ -4197,10 +4198,23 @@ redo_log_key *redo_log_keys::generate_and_store_new_key(THD *thd) {
   return rk;
 }
 
+void redo_log_keys::unload_old_keys() noexcept {
+  if (m_keys.size() == 0) {
+    return;
+  }
+  redo_log_key *last = &(--m_keys.end())->second;
+  for (auto &item : m_keys) {
+    if (&item.second != last) {
+      item.second.present = false;
+      memset(item.second.key, 0, Encryption::KEY_LEN);
+    }
+  }
+}
+
 redo_log_key *redo_log_keys::fetch_or_generate_default_key(THD *thd) {
   ut_ad(m_keys.empty());
   std::string default_key_name{get_key_name("", 0)};
-  ut_ad(strlen(server_uuid) == 0);
+  ut_ad(strlen(server_uuid) != 0);
   ut_ad(default_key_name.length() == strlen("percona_redo:0") &&
         memcmp(default_key_name.c_str(), "percona_redo:0",
                default_key_name.length()) == 0);
@@ -4248,19 +4262,6 @@ redo_log_key *redo_log_keys::fetch_or_generate_default_key(THD *thd) {
   rk->version = 0;
   rk->present = true;
   return rk;
-}
-
-void redo_log_keys::unload_old_keys() noexcept {
-  if (m_keys.size() == 0) {
-    return;
-  }
-  redo_log_key *last = &(--m_keys.end())->second;
-  for (auto &item : m_keys) {
-    if (&item.second != last) {
-      item.second.present = false;
-      memset(item.second.key, 0, Encryption::KEY_LEN);
-    }
-  }
 }
 
 redo_log_keys redo_log_key_mgr;

storage/innobase/include/fil0crypt.h
@@ -387,6 +387,8 @@ class redo_log_keys final {
   MY_NODISCARD
   redo_log_key *generate_and_store_new_key(THD *thd);
 
+  void unload_old_keys() noexcept;
+
   /** Fetch if exists default percona_redo key, in case it does not
   exist - generate it in keyring. Should be used when server_uuid is not
   yet available
@@ -394,8 +396,6 @@ class redo_log_keys final {
   @return percona_redo default key */
   MY_NODISCARD
   redo_log_key *fetch_or_generate_default_key(THD *thd);
-
-  void unload_old_keys() noexcept;
 
  private:
   /**

storage/innobase/os/os0enc.cc
@@ -825,6 +825,8 @@ bool Encryption::fill_encryption_info(byte *key, byte *iv, byte *encrypt_info,
           0);
   }
 
+  default_master_key_used = (master_key_id == DEFAULT_MASTER_KEY_ID);
+
   /* Encryption info to be filled in following format
     --------------------------------------------------------------------------
    | Magic bytes | master key id | server uuid | tablespace key|iv | checksum |

storage/innobase/srv/srv0start.cc
@@ -440,7 +440,7 @@ static dberr_t create_log_files(char *logfilename, size_t dirnamelen, lsn_t lsn,
 
     log_space->flags |= FSP_FLAGS_MASK_ENCRYPTION;
 
-    ut_ad(strlen(server_uuid) == 0);
+    ut_ad(strlen(server_uuid) != 0);
     redo_log_key *mkey =
         redo_log_key_mgr.fetch_or_generate_default_key(nullptr);
 
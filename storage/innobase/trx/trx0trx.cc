mysql/mysql-server

commit e66d48b0c73d5fec278f81784bd5697502990263

BUG#27933068 USE DIFFERENT MUTEX TO PROTECT TRX_SYS->SERIALISATION_LIST
This is an optimization patch, which reduces contention on the trx_sys_t::mutex
by introducing a new mutex - the trx_sys_t::serialisation_mutex.

The new mutex protects the trx_sys_t::serialisation_list and replaces the
trx_sys_t::mutex when trx->no is being assigned.

This is a modified version of the contribution patch which was created by Zhai Weixiang.

Modifications:
1. Periodical write of max_trx_id to the transaction system header page is modified.
2. The trx_get_serial_no() is called when we do not hold trx_sys_t::mutex.
3. Members in trx_sys_t are rearranged, so they are grouped by mutex that protects them.
4. The new mutex received its own latch_id.
5. InnoDB relies on rw_trx_max_id instead of max_trx_id in few places.
6. The min_active_id is updated only when it really changes.

RB: 19712
Reviewed-by: Debarun Banerjee <debarun.banerjee@oracle.com>

storage/innobase/trx/trx0trx.cc
@@ -107,11 +107,11 @@ flush fails, and T never gets committed, also T2 will never get
 committed.
 @param[in,out]  trx         The transaction for which will be committed in
                             memory
-@param[in]      serialized  true if serialisation log was written. Affects the
+@param[in]      serialised  true if serialisation log was written. Affects the
                             list of things we need to clean up during
                             trx_erase_lists.
 */
-static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialized);
+static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialised);
 
 /** Set flush observer for the transaction
 @param[in,out]	trx		transaction struct
@@ -1019,6 +1019,10 @@ void trx_lists_init_at_db_start(void) {
   for (TrxIdSet::iterator it = trx_sys->rw_trx_set.begin(); it != end; ++it) {
     ut_ad(it->m_trx->in_rw_trx_list);
 
+    if (it->m_trx->id > trx_sys->rw_max_trx_id) {
+      trx_sys->rw_max_trx_id = it->m_trx->id;
+    }
+
     if (it->m_trx->state == TRX_STATE_ACTIVE ||
         it->m_trx->state == TRX_STATE_PREPARED) {
       trx_sys->rw_trx_ids.push_back(it->m_id);
@@ -1191,15 +1195,15 @@ void trx_assign_rseg_temp(trx_t *trx) {
       srv_read_only_mode ? nullptr : get_next_temp_rseg();
 
   if (trx->id == 0) {
-    mutex_enter(&trx_sys->mutex);
+    trx_sys_mutex_enter();
 
-    trx->id = trx_sys_get_new_trx_id();
+    trx->id = trx_sys_allocate_trx_id();
 
     trx_sys->rw_trx_ids.push_back(trx->id);
 
     trx_sys->rw_trx_set.insert(TrxTrack(trx->id, trx));
 
-    mutex_exit(&trx_sys->mutex);
+    trx_sys_mutex_exit();
   }
 }
 
@@ -1286,10 +1290,11 @@ static void trx_start_low(
 
     /* Temporary rseg is assigned only if the transaction
     updates a temporary table */
+    DEBUG_SYNC_C("trx_sys_before_assign_id");
 
     trx_sys_mutex_enter();
 
-    trx->id = trx_sys_get_new_trx_id();
+    trx->id = trx_sys_allocate_trx_id();
 
     trx_sys->rw_trx_ids.push_back(trx->id);
 
@@ -1321,7 +1326,7 @@ static void trx_start_low(
 
         ut_ad(!srv_read_only_mode);
 
-        trx->id = trx_sys_get_new_trx_id();
+        trx->id = trx_sys_allocate_trx_id();
 
         trx_sys->rw_trx_ids.push_back(trx->id);
 
@@ -1353,6 +1358,45 @@ static void trx_start_low(
   MONITOR_INC(MONITOR_TRX_ACTIVE);
 }
 
+/** Assigns the trx->no and add the transaction to the serialisation_list.
+Skips adding to the serialisation_list if the transaction is read-only, in
+which case still the trx->no is assigned.
+@param[in,out]  trx   the modified transaction
+@return true if added to the serialisation_list (non read-only trx) */
+static bool trx_add_to_serialisation_list(trx_t *trx) {
+  ut_ad(trx_sys_serialisation_mutex_own());
+
+  trx->no = trx_sys_allocate_trx_no();
+
+  if (trx->read_only) {
+    return false;
+  }
+
+  UT_LIST_ADD_LAST(trx_sys->serialisation_list, trx);
+
+  if (UT_LIST_GET_LEN(trx_sys->serialisation_list) == 1) {
+    trx_sys->serialisation_min_trx_no.store(trx->no);
+  }
+
+  return true;
+}
+
+/** Erases transaction from the serialisation_list.
+@param[in,out]  trx   the transaction to erase */
+static void trx_erase_from_serialisation_list(trx_t *trx) {
+  ut_ad(trx_sys_serialisation_mutex_own());
+
+  UT_LIST_REMOVE(trx_sys->serialisation_list, trx);
+
+  if (UT_LIST_GET_LEN(trx_sys->serialisation_list) > 0) {
+    trx_sys->serialisation_min_trx_no.store(
+        UT_LIST_GET_FIRST(trx_sys->serialisation_list)->no);
+
+  } else {
+    trx_sys->serialisation_min_trx_no.store(trx_sys_get_next_trx_id_or_no());
+  }
+}
+
 /** Set the transaction serialisation number.
  @return true if the transaction number was added to the serialisation_list. */
 static bool trx_serialisation_number_get(
@@ -1378,20 +1422,12 @@ static bool trx_serialisation_number_get(
     temp_rseg = temp_rseg_undo_ptr->rseg;
   }
 
-  trx_sys_mutex_enter();
+  trx_sys_serialisation_mutex_enter();
 
-  trx->no = trx_sys_get_new_trx_id();
+  added_trx_no = trx_add_to_serialisation_list(trx);
 
   /* Update the latest transaction number. */
   ut_d(trx_sys->rw_max_trx_no = trx->no);
-
-  /* Track the minimum serialisation number. */
-  if (!trx->read_only) {
-    UT_LIST_ADD_LAST(trx_sys->serialisation_list, trx);
-    added_trx_no = true;
-  } else {
-    added_trx_no = false;
-  }
 
   /* If the rollack segment is not empty then the
   new trx_t::no can't be less than any trx_t::no
@@ -1411,18 +1447,18 @@ static bool trx_serialisation_number_get(
 
     mutex_enter(&purge_sys->pq_mutex);
 
-    /* This is to reduce the pressure on the trx_sys_t::mutex
-    though in reality it should make very little (read no)
-    difference because this code path is only taken when the
-    rbs is empty. */
+    /* This is to reduce the pressure on the trx_sys_t::serialisation_mutex
+    though in reality it should make very little (read no) difference
+    because this code path is only taken when the rbs is empty. */
 
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
 
     purge_sys->purge_queue->push(elem);
 
     mutex_exit(&purge_sys->pq_mutex);
+
   } else {
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
   }
 
   return (added_trx_no);
@@ -1700,33 +1736,28 @@ Erase the transaction from running transaction lists and serialization
 list. Active RW transaction list of a MVCC snapshot(ReadView::prepare)
 won't include this transaction after this call. All implicit locks are
 also released by this call as trx is removed from rw_trx_list.
-@param[in]	trx		Transaction to erase, must have an ID > 0
-@param[in]	serialised	true if serialisation log was written
-@param[in]	gtid_desc	GTID information to persist */
-static void trx_erase_lists(trx_t *trx, bool serialised, Gtid_desc &gtid_desc) {
+@param[in]	trx		Transaction to erase, must have an ID > 0 */
+static void trx_erase_lists(trx_t *trx) {
   ut_ad(trx->id > 0);
   ut_ad(trx_sys_mutex_own());
 
-  if (serialised) {
-    /* Add GTID to be persisted to disk table. It must be done ...
-    1.After the transaction is marked committed in undo. Otherwise
-      GTID might get committed before the transaction commit on disk.
-    2.Before it is removed from serialization list. Otherwise the transaction
-      undo could get purged before persisting GTID on disk table. */
-    if (gtid_desc.m_is_set) {
-      auto &gtid_persistor = clone_sys->get_gtid_persistor();
-      gtid_persistor.add(gtid_desc);
-    }
-    /* Do after adding GTID as trx_sys mutex could now be released and
-    re-acquired while adding GTID and we still need to satisfy condition
-    [2] above. */
-    UT_LIST_REMOVE(trx_sys->serialisation_list, trx);
-  }
-
   trx_ids_t::iterator it = std::lower_bound(trx_sys->rw_trx_ids.begin(),
                                             trx_sys->rw_trx_ids.end(), trx->id);
+
+  const bool update_min_active = it == trx_sys->rw_trx_ids.begin();
+
   ut_ad(*it == trx->id);
   trx_sys->rw_trx_ids.erase(it);
+
+  /* We update min_active_trx_id only if needed (seperate cache line). */
+  if (update_min_active) {
+    trx_id_t min_id = trx_sys->rw_trx_ids.empty() ? trx_sys->rw_max_trx_id + 1
+                                                  : trx_sys->rw_trx_ids.front();
+
+    ut_ad(min_id > trx_sys->min_active_trx_id.load());
+
+    trx_sys->min_active_trx_id.store(min_id);
+  }
 
   if (trx->read_only || trx->rsegs.m_redo.rseg == nullptr) {
     ut_ad(!trx->in_rw_trx_list);
@@ -1741,26 +1772,15 @@ static void trx_erase_lists(trx_t *trx, bool serialised, Gtid_desc &gtid_desc) {
   }
 
   trx_sys->rw_trx_set.erase(TrxTrack(trx->id));
-
-  /* Set minimal active trx id. */
-  trx_id_t min_id = trx_sys->rw_trx_ids.empty() ? trx_sys->max_trx_id
-                                                : trx_sys->rw_trx_ids.front();
-
-  trx_sys->min_active_id.store(min_id);
 }
 
-static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialized) {
+static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialised) {
   check_trx_state(trx);
   ut_ad(trx_state_eq(trx, TRX_STATE_ACTIVE) ||
         trx_state_eq(trx, TRX_STATE_PREPARED));
 
   bool trx_sys_latch_is_needed =
       (trx->id > 0) || trx_state_eq(trx, TRX_STATE_PREPARED);
-
-  /* Check and get GTID to be persisted. Do it outside trx_sys mutex. */
-  Gtid_desc gtid_desc;
-  auto &gtid_persistor = clone_sys->get_gtid_persistor();
-  gtid_persistor.get_gtid_info(trx, gtid_desc);
 
   if (trx_sys_latch_is_needed) {
     trx_sys_mutex_enter();
@@ -1770,7 +1790,7 @@ static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialized) {
     /* For consistent snapshot, we need to remove current
     transaction from running transaction id list for mvcc
     before doing commit and releasing locks. */
-    trx_erase_lists(trx, serialized, gtid_desc);
+    trx_erase_lists(trx);
   }
 
   if (trx_state_eq(trx, TRX_STATE_PREPARED)) {
@@ -1801,6 +1821,38 @@ static void trx_release_impl_and_expl_locks(trx_t *trx, bool serialized) {
 
   if (trx_sys_latch_is_needed) {
     trx_sys_mutex_exit();
+  }
+
+  /* It is important to remove the transaction from the serialisation list
+  after it is erased from the rw_trx_ids / rw_trx_list (not before!).
+  Otherwise a read-view could be created, which could still pretend that
+  changes of this transaction are invisible, but related undo records could
+  become purged (because trx->no would no longer protect them). */
+
+  if (serialised) {
+    /* Check and get GTID to be persisted. Do it outside mutex. */
+    Gtid_desc gtid_desc;
+    auto &gtid_persistor = clone_sys->get_gtid_persistor();
+    gtid_persistor.get_gtid_info(trx, gtid_desc);
+
+    trx_sys_serialisation_mutex_enter();
+
+    /* Add GTID to be persisted to disk table. It must be done ...
+    1.After the transaction is marked committed in undo. Otherwise
+      GTID might get committed before the transaction commit on disk.
+    2.Before it is removed from serialization list. Otherwise the transaction
+      undo could get purged before persisting GTID on disk table. */
+    if (gtid_desc.m_is_set) {
+      auto &gtid_persistor = clone_sys->get_gtid_persistor();
+      /* The gtid_persistor.add(gtid_desc) might release and re-acquire
+      the trx_sys_serialisation_mutex, so must be called before trx is
+      removed from the serialisation_list - to satisfy [2]. */
+      gtid_persistor.add(gtid_desc);
+    }
+
+    trx_erase_from_serialisation_list(trx);
+
+    trx_sys_serialisation_mutex_exit();
   }
 
   lock_trx_release_locks(trx);
@@ -1857,8 +1909,8 @@ written */
   } else {
     trx_release_impl_and_expl_locks(trx, serialised);
 
-    /* Remove the transaction from the list of active
-    transactions now that it no longer holds any user locks. */
+    /* Removed the transaction from the list of active transactions.
+    It no longer holds any user locks. */
 
     ut_ad(trx_state_eq(trx, TRX_STATE_COMMITTED_IN_MEMORY));
     DEBUG_SYNC_C("after_trx_committed_in_memory");
@@ -2026,6 +2078,8 @@ void trx_commit_low(trx_t *trx, mtr_t *mtr) {
 
   if (mtr != nullptr) {
     mtr->set_sync();
+
+    DEBUG_SYNC_C("trx_sys_before_assign_no");
 
     serialised = trx_write_serialisation_history(trx, mtr);
 
@@ -2813,11 +2867,16 @@ static void trx_prepare(trx_t *trx) /*!< in/out: transaction */
   trx_sys_mutex_enter();
   trx->state = TRX_STATE_PREPARED;
   trx_sys->n_prepared_trx++;
+  trx_sys_mutex_exit();
+
   /* Add GTID to be persisted to disk table, if needed. */
   if (gtid_desc.m_is_set) {
+    /* The gtid_persistor.add() might release and re-acquire the mutex. */
+    trx_sys_serialisation_mutex_enter();
     gtid_persistor.add(gtid_desc);
+    trx_sys_serialisation_mutex_exit();
   }
-  trx_sys_mutex_exit();
+
   /*--------------------------------------*/
 
   /* Reset after successfully adding GTID to in memory table. */
@@ -3180,10 +3239,12 @@ void trx_set_rw_mode(trx_t *trx) /*!< in/out: transaction that is RW */
 
   ut_ad(trx->rsegs.m_redo.rseg != nullptr);
 
-  mutex_enter(&trx_sys->mutex);
+  DEBUG_SYNC_C("trx_sys_before_assign_id");
+
+  trx_sys_mutex_enter();
 
   ut_ad(trx->id == 0);
-  trx->id = trx_sys_get_new_trx_id();
+  trx->id = trx_sys_allocate_trx_id();
 
   trx_sys->rw_trx_ids.push_back(trx->id);
 
@@ -3198,7 +3259,7 @@ void trx_set_rw_mode(trx_t *trx) /*!< in/out: transaction that is RW */
 
   ut_d(trx->in_rw_trx_list = true);
 
-  mutex_exit(&trx_sys->mutex);
+  trx_sys_mutex_exit();
 }
 
 void trx_kill_blocking(trx_t *trx) {

mysql-test/suite/innodb/include/initialize_datadir.inc
@@ -1,0 +1,7 @@
+let $bootstrap_sql_file = $MYSQLTEST_VARDIR/tmp/bootstrap.sql;
+write_file $bootstrap_sql_file;
+CREATE DATABASE test;
+EOF
+let $MYSQLD_ARGS = --console --log-error-verbosity=3 --datadir=$MYSQLD_DATADIR $MYSQLD_EXTRA_ARGS > $MYSQLD_ERROR_LOG 2>&1 ;
+--exec $MYSQLD_CMD $MYSQLD_ARGS --initialize-insecure --init-file=$bootstrap_sql_file
+--remove_file $bootstrap_sql_file

mysql-test/suite/innodb/r/max_trx_id.result
@@ -1,0 +1,82 @@
+# Initialize new data directory and restart MySQL on that data directory.
+# The new data directory will be initialized by instance which has:
+# --debug="d,trx_sys_low_trx_id_write_margin" so it will do reservations
+# of trx_id values every 8 trx_id values (instead of every 256 values).
+# This way we could collect enough connections in mtr test runner to fill
+# concurrently the reservation interval (limit for mtr connections is 128).
+# Restart MySQL using the new data directory.
+# restart: --datadir=tmp/max_trx_id --log-error=my_restart.err --debug=d,trx_sys_low_trx_id_write_margin --innodb-log-file-size=4M
+# Create 7 connections which keep creating and comitting transactions.
+# Pause the created connections before trx_sys_mutex_enter() is called
+# before assigning trx->id.
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting1 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 1;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting2 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 2;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting3 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 3;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting4 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 4;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting5 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 5;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting6 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 6;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting7 WAIT_FOR con_go NO_CLEAR_EVENT";
+SET @id = 7;
+START TRANSACTION;
+UPDATE t SET c = c+1 WHERE a = @id;
+# Wait.
+# Wait for 1....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting1";
+# Wait for 2....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting2";
+# Wait for 3....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting3";
+# Wait for 4....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting4";
+# Wait for 5....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting5";
+# Wait for 6....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting6";
+# Wait for 7....
+SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting7";
+# Create a connection which will hang inside trx_sys_write_max_trx_id()
+# when holding the trx_sys_serialisation_mutex.
+SET DEBUG_SYNC = "trx_sys_write_max_trx_id__ser SIGNAL write_waiting WAIT_FOR write_go";
+CALL trx_loop(-1);
+# Wait until the created connection is hanging.
+SET DEBUG_SYNC = "now WAIT_FOR write_waiting";
+# Let the paused connections go, to check if they could corrupt the database.
+# Do not allow them to write max_trx_id to the transaction system header page.
+SET GLOBAL DEBUG = "+d,trx_sys_write_max_trx_id__all_blocked";
+SET DEBUG_SYNC = "now SIGNAL con_go";
+# Wait a little bit, giving a chance to corrupt the database to the resumed
+# connections (by writing down too high trx_id to some page / row).
+# Kill and restart MySQL to see if the database is not corrupted.
+# Kill and restart: --datadir=tmp/max_trx_id --log-error=my_restart.err --debug=d,trx_sys_low_trx_id_write_margin --innodb-log-file-size=4M
+# Update rows using a new transaction. It would crash if the transaction
+# received a trx->id which had been assigned to some other transaction
+# and written to some page/row before MySQL crashed.
+UPDATE t SET c = c + 1;
+# Check if there is no corruption if MySQL instantly crashed after using
+# the new transaction (after restart).
+# Kill and restart: --datadir=tmp/max_trx_id --log-error=my_restart.err --debug=d,trx_sys_low_trx_id_write_margin --innodb-log-file-size=4M
+# Update all rows using a new transaction to see if there is no assertion
+# failure on trx_id for any of the related pages.
+UPDATE t SET c = c + 1;
+# Turn off MySQL and remove its data directory.
+# Kill the server
+# Restart usual instance of MySQL which is used for mtr tests.
+# restart:

mysql-test/suite/innodb/t/max_trx_id.test
@@ -1,0 +1,132 @@
+--source include/have_debug.inc
+--source include/have_debug_sync.inc
+--source include/count_sessions.inc
+
+--echo # Initialize new data directory and restart MySQL on that data directory.
+--echo # The new data directory will be initialized by instance which has:
+--echo # --debug="d,trx_sys_low_trx_id_write_margin" so it will do reservations
+--echo # of trx_id values every 8 trx_id values (instead of every 256 values).
+--echo # This way we could collect enough connections in mtr test runner to fill
+--echo # concurrently the reservation interval (limit for mtr connections is 128).
+
+let $tmp_dir = $MYSQLTEST_VARDIR/tmp;
+let $MYSQLD_DATADIR = $tmp_dir/max_trx_id;
+let $MYSQLD_ERROR_LOG = $tmp_dir/my_restart.err;
+let $MYSQLD_EXTRA_ARGS = --debug="d,trx_sys_low_trx_id_write_margin" --innodb-log-file-size=4M;
+--source ../include/initialize_datadir.inc
+
+--echo # Restart MySQL using the new data directory.
+let $restart_parameters = restart: --datadir=$MYSQLD_DATADIR --log-error=$MYSQLD_ERROR_LOG $MYSQLD_EXTRA_ARGS;
+--replace_result $MYSQLD_ERROR_LOG my_restart.err $MYSQLD_DATADIR tmp/max_trx_id
+--source include/restart_mysqld.inc
+
+--disable_query_log
+CREATE TABLE t (a INT NOT NULL AUTO_INCREMENT PRIMARY KEY, c INT NOT NULL, INDEX(c)) ENGINE=InnoDB;
+
+--let $N=7
+
+--let $i=1
+while ($i <= $N) {
+  INSERT INTO t (a, c) VALUES (NULL, 0);
+  --inc $i
+}
+
+INSERT INTO t (a, c) VALUES (-1, 0);
+
+DELIMITER |;
+CREATE PROCEDURE trx_loop(IN id INT)
+BEGIN
+    WHILE 1 DO
+      START TRANSACTION;
+        UPDATE t SET c = c + 1 WHERE a = id;
+      COMMIT;
+      START TRANSACTION;
+        SELECT * FROM t WHERE a = id FOR UPDATE;
+      COMMIT;
+    END WHILE;
+END|
+DELIMITER ;|
+--enable_query_log
+
+--echo # Create $N connections which keep creating and comitting transactions.
+--echo # Pause the created connections before trx_sys_mutex_enter() is called
+--echo # before assigning trx->id.
+
+--let $i=1
+while ($i <= $N) {
+  --connect(con_id$i,localhost,root,,)
+  --eval SET DEBUG_SYNC = "trx_sys_before_assign_id SIGNAL con_id_waiting$i WAIT_FOR con_go NO_CLEAR_EVENT"
+  --eval SET @id = $i
+
+  START TRANSACTION;
+  --send UPDATE t SET c = c+1 WHERE a = @id
+  --inc $i
+}
+
+--echo # Wait.
+
+--connection default
+--let $i=1
+while ($i <= $N) {
+  --echo # Wait for $i....
+  --eval SET DEBUG_SYNC = "now WAIT_FOR con_id_waiting$i"
+  --inc $i
+}
+
+--echo # Create a connection which will hang inside trx_sys_write_max_trx_id()
+--echo # when holding the trx_sys_serialisation_mutex.
+
+--connect(con_hang,localhost,root,,)
+SET DEBUG_SYNC = "trx_sys_write_max_trx_id__ser SIGNAL write_waiting WAIT_FOR write_go";
+--send CALL trx_loop(-1)
+
+--echo # Wait until the created connection is hanging.
+
+--connection default
+SET DEBUG_SYNC = "now WAIT_FOR write_waiting";
+
+--echo # Let the paused connections go, to check if they could corrupt the database.
+--echo # Do not allow them to write max_trx_id to the transaction system header page.
+
+SET GLOBAL DEBUG = "+d,trx_sys_write_max_trx_id__all_blocked";
+SET DEBUG_SYNC = "now SIGNAL con_go";
+
+--echo # Wait a little bit, giving a chance to corrupt the database to the resumed
+--echo # connections (by writing down too high trx_id to some page / row).
+
+--sleep 5
+
+--echo # Kill and restart MySQL to see if the database is not corrupted.
+
+--replace_result $MYSQLD_ERROR_LOG my_restart.err $MYSQLD_DATADIR tmp/max_trx_id
+--source include/kill_and_restart_mysqld.inc
+
+--echo # Update rows using a new transaction. It would crash if the transaction
+--echo # received a trx->id which had been assigned to some other transaction
+--echo # and written to some page/row before MySQL crashed.
+
+UPDATE t SET c = c + 1;
+
+--echo # Check if there is no corruption if MySQL instantly crashed after using
+--echo # the new transaction (after restart).
+
+--replace_result $MYSQLD_ERROR_LOG my_restart.err $MYSQLD_DATADIR tmp/max_trx_id
+--source include/kill_and_restart_mysqld.inc
+
+--echo # Update all rows using a new transaction to see if there is no assertion
+--echo # failure on trx_id for any of the related pages.
+
+UPDATE t SET c = c + 1;
+
+--echo # Turn off MySQL and remove its data directory.
+
+--source include/kill_mysqld.inc
+--force-rmdir $MYSQLD_DATADIR
+--remove_file $MYSQLD_ERROR_LOG
+
+--echo # Restart usual instance of MySQL which is used for mtr tests.
+
+let $restart_parameters = restart:;
+--source include/start_mysqld.inc
+
+--source include/wait_until_count_sessions.inc

storage/innobase/clone/clone0repl.cc
@@ -49,20 +49,20 @@ void Clone_persist_gtid::add(const Gtid_desc &gtid_desc) {
   if (!is_active() || gtid_table_persistor == nullptr) {
     return;
   }
-  ut_ad(trx_sys_mutex_own());
+  ut_ad(trx_sys_serialisation_mutex_own());
 
   /* If too many GTIDs are accumulated, wait for all to get flushed. */
   while (check_max_gtid_threshold()) {
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
     wait_flush(false, false, nullptr);
-    trx_sys_mutex_enter();
+    trx_sys_serialisation_mutex_enter();
     /* Starvation is possible theoretically here, if the active list gets
     filled to threshold before a transaction could get hold of the mutex
     after being woken up. Practically not feasible as the number of waiting
     transactions at any point in time is far less than the threshold. */
   }
 
-  ut_ad(trx_sys_mutex_own());
+  ut_ad(trx_sys_serialisation_mutex_own());
   /* Get active GTID list */
   auto &current_gtids = get_active_list();
 
@@ -78,9 +78,9 @@ void Clone_persist_gtid::add(const Gtid_desc &gtid_desc) {
 
   DBUG_EXECUTE_IF("dont_compress_gtid_table", {
     /* For predictable outcome of mtr test we flush the GTID immediately. */
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
     wait_flush(false, false, nullptr);
-    trx_sys_mutex_enter();
+    trx_sys_serialisation_mutex_enter();
   });
 }
 
@@ -481,7 +481,7 @@ void Clone_persist_gtid::flush_gtids(THD *thd) {
 
   bool explicit_request = m_explicit_request.load();
 
-  trx_sys_mutex_enter();
+  trx_sys_serialisation_mutex_enter();
   /* Get oldest transaction number that is yet to be committed. Any transaction
   with lower transaction number is committed and is added to GTID list. */
   auto oldest_trx_no = trx_sys_oldest_trx_no();
@@ -492,7 +492,7 @@ void Clone_persist_gtid::flush_gtids(THD *thd) {
     /* Switch active list and get the previous list to write to disk table. */
     auto flush_list_number = switch_active_list();
     /* Exit trx mutex during write to table. */
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
     err = write_to_table(flush_list_number, table_gtid_set, sid_map);
     m_flush_in_progress.store(false);
     /* Compress always after recovery, if GTIDs are added. */
@@ -501,7 +501,7 @@ void Clone_persist_gtid::flush_gtids(THD *thd) {
       ib::info(ER_IB_CLONE_GTID_PERSIST) << "GTID compression after recovery. ";
     }
   } else {
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
   }
 
   if (is_recovery) {
@@ -545,7 +545,7 @@ void Clone_persist_gtid::flush_gtids(THD *thd) {
 }
 
 bool Clone_persist_gtid::check_max_gtid_threshold() {
-  ut_ad(trx_sys_mutex_own());
+  ut_ad(trx_sys_serialisation_mutex_own());
   /* Allow only one GTID to flush at a time. */
   DBUG_EXECUTE_IF("dont_compress_gtid_table",
                   { return m_num_gtid_mem.load() > 0; });

storage/innobase/handler/ha_innodb.cc
@@ -743,6 +743,7 @@ static PSI_mutex_info all_innodb_mutexes[] = {
     PSI_MUTEX_KEY(rtr_path_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(rtr_ssn_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(trx_sys_mutex, 0, 0, PSI_DOCUMENT_ME),
+    PSI_MUTEX_KEY(trx_sys_serialisation_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(zip_pad_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(master_key_id_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(sync_array_mutex, 0, 0, PSI_DOCUMENT_ME),

storage/innobase/include/clone0repl.h
@@ -232,7 +232,7 @@ class Clone_persist_gtid {
 
   /** @return current active GTID list */
   Gitd_info_list &get_active_list() {
-    ut_ad(trx_sys_mutex_own());
+    ut_ad(trx_sys_serialisation_mutex_own());
     return (get_list(m_active_number));
   }
 
@@ -253,7 +253,7 @@ class Clone_persist_gtid {
   @param[in]	compress	request compression of GTID table
   @return flush list number to track and wait for flush to complete. */
   uint64_t request_immediate_flush(bool compress) {
-    trx_sys_mutex_enter();
+    trx_sys_serialisation_mutex_enter();
     /* We want to flush all GTIDs. */
     uint64_t request_number = m_active_number.load();
     /* If no GTIDs added to active, wait for previous index. */
@@ -262,7 +262,7 @@ class Clone_persist_gtid {
       --request_number;
     }
     m_flush_request_number = request_number;
-    trx_sys_mutex_exit();
+    trx_sys_serialisation_mutex_exit();
 
     if (compress) {
       m_explicit_request.store(true);
@@ -289,7 +289,7 @@ class Clone_persist_gtid {
   /** Switch active GTID list. */
   uint64_t switch_active_list() {
     /* Switch active list under transaction system mutex. */
-    ut_ad(trx_sys_mutex_own());
+    ut_ad(trx_sys_serialisation_mutex_own());
     uint64_t flush_number = m_active_number;
     ++m_active_number;
     m_compression_gtid_counter += m_num_gtid_mem;

percona/percona-server

commit 851eb843cca6fcea13a2ff4d6fabf0bc1b3ceb9c

PS-7811 merge: Merge MySQL 8.0.26 (fix update of trx_sys->min_active_trx_id)
https://jira.percona.com/browse/PS-7811

When assigning a `trx->preallocated_id` the value of
`trx_sys->min_active_trx_id` needs to be updated.

This issue does not manifest in MySQL because the use of
`trx->preallocated_id` is a functionality of Percona and the
preallocated_id is not provided in ascending order.

This issue was detected by a failing assertion introduced in
e66d48b0c73d and occurred when testing `main.clone_consistent_snapshot`.

storage/innobase/trx/trx0trx.cc
@@ -1221,17 +1221,20 @@ void trx_assign_rseg_durable(trx_t *trx) {
 /** Assign an id for this RW transaction and insert it into trx_sys->rw_trx_ids
 @param trx	transaction to assign an id for */
 static void trx_assign_id_for_rw(trx_t *trx) {
-  ut_ad(mutex_own(&trx_sys->mutex));
+  ut_ad(trx_sys_mutex_own());
 
   trx->id =
       trx->preallocated_id ? trx->preallocated_id : trx_sys_allocate_trx_id();
 
   if (trx->preallocated_id) {
-    // Maintain ordering in rw_trx_ids
-    trx_sys->rw_trx_ids.insert(
-        std::upper_bound(trx_sys->rw_trx_ids.begin(), trx_sys->rw_trx_ids.end(),
-                         trx->id),
-        trx->id);
+    // preallocated_id might not be received in ascending order,
+    // so we need to maintain ordering in rw_trx_ids and update min_active_trx_id
+    auto upper_bound_it = std::upper_bound(trx_sys->rw_trx_ids.begin(), trx_sys->rw_trx_ids.end(), trx->id);
+    auto insert_it = trx_sys->rw_trx_ids.insert(upper_bound_it, trx->id);
+    if (insert_it == trx_sys->rw_trx_ids.begin()) {
+      trx_sys->min_active_trx_id.store(trx->id);
+    }
+
   } else {
     // The id is known to be greatest
     trx_sys->rw_trx_ids.push_back(trx->id);
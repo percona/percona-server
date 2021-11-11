5d20e846b6c2a6b4b199c5280066398ca48bd92f
Luis Donoso <luis.donoso@percona.com>
2021-11-11 10:33:02 +0100

    [sql/range_optimizer] PS-7949 Migrate percona sql/opt_range.cc
    https://jira.percona.com/browse/PS-7949
    
    ***
    
    mysql/796ea5f2444495159f52ae93e9492f394ae2a0e7 deleted the file and
    refactored its functionality.
    
    This commits implements existing Percona changes in the new files.
    
    -------------------------------------------------------------------------
    
    PS-6995 (Optimizer not picking best execution plan for Primary Key lookup)
    
    https://jira.percona.com/browse/PS-6995
    
    In some cases optimizer prefers to do a Full Table Scan instead of a
    PK range scan.
    Introduced a new optimizer switch to allow the user to reduce the cost
    of a range scan. If favor_range_scan switch is on, the cost for range
    plan will be reduced by 90%. Patch has been provided by Yves Trudeau.
    
    -------------------------------------------------------------------------
    
    PS-8047 - Migrate range_scan.cc and range_scan_desc.cc
    
    https://jira.percona.com/browse/PS-8047
    
    ***
    
    Migrate Percona's changes on the following files (that have been deleted
    on mysql@736fb2d85c1afab6d49c0feeaf6bea094b61cd50) to the new files:
    - `sql/range_optimizer/range_scan.cc`
    - `sql/range_optimizer/range_scan_desc.cc`

--- sql/range_optimizer/index_skip_scan.cc
+++ sql/range_optimizer/index_skip_scan.cc
@@ -278,6 +278,9 @@ int IndexSkipScanIterator::DoRead() {
   }};
   do {
     if (!is_prefix_valid) {
+      // This change is necessary for MyRocks PS-7116.
+      table()->file->set_end_range(NULL, handler::RANGE_SCAN_ASC);
+
       if (!seen_first_key) {
         if (eq_prefix_key_parts == 0) {
           result = table()->file->ha_index_first(table()->record[0]);

bf2eff0281f227cef1a9a8bc20d8f6869cd44378
Luis Donoso <luis.donoso@percona.com>
2022-01-04 14:57:59 +0100

    [encryption] PS-7989: Migrate innodb_encrypt_online_alter_logs to 8.0.27
    https://jira.percona.com/browse/PS-7989
    
    ***
    
    Problem:
    
    `log_tmp_block_decrypt` uses the argument `offs` to work out the
    initialization vector which was used in encryption. If the provided
    `offs` is not the same as the one use in encryption the decrypted data
    will be corrupted.
    
    MySQL 8.0.27 has refactored the code so the `size` and `offs` are not
    the same during write as during read so the current
    `log_tmp_block_decrypt` is returning corrupted data.
    
    Another upstream change, that affects these functions, is that the size
    is not longer a multiple of MY_AES_BLOCK_SIZE. This causes we can't
    neither use `my_aes_mode::ECB`.
    
    The issue was discovered when running
    `keyring_vault.innodb_online_alter_encryption`.
    
    Solution:
    
    Save the offsets used when the data was written and use those same
    offset to read the data.
    
    Testing:
    
    - Affected tests:
    
    - innodb.innodb_online_alter_encryption
    - keyring_vault.innodb_online_alter_encryption
    - keyring_vault.innodb_row_log_encryption
    - innodb.innodb_row_log_encryption
    
    - Run the `innodb` suite with this feature on:
    
    ```
    --mysqld=--innodb-encrypt-online-alter-logs=ON \
    --suite=innodb
    ```
    
    If no encryption is set, AES is used.

--- storage/innobase/include/ddl0impl-file-reader.h
+++ storage/innobase/include/ddl0impl-file-reader.h
@@ -52,13 +52,14 @@ struct File_reader : private ut::Non_copyable {
   @param[in] buffer_size        Size of file buffer for reading.
   @param[in] range              Offsets of the chunk to read */
   File_reader(const Unique_os_file_descriptor &file, dict_index_t *index,
-              size_t buffer_size, const Range &range,
-              space_id_t space_id) noexcept
+              size_t buffer_size, const Range &range, space_id_t space_id,
+              const Write_offsets &write_offsets) noexcept
       : m_index(index),
         m_file(file),
         m_range(range),
         m_buffer_size(buffer_size),
-        m_space_id(space_id) {
+        m_space_id(space_id),
+        m_write_offsets(write_offsets) {
     ut_a(range.first < range.second);
     ut_a(m_buffer_size > 0);
     ut_a(m_index != nullptr);
@@ -115,6 +116,8 @@ struct File_reader : private ut::Non_copyable {
   @return DB_SUCCESS or error code. */
   [[nodiscard]] dberr_t next() noexcept;
 
+  [[nodiscard]] size_t get_read_len_next() const noexcept;
+
  public:
   using Offsets = std::vector<ulint, ut::allocator<ulint>>;
 
@@ -131,16 +134,16 @@ struct File_reader : private ut::Non_copyable {
   const Unique_os_file_descriptor &m_file;
 
  private:
-  using Bounds = std::pair<const byte *, const byte *>;
-
   /* Coordinates of the chunk that this file reader can read the rows from */
   Range m_range{};
 
+  /** Last read length */
+  os_offset_t m_read_len{};
+
+  byte *get_io_buffer_end() { return m_io_buffer.first + m_read_len; }
+
   /** Pointer current offset within file buffer. */
   const byte *m_ptr{};
-
-  /** File buffer bounds. */
-  Bounds m_bounds{};
 
   /** Auxiliary buffer for records that span across pages. */
   byte *m_aux_buf{};
@@ -162,6 +165,8 @@ struct File_reader : private ut::Non_copyable {
 
   /** Space id used to encrypt the file */
   space_id_t m_space_id{};
+
+  Write_offsets m_write_offsets;
 
   /** Number of rows read from the file. */
   uint64_t m_n_rows_read{};

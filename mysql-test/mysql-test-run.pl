314ee604862d21c8cc279291883ba9a3716915a3
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-04-21 11:16:16 +0200

    [mysql-test-run] PS-8164: Add "--only-combinations=" to mysql-test-run.pl
    "--only-combinations" allows to restrict combinations to given names separated by "," e.g.:
    
    `mysql-test-run.pl --sute=main --only-combinations=innodb_intrinsic_table`
    `mysql-test-run.pl --sute=rocksdb --only-combinations=write_committed,write_prepared`

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -351,6 +351,7 @@ our $opt_warnings  = 1;
 
 our @opt_cases;
 our @opt_combinations;
+our $opt_only_combinations;
 our @opt_extra_bootstrap_opt;
 our @opt_extra_mysqld_opt;
 our @opt_extra_mysqltest_opt;
@@ -1730,6 +1731,7 @@ sub command_line_setup {
     # Control what test suites or cases to run
     'big-test'                           => \$opt_big_test,
     'combination=s'                      => \@opt_combinations,
+    'only-combinations=s'                => \$opt_only_combinations,
     'do-suite=s'                         => \$opt_do_suite,
     'do-test=s'                          => \&collect_option,
     'force'                              => \$opt_force,
@@ -7958,6 +7960,7 @@ Options to control what engine/variation to run
 
   combination=<opt>     Use at least twice to run tests with specified
                         options to mysqld.
+  only-combinations=<name>  Use only given combinations, separated by ",".
   compress              Use the compressed protocol between client and server.
   async-client          Use async-client with select() to run the test case
   cursor-protocol       Use the cursor protocol between client and server

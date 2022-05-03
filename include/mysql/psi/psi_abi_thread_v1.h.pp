4cedc076f7323b643cf188ba5e4efb0ee7d0f5ba
Luis Donoso <luis.donoso@percona.com>
2022-05-03 13:27:39 +0200

    PS-8185 - Fix struct PSI_thread_service_v6
    https://jira.percona.com/browse/PS-8185
    
    ***
    
    Add Percona callbacks to the new version of the interface.

--- include/mysql/psi/psi_abi_thread_v1.h.pp
+++ include/mysql/psi/psi_abi_thread_v1.h.pp
@@ -268,6 +268,9 @@ struct PSI_thread_service_v6 {
   set_thread_command_v1_t set_thread_command;
   set_connection_type_v1_t set_connection_type;
   set_thread_start_time_v1_t set_thread_start_time;
+  set_thread_start_time_usec_v4_t set_thread_start_time_usec;
+  set_thread_rows_sent_v4_t set_thread_rows_sent;
+  set_thread_rows_examined_v4_t set_thread_rows_examined;
   set_thread_info_v1_t set_thread_info;
   set_thread_secondary_engine_v6_t set_thread_secondary_engine;
   set_thread_resource_group_v1_t set_thread_resource_group;

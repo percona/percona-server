44a6ba8cfbe627c222eefe5d9749e82391f9e19d
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-07 13:18:56 +0200

    [storage/rocksdb] PS-269: Initial Percona Server 8.0.12 tree
    ------------------------------------------------------------
    
    Fix clang 15 -Wunused-but-set-variable warnings in MyRocks (#1304)
    
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/4475c80fb7eb300fedd338061909acbbb4d8848b
    PS-8951: Merge percona-202305 (https://jira.percona.com/browse/PS-8951)
    
    Summary:
    - Make infinite debug loop counter volatile. Otherwise it's undefined behavior and/or the loop might be optimized away.
    - Remove another write-only variable
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1304
    
    Reviewed By: hermanlee
    
    Differential Revision: D45446127
    
    Pulled By: sunshine-Chun
    
    fbshipit-source-id: c0dfddfa76b4004ae61d62b5de305cb3264afb1c

---
+++ storage/rocksdb/rdb_cf_manager.cc
@@ -0,0 +1,190 @@
+/*
+   Copyright (c) 2014, SkySQL Ab
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+#ifdef USE_PRAGMA_IMPLEMENTATION
+#pragma implementation  // gcc: Class implementation
+#endif
+
+/* This C++ files header file */
+#include "./rdb_cf_manager.h"
+
+/* MyRocks header files */
+#include "./ha_rocksdb.h"
+#include "./ha_rocksdb_proto.h"
+#include "./rdb_psi.h"
+
+namespace myrocks {
+
+/* Check if ColumnFamily name says it's a reverse-ordered CF */
+bool Rdb_cf_manager::is_cf_name_reverse(const char *const name) {
+  /* nullptr means the default CF is used.. (TODO: can the default CF be
+   * reverse?) */
+  return (name && !strncmp(name, "rev:", 4));
+}
+
+void Rdb_cf_manager::init(
+    std::unique_ptr<Rdb_cf_options> cf_options,
+    std::vector<rocksdb::ColumnFamilyHandle *> *const handles) {
+#ifdef HAVE_PSI_INTERFACE
+  mysql_mutex_init(rdb_cfm_mutex_key, &m_mutex, MY_MUTEX_INIT_FAST);
+#else
+  mysql_mutex_init(&m_mutex, MY_MUTEX_INIT_FAST);
+#endif
+
+  assert(cf_options != nullptr);
+  assert(handles != nullptr);
+  assert(handles->size() > 0);
+
+  m_cf_options = std::move(cf_options);
+
+  for (auto cfh : *handles) {
+    assert(cfh != nullptr);
+    m_cf_name_map[cfh->GetName()] = cfh;
+    m_cf_id_map[cfh->GetID()] = cfh;
+  }
+}
+
+void Rdb_cf_manager::cleanup() {
+  for (auto it : m_cf_name_map) {
+    delete it.second;
+  }
+  mysql_mutex_destroy(&m_mutex);
+  m_cf_options = nullptr;
+}
+
+/*
+  @brief
+  Find column family by name. If it doesn't exist, create it
+
+  @detail
+    See Rdb_cf_manager::get_cf
+*/
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_or_create_cf(rocksdb::DB *const rdb,
+                                 const std::string &cf_name_arg) {
+  assert(rdb != nullptr);
+
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  if (cf_name_arg == PER_INDEX_CF_NAME) {
+    // per-index column families is no longer supported.
+    my_error(ER_PER_INDEX_CF_DEPRECATED, MYF(0));
+    return nullptr;
+  }
+
+  const std::string &cf_name =
+      cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  const auto it = m_cf_name_map.find(cf_name);
+
+  if (it != m_cf_name_map.end()) {
+    cf_handle = it->second;
+  } else {
+    /* Create a Column Family. */
+    rocksdb::ColumnFamilyOptions opts;
+    m_cf_options->get_cf_options(cf_name, &opts);
+
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "Creating a column family %s",
+                    cf_name.c_str());
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    write_buffer_size=%ld",
+                    opts.write_buffer_size);
+    LogPluginErrMsg(INFORMATION_LEVEL, 0, "    target_file_size_base=%" PRIu64,
+                    opts.target_file_size_base);
+
+    const rocksdb::Status s =
+        rdb->CreateColumnFamily(opts, cf_name, &cf_handle);
+
+    if (s.ok()) {
+      m_cf_name_map[cf_handle->GetName()] = cf_handle;
+      m_cf_id_map[cf_handle->GetID()] = cf_handle;
+    } else {
+      cf_handle = nullptr;
+    }
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+/*
+  Find column family by its cf_name.
+*/
+
+rocksdb::ColumnFamilyHandle *
+Rdb_cf_manager::get_cf(const std::string &cf_name_arg) const {
+  rocksdb::ColumnFamilyHandle *cf_handle;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  std::string cf_name = cf_name_arg.empty() ? DEFAULT_CF_NAME : cf_name_arg;
+
+  const auto it = m_cf_name_map.find(cf_name);
+  cf_handle = (it != m_cf_name_map.end()) ? it->second : nullptr;
+
+  if (!cf_handle) {
+    LogPluginErrMsg(WARNING_LEVEL, 0, "Column family '%s' not found.",
+                    cf_name.c_str());
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+rocksdb::ColumnFamilyHandle *Rdb_cf_manager::get_cf(const uint32_t &id) const {
+  rocksdb::ColumnFamilyHandle *cf_handle = nullptr;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  const auto it = m_cf_id_map.find(id);
+  if (it != m_cf_id_map.end())
+    cf_handle = it->second;
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return cf_handle;
+}
+
+std::vector<std::string> Rdb_cf_manager::get_cf_names(void) const {
+  std::vector<std::string> names;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+  for (auto it : m_cf_name_map) {
+    names.push_back(it.first);
+  }
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return names;
+}
+
+std::vector<rocksdb::ColumnFamilyHandle *>
+Rdb_cf_manager::get_all_cf(void) const {
+  std::vector<rocksdb::ColumnFamilyHandle *> list;
+
+  RDB_MUTEX_LOCK_CHECK(m_mutex);
+
+  for (auto it : m_cf_id_map) {
+    assert(it.second != nullptr);
+    list.push_back(it.second);
+  }
+
+  RDB_MUTEX_UNLOCK_CHECK(m_mutex);
+
+  return list;
+}
+
+}  // namespace myrocks

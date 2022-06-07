7f4c5fe6e54c36ce113e7e16d40163e82b89b607
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-07 13:12:47 +0200

    [MTR TESTS] PS-269 (Initial Percona Server 8.0.12 tree)
    -----
    PS-8979: Merge MySQL 8.2.0
    
    Rerecord rocksdb.type_decimal due to optimizer improvements in
    https://github.com/mysql/mysql-server/commit/2a57e948ca9b238262161ae854119f60c8fd347e.
    
    Binlog decryption code removed along with binlog_57_decryption test suite and
    rpl.percona_rpl_encryption_master_binlog_ps_encrypted,
    rpl.percona_rpl_encryption_slave_binlog_ps_encrypted tests.
    
    -----
    PS-8979: Merge MySQL 8.2.0 (rerecord group_by_fd_ps_prot test)
    
    https://jira.percona.com/browse/PS-8979
    
    Change similar to upstream's https://github.com/mysql/mysql-server/commit/5d33cd43ce58c7767678c8de8d2e4fc25b4f5000
    
    -----
    PS-8979: Merge MySQL 8.2.0 (fix unstable binlog_mysqlbinlog_4g_start_position test)
    
    https://jira.percona.com/browse/PS-8979
    
    -----
    PS-8725 : Assertion `m_opened_table != nullptr' failed in temptable::Handler::opened_table_validate()
    
    Test case only.
    
    Original problem was fixed in MySQL Server 8.0.35 by the
    commit a1d235e83859ed39610b2d39eb1a7642964de72c (Bug#35105404
    "Server crashes with signal 11 and asserts in temptable code.").
    However, this commit doesn't contain a public test case.
    
    There was a GitHub contribution with a fix and test case
    by Nicholas Othieno, but we have chosen upstream's version
    of fix and a somewhat different test case.
    
    (cherry picked from commit e2b1f1939ed846bda45b2823bb2303cd89b1fb76)
    
    -----
    
    PS-9071: Merge MySQL 8.3.0 (exclude some tests from ASAN tests run)
    
    https://perconadev.atlassian.net/browse/PS-9071
    
    Exclude the following tests from ASAN tests run:
    - binlog_mysqlbinlog_4g_start_position is a big tests and requires even
      more time with ASAN.
    - buffered_error_log, processlist_tid - report internal lib issues.
    
    -----
    
    PS-9218: Merge MySQL 8.4.0 (fix innodb.tablespace_encrypt_9)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    - Added CR_CONNECTION_ERROR as expected code on server crash
    - Removed connection_id() comparison before and after crash (for
      different connections)
    
    -----
    
    PS-9218: Merge MySQL 8.4.0 (fix terminology in replication tests)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    https://github.com/mysql/mysql-server/commit/44a77b5ba29994f8d3634b62c2825481574fc057
    
    -----
    
    PS-9218: Merge MySQL 8.4.0 (replace keyring_file plugin with component_keyring_file in MTR tests)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    -----
    
    PS-9218: Merge MySQL 8.4.0 (MTR test fixes)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    - re-record innodb.log_encrypt_kill test
    - re-record rocksdb.index_merge_rocksdb2 due to https://github.com/mysql/mysql-server/commit/6ae1d0216c416c3053a301c7b4df2ce4c0bf212d

--- mysql-test/mysql-test-run.pl
+++ mysql-test/mysql-test-run.pl
@@ -303,6 +303,9 @@ our @DEFAULT_SUITES = qw(
   component_keyring_file
   component_connection_control
 
+  component_encryption_udf
+  percona
+  percona_innodb
   percona-pam-for-mysql
   component_masking_functions
   procfs

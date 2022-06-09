bab9c7173c401d4192aaa85870c33e0f00f2b3a4
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 22:13:37 +0200

    [storage/heap] PS-269: Initial Percona Server 8.0.12 tree

--- storage/heap/hp_update.cc
+++ storage/heap/hp_update.cc
@@ -29,7 +29,8 @@
 #include "my_inttypes.h"
 #include "storage/heap/heapdef.h"
 
-int heap_update(HP_INFO *info, const uchar *old, const uchar *heap_new) {
+int heap_update(HP_INFO *info, const uchar *old_record,
+                const uchar *new_record) {
   HP_KEYDEF *keydef, *end, *p_lastinx;
   uchar *pos;
   bool auto_key_changed = false;
@@ -39,30 +40,47 @@ int heap_update(HP_INFO *info, const uchar *old, const uchar *heap_new) {
   test_active(info);
   pos = info->current_ptr;
 
-  if (info->opt_flag & READ_CHECK_USED && hp_rectest(info, old))
+  if (info->opt_flag & READ_CHECK_USED && hp_rectest(info, old_record))
     return my_errno(); /* Record changed */
+
+  uint old_chunk_count, new_chunk_count;
+  hp_get_encoded_data_length(*share, old_record, &old_chunk_count);
+  hp_get_encoded_data_length(*share, new_record, &new_chunk_count);
+
+  if (new_chunk_count > old_chunk_count) {
+    /* extend the old chunkset size as necessary, but do not shrink yet */
+    if (hp_reallocate_chunkset(&share->recordspace, new_chunk_count, pos)) {
+      return my_errno(); /* Out of memory or table space */
+    }
+  }
+
   if (--(share->records) < share->blength >> 1) share->blength >>= 1;
   share->changed = 1;
 
   p_lastinx = share->keydef + info->lastinx;
   for (keydef = share->keydef, end = keydef + share->keys; keydef < end;
        keydef++) {
-    if (hp_rec_key_cmp(keydef, old, heap_new)) {
-      if ((*keydef->delete_key)(info, keydef, old, pos, keydef == p_lastinx) ||
-          (*keydef->write_key)(info, keydef, heap_new, pos))
+    if (hp_rec_key_cmp(keydef, old_record, new_record)) {
+      if ((*keydef->delete_key)(info, keydef, old_record, pos,
+                                keydef == p_lastinx) ||
+          (*keydef->write_key)(info, keydef, new_record, pos))
         goto err;
       if (share->auto_key == (uint)(keydef - share->keydef + 1))
         auto_key_changed = true;
     }
   }
 
-  memcpy(pos, heap_new, (size_t)share->reclength);
+  hp_copy_record_data_to_chunkset(*share, new_record, pos);
   if (++(share->records) == share->blength) share->blength += share->blength;
+  if (new_chunk_count < old_chunk_count) {
+    /* Shrink the chunkset to its new size */
+    hp_reallocate_chunkset(&share->recordspace, new_chunk_count, pos);
+  }
 
 #if !defined(NDEBUG) && defined(EXTRA_HEAP_DEBUG)
   DBUG_EXECUTE("check_heap", heap_check_heap(info, 0););
 #endif
-  if (auto_key_changed) heap_update_auto_increment(info, heap_new);
+  if (auto_key_changed) heap_update_auto_increment(info, new_record);
   return 0;
 
 err:
@@ -70,7 +88,7 @@ err:
     info->errkey = (int)(keydef - share->keydef);
     if (keydef->algorithm == HA_KEY_ALG_BTREE) {
       /* we don't need to delete non-inserted key from rb-tree */
-      if ((*keydef->write_key)(info, keydef, old, pos)) {
+      if ((*keydef->write_key)(info, keydef, old_record, pos)) {
         if (++(share->records) == share->blength)
           share->blength += share->blength;
         return my_errno();
@@ -78,14 +96,18 @@ err:
       keydef--;
     }
     while (keydef >= share->keydef) {
-      if (hp_rec_key_cmp(keydef, old, heap_new)) {
-        if ((*keydef->delete_key)(info, keydef, heap_new, pos, 0) ||
-            (*keydef->write_key)(info, keydef, old, pos))
+      if (hp_rec_key_cmp(keydef, old_record, new_record)) {
+        if ((*keydef->delete_key)(info, keydef, new_record, pos, 0) ||
+            (*keydef->write_key)(info, keydef, old_record, pos))
           break;
       }
       keydef--;
     }
   }
   if (++(share->records) == share->blength) share->blength += share->blength;
+  if (new_chunk_count > old_chunk_count) {
+    /* Shrink the chunkset to its original size */
+    hp_reallocate_chunkset(&share->recordspace, old_chunk_count, pos);
+  }
   return my_errno();
 } /* heap_update */

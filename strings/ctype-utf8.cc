99c1b52653638bec55e2c4326523c51fdeba74ea
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 18:52:43 +0200

    [strings] PS-269: Initial Percona Server 8.0.12 tree

--- strings/ctype-utf8.cc
+++ strings/ctype-utf8.cc
@@ -5030,7 +5030,7 @@ static inline size_t my_strnxfrm_unicode_tmpl(const CHARSET_INFO *cs,
   uchar *dst0 = dst;
   uchar *de = dst + dstlen;
   const uchar *se = src + srclen;
-  assert(src);
+  assert(src || srclen == 0);
 
   // We manually hoist this if test out of the loop; seemingly GCC
   // (at least 6.1.1) isn't smart enough to do it on its own.
@@ -5134,7 +5134,7 @@ size_t my_strnxfrm_unicode_full_bin(const CHARSET_INFO *cs, uchar *dst,
   uchar *de = dst + dstlen;
   const uchar *se = src + srclen;
 
-  assert(src);
+  assert(src || srclen == 0);
   assert(cs->state & MY_CS_BINSORT);
 
   for (; dst < de && nweights; nweights--) {

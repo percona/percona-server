90bd5ee0c101e05654da015f5cbaf6b56c1ccac0
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 20:53:49 +0200

    [unittest] PS-269: Initial Percona Server 8.0.12 tree
    -----------------------------------------------------------
    
    PS-7949 Fix memry leak in gcs_xcom_control_interface-t test
    
    https://jira.percona.com/browse/PS-7949
    
    -----------------------------------------------------------
    
    PS-7949 Fix memory leaks in temptable::Allocator tests
    
    https://jira.percona.com/browse/PS-7949
    
    -----------------------------------------------------------
    
    PS-8979: Fix linking issue using `WITH_VALGRIND=ON`
    
    Fix the following issue when `lld` is not detected or `-DUSE_LD_LLD=OFF` is used:
    ```
    /usr/bin/ld: CMakeFiles/merge_large_tests-t.dir/dd_sdi-t.cc.o: warning: relocation against `_ZThn56_N2dd10Index_impl15se_private_dataEv' in read-only section `.text._ZN12sdi_unittestL11mock_dd_objEPN2dd5IndexEPNS0_6ColumnE'
    /usr/bin/ld: CMakeFiles/merge_large_tests-t.dir/dd_sdi-t.cc.o: relocation R_X86_64_PC32 against symbol `_ZThn56_N2dd11Column_impl15se_private_dataEv' can not be used when making a PIE object; recompile with -fPIE
    ```
    
    (cherry picked from commit c75e96abb48e36307fa9d7f3da593d52d73e7a08)
    
    -----------------------------------------------------------
    
    PS-9071: Merge MySQL 8.3.0 (fix test_routing_direct tests)
    
    https://perconadev.atlassian.net/browse/PS-9071
    
    Tests lack knowledge of some Percona and debug build specific system
    variable names.
    Updated known system variables lists for tests to be able to categorize
    them properly.
    
    -----------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix gcc-14 build)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    -----------------------------------------------------------
    
    PS-9328 merge: Merge 8.4.2 - Fixed gcc-12 RelWithDebInfo compilation warnings (#5406)
    
    https://perconadev.atlassian.net/browse/PS-9328
    
    Suppressed '-Wrestrict' warnings in selected GIS and Router unit tests.
    
    /usr/include/c++/12/bits/char_traits.h:435:56: error: ‘void* __builtin_memcpy(void*, const void*, long unsigned int)’ accessing 9223372036854775810 or more bytes at offsets [2, 9223372036854775807] and 1 may overlap up to 9223372036854775813 bytes at offset -3 [-Werror=restrict]
      435 |         return static_cast<char_type*>(__builtin_memcpy(__s1, __s2, __n));
          |                                        ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~
    
    -----------------------------------------------------------
    
    PS-9470 : Code refresh for PS 8.4.3 - post merge MTR fix (routertest_integration_routing_direct issue)
    
    Fixed failure of routertest_integration_routing_direct unit test which
    were caused by server failing on assert while trying to set component
    system variable (i.e. percona_telemetry.grace_interval) using quoting
    which is wrong for a component variable name
    (i.e. set @@SESSION.`percona_telemetry.grace_interval`=100;)
    
    This issue affects PS 8.4.2-2 as well but was not noticed during its
    release process.
    
    The real cause of the issue is a problem in the server code.
    On the one hand, server code interprets `percona_telemetry.grace_interval`
    as a single quoted identifier, and therefore plugin variable name in SET
    statement, but on the other hand, attempt to find a plugin variable
    using find_dynamic_system_variable() with such a name doesn't fail,
    and returns corresponding component variable instead, breaking
    asserts in the code.
    
    This issue probably also affects Upstream, but is likely not to get
    much attention there since there are no components loaded by default
    there (unlike in Percona Server where we have Telemetry component).
    
    The problem affects debug builds of server only and doesn't affect
    release builds. OTOH fixing it correctly is likely to require fairly
    intrusive code change.
    
    Taking into the account the above and since, as far as I can see, there
    is no good reason for router unit test to use quoting for system variable
    names, this patch choses not to fix the problem in server code, but
    instead, workarounds it by changing unit test not to use quoting for
    system variable names.

--- router/tests/integration/test_routing_direct.cc
+++ router/tests/integration/test_routing_direct.cc
@@ -3570,7 +3570,7 @@ TEST_P(ConnectionTest, classic_protocol_replay_session_trackers) {
     }
 
     std::ostringstream oss;
-    oss << "SET @@SESSION." << std::quoted(var[0], '`') << "=";
+    oss << "SET @@SESSION." << var[0] << "=";
 
     if (var[1].empty()) {
       if (var[0] == "innodb_ft_user_stopword_table") {
@@ -3658,7 +3658,7 @@ TEST_P(ConnectionTest, classic_protocol_session_vars_nullable) {
 
   for (auto var : session_vars) {
     std::ostringstream oss;
-    oss << "SET @@SESSION." << std::quoted(var[0], '`') << "="
+    oss << "SET @@SESSION." << var[0] << "="
         << "NULL";
 
     SCOPED_TRACE("// " + oss.str());
@@ -3684,7 +3684,9 @@ TEST_P(ConnectionTest, classic_protocol_session_vars_nullable) {
                          "character_set_results",                       //
                          "innodb_ft_user_stopword_table",               //
                          "innodb_interpreter_output",                   // debug
-                         "session_track_system_variables"));
+                         "session_track_system_variables",
+                         "debug_set_operations_secondary_overflow_at",
+                         "log_query_errors"));
     }
   }
 }

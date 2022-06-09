493cdd4414d5be87a22510c5743a175c447e8ad0
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 21:25:26 +0200

    [include] PS-269: Initial Percona Server 8.0.12 tree

--- include/my_alloc.h
+++ include/my_alloc.h
@@ -165,6 +165,14 @@ struct MEM_ROOT {
     return AllocSlow(length);
   }
 
+  void *Alloc_aligned(size_t length, size_t alignment) MY_ATTRIBUTE((malloc)) {
+    void *ptr = Alloc(length + alignment);
+    if (!ptr) return nullptr;
+    ptr = reinterpret_cast<void *>(
+        MY_ALIGN(reinterpret_cast<std::uintptr_t>(ptr), alignment));
+    return ptr;
+  }
+
   /**
     Allocate “num” objects of type T, and initialize them to a default value
     that is created by passing the supplied args to T's constructor. If args

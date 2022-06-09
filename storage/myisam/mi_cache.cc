4a54ad7c1391e1c9db0e57e18b5cc4bccc65c8f3
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-06-09 22:03:20 +0200

    [storage/myisam] PS-269: Initial Percona Server 8.0.12 tree

--- storage/myisam/mi_cache.cc
+++ storage/myisam/mi_cache.cc
@@ -1,4 +1,5 @@
 /* Copyright (c) 2000, 2024, Oracle and/or its affiliates.
+   Copyright (c) 2018, Percona and/or its affiliates.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License, version 2.0,
@@ -56,6 +57,7 @@ int _mi_read_cache(IO_CACHE *info, uchar *buff, my_off_t pos, uint length,
   my_off_t offset;
   uchar *in_buff_pos;
   DBUG_TRACE;
+  assert(info->m_encryptor == nullptr && info->m_decryptor == nullptr);
 
   if (pos < info->pos_in_file) {
     read_length = length;

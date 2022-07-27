b32557212e773ffda5e7c1757b7e02a2f281fbd7
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-07-27 21:09:03 +0200

    [libbinlogevents] PS-269: Initial Percona Server 8.0.12 tree

--- libs/mysql/binlog/event/compression/base.h
+++ libs/mysql/binlog/event/compression/base.h
@@ -38,8 +38,11 @@ constexpr unsigned int ZSTD_INSTRUMENTED_BELOW_VERSION = 10505;
 // Todo: use enum class and a more specific name.
 // Use contiguous values.
 enum type {
-  /* ZSTD compression. */
-  ZSTD = 0,
+  /* ZSTD compression.
+  Original name 'ZSTD' that came from upstream changed to 'ALGORITHM_ZSTD'
+  to avoiud conflicts with 'RocksDB' library where 'ZSTD' is defined as a
+  macro. */
+  ALGORITHM_ZSTD = 0,
 
   /* No compression. */
   NONE = 255,
@@ -48,7 +51,7 @@ enum type {
 template <class T>
 bool type_is_valid(T t) {
   switch (t) {
-    case ZSTD:
+    case ALGORITHM_ZSTD:
       return true;
     case NONE:
       return true;

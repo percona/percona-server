5e8aa9636cf5a854d113d58bb88842eda45e59de
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2022-09-06 11:25:01 +0200

    [myrocks] PS-8164: Make MyRocks buildable
    Import all functions and changes required to compile MyRocks (-DWITH_ROCKSDB=1).
    
    ----------------------------------------------------------------------
    
    PS-9218 merge: Merge MySQL 8.4.0 (GCC 12.3 RelWithDebInfo fixes) (#5356)
    
    https://perconadev.atlassian.net/browse/PS-9218
    
    Added more warning suppressions for RocksDB submodule files that
    appeared in GCC 12.3 in RelWithDebInfo mode.
    
    ----------------------------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix gcc-14 build) #2 (rocksdb part)

--- sql/handler.cc
+++ sql/handler.cc
@@ -3399,6 +3399,12 @@ int handler::ha_index_next(uchar *buf) {
   return result;
 }
 
+bool handler::is_using_full_key(key_part_map keypart_map,
+                                uint actual_key_parts) noexcept {
+  return (keypart_map == HA_WHOLE_KEY) ||
+         (keypart_map == ((key_part_map(1) << actual_key_parts) - 1));
+}
+
 /**
   Reads the previous row via index.
 
--- sql/partitioning/partition_handler.h
+++ sql/partitioning/partition_handler.h
@@ -454,7 +454,8 @@ class Partition_helper {
       @returns    0 Success
       @returns != 0 Error code
   */
-  int ph_update_row(const uchar *old_data, uchar *new_data);
+  int ph_update_row(const uchar *old_data, uchar *new_data,
+                    bool lookup_rows = true);
   /**
     Delete an existing row in the partitioned table.
 
@@ -469,12 +470,13 @@ class Partition_helper {
     buf is either record[0] or record[1]
 
     @param buf  The record in MySQL Row Format.
+    @param lookup_rows Indicator for TokuDB read free replication.
 
     @return Operation status.
       @retval    0 Success
       @retval != 0 Error code
   */
-  int ph_delete_row(const uchar *buf);
+  int ph_delete_row(const uchar *buf, bool lookup_rows = true);
 
   /** @} */
 

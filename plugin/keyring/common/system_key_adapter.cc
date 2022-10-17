261a7dfa55adce0eec16eeda38c644631dde6519
Oleksandr Kachan <oleksandr.kachan@percona.com>
2022-10-17 12:10:38 +0300

    PS-8422: Merge MySQL 8.0.31 (fix build issues)
    https://jira.percona.com/browse/PS-8422
    
    - Remove workaround for -Werror=maybe-uninitialized for googlemock.
      The DISABLE_GOOGLEMOCK_UNINITIALIZED_WARNING() should be removed
      after googletest sources updated in
      https://github.com/mysql/mysql-server/commit/01f799ca2805c556ddfc0fbd0e2c33c7168cc38a.
    
    - Fix assertion for MDL_key::NAMESPACE_END size.
    
    - Replace usages of _utf8_ functions with corresponding _utf8mb4_ variants according to
      https://github.com/mysql/mysql-server/commit/81e1df713003461beb1c4d8388cf597776282fb1.
    
    - Prepared_statement.lex renamed to Prepared_statement.m_lex.
    
    - Fix GCC build by removing fix from
      https://github.com/percona/percona-server/commit/3779fed6a7668f41fae23a5a338de1a4505b68d4.

--- plugin/keyring/common/system_key_adapter.cc
+++ plugin/keyring/common/system_key_adapter.cc
@@ -15,6 +15,8 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
 
 #include "system_key_adapter.h"
+
+#include "my_sys.h"
 #include "secure_string.h"
 
 namespace keyring {

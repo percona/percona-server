e07b5f48aaf7194488a5c20d25dc422ce81a36b2
Zsolt Parragi <zsolt.parragi@percona.com>
2022-10-20 13:21:39 +0200

    [encryption] PS-8337: Minimal keyring encryption code after encrpytion removal
    This commit contains parts of the original many keyring encryption
    commits, which were required to cherry-pick the other unrelated
    encryption commits.
    
    The code contained here doesn't mean that the other features work or
    compile, only that they can be easily cherry picked without conflicts
    related to keyring encryption.

--- sql/handler.h
+++ sql/handler.h
@@ -895,6 +895,7 @@ class st_alter_tablespace {
   bool wait_until_completed = true;
   const char *ts_comment = nullptr;
   const char *encryption = nullptr;
+  bool explicit_encryption{false};
 
   bool is_tablespace_command() {
     return ts_cmd_type == CREATE_TABLESPACE ||
@@ -2112,6 +2113,15 @@ typedef bool (*notify_truncate_table_t)(THD *thd, const MDL_key *mdl_key,
 */
 typedef bool (*rotate_encryption_master_key_t)(void);
 
+/**
+ @brief
+ This is used by encryption threads. It updates innodb's copy of
+ default_table_encryption variable according to the parameter.
+ @param value for innodb's copy of default_table_encryption
+ @param is_starting True if the server is starting
+*/
+using fix_default_table_encryption_t = bool (*)(ulong value, bool is_starting);
+
 using compression_dict_data_vec_t =
     std::vector<std::pair<std::string, std::string>>;
 
@@ -2898,6 +2908,7 @@ struct handlerton {
   notify_rename_table_t notify_rename_table;
   notify_truncate_table_t notify_truncate_table;
   rotate_encryption_master_key_t rotate_encryption_master_key;
+  fix_default_table_encryption_t fix_default_table_encryption;
   upgrade_get_compression_dict_data_t upgrade_get_compression_dict_data;
   redo_log_set_state_t redo_log_set_state;
 
@@ -3288,6 +3299,11 @@ struct HA_CREATE_INFO {
   and ignored by the Server layer. */
 
   LEX_STRING encrypt_type{nullptr, 0};
+  // explicit_encryption should be true if table was originally created with
+  // ENCRYPTION clause. We keep it separate from used_fields &
+  // HA_CREATE_USED_ENCRYPT which can indicate whether current ALTER used
+  // ENCRYPTION clause.
+  bool explicit_encryption{false};
 
   /**
    * Secondary engine of the table.

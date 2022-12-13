6f144d2a8bb223fe6637d40329339475ed425d3b
Anton Matvienko <am@tempesta-tech.com>
2022-12-13 17:35:13 +0000

    PS-8073: Replication crash when updating a table with a stored generated field in the primary key
    https://jira.percona.com/browse/PS-8073
    
    Problem:
    When updating a table with a stored generated field in a primary key with binlog_row_image=minimal on a master, MySQL can't find the updated record on a replica.
    
    Cause:
    In f14a832e464b fix (a commit causing the bug) generated fields are updated in unpack_current_row. If a binlog row image doesn't contain base fields which a generated column depends on, generated fields will be updated incorrectly. Then they won't be found by Innodb.
    
    Solution:
    Exclude generated fields from update which base fields are excluded from the binlog row image.

--- sql/log_event.cc
+++ sql/log_event.cc
@@ -8018,6 +8018,7 @@ Rows_log_event::~Rows_log_event() {
 #ifdef MYSQL_SERVER
 int Rows_log_event::unpack_current_row(const Relay_log_info *const rli,
                                        MY_BITMAP const *cols,
+                                       MY_BITMAP const *local_cols,
                                        bool is_after_image, bool only_seek) {
   assert(m_table);
 
@@ -8060,14 +8061,19 @@ int Rows_log_event::unpack_current_row(const Relay_log_info *const rli,
           // A table view for generated columns that need to be updated on the
           // replica, excluding columns for functional indexes
           this->m_table,
-          [is_after_image, this](TABLE const *table,
-                                 size_t column_index) -> bool {
+          [local_cols, is_after_image, this](TABLE const *table,
+                                             size_t column_index) -> bool {
             auto field = table->field[column_index];
             if (field->is_field_for_functional_index())  // Always exclude
                                                          // functional indexes
               return true;
             if (!is_after_image &&  // Always exclude virtual generated columns
                 field->is_virtual_gcol())  // if not processing after-image
+              return true;
+            if (!bitmap_is_subset(  // Exclude generated columns which base
+                                    // columns are excluded from the binlog row
+                                    // image
+                    &field->gcol_info->base_columns_map, local_cols))
               return true;
             if (field->m_indexed)  // Never exclude generated columns that
                                    // have indexes
@@ -8773,7 +8779,7 @@ void Rows_log_event::do_post_row_operations(Relay_log_info const *rli,
       conditions. So this is probably a case of a corrupt event.
     */
     const uchar *previous_m_curr_row = m_curr_row;
-    error = unpack_current_row(rli, &m_cols, true /*is AI*/);
+    error = unpack_current_row(rli, &m_cols, &m_local_cols, true /*is AI*/);
 
     if (!error && previous_m_curr_row == m_curr_row) {
       error = 1;
@@ -9058,7 +9064,9 @@ int Rows_log_event::do_index_scan_and_update(Relay_log_info const *rli) {
   */
 
   prepare_record(m_table, &this->m_local_cols, false);
-  if ((error = unpack_current_row(rli, &m_cols, false /*is not AI*/))) goto end;
+  if ((error = unpack_current_row(rli, &m_cols, &m_local_cols,
+                                  false /*is not AI*/)))
+    goto end;
 
 #ifndef NDEBUG
   DBUG_PRINT("info", ("looking for the following record"));
@@ -9268,7 +9276,7 @@ int Update_rows_log_event::skip_after_image_for_update_event(
       m_curr_row==curr_bi_start.
     */
     m_curr_row = m_curr_row_end;
-    return unpack_current_row(rli, &m_cols_ai, true /*is AI*/,
+    return unpack_current_row(rli, &m_cols_ai, &m_local_cols_ai, true /*is AI*/,
                               true /*only_seek*/);
   }
   return 0;
@@ -9285,7 +9293,8 @@ int Rows_log_event::do_hash_row(Relay_log_info const *rli) {
   /* Prepare the record, unpack and save positions. */
   entry->positions->bi_start = m_curr_row;  // save the bi start pos
   prepare_record(m_table, &this->m_local_cols, false);
-  if ((error = unpack_current_row(rli, &m_cols, false /*is not AI*/))) {
+  if ((error = unpack_current_row(rli, &m_cols, &m_local_cols,
+                                  false /*is not AI*/))) {
     hash_slave_rows_free_entry freer;
     freer(entry);
     goto end;
@@ -9323,8 +9332,8 @@ int Rows_log_event::do_hash_row(Relay_log_info const *rli) {
 
     /* We shouldn't need this, but lets not leave loose ends */
     prepare_record(m_table, &this->m_local_cols, false);
-    error =
-        unpack_current_row(rli, &m_cols_ai, true /*is AI*/, true /*only_seek*/);
+    error = unpack_current_row(rli, &m_cols_ai, &m_local_cols_ai,
+                               true /*is AI*/, true /*only_seek*/);
 
     /*
       This is the situation after unpacking the AI:
@@ -9414,7 +9423,8 @@ int Rows_log_event::do_scan_and_update(Relay_log_info const *rli) {
             m_curr_row_end = entry->positions->bi_ends;
 
             prepare_record(table, &this->m_local_cols, false);
-            if ((error = unpack_current_row(rli, &m_cols, false /*is not AI*/)))
+            if ((error = unpack_current_row(rli, &m_cols, &m_local_cols,
+                                            false /*is not AI*/)))
               goto close_table;
 
             if (record_compare(table, &this->m_local_cols))
@@ -9551,7 +9561,8 @@ int Rows_log_event::do_table_scan_and_update(Relay_log_info const *rli) {
 
   /** unpack the before image */
   prepare_record(table, &this->m_local_cols, false);
-  if (!(error = unpack_current_row(rli, &m_cols, false /*is not AI*/))) {
+  if (!(error = unpack_current_row(rli, &m_cols, &m_local_cols,
+                                   false /*is not AI*/))) {
     /** save a copy so that we can compare against it later */
     store_record(m_table, record[1]);
 
@@ -12199,7 +12210,8 @@ int Write_rows_log_event::write_row(const Relay_log_info *const rli,
                  table->file->ht->db_type != DB_TYPE_NDBCLUSTER);
 
   /* unpack row into table->record[0] */
-  if ((error = unpack_current_row(rli, &m_cols, true /*is AI*/))) return error;
+  if ((error = unpack_current_row(rli, &m_cols, &m_local_cols, true /*is AI*/)))
+    return error;
 
   /*
     When m_curr_row == m_curr_row_end, it means a row that contains nothing,
@@ -12360,7 +12372,7 @@ int Write_rows_log_event::write_row(const Relay_log_info *const rli,
     */
     if (!get_flags(COMPLETE_ROWS_F)) {
       restore_record(table, record[1]);
-      error = unpack_current_row(rli, &m_cols, true /*is AI*/);
+      error = unpack_current_row(rli, &m_cols, &m_local_cols, true /*is AI*/);
     }
 
 #ifndef NDEBUG
@@ -12534,7 +12546,7 @@ int Delete_rows_log_event::do_exec_row(const Relay_log_info *const rli) {
   int error;
   assert(m_table != nullptr);
   if (m_rows_lookup_algorithm == ROW_LOOKUP_NOT_NEEDED) {
-    error = unpack_current_row(rli, &m_cols, false, false);
+    error = unpack_current_row(rli, &m_cols, &m_local_cols, false, false);
     if (error) return error;
   }
   /* m_table->record[0] contains the BI */
@@ -12679,7 +12691,7 @@ int Update_rows_log_event::do_exec_row(const Relay_log_info *const rli) {
   int error = 0;
 
   if (m_rows_lookup_algorithm == ROW_LOOKUP_NOT_NEEDED) {
-    error = unpack_current_row(rli, &m_cols, false, false);
+    error = unpack_current_row(rli, &m_cols, &m_local_cols, false, false);
     if (error) return error;
   }
 
@@ -12698,7 +12710,8 @@ int Update_rows_log_event::do_exec_row(const Relay_log_info *const rli) {
 
   m_curr_row = m_curr_row_end;
   /* this also updates m_curr_row_end */
-  if ((error = unpack_current_row(rli, &m_cols_ai, true /*is AI*/)))
+  if ((error = unpack_current_row(rli, &m_cols_ai, &m_local_cols_ai,
+                                  true /*is AI*/)))
     return error;
 
   // Invoke check constraints on the unpacked row.

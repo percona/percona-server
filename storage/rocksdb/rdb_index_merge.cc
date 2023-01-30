3344a1c608f3c58d7eccabaf16ed9ca5ff31efda
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2023-01-30 09:59:50 -0800

    Use PRIu64 instead of %lu to format std::uint64_t values (#1261)
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/65f8d2070543ed11952dac079e2bdfdff7af2863
    PS-8755: Merge percona-202301 (https://jira.percona.com/browse/PS-8755)
    
    Summary:
    This fixes the macOS build, where std::uint64_t is unsigned long long.
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1261
    
    Reviewed By: sunshine-Chun
    
    Differential Revision: D42850107
    
    Pulled By: hermanlee
    
    fbshipit-source-id: 7520d90efc4369e203d0c4ee6f52ab02863ca10d

--- storage/rocksdb/rdb_index_merge.cc
+++ storage/rocksdb/rdb_index_merge.cc
@@ -17,6 +17,8 @@
 /* This C++ file's header file */
 #include "./rdb_index_merge.h"
 
+#include <cinttypes>
+
 /* MySQL header files */
 #include "mysql/plugin.h"
 #include "mysql/psi/mysql_file.h"
@@ -163,9 +165,9 @@ int Rdb_index_merge::add(const rocksdb::Slice &key, const rocksdb::Slice &val) {
       add is too large for the buffer.
     */
     if (m_offset_tree.empty()) {
-      LogPluginErrMsg(ERROR_LEVEL, 0,
-                      "Current value of rocksdb_merge_buf_size=%llu is too "
-                      "small. At least %u bytes required.",
+      LogPluginErrMsg(ERROR_LEVEL, ER_LOG_PRINTF_MSG,
+                      "Current value of rocksdb_merge_buf_size=%" PRIu64
+                      " is too small. At least %u bytes required.",
                       m_rec_buf_unsorted->m_total_size, total_offset);
       return HA_ERR_ROCKSDB_MERGE_FILE_ERR;
     }
@@ -181,9 +183,9 @@ int Rdb_index_merge::add(const rocksdb::Slice &key, const rocksdb::Slice &val) {
     const uint data_size = RDB_MERGE_CHUNK_LEN + RDB_MERGE_KEY_DELIMITER +
                            RDB_MERGE_VAL_DELIMITER + key.size() + val.size();
     if (data_size > m_rec_buf_unsorted->m_total_size) {
-      LogPluginErrMsg(ERROR_LEVEL, 0,
-                      "Current value of rocksdb_merge_buf_size=%llu is too "
-                      "small. At least %u bytes required.",
+      LogPluginErrMsg(ERROR_LEVEL, ER_LOG_PRINTF_MSG,
+                      "Current value of rocksdb_merge_buf_size=%" PRIu64
+                      " is too small. At least %u bytes required.",
                       m_rec_buf_unsorted->m_total_size, data_size);
       return HA_ERR_ROCKSDB_MERGE_FILE_ERR;
     }

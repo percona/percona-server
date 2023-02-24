76aa9f575a5fc485ce9144e72d09ad2223ae5376
Chun Ni <chni@meta.com>
2023-02-24 00:13:04 -0800

    Add private dd table: max index id dd
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/86fd2099b9766ae497b075555a5361a9c1611d2b
    PS-8755: Merge percona-202301 (https://jira.percona.com/browse/PS-8755)
    
    Summary:
    Add a new private data dictionary table to store the max index id of data dictionary table.
    
    Include the following changes:
    
    1. Add MAX_INDEX_ID_DD = 11 in DATA_DICT_TYPE
    2. When updating or getting max index id, we need to check it is a dd table or user table. And we need to update/get the corresponding value depending on the table type.
    3. We can get the max_index_id_dd in information_schema.ROCKSDB_GLOBAL_INFO
    
    Reviewed By: luqun
    
    Differential Revision: D43563692
    
    fbshipit-source-id: b728bf11b433adb17ea29a3a01dd71a98029c074

--- storage/rocksdb/rdb_datadic.cc
+++ storage/rocksdb/rdb_datadic.cc
@@ -5443,8 +5443,13 @@ bool Rdb_dict_manager::init(rocksdb::TransactionDB *const rdb_dict,
     return HA_EXIT_FAILURE;
   }
 
-  rdb_netbuf_store_index(m_key_buf_max_index_id, Rdb_key_def::MAX_INDEX_ID);
+  rdb_netbuf_store_index(m_key_buf_max_dd_index_id,
+                         Rdb_key_def::MAX_DD_INDEX_ID);
+  m_key_slice_max_dd_index_id =
+      rocksdb::Slice(reinterpret_cast<char *>(m_key_buf_max_dd_index_id),
+                     Rdb_key_def::INDEX_NUMBER_SIZE);
 
+  rdb_netbuf_store_index(m_key_buf_max_index_id, Rdb_key_def::MAX_INDEX_ID);
   m_key_slice_max_index_id =
       rocksdb::Slice(reinterpret_cast<char *>(m_key_buf_max_index_id),
                      Rdb_key_def::INDEX_NUMBER_SIZE);
@@ -6142,15 +6147,20 @@ void Rdb_dict_manager::log_start_drop_index(GL_INDEX_ID gl_index_id,
   }
 }
 
-bool Rdb_dict_manager::get_max_index_id(uint32_t *const index_id) const {
+bool Rdb_dict_manager::get_max_index_id(uint32_t *const index_id,
+                                        bool is_dd_tbl) const {
   bool found = false;
   std::string value;
 
-  const rocksdb::Status status = get_value(m_key_slice_max_index_id, &value);
+  const rocksdb::Status status =
+      is_dd_tbl ? get_value(m_key_slice_max_dd_index_id, &value)
+                : get_value(m_key_slice_max_index_id, &value);
+  const uint16_t cur_version = is_dd_tbl ? Rdb_key_def::MAX_DD_INDEX_ID_VERSION
+                                         : Rdb_key_def::MAX_INDEX_ID_VERSION;
   if (status.ok()) {
     const uchar *const val = (const uchar *)value.c_str();
     const uint16_t version = rdb_netbuf_to_uint16(val);
-    if (version == Rdb_key_def::MAX_INDEX_ID_VERSION) {
+    if (version == cur_version) {
       *index_id = rdb_netbuf_to_uint32(val + Rdb_key_def::VERSION_SIZE);
       found = true;
     }
@@ -6159,11 +6169,15 @@ bool Rdb_dict_manager::get_max_index_id(uint32_t *const index_id) const {
 }
 
 bool Rdb_dict_manager::update_max_index_id(rocksdb::WriteBatch *const batch,
-                                           const uint32_t index_id) const {
+                                           const uint32_t index_id,
+                                           bool is_dd_tbl) const {
   assert(batch != nullptr);
 
+  DBUG_EXECUTE_IF("simulate_dd_table_for_adding_max_dd_index_id",
+                  { is_dd_tbl = true; });
+
   uint32_t old_index_id = -1;
-  if (get_max_index_id(&old_index_id)) {
+  if (get_max_index_id(&old_index_id, is_dd_tbl)) {
     if (old_index_id > index_id) {
       LogPluginErrMsg(ERROR_LEVEL, 0,
                       "Found max index id %u from data dictionary but trying "
@@ -6176,10 +6190,18 @@ bool Rdb_dict_manager::update_max_index_id(rocksdb::WriteBatch *const batch,
 
   Rdb_buf_writer<Rdb_key_def::VERSION_SIZE + Rdb_key_def::INDEX_NUMBER_SIZE>
       value_writer;
-  value_writer.write_uint16(Rdb_key_def::MAX_INDEX_ID_VERSION);
-  value_writer.write_uint32(index_id);
 
-  batch->Put(m_system_cfh, m_key_slice_max_index_id, value_writer.to_slice());
+  if (is_dd_tbl) {
+    value_writer.write_uint16(Rdb_key_def::MAX_DD_INDEX_ID_VERSION);
+    value_writer.write_uint32(index_id);
+    batch->Put(m_system_cfh, m_key_slice_max_dd_index_id,
+               value_writer.to_slice());
+  } else {
+    value_writer.write_uint16(Rdb_key_def::MAX_INDEX_ID_VERSION);
+    value_writer.write_uint32(index_id);
+    batch->Put(m_system_cfh, m_key_slice_max_index_id, value_writer.to_slice());
+  }
+
   return false;
 }
 
@@ -6258,8 +6280,8 @@ bool Rdb_dict_manager::get_auto_incr_val(const GL_INDEX_ID &gl_index_id,
   return false;
 }
 
-uint Rdb_seq_generator::get_and_update_next_number(
-    Rdb_dict_manager *const dict) {
+uint Rdb_seq_generator::get_and_update_next_number(Rdb_dict_manager *const dict,
+                                                   bool is_dd_tbl) {
   assert(dict != nullptr);
 
   uint res;
@@ -6271,7 +6293,7 @@ uint Rdb_seq_generator::get_and_update_next_number(
   rocksdb::WriteBatch *const batch = wb.get();
 
   assert(batch != nullptr);
-  dict->update_max_index_id(batch, res);
+  dict->update_max_index_id(batch, res, is_dd_tbl);
   dict->commit(batch);
 
   RDB_MUTEX_UNLOCK_CHECK(m_mutex);

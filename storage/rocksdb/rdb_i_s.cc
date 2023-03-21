5bffa14a0daa1570dd2f49c00a66b777e0f209fa
Andrew Kryczka <andrewkr@fb.com>
2023-03-21 10:21:20 -0700

    Update RocksDB usage for 8.0 release
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/653eba2e56cfba4eac0c851ac9a70b2da9607527
    PS-8755: Merge percona-202301 (https://jira.percona.com/browse/PS-8755)
    
    Summary:
    - Deleted references to removed compressed block cache APIs
    - Migrated header includes to fix the build
    
    Reviewed By: anirbanr-fb
    
    Differential Revision: D44207824
    
    fbshipit-source-id: fea5acf7b4f991fa0d516efe36c543cd935a7bbd

--- storage/rocksdb/rdb_i_s.cc
+++ storage/rocksdb/rdb_i_s.cc
@@ -36,6 +36,10 @@
 #include "sql/table.h"  // ST_FIELD_INFO
 
 /* RocksDB header files */
+#include "rocksdb/version.h"
+#if ROCKSDB_MAJOR >= 8
+#include "rocksdb/advanced_cache.h"
+#endif  // ROCKSDB_MAJOR >= 8
 #include "rocksdb/compaction_filter.h"
 #include "rocksdb/convenience.h"
 #include "rocksdb/filter_policy.h"

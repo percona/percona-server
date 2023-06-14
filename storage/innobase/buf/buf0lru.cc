be2af018c17427cd177fa28a90518f72cda48364
Oleksandr Kachan <oleksandr.kachan@percona.com>
2023-06-15 00:19:30 +0300

    PS-8785: Increment metrics for the first iteration of buf_LRU_free_from_common_LRU_list()
    https://jira.percona.com/browse/PS-8785
    
    Currently buffer_LRU_search_scanned and buffer_LRU_unzip_search_scanned
    metrics are not incremented by buf_LRU_free_from_common_LRU_list() and
    buf_LRU_free_from_unzip_LRU_list() methods in case the first item from
    the list gets evicted.
    
    - Updated logic in buf_LRU_free_from_common_LRU_list() and
      buf_LRU_free_from_unzip_LRU_list() to take into account first item in
      unzip_LRU and LRU lists.
    - Removed now redundant "!freed" condition in 'for' loop condition.

--- storage/innobase/buf/buf0lru.cc
+++ storage/innobase/buf/buf0lru.cc
@@ -1056,7 +1056,7 @@ static bool buf_LRU_free_from_unzip_LRU_list(buf_pool_t *buf_pool,
   bool freed = false;
 
   for (buf_block_t *block = UT_LIST_GET_LAST(buf_pool->unzip_LRU);
-       block != nullptr && !freed && (scan_all || scanned < srv_LRU_scan_depth);
+       block != nullptr && (scan_all || scanned < srv_LRU_scan_depth);
        ++scanned) {
     buf_block_t *prev_block;
 
@@ -1070,10 +1070,12 @@ static bool buf_LRU_free_from_unzip_LRU_list(buf_pool_t *buf_pool,
 
     freed = buf_LRU_free_page(&block->page, false);
 
-    if (!freed) {
-      mutex_exit(&block->mutex);
+    if (freed) {
+      ++scanned;
+      break;
     }
 
+    mutex_exit(&block->mutex);
     block = prev_block;
   }
 
@@ -1100,7 +1102,7 @@ static bool buf_LRU_free_from_common_LRU_list(buf_pool_t *buf_pool,
   ulint scanned{};
 
   for (buf_page_t *bpage = buf_pool->lru_scan_itr.start();
-       bpage != nullptr && !freed &&
+       bpage != nullptr &&
        (scan_all || scanned < BUF_LRU_SEARCH_SCAN_THRESHOLD);
        ++scanned, bpage = buf_pool->lru_scan_itr.get()) {
     ut_ad(mutex_own(&buf_pool->LRU_list_mutex));
@@ -1138,6 +1140,7 @@ static bool buf_LRU_free_from_common_LRU_list(buf_pool_t *buf_pool,
     ut_ad(!mutex_own(block_mutex));
 
     if (freed) {
+      ++scanned;
       break;
     }
   }

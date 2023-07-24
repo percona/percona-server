a26f3a88182fa65cec2694e68544fd38494e1846
Oleksandr Kachan <oleksandr.kachan@percona.com>
2023-07-25 01:13:01 +0300

    [plugin/audit_log_filter] PS-8042: Improve audit_log_filter MTR test suite
    https://jira.percona.com/browse/PS-8042
    
    Creating the plugin related tables during the test instance bootstrap
    brakes many of tests not related to the plugin itself.
    To avoid this create/remove corresponding tables in each MTR test which
    needs them.
    
    --------------------------------------------------------------------------
    
    PS-8042: Improve audit_log_filter.log_prune_seconds test
    
    https://jira.percona.com/browse/PS-8042
    
    The audit_log_filter.log_prune_seconds MTR tests expects the plugin to
    generate a set of rotated log files. Log rotation timestamp at the
    moment is taken from current system time. When test is run on fast hardware,
    log rotation happens too soon and with the same timestamp for different logs.
    As a result, rotated logs get rewritten and expected files set is not
    generated.
    
    To fix that, modified the test to use DEBUG='+d,audit_log_filter_debug_timestamp'.
    This ensures each next rotated log file has unique timestamp in its
    name.

--- plugin/audit_log_filter/audit_log_filter_linux_install.sql
+++ plugin/audit_log_filter/audit_log_filter_linux_install.sql
@@ -19,7 +19,7 @@ SET @db_name = IFNULL(IFNULL(@sys_var_database, DATABASE()), 'mysql');
 SET @create_filter = CONCAT(
   'CREATE TABLE IF NOT EXISTS ', @db_name, '.audit_log_filter (',
     'filter_id INT UNSIGNED NOT NULL AUTO_INCREMENT,',
-    'name VARCHAR(255) COLLATE utf8_bin NOT NULL,',
+    'name VARCHAR(255) NOT NULL,',
     'filter JSON NOT NULL,',
     'PRIMARY KEY (`filter_id`),',
     'UNIQUE KEY `filter_name` (`name`)',
@@ -28,9 +28,9 @@ SET @create_filter = CONCAT(
 
 SET @create_user = CONCAT(
   'CREATE TABLE IF NOT EXISTS ', @db_name, '.audit_log_user(',
-    'username VARCHAR(32) COLLATE utf8_bin NOT NULL,',
-    'userhost VARCHAR(255) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,',
-    'filtername VARCHAR(255) COLLATE utf8_bin NOT NULL,',
+    'username VARCHAR(32) NOT NULL,',
+    'userhost VARCHAR(255) NOT NULL,',
+    'filtername VARCHAR(255) NOT NULL,',
     'PRIMARY KEY (username, userhost), FOREIGN KEY `filter_name` (filtername) REFERENCES ', @db_name, '.audit_log_filter(name)'
   ') Engine = InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_as_ci'
 );

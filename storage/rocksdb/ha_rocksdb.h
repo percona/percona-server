4c482e0e058edeaf7d2d335f06305fcfbdfcc293
Manuel Ung <mung@meta.com>
2023-08-28 23:33:48 -0700

    Add option to ignore killed flag for materialization
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/7d17329e6b686653a4661b166679c357a284df37
    PS-8951: Merge percona-202305 (https://jira.percona.com/browse/PS-8951)
    
    Summary:
    When queries time out, we abandon any work that was done to materialize partial indexes. This may not be desirable since the client will most likely retry, and still fail because it is querying a large group that requires materialization.
    
    Add a flag for the materialization code path to ignore the kill flag and finish materialization. In case we have bugs, we can always turn it off via a flag to check the kill flag.
    
    Differential Revision: D48786848
    
    fbshipit-source-id: 7a9fd996b4a403392c6005dacbea43a23240f568

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -1193,6 +1193,7 @@ extern std::atomic<uint64_t> rocksdb_partial_index_rows_materialized;
 extern bool rocksdb_enable_tmp_table;
 extern bool rocksdb_enable_delete_range_for_drop_index;
 extern bool rocksdb_disable_instant_ddl;
+extern bool rocksdb_partial_index_ignore_killed;
 
 extern unsigned long long rocksdb_converter_record_cached_length;
 }  // namespace myrocks

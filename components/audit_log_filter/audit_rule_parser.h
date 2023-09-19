8584ba5037f343f3c5371b4e2740b0f9e702751d
Oleksandr Kachan <oleksandr.kachan@percona.com>
2023-09-19 19:56:06 +0300

    [components/audit_log_filter] PS-8854: Convert audit_log_filter plugin to a component
    https://jira.percona.com/browse/PS-8854
    
    The most significant changes implemented during audit_log_filter
    plugin conversion to a component:
    - System and status variables rewritten to work with
      component_status_var_service and component_sys_var_service API.
    - The read_buffer_size sysvar is global now. The reason for this change
      is that component_sys_var_service doesn't support session variables.
    - Removed filter_id system variable. Added audit_log_session_filter_id()
      UDF instead. The reason for the change is again lack of support for
      session variables in component_sys_var_service.
    - Added support for audit events of mysql_event_tracking_parse type.
    - Data sent along with audit events was changed in 8.1 for most of the
      audit events. Component was adjusted to handle changed events format.
    
    ---------------------------------------------------
    
    PS-10049 [9.x] [2] Test result re-recording after sync with release-8.0.43-34
    
    https://perconadev.atlassian.net/browse/PS-10049
    
    The result files were changed due to changes of the restart message in
    
    mysql-test/include/kill_and_restart_mysqld.inc and
    mysql-test/include/restart_mysqld.inc
    
    ---------------------------------------------------
    
    PS-10049 [9.x] merge: Code refresh for - PS 8.4.6-6 (mtr-checker cleanup)
    
    https://perconadev.atlassian.net/browse/PS-10049
    
    Cleaned up orphaned MTR artefacts based on the output of the "mtr_checker"
    utility (https://github.com/Percona-Lab/mtr-checker).
    
    ...
    
    ***
    *** Suite: component_audit_log_filter ***
    ***
    [error]: inconsistent test case "log_format_json": [ result ]
    [error]: inconsistent test case "log_format_new": [ result ]
    [error]: inconsistent test case "log_format_old": [ result ]

---
+++ components/audit_log_filter/audit_rule_parser.h
@@ -0,0 +1,197 @@
+/* Copyright (c) 2022 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef AUDIT_LOG_FILTER_RULE_PARSER_H_INCLUDED
+#define AUDIT_LOG_FILTER_RULE_PARSER_H_INCLUDED
+
+#include "components/audit_log_filter/audit_rule.h"
+
+#include "my_rapidjson_size_t.h"
+#include "rapidjson/document.h"
+
+namespace audit_log_filter {
+
+class AuditRuleParser {
+ public:
+  /**
+   * @brief Parse audit filter rule represented as a string.
+   *
+   * @param rule_str String representation of audit filtering rule
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case rule is parsed successfully,
+   *         false otherwise
+   */
+  static bool parse(const char *rule_str, AuditRule *audit_rule) noexcept;
+
+ private:
+  /**
+   * @brief Parse audit filter rule represented as a JSON document object.
+   *
+   * @param json_doc JSON document representation of audit filtering rule
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case rule is parsed successfully,
+   *         false otherwise
+   */
+  static bool parse(rapidjson::Document &json_doc,
+                    AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Determine the default logging action for unmatched audit events.
+   *
+   * @param json_doc JSON document representation of audit filtering rule
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case default action parsed successfully,
+   *         false otherwise
+   */
+  static bool parse_default_log_action_json(const rapidjson::Document &json_doc,
+                                            AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse audit event class defined actions in audit filtering rule.
+   *
+   * @param json_doc JSON document representation of audit filtering rule
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case event class definition parsed successfully,
+   *         false otherwise
+   */
+  static bool parse_event_class_json(const rapidjson::Document &json_doc,
+                                     AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse one JSON object related to audit event class definition.
+   *
+   * @param event_class_json JSON object representing rule definition for
+   *                         one audit event class
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case event class definition parsed successfully,
+   *         false otherwise
+   */
+  static bool parse_event_class_obj_json(
+      const rapidjson::Value &event_class_json, AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse audit event subclass related definitions in a filtering rule
+   *        represented by a JSON object.
+   *
+   * @param class_name Audit event class name
+   * @param event_subclass_json JSON object representing audit event
+   *                            subclass info
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case of success, false otherwise
+   */
+  static bool parse_event_subclass_json(
+      const std::string &class_name,
+      const rapidjson::Value &event_subclass_json,
+      AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse JSON definition for one event subclass in a filtering rule.
+   *
+   * @param class_name Audit event class name
+   * @param event_subclass_json JSON object representing audit event
+   *                            subclass info
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return true in case of success, false otherwise
+   */
+  static bool parse_event_subclass_obj_json(
+      const std::string &class_name,
+      const rapidjson::Value &event_subclass_json,
+      AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse definition of a logical condition in audit event filter
+   *        represented by a JSON string.
+   *
+   * @param condition_json JSON object representing condition definition
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return Pointer to an instance representing parsed condition
+   */
+  static std::shared_ptr<EventFieldConditionBase> parse_condition(
+      const rapidjson::Value &condition_json, AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Get type of condition specified for an event field.
+   *
+   * @param event_field_obj JSON object representing audit event field info
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return One of condition types defined by @ref EventFieldConditionType
+   */
+  static EventFieldConditionType get_condition_type(
+      const rapidjson::Value &event_field_obj, AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse audit event field condition in a filtering rule
+   *        represented by a JSON string.
+   *
+   * @param condition_json JSON object representing audit event filter condition
+   * @param cond_type Condition type, one of @ref EventFieldConditionType
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return Logical condition instance
+   */
+  static std::shared_ptr<EventFieldConditionBase> parse_condition_json(
+      const rapidjson::Value &condition_json, EventFieldConditionType cond_type,
+      AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse function definition in a filtering rule represented by
+   *        a JSON string.
+   *
+   * @param function_json JSON object representing a function
+   * @param expected_return_type Expected return type of the function
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return Function instance
+   */
+  static std::unique_ptr<EventFilterFunctionBase> parse_function(
+      const rapidjson::Value &function_json,
+      FunctionReturnType expected_return_type, AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Parse function arguments in a filtering rule represented
+   *        by a JSON string.
+   *
+   * @param function_args_obj JSON object representing function arguments
+   * @param args Container to store parsed arguments in
+   * @return true in case of success, false otherwise
+   */
+  static bool parse_function_args_json(
+      const rapidjson::Value &function_args_obj,
+      FunctionArgsList &args) noexcept;
+
+  /**
+   * @brief Parse action definition in a filtering rule represented by
+   *        a JSON string.
+   *
+   * @param action_type Action type
+   * @param action_json JSON object representing action
+   * @param audit_rule Audit filtering rule instance to be initialized
+   * @return Action instance
+   */
+  [[nodiscard]] static std::shared_ptr<EventFieldActionBase> parse_action_json(
+      EventActionType action_type, const rapidjson::Value &action_json,
+      AuditRule *audit_rule) noexcept;
+
+  /**
+   * @brief Build replacement filtering rule.
+   *
+   * @param rule_json JSON definition of replacement filtering rule
+   * @return Replacement filtering rule instance
+   */
+  [[nodiscard]] static std::shared_ptr<AuditRule> make_replacement_rule(
+      const rapidjson::Value &rule_json) noexcept;
+};
+
+}  // namespace audit_log_filter
+
+#endif  // AUDIT_LOG_FILTER_RULE_PARSER_H_INCLUDED
---
+++ components/audit_log_filter/log_record_formatter/old.h
@@ -0,0 +1,171 @@
+/* Copyright (c) 2022 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#ifndef AUDIT_LOG_FILTER_LOG_RECORD_FORMATTER_OLD_H_INCLUDED
+#define AUDIT_LOG_FILTER_LOG_RECORD_FORMATTER_OLD_H_INCLUDED
+
+#include "base.h"
+
+namespace audit_log_filter::log_record_formatter {
+
+template <>
+class LogRecordFormatter<AuditLogFormatType::Old>
+    : public LogRecordFormatterBaseXml {
+ public:
+  /**
+   * @brief Apply formatting to AuditRecordGeneral audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordGeneral &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordConnection audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordConnection &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordTableAccess audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordTableAccess &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordGlobalVariable audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordGlobalVariable &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordServerStartup audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordServerStartup &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordServerShutdown audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordServerShutdown &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordCommand audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordCommand &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordQuery audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordQuery &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordStoredProgram audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordStoredProgram &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordAuthentication audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordAuthentication &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordMessage audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordMessage &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordParse audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordParse &audit_record) const noexcept override;
+
+  /**
+   * @brief Apply formatting to AuditRecordAudit audit record.
+   *
+   * @param [in] audit_record Audit record
+   * @return String representing formatted audit record
+   */
+  [[nodiscard]] AuditRecordString apply(
+      const AuditRecordAudit &audit_record) const noexcept override;
+
+  /**
+   * @brief Insert audit event class and subclass names into record printed to
+   *        log. Needed for testing.
+   *
+   * @param event_class_name Event class name
+   * @param event_subclass_name Event subclass name
+   * @param record_str String representation of audit record
+   */
+  void apply_debug_info(std::string_view event_class_name,
+                        std::string_view event_subclass_name,
+                        std::string &record_str) noexcept override;
+
+ private:
+  /**
+   * @brief Get string representation of extra attributes
+   *        for audit log record.
+   * @param info Extended record info
+   * @return Formatted string
+   */
+  [[nodiscard]] std::string extra_attrs_to_string(
+      const ExtendedInfo &info) const noexcept override;
+};
+
+using LogRecordFormatterOld = LogRecordFormatter<AuditLogFormatType::Old>;
+
+}  // namespace audit_log_filter::log_record_formatter
+
+#endif  // AUDIT_LOG_FILTER_LOG_RECORD_FORMATTER_OLD_H_INCLUDED

44eeeedf0bdecb0d778e306e232930e8e1e32b4e
Oleksandr Kachan <oleksandr.kachan@percona.com>
2023-09-19 19:56:06 +0300

    [components/audit_log_filter] PS-8854: Convert audit_log_filter plugin to a component
    https://jira.percona.com/browse/PS-8854
    
    The most significant changes implemented during audit_log_filter
    plugin conversion to a component:
    - System and status variables rewritten to work with
      component_status_var_service and component_sys_var_service API.
    - The read_buffer_size sysvar is global now. The reason for this change
      is that component_sys_var_service doesn't support session variables.
    - Removed filter_id system variable. Added audit_log_session_filter_id()
      UDF instead. The reason for the change is again lack of support for
      session variables in component_sys_var_service.
    - Added support for audit events of mysql_event_tracking_parse type.
    - Data sent along with audit events was changed in 8.1 for most of the
      audit events. Component was adjusted to handle changed events format.

---
+++ components/audit_log_filter/log_writer/file_writer_compressing.cc
@@ -0,0 +1,85 @@
+/* Copyright (c) 2023 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
+#include "file_writer_compressing.h"
+
+#include "components/audit_log_filter/audit_error_log.h"
+
+namespace audit_log_filter::log_writer {
+
+FileWriterCompressing::FileWriterCompressing(
+    std::unique_ptr<FileWriterBase> file_writer)
+    : FileWriterDecoratorBase(std::move(file_writer)) {}
+
+FileWriterCompressing::~FileWriterCompressing() { deflateEnd(&m_strm); }
+
+bool FileWriterCompressing::init() noexcept {
+  return FileWriterDecoratorBase::init();
+}
+
+bool FileWriterCompressing::open() noexcept {
+  m_strm.zalloc = Z_NULL;
+  m_strm.zfree = Z_NULL;
+  m_strm.opaque = Z_NULL;
+
+  auto ret = deflateInit2(&m_strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED,
+                          MAX_WBITS + 16, MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY);
+
+  if (ret != Z_OK) {
+    LogComponentErr(ERROR_LEVEL, ER_LOG_PRINTF_MSG,
+                    "Failed to init compressing: %i", ret);
+    return false;
+  }
+
+  return FileWriterDecoratorBase::open();
+}
+
+void FileWriterCompressing::close() noexcept {
+  m_flush = Z_FINISH;
+  do_deflate();
+  deflateEnd(&m_strm);
+
+  FileWriterDecoratorBase::close();
+}
+
+void FileWriterCompressing::write(const char *record, size_t size) noexcept {
+  m_strm.avail_in = size;
+  m_strm.next_in =
+      reinterpret_cast<unsigned char *>(const_cast<char *>(record));
+  m_flush = Z_NO_FLUSH;
+
+  do_deflate();
+}
+
+void FileWriterCompressing::do_deflate() noexcept {
+  do {
+    m_strm.avail_out = COMPRESSION_CHUNK;
+    m_strm.next_out = m_out_buff;
+
+    [[maybe_unused]] int ret = deflate(&m_strm, m_flush);
+    assert(ret != Z_STREAM_ERROR);
+
+    size_t compressed_size = COMPRESSION_CHUNK - m_strm.avail_out;
+
+    if (compressed_size > 0) {
+      FileWriterDecoratorBase::write(reinterpret_cast<const char *>(m_out_buff),
+                                     compressed_size);
+    }
+  } while (m_strm.avail_out == 0);
+
+  assert(m_strm.avail_in == 0);
+}
+
+}  // namespace audit_log_filter::log_writer

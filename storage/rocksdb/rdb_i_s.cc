e5bad7491b506115e462c0c95cfa32764e217ecd
Changyu Bi <changyubi@meta.com>
2023-09-28 16:10:59 -0700

    Update definition for `GetCompactionReasonString`
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/62f4a46ee492
    PS-9395: Merge percona-202401 (https://jira.percona.com/browse/PS-9395)
    
    Summary:
    `GetCompactionReasonString()` will be exposed in
    RocksDB 8.7, which includes PR https://github.com/facebook/rocksdb/pull/11778 that exposes it. So update its definition here to be based on RocksDB version.
    
    Differential Revision: D49755853

--- storage/rocksdb/rdb_i_s.cc
+++ storage/rocksdb/rdb_i_s.cc
@@ -965,6 +965,7 @@ static int rdb_i_s_compact_stats_fill_table(
   DBUG_RETURN(ret);
 }
 
+#if ROCKSDB_MAJOR < 8 || (ROCKSDB_MAJOR == 8 && ROCKSDB_MINOR < 7)
 namespace {
 
 using rocksdb::CompactionReason;
@@ -1015,6 +1016,7 @@ const char *GetCompactionReasonString(CompactionReason compaction_reason) {
 }
 
 }  // anonymous namespace
+#endif
 
 /*
   Support for INFORMATION_SCHEMA.ROCKSDB_ACTIVE_COMPACTION_STATS dynamic table

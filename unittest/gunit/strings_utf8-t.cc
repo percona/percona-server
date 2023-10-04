1756851fe5682cf6d0aa569aa638e1b28e6b3503
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2023-10-04 13:51:42 +0200

    PS-8952: Fix clang-17 compilation issues
    Fix the following clang-17 compilation issues:
    
    1.
    ```
    /home/yura/ws/percona-server-8.0/unittest/gunit/strings_utf8-t.cc:200:26: error: identifier '_sl' preceded by whitespace in a literal operator declaration is deprecated [-Werror,-Wdeprecated-literal-operator]
      200 | StringLiteral operator"" _sl(const char *ptr, size_t size) {
          |               ~~~~~~~~~~~^~~
          |               operator""_sl
    1 error generated.
    ```
    
    2.
    ```
    /home/yura/ws/percona-server-8.0/unittest/gunit/libmysqlgcs/xcom/gcs_xcom_control_interface-t.cc:1216:17: error: reference cannot be bound to dereferenced null pointer in well-defined C++ code; comparison may be assumed to always evaluate to true [-Werror,-Wtautological-undefined-compare]
     1216 |   ASSERT_TRUE((&current_view_id) != nullptr);
          |                 ^~~~~~~~~~~~~~~     ~~~~~~~
    /home/yura/ws/percona-server-8.0/extra/googletest/googletest-release-1.12.0/googletest/include/gtest/gtest.h:1794:50: note: expanded from macro 'ASSERT_TRUE'
     1794 | #define ASSERT_TRUE(condition) GTEST_ASSERT_TRUE(condition)
          |                                                  ^~~~~~~~~
    /home/yura/ws/percona-server-8.0/extra/googletest/googletest-release-1.12.0/googletest/include/gtest/gtest.h:1777:23: note: expanded from macro 'GTEST_ASSERT_TRUE'
     1777 |   GTEST_TEST_BOOLEAN_(condition, #condition, false, true, GTEST_FATAL_FAILURE_)
          |                       ^~~~~~~~~
    /home/yura/ws/percona-server-8.0/extra/googletest/googletest-release-1.12.0/googletest/include/gtest/internal/gtest-internal.h:1504:38: note: expanded from macro 'GTEST_TEST_BOOLEAN_'
     1504 |           ::testing::AssertionResult(expression))                     \
          |                                      ^~~~~~~~~~
    1 error generated.
    ```

--- unittest/gunit/strings_utf8-t.cc
+++ unittest/gunit/strings_utf8-t.cc
@@ -197,7 +197,7 @@ class StringLiteral {
   size_t size;
 };
 
-StringLiteral operator"" _sl(const char *ptr, size_t size) {
+StringLiteral operator""_sl(const char *ptr, size_t size) {
   return StringLiteral(ptr, size);
 }
 

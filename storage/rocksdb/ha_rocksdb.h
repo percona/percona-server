59a7853b6680f3fd7a039bc8a97eb1eeb51f736d
Prerit Jain <prerit@meta.com>
2023-12-16 21:02:46 -0800

    Add support for instant alter of column default value
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/8f13afd2a198
    PS-9395: Merge percona-202401 (https://jira.percona.com/browse/PS-9395)
    
    Summary: With these changes, we support instant alter for column default values.
    
    Differential Revision: D49562448
    
    fbshipit-source-id: 8cc0d802ac071695ad2046968944d5ea34875c66

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -1185,6 +1185,7 @@ extern bool rocksdb_enable_tmp_table;
 extern bool rocksdb_enable_delete_range_for_drop_index;
 extern bool rocksdb_disable_instant_ddl;
 extern bool rocksdb_partial_index_ignore_killed;
+extern bool rocksdb_enable_instant_ddl_for_column_default_changes;
 
 extern unsigned long long rocksdb_converter_record_cached_length;
 }  // namespace myrocks

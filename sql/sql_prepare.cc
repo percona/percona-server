7eb15ca8f3aded5ae91bb9191d69356f25d1fa25
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2024-01-19 13:14:04 +0100

    [upstream] PS-9071: Merge MySQL 8.3.0 (fix crash in engines/funcs.ps_number_null)
    Fix the following crash:
    ```
    mysqld-debug: /data/mysql-server/8.x/sql/sql_prepare.cc:2957: bool Prepared_statement::check_parameter_types(): Assertion `false' failed.
    2024-01-19T12:14:02Z UTC - mysqld got signal 6 ;
    ```

--- sql/sql_prepare.cc
+++ sql/sql_prepare.cc
@@ -2743,6 +2743,7 @@ bool Prepared_statement::check_parameter_types() {
     }
 
     switch (item->data_type()) {
+      case MYSQL_TYPE_NULL:
       case MYSQL_TYPE_BOOL:
       case MYSQL_TYPE_TINY:
       case MYSQL_TYPE_SHORT:

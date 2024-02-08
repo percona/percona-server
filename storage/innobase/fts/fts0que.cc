0a53120e23719bfc66687424e8eb009aef5e2c2a
Yura Sorokin <yura.sorokin@percona.com>
2024-02-08 02:53:04 +0100

    [plugin/fulltext] PS-9048 fix: Fixed problem with percent character in n-grams
    https://perconadev.atlassian.net/browse/PS-9048
    
    Fixed problem with 'fts_index_fetch_nodes()' function not being able to
    properly handle situations when 'word' parameter contained special characters
    used by 'LIKE' clauses ('%' and '_'). Introduced additional boolean parameter
    'exact_match' that instructs this function to use either 'WHERE word = :word'
    or 'WHERE word LIKE :word' clauses when selecting records from internal FTS
    tables. We call 'fts_index_fetch_nodes()' with 'exact_match' set to 'true'
    only from 'fts_optimize_words()' (when we perform 'OPTIMIZE TABLE' under
    'innodb_optimize_fulltext_only' enabled). In every other place
    * fts_query_difference()
    * fts_query_intersect()
    * fts_query_union()
    * fts_query_phrase_search()
    where we indeed need pattern matching we call this function with 'exact_match'
    set to 'false' (instructing the function to use the 'LIKE' clause).
    
    Added six new MTR test cases:
    * 'innodb_fts.percona_ft_special_chars_default_ewc_on'
    * 'innodb_fts.percona_ft_special_chars_default_ewc_off'
    * 'innodb_fts.percona_ft_special_chars_ngram_ewc_on'
    * 'innodb_fts.percona_ft_special_chars_ngram_ewc_off'
    * 'innodb_fts.percona_ft_special_chars_mecab_ewc_on'
    * 'innodb_fts.percona_ft_special_chars_mecab_ewc_off'
    which reproduce original crash scenario using default / ngram / mecab parsers,
    under both 'ft_query_extra_word_chars' set to 'ON' and 'OFF'. These tests cases
    also check parsing / querying strings containing various special characters.
    Please note that they have "result mismatch" status strings recorded in the
    '.result' files for certain combinations of special characters and this is
    expected behavior.
    
    Introduced two new MTR helper '.inc' files:
    * percona_install_mecab_plugin.inc
    * percona_uninstall_mecab_plugin.inc
    which help install / uninstall the mecab plugin along with creating / removing
    its settings file.
    'innodb_fts.percona_mecab_null_character' MTR test case reworked with these two
    include files.
    
    ----------------------------------------------------------------------------
    
    PS-9380: Fix innodb_fts.percona_ft_special_chars_*

--- storage/innobase/fts/fts0que.cc
+++ storage/innobase/fts/fts0que.cc
@@ -1074,7 +1074,7 @@ static ulint fts_cache_find_wildcard(
     fetch.read_record = fts_query_index_fetch_nodes;
 
     error = fts_index_fetch_nodes(trx, &graph, &query->fts_index_table, token,
-                                  &fetch);
+                                  &fetch, false);
 
     /* DB_FTS_EXCEED_RESULT_CACHE_LIMIT passed by 'query->error' */
     ut_ad(!(query->error != DB_SUCCESS && error != DB_SUCCESS));
@@ -1194,7 +1194,7 @@ static ulint fts_cache_find_wildcard(
     fetch.read_record = fts_query_index_fetch_nodes;
 
     error = fts_index_fetch_nodes(trx, &graph, &query->fts_index_table, token,
-                                  &fetch);
+                                  &fetch, false);
 
     /* DB_FTS_EXCEED_RESULT_CACHE_LIMIT passed by 'query->error' */
     ut_ad(!(query->error != DB_SUCCESS && error != DB_SUCCESS));
@@ -1305,7 +1305,7 @@ static dberr_t fts_query_cache(
 
   /* Read the nodes from disk. */
   error = fts_index_fetch_nodes(trx, &graph, &query->fts_index_table, token,
-                                &fetch);
+                                &fetch, false);
 
   /* DB_FTS_EXCEED_RESULT_CACHE_LIMIT passed by 'query->error' */
   ut_ad(!(query->error != DB_SUCCESS && error != DB_SUCCESS));
@@ -2564,7 +2564,7 @@ static void fts_query_phrase_split(fts_query_t *query,
       }
 
       error = fts_index_fetch_nodes(trx, &graph, &query->fts_index_table, token,
-                                    &fetch);
+                                    &fetch, false);
 
       /* DB_FTS_EXCEED_RESULT_CACHE_LIMIT passed by 'query->error' */
       ut_ad(!(query->error != DB_SUCCESS && error != DB_SUCCESS));

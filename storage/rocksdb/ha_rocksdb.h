b35d76e35f14be1179dfcbe7e2bc4b7372797bd1
Luqun Lou <luqun@meta.com>
2024-02-12 22:32:36 -0800

    update instant ddl variables for myrocks
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/ce211faf772e
    PS-9395: Merge percona-202404 (https://jira.percona.com/browse/PS-9395)
    
    Summary:
    Previous the variable name rocksdb_disable_instant_ddl is confusing.  Use rocksdb_enable_instant_ddl  instead and  deprecate rocksdb_disable_instant_ddl. rocksdb_enable_instant_ddl is for emergency use to turn off all instant-ddl knobs.
    
    add a new variable rocksdb-enable-instant-ddl-for-append-column to enable/disable append column DDL scenario.
    
    In future, each instant scenario should add its own switch to control whether support that scenario.
    
    NOTE: These INSTANT variables are only to control future INSTANT DDLs behavior. Doesn't affect the DDL has been committed.
    
    Differential Revision: D53697104
    
    fbshipit-source-id: 31fc60f0f60bdb5950164d5c055d5064d635f27e

--- storage/rocksdb/ha_rocksdb.h
+++ storage/rocksdb/ha_rocksdb.h
@@ -1194,7 +1194,10 @@ extern std::atomic<uint64_t> rocksdb_partial_index_rows_materialized;
 extern bool rocksdb_enable_tmp_table;
 extern bool rocksdb_enable_delete_range_for_drop_index;
 extern bool rocksdb_disable_instant_ddl;
+extern bool rocksdb_enable_instant_ddl;
 extern bool rocksdb_partial_index_ignore_killed;
+
+extern bool rocksdb_enable_instant_ddl_for_append_column;
 extern bool rocksdb_enable_instant_ddl_for_column_default_changes;
 extern bool rocksdb_enable_instant_ddl_for_table_comment_changes;
 extern bool rocksdb_enable_instant_ddl_for_drop_index_changes;

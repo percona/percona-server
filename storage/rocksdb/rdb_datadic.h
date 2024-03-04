43aceac0bf5e42f77c51ba673c5556c1f72d3abd
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-03-03 23:26:45 -0800

    Do clangd-suggested include fixes in storage/rocksdb (#1401)
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/19db3e88953b
    PS-9395: Merge percona-202402 (https://jira.percona.com/browse/PS-9395)
    
    Summary:
    To reduce the number of annoying wiggles under headers in the editor. Appears to be a net removal, which is good.
    
    This is like `include-what-you-use` but less thorough, i.e. some `#include <string>` etc. are clearly missing but not suggested. https://clangd.llvm.org/guides/include-cleaner
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1401
    
    Differential Revision: D51951100
    
    fbshipit-source-id: 309be6b177baba0409fef346b1ee542393c5fabc

--- storage/rocksdb/rdb_datadic.h
+++ storage/rocksdb/rdb_datadic.h
@@ -22,7 +22,6 @@
 #include <array>
 #include <atomic>
 #include <map>
-#include <mutex>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -36,11 +35,23 @@
 #include "./ha_rocksdb.h"
 #include "./properties_collector.h"
 #include "./rdb_buff.h"
+#include "./rdb_global.h"
 #include "./rdb_mutex_wrapper.h"
 #include "./rdb_utils.h"
 
 /* Server header files */
 #include "sql/dd/object_id.h"
+
+// Forward declarations
+#ifdef ROCKSDB_CUSTOM_NAMESPACE
+namespace ROCKSDB_CUSTOM_NAMESPACE {
+#else
+namespace rocksdb {
+#endif
+
+class TransactionDB;
+
+}  // namespace ROCKSDB_CUSTOM_NAMESPACE / rocksdb
 
 namespace myrocks {
 

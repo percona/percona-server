21035cd5743e9303d9752b7f1cc3889d4e97e437
Oleksandr Kachan <oleksandr.kachan@percona.com>
2024-03-27 16:50:59 +0200

    PS-9148: Add caching of dictionary table for component_masking_functions
    https://perconadev.atlassian.net/browse/PS-9148
    
    - Added caching of mysql.masking_dictionaries table content.
    - Implemented masking_dictionaries_flush() UDF which flushes data
      from the masking dictionaries table to the memory cache.

--- components/masking_functions/src/masking_functions/query_builder.cpp
+++ components/masking_functions/src/masking_functions/query_builder.cpp
@@ -17,52 +17,40 @@
 
 #include "masking_functions/query_builder.hpp"
 
-#include "masking_functions/sql_escape_functions.hpp"
-
 namespace masking_functions {
 
 std::string query_builder::insert_ignore_record(
-    const charset_string &dictionary_name, const charset_string &term) const {
+    const std::string &dictionary_name, const std::string &term) const {
   std::ostringstream oss;
   oss << "INSERT IGNORE INTO " << get_database_name() << '.' << get_table_name()
       << " (" << get_dictionary_field_name() << ", " << get_term_field_name()
-      << ')' << " VALUES('" << escape_string(dictionary_name) << "', '"
-      << escape_string(term) << "')";
+      << ')' << " VALUES('" << dictionary_name << "', '" << term << "')";
   return oss.str();
 }
 
-std::string query_builder::select_term_for_dictionary_internal(
-    const charset_string &dictionary_name,
-    const charset_string *opt_term) const {
+std::string query_builder::select_all_from_dictionary() const {
   std::ostringstream oss;
   // In our implementation there is no requirement that the `Term` field in
   // the `mysql.masking_dictionaries` table must be in `utf8mb4`. So, by
   // adding CONVERT(Term USING utf8mb4) we support other character sets in
   // the underlying table as well.
   oss << "SELECT "
+      << "CONVERT(" << get_dictionary_field_name() << " USING "
+      << default_result_character_set << "), "
       << "CONVERT(" << get_term_field_name() << " USING "
       << default_result_character_set << ") FROM " << get_database_name() << '.'
-      << get_table_name() << " WHERE " << get_dictionary_field_name() << " = '"
-      << escape_string(dictionary_name) << '\'';
-  if (opt_term != nullptr) {
-    oss << " AND " << get_term_field_name() << " = '"
-        << escape_string(*opt_term) << '\'';
-  } else {
-    oss << " ORDER BY RAND() LIMIT 1";
-  }
+      << get_table_name();
   return oss.str();
 }
 
 std::string query_builder::delete_for_dictionary_and_opt_term_internal(
-    const charset_string &dictionary_name,
-    const charset_string *opt_term) const {
+    const std::string &dictionary_name, const std::string *opt_term) const {
   std::ostringstream oss;
   oss << "DELETE FROM " << get_database_name() << '.' << get_table_name()
-      << " WHERE " << get_dictionary_field_name() << " = '"
-      << escape_string(dictionary_name) << '\'';
+      << " WHERE " << get_dictionary_field_name() << " = '" << dictionary_name
+      << '\'';
   if (opt_term != nullptr) {
-    oss << " AND " << get_term_field_name() << " = '"
-        << escape_string(*opt_term) << '\'';
+    oss << " AND " << get_term_field_name() << " = '" << *opt_term << '\'';
   }
   return oss.str();
 }

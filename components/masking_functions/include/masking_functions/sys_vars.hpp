15cfaae2aa1af58d69d8f77a0ea2684991319533
Oleksandr Kachan <oleksandr.kachan@percona.com>
2024-03-29 19:17:14 +0200

    PS-9148: Add masking_functions.masking_database sys var support
    https://perconadev.atlassian.net/browse/PS-9148
    
    The masking_functions.masking_database system variable for the
    masking_functions component specifies database used for data
    masking dictionaries.

---
+++ components/masking_functions/include/masking_functions/sys_vars.hpp
@@ -0,0 +1,31 @@
+/* Copyright (c) 2024 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 of the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   51 Franklin Street, Suite 500, Boston, MA 02110-1335 USA */
+
+#ifndef MASKING_FUNCTIONS_SYS_VARS_HPP
+#define MASKING_FUNCTIONS_SYS_VARS_HPP
+
+#include <string_view>
+
+namespace masking_functions::sys_vars {
+
+std::string_view get_dict_database_name() noexcept;
+
+bool register_sys_vars();
+bool unregister_sys_vars();
+bool validate();
+
+}  // namespace masking_functions::sys_vars
+
+#endif  // MASKING_FUNCTIONS_SYS_VARS_HPP

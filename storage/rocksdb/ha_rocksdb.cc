6ea38dc23d46f99ae1f757a0f6979d1450368ec4
Luqun Lou <luqun@meta.com>
2024-04-03 22:55:49 -0700

    add compress parallel threads variable for bulk loading
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/160466ceaa8f
    PS-9395: Merge percona-202403 (https://jira.percona.com/browse/PS-9395)
    
    Summary:
    perf data show bulk loading spend a lot time to compress data block during creating PK/SK sst files.
    
    From rocksdb code, it support compression_opts.parallel_threads, which use multiple thread to do parallel compression.
    
    Add a new variable to enable parallel compression during bulk load.
    
    initial data : bulk-load-compression-parallel-threads == 4
      - for big table with multiple SKs, the time to generate SK ssts file will 3X faster.
      - for big table, overall bulk loading process will take 50-60% time, 2X faster.
      -  these sst file size will increase by 0.1%
    
    Differential Revision: D56340150
    
    fbshipit-source-id: feebf587b11395b1168e0dece4fd76ebadfc5060

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -1321,6 +1321,13 @@ static MYSQL_SYSVAR_BOOL(
     "DBOptions::enable_pipelined_write for RocksDB", nullptr, nullptr,
     rocksdb_db_options->enable_pipelined_write);
 
+static MYSQL_THDVAR_UINT(bulk_load_compression_parallel_threads,
+                         PLUGIN_VAR_RQCMDARG,
+                         "The number of parallel worker threads to compress "
+                         "SST data blocks during bulk load",
+                         nullptr, nullptr, /*default*/ 1, /*min*/ 1,
+                         /*max*/ 1024, 0);
+
 static MYSQL_SYSVAR_BOOL(enable_remove_orphaned_dropped_cfs,
                          rocksdb_enable_remove_orphaned_dropped_cfs,
                          PLUGIN_VAR_RQCMDARG,
@@ -2709,6 +2716,7 @@ static struct SYS_VAR *rocksdb_system_variables[] = {
     MYSQL_SYSVAR(bulk_load_fail_if_not_bottommost_level),
     MYSQL_SYSVAR(bulk_load_use_sst_partitioner),
     MYSQL_SYSVAR(bulk_load_enable_unique_key_check),
+    MYSQL_SYSVAR(bulk_load_compression_parallel_threads),
     MYSQL_SYSVAR(trace_sst_api),
     MYSQL_SYSVAR(commit_in_the_middle),
     MYSQL_SYSVAR(blind_delete_primary_key),
@@ -3711,7 +3719,8 @@ class Rdb_transaction {
 
         auto sst_info = std::make_shared<Rdb_sst_info>(
             rdb, table_name, index_name, rdb_merge.get_cf(),
-            *rocksdb_db_options, THDVAR(get_thd(), trace_sst_api));
+            *rocksdb_db_options, THDVAR(get_thd(), trace_sst_api),
+            THDVAR(get_thd(), bulk_load_compression_parallel_threads));
 
         if (keydef->is_partial_index()) {
           if (!THDVAR(m_thd, bulk_load_partial_index)) continue;
@@ -11158,9 +11167,10 @@ int ha_rocksdb::bulk_load_key(Rdb_transaction *const tx, const Rdb_key_def &kd,
   // used to store the keys. It is still used to indicate when tables
   // are switched.
   if (m_sst_info == nullptr || m_sst_info->is_done()) {
-    m_sst_info.reset(new Rdb_sst_info(rdb, m_table_handler->m_table_name,
-                                      kd.get_name(), cf, *rocksdb_db_options,
-                                      THDVAR(ha_thd(), trace_sst_api)));
+    m_sst_info.reset(new Rdb_sst_info(
+        rdb, m_table_handler->m_table_name, kd.get_name(), cf,
+        *rocksdb_db_options, THDVAR(ha_thd(), trace_sst_api),
+        THDVAR(ha_thd(), bulk_load_compression_parallel_threads)));
     res = tx->start_bulk_load(this, m_sst_info);
     if (res != HA_EXIT_SUCCESS) {
       DBUG_RETURN(res);

5c7a482207ec79889a00d47f6f48af9abe742811
Kamil Holubicki <kamil.holubicki@percona.com>
2024-05-31 13:16:05 +0200

    [components/percona_telemetry] PS-9165: Product Usage Tracking - phase 1
    https://perconadev.atlassian.net/browse/PS-9165
    
    Updated copyright info.
    
    ---------------------------------------------------------------------
    
    PS-9165 postfix: Product Usage Tracking - phase 1 (MTR fixes)
    
    https://perconadev.atlassian.net/browse/PS-9165
    
    'sys_vars.plugin_dir_basic' and  'clone.plugin_mismatch' MTR test cases modified
    so that they could be run on a server built both with and without telemetry
    component ('-DWITH_PERCONA_TELEMETRY=ON' CMake option).
    
    ---------------------------------------------------------------------
    
    PS-9165 postfix 8.0: Product Usage Tracking - phase 1
    
    https://perconadev.atlassian.net/browse/PS-9165
    
    When the server is started with --innodb-read-only flag,
    Percona Telemetry Component can not be installed/uninstalled,
    because the it is prohibited to add/delete the row in mysql.component
    table. In such a case
    "Skip updating mysql metadata in InnoDB read-only mode."
    warning was printed into server's error log which caused several MTR
    tests to fail.
    
    Solution: Do not call dd::check_if_server_ddse_readonly()
    but do the check directly and print information level message if needed.
    
    ---------------------------------------------------------------------
    
    PS-9165 postfix 8.0: Product Usage Tracking - phase 1 (MTR fixes) (#5340)
    
    * PS-9165 postfix 8.0: Product Usage Tracking - phase 1 (MTR fixes)
    
    https://perconadev.atlassian.net/browse/PS-9165
    
    1. test_session_info.test duplicated with IDs recorded for the case
    when Percona Telemetry is built-in. It is better than masking IDs in
    output, because the test relies on real ID values
    
    2. regression.test - ID masked in test output
    
    3. prep_stmt_sundries - make assertion value dependant on Percona
    Telemetry being built-in
    
    ---------------------------------------------------------------------
    
    PS-9165 postfix: Product Usage Tracking - phase 1 (MTR KV fixes) (#5342)
    
    https://perconadev.atlassian.net/browse/PS-9165
    
    'keyring_vault.keyring_udf' MTR test case modified so that it could be run
    on a server built both with and without telemetry component
    ('-DWITH_PERCONA_TELEMETRY=ON' CMake option).
    
    ---------------------------------------------------------------------
    
    PS-9621: Initialization of std::atomic_flag caller_active_ in Percona Telemetry component is not compliant with the standard
    
    https://perconadev.atlassian.net/browse/PS-9621
    
    Initialization of std::atomic_flag in the constructor's initializer
    list is not compliant with the standard. Gcc allows it, however clang
    issues a compilation warning which turns into error in maintainer mode
    build.
    (error: braces around scalar initializer [-Werror,-Wbraced-scalar-init])

--- components/percona_telemetry/worker.cc
+++ components/percona_telemetry/worker.cc
@@ -1,3 +1,19 @@
+/* Copyright (c) 2024 Percona LLC and/or its affiliates. All rights reserved.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; version 2 of
+   the License.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA */
+
 #include <mutex>
 
 #include "logger.h"
@@ -9,8 +25,7 @@ Worker::Worker(Config &config, Storage &storage, DataProvider &data_provider,
       storage_(storage),
       data_provider_(data_provider),
       logger_(logger),
-      stop_worker_thd_(false),
-      caller_active_(ATOMIC_FLAG_INIT) {}
+      stop_worker_thd_(false) {}
 
 bool Worker::start() {
   std::thread thd(&Worker::worker_thd_fn, this);

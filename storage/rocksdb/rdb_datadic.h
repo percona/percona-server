f98e6c4f3d3aaa3ad95f3c33473d2c84f73a4f3b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2024-06-12 15:02:46 -0700

    Remove rdb_sysvars_mutex (#1459)
    Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/81fc0cc12992
    PS-9395: Merge percona-202404 (https://jira.percona.com/browse/PS-9395)
    
    Summary:
    The mutex is used by sysvar update functions and synchronizes updates to a
    subset of global system variables. But sysvar update functions are called with
    LOCK_global_system_variables server lock taken, making the extra locking in the
    SE redundant, thus just remove it.
    
    However it was also partially (writes but not reads) protecting concurrent
    accesses to properties_collector_factory variable, thus introduce
    synchronization for its class Rdb_tbl_prop_coll_factory by adding an rwlock.
    
    At the same time convert Rdb_tbl_prop_coll_factory pointer fields to const
    references, remove redundant explicit from two-argument constructor, add
    [[nodiscard]] to touched methods, make the class non-movable. Remove redundant
    const from passed by-value arguments.
    
    To enable converting m_ddl_manager field to a const reference, mark
    Rdb_ddl_manager::m_rwlock field as mutable, which then enables marking many
    logically-const methods as such. Also mark touched methods [[nodiscard]], remove
    redundant const from passed by-value arguments, convert pass by const reference
    to pass by value for a small object arguments of GL_INDEX_ID type.
    
    Add return value handling for one call site of Rdb_ddl_manager::find_indexes as
    pointed out by [[nodiscard]].
    
    Pull Request resolved: https://github.com/facebook/mysql-5.6/pull/1459
    
    Differential Revision: D58491945
    
    fbshipit-source-id: 79f16ff26e8628cd46d1d72f98162069760300cf

--- storage/rocksdb/rdb_datadic.h
+++ storage/rocksdb/rdb_datadic.h
@@ -1478,7 +1478,7 @@ class Rdb_ddl_manager : public Ensure_initialized {
   // This is mainly used to store key definitions during ALTER TABLE.
   std::map<GL_INDEX_ID, std::shared_ptr<Rdb_key_def>>
       m_index_num_to_uncommitted_keydef;
-  mysql_rwlock_t m_rwlock;
+  mutable mysql_rwlock_t m_rwlock;
 
   Rdb_seq_generator m_dd_table_sequence;
   Rdb_seq_generator m_user_table_sequence;
@@ -1489,7 +1489,8 @@ class Rdb_ddl_manager : public Ensure_initialized {
   // and consumed by the rocksdb background thread
   std::map<GL_INDEX_ID, Rdb_index_stats> m_stats2store;
 
-  const std::shared_ptr<Rdb_key_def> &find(GL_INDEX_ID gl_index_id);
+  [[nodiscard]] const std::shared_ptr<Rdb_key_def> &find(
+      GL_INDEX_ID gl_index_id) const;
 
  public:
   Rdb_ddl_manager(const Rdb_ddl_manager &) = delete;
@@ -1505,14 +1506,16 @@ class Rdb_ddl_manager : public Ensure_initialized {
 #endif  // defined(ROCKSDB_INCLUDE_VALIDATE_TABLES) &&
         // ROCKSDB_INCLUDE_VALIDATE_TABLES
 
-  void cleanup();
+  void cleanup(bool destroy_rwlock = true);
 
-  Rdb_tbl_def *find(const std::string &table_name, const bool lock = true);
-  int find_indexes(const std::string &table_name,
-                   std::vector<GL_INDEX_ID> *indexes);
-  int find_table_stats(const std::string &table_name,
-                       Rdb_table_stats *tbl_stats);
-  std::shared_ptr<const Rdb_key_def> safe_find(GL_INDEX_ID gl_index_id);
+  [[nodiscard]] Rdb_tbl_def *find(const std::string &table_name,
+                                  bool lock = true) const;
+  [[nodiscard]] int find_indexes(const std::string &table_name,
+                                 std::vector<GL_INDEX_ID> *indexes) const;
+  [[nodiscard]] int find_table_stats(const std::string &table_name,
+                                     Rdb_table_stats *tbl_stats) const;
+  [[nodiscard]] std::shared_ptr<const Rdb_key_def> safe_find(
+      GL_INDEX_ID gl_index_id) const;
   void set_stats(const std::unordered_map<GL_INDEX_ID, Rdb_index_stats> &stats);
   void adjust_stats(const std::vector<Rdb_index_stats> &new_data,
                     const std::vector<Rdb_index_stats> &deleted_data =
@@ -1531,17 +1534,18 @@ class Rdb_ddl_manager : public Ensure_initialized {
 
   uint get_and_update_next_number(uint cf_id, bool is_dd_tbl);
 
-  const std::string safe_get_table_name(const GL_INDEX_ID &gl_index_id);
+  [[nodiscard]] const std::string safe_get_table_name(
+      GL_INDEX_ID gl_index_id) const;
 
   /* Walk the data dictionary */
-  int scan_for_tables(Rdb_tables_scanner *tables_scanner);
+  int scan_for_tables(Rdb_tables_scanner *tables_scanner) const;
 
   void erase_index_num(const GL_INDEX_ID &gl_index_id);
   void add_uncommitted_keydefs(
       const std::unordered_set<std::shared_ptr<Rdb_key_def>> &indexes);
   void remove_uncommitted_keydefs(
       const std::unordered_set<std::shared_ptr<Rdb_key_def>> &indexes);
-  int find_in_uncommitted_keydef(const uint32_t cf_id);
+  [[nodiscard]] int find_in_uncommitted_keydef(uint32_t cf_id) const;
 
  private:
   /* Put the data into in-memory table (only) */
@@ -1553,9 +1557,9 @@ class Rdb_ddl_manager : public Ensure_initialized {
   static void free_hash_elem(void *const data);
 
 #if defined(ROCKSDB_INCLUDE_VALIDATE_TABLES) && ROCKSDB_INCLUDE_VALIDATE_TABLES
-  bool validate_schemas();
+  [[nodiscard]] bool validate_schemas() const;
 
-  bool validate_auto_incr();
+  [[nodiscard]] bool validate_auto_incr() const;
 #endif  // defined(ROCKSDB_INCLUDE_VALIDATE_TABLES) &&
         // ROCKSDB_INCLUDE_VALIDATE_TABLES
 };

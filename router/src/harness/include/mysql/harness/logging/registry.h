57a5fbb4c10c8e0580ff85eac07074486b8ce592
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2024-12-03 11:20:23 +0100

    [compilation] PS-9542: Fix clang-19 compilation issues
    Fix 2 types of warnings:
    1.
    ```
    /data/mysql-server/percona-8.4/libs/mysql/serialization/serializer_default_impl.hpp:476:57: error: a template argument list is expected after a name prefixed by the template keyword [-Wmissing-template-arg-list-after-template-kw]
      476 |     auto size_id_type = Archive_concrete_type::template get_size(
    ```
    2.
    ```
    /data/mysql-server/percona-8.4/router/src/metadata_cache/src/cluster_metadata_ar.h:77:43: error: empty paragraph passed to '@throws' command [-Werror,-Wdocumentation]
       77 |    * @throws metadata_cache::metadata_error
          |      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
       78 |    */
    ```

--- router/src/harness/include/mysql/harness/logging/registry.h
+++ router/src/harness/include/mysql/harness/logging/registry.h
@@ -336,7 +336,7 @@ void clear_registry(Registry &registry);
  * @param main_app_log_domain Log domain (logger id) to be used as the main
  *                            program logger. This logger must exist, because
  *                            log_*() functions might fail
- * @throws std::logic_error
+ * @throws std::logic_error if there is an error
  */
 HARNESS_EXPORT
 void create_module_loggers(Registry &registry, const LogLevel level,
@@ -352,7 +352,7 @@ void create_module_loggers(Registry &registry, const LogLevel level,
  * @param log_level The log level of the logger
  * @param logger_name The name under which the logger is registered
  *
- * @throws std::logic_error
+ * @throws std::logic_error if there is an error
  */
 HARNESS_EXPORT
 void create_logger(Registry &registry, const LogLevel level,

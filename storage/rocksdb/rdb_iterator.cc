facebook/mysql-5.6

Avoiding unnecessary reseeks in primary key iterator
Summary:
Doing full index scans is currently more expensive than it needs to be with partial indexes. The problem is that for every distinct group, we have to reseek our primary key iterator. However, in most cases, a reseek is not necessary, as the primary key iterator is already at the correct position.

The change here is to track in `m_iterator_pk_position` the current position of the iterator relative to the current prefix. When seeking to the next prefix, we check if we are already positioned correctly, and then skip the seek if possible.

Also, as a minor optimization, use std::vector instead of std::map. This makes allocating/deallocating memory much cheaper.

Reviewed By: george-reynya

Differential Revision: D30681736

fbshipit-source-id: 4de4e0196d8

storage/rocksdb/rdb_iterator.cc
@@ -372,11 +372,12 @@ Rdb_iterator_partial::Rdb_iterator_partial(
       m_converter(thd, tbl_def, table),
       m_valid(false),
       m_materialized(false),
+      m_iterator_pk_position(Iterator_position::UNKNOWN),
       m_threshold(kd->partial_index_threshold()),
       m_prefix_keyparts(kd->partial_index_keyparts()),
       m_cur_prefix_key_len(0),
-      m_records(slice_comparator(m_kd->get_cf()->GetComparator())),
-      m_records_it(m_records.end()) {
+      m_records_it(m_records.end()),
+      m_comparator(slice_comparator(m_kd->get_cf()->GetComparator())) {
   init_sql_alloc(PSI_NOT_INSTRUMENTED, &m_mem_root, 4096, 0);
   auto max_mem = get_partial_index_sort_max_mem(thd);
   if (max_mem) {
@@ -458,6 +459,7 @@ int Rdb_iterator_partial::get_prefix_from_start(
   // 1. There are not enough keyparts in the start_key.
   // 2. An exclusive seek key is provided, meaning that we need to read the next
   // prefix.
+  m_iterator_pk_position = Iterator_position::UNKNOWN;
   if (prefix_cnt < m_prefix_keyparts ||
       (prefix_len == start_key.size() &&
        (find_flag == HA_READ_AFTER_KEY || find_flag == HA_READ_BEFORE_KEY))) {
@@ -498,9 +500,29 @@ int Rdb_iterator_partial::get_next_prefix(bool direction) {
   rocksdb::Slice cur_prefix_key((const char *)m_cur_prefix_key,
                                 m_cur_prefix_key_len);
   uint tmp;
+  int rc = 0;
 
-  int rc = get_prefix_from_start(
-      direction ? HA_READ_AFTER_KEY : HA_READ_BEFORE_KEY, cur_prefix_key);
+  if (direction && m_iterator_pk_position != Iterator_position::UNKNOWN &&
+      m_iterator_pk_position != Iterator_position::START_CUR_PREFIX) {
+    if (m_iterator_pk_position == Iterator_position::END_OF_FILE) {
+      return HA_ERR_END_OF_FILE;
+    } else if (m_iterator_pk_position == Iterator_position::START_NEXT_PREFIX) {
+      uint prefix_cnt = 0;
+      uint prefix_len = 0;
+      rc = get_prefix_len(m_iterator_pk.key(), &prefix_cnt, &prefix_len);
+      if (rc) {
+        m_iterator_pk_position = Iterator_position::UNKNOWN;
+        return rc;
+      }
+      memcpy(m_cur_prefix_key, m_iterator_pk.key().data(), prefix_len);
+      m_cur_prefix_key_len = prefix_len;
+      m_iterator_pk_position = Iterator_position::START_CUR_PREFIX;
+    }
+  } else {
+    m_iterator_pk_position = Iterator_position::UNKNOWN;
+    rc = get_prefix_from_start(
+        direction ? HA_READ_AFTER_KEY : HA_READ_BEFORE_KEY, cur_prefix_key);
+  }
   m_kd->get_infimum_key(m_cur_prefix_key, &tmp);
 
   cur_prefix_key =
@@ -693,13 +715,27 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
   // the forwards direction (even PK is a reverse cf). This ensures that we can
   // make good use of bloom filters.
   //
-  rc = m_iterator_pk.seek(HA_READ_KEY_EXACT, cur_prefix_key, false,
-                          cur_prefix_key);
+  DBUG_ASSERT(m_iterator_pk_position != Iterator_position::END_OF_FILE);
+  if (m_iterator_pk_position != Iterator_position::START_CUR_PREFIX) {
+    rocksdb::Slice empty_end_key;
+    rc = m_iterator_pk.seek(HA_READ_KEY_OR_NEXT, cur_prefix_key, false,
+                            empty_end_key);
+  }
 
-  while (!rc) {
+  while (true) {
     if (thd_killed(m_thd)) {
       rc = HA_ERR_QUERY_INTERRUPTED;
       goto exit;
+    }
+
+    if (rc == HA_ERR_END_OF_FILE) {
+      m_iterator_pk_position = Iterator_position::END_OF_FILE;
+      break;
+    } else if (!m_pkd->value_matches_prefix(m_iterator_pk.key(),
+                                            cur_prefix_key)) {
+      rc = HA_ERR_END_OF_FILE;
+      m_iterator_pk_position = Iterator_position::START_NEXT_PREFIX;
+      break;
     }
 
     const rocksdb::Slice &rkey = m_iterator_pk.key();
@@ -725,8 +761,8 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
       goto exit;
     }
 
-    m_records.emplace(rocksdb::Slice(key, sk_packed_size),
-                      rocksdb::Slice(val, m_sk_tails.get_current_pos()));
+    m_records.emplace_back(rocksdb::Slice(key, sk_packed_size),
+                           rocksdb::Slice(val, m_sk_tails.get_current_pos()));
 
     num_rows++;
     rc = m_iterator_pk.next();
@@ -735,6 +771,7 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
   if (rc != HA_ERR_END_OF_FILE) goto exit;
   rc = HA_EXIT_SUCCESS;
 
+  std::sort(m_records.begin(), m_records.end(), m_comparator);
   rocksdb_partial_index_groups_sorted++;
   rocksdb_partial_index_rows_sorted += num_rows;
 
@@ -819,27 +856,31 @@ int Rdb_iterator_partial::seek(enum ha_rkey_function find_flag,
       if (direction) {
         if (m_kd->m_is_reverse_cf) {
           // Emulate "SeekForPrev" behaviour.
-          m_records_it = m_records.upper_bound(start_key);
+          m_records_it = std::upper_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.begin()) {
             next_prefix = true;
           } else {
             m_records_it--;
           }
         } else {
-          m_records_it = m_records.lower_bound(start_key);
+          m_records_it = std::lower_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.end()) {
             next_prefix = true;
           }
         }
       } else {
         if (m_kd->m_is_reverse_cf) {
-          m_records_it = m_records.upper_bound(start_key);
+          m_records_it = std::upper_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.end()) {
             next_prefix = true;
           }
         } else {
           // Emulate "SeekForPrev" behaviour.
-          m_records_it = m_records.lower_bound(start_key);
+          m_records_it = std::lower_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.begin()) {
             next_prefix = true;
           } else {
@@ -976,6 +1017,7 @@ int Rdb_iterator_partial::prev() {
 void Rdb_iterator_partial::reset() {
   m_valid = false;
   m_materialized = false;
+  m_iterator_pk_position = Iterator_position::UNKNOWN;
   free_root(&m_mem_root, MYF(MY_KEEP_PREALLOC));
   m_records.clear();
   m_iterator_pk.reset();

storage/rocksdb/rdb_iterator.h
@@ -149,6 +149,15 @@ class Rdb_iterator_partial : public Rdb_iterator_base {
   bool m_valid;
   bool m_materialized;
 
+  enum class Iterator_position {
+    UNKNOWN,
+    START_NEXT_PREFIX,
+    START_CUR_PREFIX,
+    END_OF_FILE
+  };
+
+  Iterator_position m_iterator_pk_position;
+
   const uint m_threshold;
   const uint m_prefix_keyparts;
 
@@ -172,19 +181,27 @@ class Rdb_iterator_partial : public Rdb_iterator_base {
   int next_with_direction_in_group(bool direction);
   int next_with_direction(bool direction);
 
+  using Slice_pair = std::pair<my_rocksdb::Slice, my_rocksdb::Slice>;
+  using Records = std::vector<Slice_pair>;
+
   struct slice_comparator {
     explicit slice_comparator(const rocksdb::Comparator *c) : m_comparator(c) {}
     const rocksdb::Comparator *const m_comparator;
 
-    bool operator()(const rocksdb::Slice &lhs, const rocksdb::Slice &rhs) {
-      return m_comparator->Compare(lhs, rhs) < 0;
+    bool operator()(const my_rocksdb::Slice &lhs, const Slice_pair &rhs) {
+      return m_comparator->Compare(lhs, rhs.first) < 0;
+    }
+    bool operator()(const Slice_pair &lhs, const my_rocksdb::Slice &rhs) {
+      return m_comparator->Compare(lhs.first, rhs) < 0;
+    }
+    bool operator()(const Slice_pair &lhs, const Slice_pair &rhs) {
+      return m_comparator->Compare(lhs.first, rhs.first) < 0;
     }
   };
 
-  std::map<const rocksdb::Slice, const rocksdb::Slice, slice_comparator>
-      m_records;
-  std::map<const rocksdb::Slice, const rocksdb::Slice,
-           slice_comparator>::iterator m_records_it;
+  Records m_records;
+  Records::iterator m_records_it;
+  slice_comparator m_comparator;
 
  public:
   Rdb_iterator_partial(THD *thd, const std::shared_ptr<Rdb_key_def> kd,

percona/percona-server

Upstream commit ID : fb-mysql-5.6.35/aa09ab287c1f34a3ad21e87627e3ecd5fb175b21
PS-8216 : Merge percona-202108

storage/rocksdb/rdb_iterator.cc
@@ -373,11 +373,12 @@ Rdb_iterator_partial::Rdb_iterator_partial(
       m_converter(thd, tbl_def, table),
       m_valid(false),
       m_materialized(false),
+      m_iterator_pk_position(Iterator_position::UNKNOWN),
       m_threshold(kd->partial_index_threshold()),
       m_prefix_keyparts(kd->partial_index_keyparts()),
       m_cur_prefix_key_len(0),
-      m_records(slice_comparator(m_kd->get_cf()->GetComparator())),
-      m_records_it(m_records.end()) {
+      m_records_it(m_records.end()),
+      m_comparator(slice_comparator(m_kd->get_cf()->GetComparator())) {
   init_sql_alloc(PSI_NOT_INSTRUMENTED, &m_mem_root, 4096);
   auto max_mem = get_partial_index_sort_max_mem(thd);
   if (max_mem) {
@@ -459,6 +460,7 @@ int Rdb_iterator_partial::get_prefix_from_start(
   // 1. There are not enough keyparts in the start_key.
   // 2. An exclusive seek key is provided, meaning that we need to read the next
   // prefix.
+  m_iterator_pk_position = Iterator_position::UNKNOWN;
   if (prefix_cnt < m_prefix_keyparts ||
       (prefix_len == start_key.size() &&
        (find_flag == HA_READ_AFTER_KEY || find_flag == HA_READ_BEFORE_KEY))) {
@@ -499,9 +501,29 @@ int Rdb_iterator_partial::get_next_prefix(bool direction) {
   rocksdb::Slice cur_prefix_key((const char *)m_cur_prefix_key,
                                 m_cur_prefix_key_len);
   uint tmp;
+  int rc = 0;
 
-  int rc = get_prefix_from_start(
-      direction ? HA_READ_AFTER_KEY : HA_READ_BEFORE_KEY, cur_prefix_key);
+  if (direction && m_iterator_pk_position != Iterator_position::UNKNOWN &&
+      m_iterator_pk_position != Iterator_position::START_CUR_PREFIX) {
+    if (m_iterator_pk_position == Iterator_position::END_OF_FILE) {
+      return HA_ERR_END_OF_FILE;
+    } else if (m_iterator_pk_position == Iterator_position::START_NEXT_PREFIX) {
+      uint prefix_cnt = 0;
+      uint prefix_len = 0;
+      rc = get_prefix_len(m_iterator_pk.key(), &prefix_cnt, &prefix_len);
+      if (rc) {
+        m_iterator_pk_position = Iterator_position::UNKNOWN;
+        return rc;
+      }
+      memcpy(m_cur_prefix_key, m_iterator_pk.key().data(), prefix_len);
+      m_cur_prefix_key_len = prefix_len;
+      m_iterator_pk_position = Iterator_position::START_CUR_PREFIX;
+    }
+  } else {
+    m_iterator_pk_position = Iterator_position::UNKNOWN;
+    rc = get_prefix_from_start(
+        direction ? HA_READ_AFTER_KEY : HA_READ_BEFORE_KEY, cur_prefix_key);
+  }
   m_kd->get_infimum_key(m_cur_prefix_key, &tmp);
 
   cur_prefix_key =
@@ -694,13 +716,27 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
   // the forwards direction (even PK is a reverse cf). This ensures that we can
   // make good use of bloom filters.
   //
-  rc = m_iterator_pk.seek(HA_READ_KEY_EXACT, cur_prefix_key, false,
-                          cur_prefix_key);
+  assert(m_iterator_pk_position != Iterator_position::END_OF_FILE);
+  if (m_iterator_pk_position != Iterator_position::START_CUR_PREFIX) {
+    rocksdb::Slice empty_end_key;
+    rc = m_iterator_pk.seek(HA_READ_KEY_OR_NEXT, cur_prefix_key, false,
+                            empty_end_key);
+  }
 
-  while (!rc) {
+  while (true) {
     if (thd_killed(m_thd)) {
       rc = HA_ERR_QUERY_INTERRUPTED;
       goto exit;
+    }
+
+    if (rc == HA_ERR_END_OF_FILE) {
+      m_iterator_pk_position = Iterator_position::END_OF_FILE;
+      break;
+    } else if (!m_pkd->value_matches_prefix(m_iterator_pk.key(),
+                                            cur_prefix_key)) {
+      rc = HA_ERR_END_OF_FILE;
+      m_iterator_pk_position = Iterator_position::START_NEXT_PREFIX;
+      break;
     }
 
     const rocksdb::Slice &rkey = m_iterator_pk.key();
@@ -726,8 +762,8 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
       goto exit;
     }
 
-    m_records.emplace(rocksdb::Slice(key, sk_packed_size),
-                      rocksdb::Slice(val, m_sk_tails.get_current_pos()));
+    m_records.emplace_back(rocksdb::Slice(key, sk_packed_size),
+                           rocksdb::Slice(val, m_sk_tails.get_current_pos()));
 
     num_rows++;
     rc = m_iterator_pk.next();
@@ -736,6 +772,7 @@ int Rdb_iterator_partial::read_prefix_from_pk() {
   if (rc != HA_ERR_END_OF_FILE) goto exit;
   rc = HA_EXIT_SUCCESS;
 
+  std::sort(m_records.begin(), m_records.end(), m_comparator);
   rocksdb_partial_index_groups_sorted++;
   rocksdb_partial_index_rows_sorted += num_rows;
 
@@ -820,27 +857,31 @@ int Rdb_iterator_partial::seek(enum ha_rkey_function find_flag,
       if (direction) {
         if (m_kd->m_is_reverse_cf) {
           // Emulate "SeekForPrev" behaviour.
-          m_records_it = m_records.upper_bound(start_key);
+          m_records_it = std::upper_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.begin()) {
             next_prefix = true;
           } else {
             m_records_it--;
           }
         } else {
-          m_records_it = m_records.lower_bound(start_key);
+          m_records_it = std::lower_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.end()) {
             next_prefix = true;
           }
         }
       } else {
         if (m_kd->m_is_reverse_cf) {
-          m_records_it = m_records.upper_bound(start_key);
+          m_records_it = std::upper_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.end()) {
             next_prefix = true;
           }
         } else {
           // Emulate "SeekForPrev" behaviour.
-          m_records_it = m_records.lower_bound(start_key);
+          m_records_it = std::lower_bound(m_records.begin(), m_records.end(),
+                                          start_key, m_comparator);
           if (m_records_it == m_records.begin()) {
             next_prefix = true;
           } else {
@@ -977,6 +1018,7 @@ int Rdb_iterator_partial::prev() {
 void Rdb_iterator_partial::reset() {
   m_valid = false;
   m_materialized = false;
+  m_iterator_pk_position = Iterator_position::UNKNOWN;
   m_mem_root.ClearForReuse();
   m_records.clear();
   m_iterator_pk.reset();

storage/rocksdb/rdb_iterator.h
@@ -133,6 +133,15 @@ class Rdb_iterator_partial : public Rdb_iterator_base {
   bool m_valid;
   bool m_materialized;
 
+  enum class Iterator_position {
+    UNKNOWN,
+    START_NEXT_PREFIX,
+    START_CUR_PREFIX,
+    END_OF_FILE
+  };
+
+  Iterator_position m_iterator_pk_position;
+
   const uint m_threshold;
   const uint m_prefix_keyparts;
 
@@ -156,20 +165,27 @@ class Rdb_iterator_partial : public Rdb_iterator_base {
   int next_with_direction_in_group(bool direction);
   int next_with_direction(bool direction);
 
+  using Slice_pair = std::pair<rocksdb::Slice, rocksdb::Slice>;
+  using Records = std::vector<Slice_pair>;
+
   struct slice_comparator {
     explicit slice_comparator(const rocksdb::Comparator *c) : m_comparator(c) {}
     const rocksdb::Comparator *const m_comparator;
 
-    bool operator()(const rocksdb::Slice &lhs,
-                    const rocksdb::Slice &rhs) const {
-      return m_comparator->Compare(lhs, rhs) < 0;
+    bool operator()(const rocksdb::Slice &lhs, const Slice_pair &rhs) const {
+      return m_comparator->Compare(lhs, rhs.first) < 0;
+    }
+    bool operator()(const Slice_pair &lhs, const rocksdb::Slice &rhs) {
+      return m_comparator->Compare(lhs.first, rhs) < 0;
+    }
+    bool operator()(const Slice_pair &lhs, const Slice_pair &rhs) {
+      return m_comparator->Compare(lhs.first, rhs.first) < 0;
     }
   };
 
-  std::map<const rocksdb::Slice, const rocksdb::Slice, slice_comparator>
-      m_records;
-  std::map<const rocksdb::Slice, const rocksdb::Slice,
-           slice_comparator>::iterator m_records_it;
+  Records m_records;
+  Records::iterator m_records_it;
+  slice_comparator m_comparator;
 
  public:
   Rdb_iterator_partial(THD *thd, const std::shared_ptr<Rdb_key_def> kd,
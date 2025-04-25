6c05103e869d8be68f15b29a8695fdab024e17b8
Kamil Holubicki <kamil.holubicki@percona.com>
2025-04-25 09:56:32 +0200

    [components/binlog_utils_udf] PS-9777: binlog_utils_udf plugin should handle binlog.index entries the same as the server code does
    https://perconadev.atlassian.net/browse/PS-9777
    
    Problem:
    Some tools like PXB, when moving back a backup, can produce a malformed
    binlog.index file. This file contains an entry that is just a binlog
    file name without relative/absolute path.
    Server is able to handle sucha case in a proper way, however
    binlog_utils_udf plugin complains about not being able to find a binlog
    file.
    
    Cause:
    When the server processes queries like SHOW BINARY LOGS or SHOW BINLOG
    EVENTS IN, it does two things:
    1. expand provided binlog name to the full binlog path
    2. expand entries in binlog.index file to their full path
    Then it does comparison (to deduce if the provided binlog file name is
    a valid one)
    
    binlog_utils_udf does the expansion of the provided binlog file name,
    but gets binlog.index entries as they are. This causes that in
    problematic case the entry is not found in binlog.index file causing
    the error returned by plugin.
    
    Solution:
    Do the full expansion of binlog.index entries in the same way as
    the expansion of the provided file name is done.

--- components/binlog_utils_udf/binlog_utils_udf.cc
+++ components/binlog_utils_udf/binlog_utils_udf.cc
@@ -109,6 +109,25 @@ const char *get_short_binlog_name(const std::string &binlog_name) noexcept {
   return binlog_name.c_str() + dirname_length(binlog_name.c_str());
 }
 
+//
+// Retrieves the contents of the index file. Then normalizes its entries (each
+// entry is prefixed with relative or absolute path)
+//
+std::pair<int, std::list<std::string>> get_normalized_log_index() {
+  auto log_index = mysql_bin_log.get_log_index(true /* need_lock_index */);
+
+  auto &binlog_names = log_index.second;
+  auto bg = std::begin(binlog_names);
+  auto en = std::end(binlog_names);
+  std::for_each(bg, en, [](std::string &s) {
+    fn_reflen_buffer buf;
+    check_and_normalize_binlog_name(get_short_binlog_name(s), buf);
+    s.assign(buf);
+  });
+
+  return log_index;
+}
+
 log_event_ptr find_first_event(std::string_view binlog_name) {
   DBUG_TRACE;
 
@@ -318,7 +337,7 @@ mysqlpp::udf_result_t<STRING_RESULT> get_binlog_by_gtid_impl::calculate(
       throw std::runtime_error("Cannot parse 'gtid_executed'");
   }
 
-  auto log_index = mysql_bin_log.get_log_index(true /* need_lock_index */);
+  auto log_index = get_normalized_log_index();
   if (log_index.first != LOG_INFO_EOF)
     throw std::runtime_error("Cannot read binary log index'");
   if (log_index.second.empty())
@@ -410,7 +429,7 @@ mysqlpp::udf_result_t<STRING_RESULT> get_gtid_set_by_binlog_impl::calculate(
     const mysqlpp::udf_context &ctx) {
   DBUG_TRACE;
 
-  auto log_index = mysql_bin_log.get_log_index(true /* need_lock_index */);
+  auto log_index = get_normalized_log_index();
   if (log_index.first != LOG_INFO_EOF)
     throw std::runtime_error("Cannot read binary log index");
   if (log_index.second.empty())
@@ -509,7 +528,7 @@ mysqlpp::udf_result_t<STRING_RESULT> get_binlog_by_gtid_set_impl::calculate(
       throw std::runtime_error("Cannot parse 'gtid_executed'");
   }
 
-  auto log_index = mysql_bin_log.get_log_index(true /* need_lock_index */);
+  auto log_index = get_normalized_log_index();
   if (log_index.first != LOG_INFO_EOF)
     throw std::runtime_error("Cannot read binary log index'");
   if (log_index.second.empty())

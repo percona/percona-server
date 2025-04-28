mydb-ru/mydb-server

PS-9614 Pool-of-Threads' timer thread fails to start if mysqld is started with --daemonize
Problem:
When pool-of-threads is enabled, a timer_thread should be created. However, if the process starts with --daemonize, the thread is missing.
This occurs because during the execution of mysqld_daemonize(), the mysqld process forks, creating a child process that runs as a daemon.
Since threads are not copied during a fork, and timer_thread is not reinitialized in the daemon process, it is lost.

Solution:
Move the timer_thread initialization to execute after the mysqld_daemonize() function.

sql/conn_handler/connection_handler_impl.h
@@ -134,6 +134,8 @@ class Thread_pool_connection_handler : public Connection_handler {
   bool add_connection(Channel_info *channel_info) override;
 
   uint get_max_threads() const override { return threadpool_max_threads; }
+
+  void post_daemonize_init() override { start_timer_thread(); }
 };
 
 #endif  // CONNECTION_HANDLER_IMPL_INCLUDED

percona/percona-server

sql/conn_handler/connection_handler_impl.h
@@ -139,6 +139,8 @@ class Thread_pool_connection_handler : public Connection_handler {
   bool add_connection(Channel_info *channel_info) override;
 
   uint get_max_threads() const override { return threadpool_max_threads; }
+
+  void post_daemonize_init() override { tp_start_timer_thread(); }
 };
 
 #endif  // CONNECTION_HANDLER_IMPL_INCLUDED
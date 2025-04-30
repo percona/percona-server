c069e6ec52a13a44f9eacde7cdb1273ff0b690b7
Varun Nagaraju <varun.nagaraju@percona.com>
2025-04-30 17:49:51 +0530

    PS-9749 MySQL server heap-use-after-free at int my_mb_wc_utf8_prototype
    https://perconadev.atlassian.net/browse/PS-9749
    
    Bug#36593253 mysql/mysql-server@0491d9b4d93
    
    Constant item objects need to support several val_str() calls,
    which means we cannot use string buffers owned by bg_resbuf_mgr
    (it will free results from previous calls on re-invokation of val_str()).
    
    For Item_func_convex_hull the interaction between val_str(),
    bg_convex_hull() and post_fix_result() is too complex for a similar
    simple fix. Instead we introduce 'tmp_value' String in Item_func_centroid
    and use this to hold a (shallow) copy of the result.

--- sql/item_geofunc.h
+++ sql/item_geofunc.h
@@ -461,6 +461,7 @@ public:
 class Item_func_convex_hull: public Item_geometry_func
 {
   BG_result_buf_mgr bg_resbuf_mgr;
+  String tmp_value;
 
   template <typename Coordsys>
   bool bg_convex_hull(const Geometry *geom, String *wkb);

1cf9b2b68a49d5bc867db73940ad3e20d5e1fef0
Kamil Holubicki <kamil.holubicki@percona.com>
2025-06-12 11:48:07 +0200

    PS-9647: MySQL Perf Improvements
    https://perconadev.atlassian.net/browse/PS-9647
    
    Addressed review comments.

--- storage/innobase/rem/rec.cc
+++ storage/innobase/rem/rec.cc
@@ -47,8 +47,9 @@ external tools. */
 @param[in]      rec     physical record
 @param[in]      index   record descriptor
 @param[in, out] offsets array of offsets */
-static inline MY_ATTRIBUTE((always_inline)) void rec_init_offsets_new(
-    const rec_t *rec, const dict_index_t *index, ulint *offsets) {
+static ALWAYS_INLINE void rec_init_offsets_new(const rec_t *rec,
+                                               const dict_index_t *index,
+                                               ulint *offsets) {
   ulint status = rec_get_status(rec);
   ulint n_node_ptr_field = ULINT_UNDEFINED;
 

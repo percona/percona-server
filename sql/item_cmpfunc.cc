7365d456cc3fe0834b3805b22770f9b6b17b8cc0
Priyanka Sangam <priyanka.sangam@oracle.com>
2025-07-03 14:51:00 +0530

    [sql] PS-10446 [8.4]: Bug#38110792 The missing semi join condition causes incorrect result
    Contributed by: Jingqi Tian at Alibaba Inc.
    
    A query which contains a SELECT on a materialised view returns extra
    rows because it contains a condition which is not added to the
    materialised semijoin. The condition is not added because the
    materialised semijoin field is not recognised as being the same as
    the condition field.
    
    This is fixed by modifying the comparison functions for equality to
    correctly compare the condition fields with the materialised semijoin
    fields.
    
    Change-Id: I62f213f15b3b205065e3726a0b0a813bb90c6e0d

--- sql/item_cmpfunc.cc
+++ sql/item_cmpfunc.cc
@@ -7572,7 +7572,7 @@ Item *Item_equal::equality_substitution_transformer(uchar *arg) {
     // Iterate over the fields selected from the subquery
     uint fieldno = 0;
     for (Item *existing : sj_nest->nested_join->sj_inner_exprs) {
-      if (existing->real_item()->eq(item, false))
+      if (existing->real_item()->eq(item->real_item(), false))
         added_fields.push_back(sj_nest->nested_join->sjm.mat_fields[fieldno]);
       fieldno++;
     }
@@ -7603,7 +7603,7 @@ Item *Item_func_eq::equality_substitution_transformer(uchar *arg) {
   // Iterate over the fields selected from the subquery
   uint fieldno = 0;
   for (Item *existing : sj_nest->nested_join->sj_inner_exprs) {
-    if (existing->real_item()->eq(args[1], false) &&
+    if (existing->real_item()->eq(args[1]->real_item(), false) &&
         (args[0]->used_tables() & ~sj_nest->sj_inner_tables))
       current_thd->change_item_tree(
           args + 1, sj_nest->nested_join->sjm.mat_fields[fieldno]);

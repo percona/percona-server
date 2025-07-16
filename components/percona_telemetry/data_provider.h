a21c536a77f4456e0e732dfbc8f307e083a1ce63
Kamil Holubicki <kamil.holubicki@percona.com>
2025-07-16 11:44:06 +0200

    PS-9826 [9.x] Include the added counters for JS Stored Routines into telemetry JSON
    https://perconadev.atlassian.net/browse/PS-9826
    
    1. Added metrics collection for component_js_lang
    2. Improved error handling. We accessed a query result vector without
    checking if it is empty or not. In general case it is not, but it is
    safer to check if there are some rows to examine.

--- components/percona_telemetry/data_provider.h
+++ components/percona_telemetry/data_provider.h
@@ -80,6 +80,7 @@ class DataProvider {
   bool collect_product_version_info(rapidjson::Document *document);
   bool collect_plugins_info(rapidjson::Document *document);
   bool collect_components_info(rapidjson::Document *document);
+  bool collect_js_lang_component_info(rapidjson::Document *document);
   bool collect_uptime_info(rapidjson::Document *document);
   bool collect_dbs_number_info(rapidjson::Document *document);
   bool collect_dbs_size_info(rapidjson::Document *document);
@@ -104,6 +105,7 @@ class DataProvider {
 
   std::string database_instance_id_cache_;
   std::string version_cache_;
+  bool js_lang_component_present_;
 };
 
 #endif /* PERCONA_TELEMETRY_DATA_PROVIDER_H */

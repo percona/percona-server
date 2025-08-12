55d9cca5acb81805f44277f6b03a0a57b5bd6ccb
Yura Sorokin <yura.sorokin@percona.com>
2025-08-12 23:05:44 +0000

    [storage/ndb] PS-10049 merge: Code refresh for - PS 8.4.6-6 (NdbTimestamp-t leak fix)
    https://perconadev.atlassian.net/browse/PS-10049
    
    Fixed memory leak in 'NdbTimestamp-t' unit test detected by ASan.
    
    =================================================================
    ==3324130==ERROR: LeakSanitizer: detected memory leaks
    
    Direct leak of 67 byte(s) in 3 object(s) allocated from:
        #0 0x5555ab78bc5a in strdup (/home/yura/ws/percona-server-8.4-build-asan_clang20/runtime_output_directory/NdbTimestamp-t+0xbdc5a) (BuildId: 9c5a51c07dd925cf13b2f9408597355a524793bc)
        #1 0x5555ab7eb685 in test_TZ(int) /home/yura/ws/percona-server-8.4/storage/ndb/src/common/portlib/NdbTimestamp.cpp:516:10
        #2 0x5555ab7eb126 in main /home/yura/ws/percona-server-8.4/storage/ndb/src/common/portlib/NdbTimestamp.cpp:576:53
        #3 0x7f0596ae4d8f in __libc_start_call_main csu/../sysdeps/nptl/libc_start_call_main.h:58:16
    
    SUMMARY: AddressSanitizer: 67 byte(s) leaked in 3 allocation(s).

--- storage/ndb/src/common/portlib/NdbTimestamp.cpp
+++ storage/ndb/src/common/portlib/NdbTimestamp.cpp
@@ -29,6 +29,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <memory>
 
 #include "ndb_config.h"
 #include "portlib/NdbTimestamp.h"
@@ -502,6 +503,9 @@ static void test_UTC() {
   }
 }
 
+using char_buffer_ptr = std::unique_ptr<char, decltype(&std::free)>;
+static char_buffer_ptr guarded_tzenv(nullptr, &std::free);
+
 static void test_TZ(int itz) {
 #ifndef _WIN32
   const char *tzenv = timezones[itz][0];
@@ -514,7 +518,9 @@ static void test_TZ(int itz) {
   }
   _putenv_s("TZ", tzenv);
 #endif
-  printf("TZ=%s\n", tzenv);
+  guarded_tzenv.reset(strdup(tzenv));
+  putenv(guarded_tzenv.get());
+  printf("%s\n", tzenv);
   NdbTimestamp_Reset();
 
   std::timespec t = NdbTimestamp_GetCurrentTime();

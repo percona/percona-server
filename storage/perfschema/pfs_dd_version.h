4a572ee9c4fcf50ad4d7adea9f4038800c2176d2
Przemyslaw Skibinski <przemyslaw.skibinski@percona.com>
2025-10-29 19:37:52 +0100

    PS-10163 [8.4]: Adjust P_S version after adding performance_schema.account_failed_login_lock_stats table

--- storage/perfschema/pfs_dd_version.h
+++ storage/perfschema/pfs_dd_version.h
@@ -244,7 +244,7 @@
      SYS.INNODB_LOCK_WAITS IN MYSQL 8.0
      - Table performance_schema.data_lock_waits, add PRIMARY KEY.
 
-  80043:
+  80044:
    - Percona Server added performance_schema.account_failed_login_lock_stats
      table.
 
@@ -279,11 +279,11 @@
      SYS.INNODB_LOCK_WAITS IN MYSQL 8.0
      - Table performance_schema.data_lock_waits, add PRIMARY KEY.
 
-  80406:
+  80407:
    - Percona Server added performance_schema.account_failed_login_lock_stats
      table.
 */
 
-static const uint PFS_DD_VERSION = 80406;
+static const uint PFS_DD_VERSION = 80407;
 
 #endif /* PFS_DD_VERSION_H */

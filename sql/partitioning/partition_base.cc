facebook/mysql-5.6

Prevent underflow of m_n_mysql_tables_in_use
Summary:
The `m_n_mysql_tables_in_use` counter is underflowing during ADD/DROP partition. It underflows during ADD because `::close()` unlocks partitions that were never locked in the first place. The fix is to remove the unlock in `::close()`.

The counter underflows during DROP because dropped partitions are unlocked in `::commit_inplace_alter_table()` and a second time in `::external_lock()`. The fix is to remove the partitions from the `m_file` array so that in the second call to `::external_lock()`, only the remaining partitions are unlocked (instead unlocking all partitions, including ones that were already dropped).

The consequence of this bug is that some statements may commit prematurely (once count drops to 0) even though the statement may hit errors later before the final `external_lock` call is made.

Squash with: D23177984 D23266554

Reviewed By: luqun

Differential Revision: D25592121

fbshipit-source-id: c4f7f266267

sql/partitioning/partition_base.cc
@@ -1073,7 +1073,6 @@ void Partition_base::close_new_partitions(bool delete_new_partitions) {
   DBUG_ENTER("Partition_base::close_new_partitions");
 
   if (m_new_file) {
-    THD *thd = ha_thd();
     for (uint i = 0; i < m_num_new_partitions; i++) {
       handler *file = m_new_file[i];
 
@@ -1081,7 +1080,6 @@ void Partition_base::close_new_partitions(bool delete_new_partitions) {
         /* Not a new partition, skip it. */
         continue;
       }
-      file->ha_external_lock(thd, F_UNLCK);
       file->ha_close();
       if (delete_new_partitions && m_new_partitions_name[i] != nullptr) {
         file->ha_delete_table(m_new_partitions_name[i], nullptr);
@@ -4126,6 +4124,7 @@ bool Partition_base::commit_inplace_alter_table(
       partition_element *part_elem;
       int num_subparts =
           part_info->is_sub_partitioned() ? part_info->num_subparts : 1;
+      uint count = 0;
       while ((part_elem = part_it++) != nullptr) {
         partition_state state = part_elem->part_state;
         switch (state) {
@@ -4144,6 +4143,9 @@ bool Partition_base::commit_inplace_alter_table(
                 if (error) goto end;
                 error = (*file)->ha_delete_table(name, old_table_def);
                 if (error) goto end;
+                (*file)->ha_close();
+                destroy(*file);
+                *file = nullptr;
                 file++;
               }
             } else {
@@ -4154,14 +4156,21 @@ bool Partition_base::commit_inplace_alter_table(
               if (error) goto end;
               error = (*file)->ha_delete_table(name, old_table_def);
               if (error) goto end;
+              (*file)->ha_close();
+              destroy(*file);
+              *file = nullptr;
               file += num_subparts;
             }
             break;
           default:
+            memmove(m_file + count, file, num_subparts * sizeof(handler *));
             file += num_subparts;
+            count += num_subparts;
             break;
         }
       }
+      m_file[count] = nullptr;
+      m_tot_parts = count;
     } else if (ha_alter_info->alter_info->flags &
                Alter_info::ALTER_ADD_PARTITION) {
       partition_info *part_info = ha_alter_info->modified_part_info;

percona/percona-server

sql/partitioning/partition_base.cc
@@ -1078,7 +1078,6 @@ void Partition_base::close_new_partitions(bool delete_new_partitions) {
   DBUG_ENTER("Partition_base::close_new_partitions");
 
   if (m_new_file) {
-    THD *thd = ha_thd();
     for (uint i = 0; i < m_num_new_partitions; i++) {
       handler *file = m_new_file[i];
 
@@ -1086,7 +1085,6 @@ void Partition_base::close_new_partitions(bool delete_new_partitions) {
         /* Not a new partition, skip it. */
         continue;
       }
-      file->ha_external_lock(thd, F_UNLCK);
       file->ha_close();
       if (delete_new_partitions && m_new_partitions_name[i] != nullptr) {
         file->ha_delete_table(m_new_partitions_name[i], nullptr);
@@ -4115,6 +4113,7 @@ bool Partition_base::commit_inplace_alter_table(
       partition_element *part_elem;
       int num_subparts =
           part_info->is_sub_partitioned() ? part_info->num_subparts : 1;
+      uint count = 0;
       while ((part_elem = part_it++) != nullptr) {
         partition_state state = part_elem->part_state;
         switch (state) {
@@ -4133,6 +4132,9 @@ bool Partition_base::commit_inplace_alter_table(
                 if (error) goto end;
                 error = (*file)->ha_delete_table(name, old_table_def);
                 if (error) goto end;
+                (*file)->ha_close();
+                destroy_at(*file);
+                *file = nullptr;
                 file++;
               }
             } else {
@@ -4143,14 +4145,21 @@ bool Partition_base::commit_inplace_alter_table(
               if (error) goto end;
               error = (*file)->ha_delete_table(name, old_table_def);
               if (error) goto end;
+              (*file)->ha_close();
+              destroy_at(*file);
+              *file = nullptr;
               file += num_subparts;
             }
             break;
           default:
+            memmove(m_file + count, file, num_subparts * sizeof(handler *));
             file += num_subparts;
+            count += num_subparts;
             break;
         }
       }
+      m_file[count] = nullptr;
+      m_tot_parts = count;
     } else if (ha_alter_info->alter_info->flags &
                Alter_info::ALTER_ADD_PARTITION) {
       partition_info *part_info = ha_alter_info->modified_part_info;
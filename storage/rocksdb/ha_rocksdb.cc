facebook/mysql-5.6

Add `INFORMATION_SCHEMA.ROCKSDB_LIVE_FILES_METADATA` table
Summary:
Add a new table `INFORMATION_SCHEMA.ROCKSDB_LIVE_FILES_METADATA`,
which lists the live files metadata information from rocksdb API
`GetLiveFilesMetaData()`.

Reviewed By: yizhang82

Differential Revision: D35415529

fbshipit-source-id: d8b6c245a2d625003bf0fd0fa9114b349c99f156

storage/rocksdb/ha_rocksdb.cc
@@ -17529,4 +17529,5 @@ mysql_declare_plugin(rocksdb_se){
     myrocks::rdb_i_s_sst_props, myrocks::rdb_i_s_index_file_map,
     myrocks::rdb_i_s_lock_info, myrocks::rdb_i_s_trx_info,
     myrocks::rdb_i_s_deadlock_info,
-    myrocks::rdb_i_s_bypass_rejected_query_history mysql_declare_plugin_end;
+    myrocks::rdb_i_s_bypass_rejected_query_history,
+    myrocks::rdb_i_s_live_files_metadata mysql_declare_plugin_end;

percona/percona-server

Upstream commit ID: https://github.com/facebook/mysql-5.6/commit/d017a0aecbfb156476764340b4590ec6aa709a2c
PS-8494: Merge percona-202205 (https://jira.percona.com/browse/PS-8494)

storage/rocksdb/ha_rocksdb.cc
@@ -16331,4 +16331,5 @@ mysql_declare_plugin(rocksdb_se){
     myrocks::rdb_i_s_global_info, myrocks::rdb_i_s_ddl,
     myrocks::rdb_i_s_sst_props, myrocks::rdb_i_s_index_file_map,
     myrocks::rdb_i_s_lock_info, myrocks::rdb_i_s_trx_info,
-    myrocks::rdb_i_s_deadlock_info mysql_declare_plugin_end;
+    myrocks::rdb_i_s_deadlock_info,
+    myrocks::rdb_i_s_live_files_metadata mysql_declare_plugin_end;
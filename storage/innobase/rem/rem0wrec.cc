067b52655550059b87d04ee02db4ea0be41a6c0d
Paweł Olchawa <pawel.olchawa@percona.com>
2026-04-09 13:05:47 +0200

    PS-11297 [trunk]: Avoid calling validate() in non-debug in rec_offs_*()
    https://perconadev.atlassian.net/browse/PS-11297
    
    validate_rec_offset() in the rec_offs_*() helpers of rem0wrec was called
    unconditionally, including in non-debug builds where it does no useful work.
    Wrap these calls in ut_d() so they are compiled only in debug builds, removing
    the per-call overhead on release builds.

--- storage/innobase/rem/rem0wrec.cc
+++ storage/innobase/rem/rem0wrec.cc
@@ -129,7 +129,7 @@ ulint rec_offs_nth_sql_null(const dict_index_t *index, const ulint *offsets,
     n = index->get_field_off_pos(n);
   }
 
-  validate_rec_offset(index, offsets, n, UT_LOCATION_HERE);
+  ut_d(validate_rec_offset(index, offsets, n, UT_LOCATION_HERE));
   return (rec_offs_nth_sql_null_low(offsets, n));
 }
 
@@ -139,7 +139,7 @@ ulint rec_offs_nth_default(const dict_index_t *index, const ulint *offsets,
     n = index->get_field_off_pos(n);
   }
 
-  validate_rec_offset(index, offsets, n, UT_LOCATION_HERE);
+  ut_d(validate_rec_offset(index, offsets, n, UT_LOCATION_HERE));
   return (rec_offs_nth_default_low(offsets, n));
 }
 
@@ -149,7 +149,7 @@ ulint rec_offs_nth_size(const dict_index_t *index, const ulint *offsets,
     n = index->get_field_off_pos(n);
   }
 
-  validate_rec_offset(index, offsets, n, UT_LOCATION_HERE);
+  ut_d(validate_rec_offset(index, offsets, n, UT_LOCATION_HERE));
   return (rec_offs_nth_size_low(offsets, n));
 }
 

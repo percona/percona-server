99448c35dea0f7c45678c6958278566fd0b44390
Catalin Besleaga <catalin.besleaga@percona.com>
2026-04-30 09:55:13 +0300

    PS-11106 [8.4] Component Percona keyring encrypted file
    - Created component_percona_keyring_encrypted_file based on
    component_keyring_file.
    - Added cmake flag: WITH_COMPONENT_PERCONA_KEYRING_ENCRYPTED_FILE
    - New PBKDF2-based encrypt/decrypt API (aes.cc): Added
    aes_encrypt_pbkdf2 and aes_decrypt_pbkdf2 functions that derive a
    256-bit AES key from a password using PKCS5_PBKDF2_HMAC (SHA-256).
    Refactored the internal EVP encrypt/decrypt logic into private helpers
    to avoid code duplication.
    - Encrypted backend (backend.cc): Renamed Keyring_file_backend to
    Keyring_encrypted_file_backend and wired in password-based encryption.
    On write, generates a random salt, IV, and iteration count; on read,
    parses v1 on-disk header ([version:1][salt:32][iterations:4
    BE][iv:16][ciphertext]) and decrypts before JSON parsing.
    - Password config options (config.cc): The component configuration now
    requires exactly one of password (inline) or password_file (path to a
    file containing the password). Validation errors are emitted for
    missing, empty, or conflicting combinations. The
    keyring_component_status table reports <SET> or <NONE> for the password
    field.

---
+++ components/keyrings/percona_keyring_encrypted_file/service_implementation/keyring_load_service_definition.cc
@@ -0,0 +1,64 @@
+/* Copyright (c) 2021, 2025, Oracle and/or its affiliates.
+   Copyright (c) 2026 Percona LLC and/or its affiliates. All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2.0,
+as published by the Free Software Foundation.
+
+This program is designed to work with certain software (including
+but not limited to OpenSSL) that is licensed under separate terms,
+as designated in a particular file or component or in included license
+documentation.  The authors of MySQL hereby grant you an additional
+permission to link the program and your derivative works with the
+separately licensed software that they have either included with
+the program or referenced in the documentation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License, version 2.0, for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
+
+#include <components/keyrings/percona_keyring_encrypted_file/percona_keyring_encrypted_file.h>
+
+#include <components/keyrings/common/component_helpers/include/keyring_load_service_definition.h>
+#include <components/keyrings/common/component_helpers/include/service_requirements.h>
+
+using percona_keyring_encrypted_file::g_keyring_file_inited;
+using percona_keyring_encrypted_file::init_or_reinit_keyring;
+using percona_keyring_encrypted_file::set_paths;
+
+namespace keyring_common {
+
+namespace service_definition {
+
+DEFINE_BOOL_METHOD(Keyring_load_service_impl::load,
+                   (const char *component_path, const char *instance_path)) {
+  std::string err;
+  try {
+    if (set_paths(component_path, instance_path) == true) {
+      LogComponentErr(ERROR_LEVEL, ER_KEYRING_COMPONENT_NOT_INITIALIZED,
+                      "Failed to set path to component");
+      return true;
+    }
+
+    if (init_or_reinit_keyring(err) == true) {
+      LogComponentErr(ERROR_LEVEL, ER_KEYRING_COMPONENT_NOT_INITIALIZED,
+                      err.c_str());
+      return true;
+    }
+    g_keyring_file_inited = true;
+    LogComponentErr(INFORMATION_LEVEL, ER_NOTE_KEYRING_COMPONENT_INITIALIZED);
+    return false;
+  } catch (...) {
+    LogComponentErr(ERROR_LEVEL, ER_KEYRING_COMPONENT_NOT_INITIALIZED,
+                    "Got an exception while loading component");
+    return true;
+  }
+}
+
+}  // namespace service_definition
+}  // namespace keyring_common

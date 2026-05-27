024600a79f762d0bedbd8ad33fc1ac8f2a4a731b
Roy Lyseng <roy.lyseng@oracle.com>
2026-05-27 13:57:38 +0200

    Bug#39321676: WHERE equality condition fails to match existing row
    This problem is triggered by the patch added for bug#35912840.
    This patch added code that made some scalar subqueries constant after
    reading a const table in an outer query block, and thus made the
    subquery a candidate for evaluation by the range optimizer. In this
    case, both the scalar subquery and the outer query block contained
    references to the same CTE, thus the optimizer would try to materialize
    the CTE and generate keys for that CTE to be used in both query blocks.
    
    This is an acceptable use of generated keys, however a problem in how
    unused keys were cleaned up was triggered. Unused keys are supposed to
    be cleaned up after optimization of a query block, in
    JOIN::finalize_derived_keys(). But in this case, some keys are generated
    in the outer query block and some keys in the subquery. Thus,
    JOIN::finalize_derived_keys() is called twice. The first call to this
    function (for the subquery) modifies the list of possible keys in such
    a way that when the outer query is optimized, wrong key information is
    generated.
    
    Note also that no possible keys are added during optimization of the
    subquery, as the derived table key generation is "owned" by the outer
    query block. Thus, we should also not do any cleanup of unused keys
    when optimizing the subquery. Hence our solution to this problem is to
    check whether owner_of_tmp_keys matches the current query block, before
    removing unused keys. Otherwise, the table in question is skipped.
    
    We also abbreviate owner_of_possible_tmp_keys to owner_of_tmp_keys, as
    the "possible" word is kind of redundant here.
    
    Note that one query plan is re-recorded as it gets a different plan due
    to a different set of potential derived table keys being available.
    
    Change-Id: Iefa974bc4664b42e4b791e6d7f7da33b8c5f0ff0

--- sql/table.cc
+++ sql/table.cc
@@ -6781,7 +6781,7 @@ int Table_ref::fetch_number_of_rows(ha_rows fallback_estimate) {
   - While QB1 is in this window, it is possible, as we saw above, that QB2
   gets optimized. Because it is not safe to have two query blocks
   reading/writing possible keys for a same table at the same time, a locking
-  mechanism is in place: TABLE_SHARE::owner_of_possible_tmp_keys is a record
+  mechanism is in place: TABLE_SHARE::owner_of_tmp_keys is a record
   of which query block entered first the window for this table and hasn't left
   it yet; only that query block is allowed to read/write possible keys for
   this table.
@@ -7029,8 +7029,8 @@ bool Table_ref::generate_keys() {
       return false;
     }
 
-  if (table->s->owner_of_possible_tmp_keys != nullptr &&
-      table->s->owner_of_possible_tmp_keys != query_block)
+  if (table->s->owner_of_tmp_keys != nullptr &&
+      table->s->owner_of_tmp_keys != query_block)
     return false;
 
   uint new_key_parts = 0;
@@ -7070,9 +7070,9 @@ bool Table_ref::generate_keys() {
     }
   }
 
-  if (table->s->keys)
-    table->s->owner_of_possible_tmp_keys = query_block;  // Acquire lock
-
+  if (table->s->keys != 0) {
+    table->s->owner_of_tmp_keys = query_block;  // Acquire lock
+  }
   return false;
 }
 
